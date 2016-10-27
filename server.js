f01c128e278bc90710c820eed1041885b042fdec
Matthew Solomonson <ggmatt@gmail.com>
2016-10-27 18:23:35 -0400

    asynchronous server function

--- server.js
+++ server.js
@@ -4,25 +4,13 @@ import GraphQLHTTP from 'express-graphql'
 
 import schema from './schema'
 
-const app = express()
-let db
+const app = express();
 
-MongoClient.connect(process.env.MONGO_URL, (error, database) => {
-  if (error) throw error
-  db = database
+(async () => {
+  const db = await MongoClient.connect(process.env.MONGO_URL)
   app.use('/graph', GraphQLHTTP({
     schema: schema(db),
     graphiql: true,
   }))
-
   app.listen(8080, () => console.log('Listening on 8080'))
-})
-
-app.get('/data/variants', (request, response) => {
-  db.collection('variants')
-    .find({ genes: 'ENSG00000169174' })
-    .toArray((error, variants) => {
-      if (error) throw error
-      response.json(variants)
-    })
-})
+})()
