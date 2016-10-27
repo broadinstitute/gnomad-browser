6f10c366615155e12a89104f0a80de5dc7aff901
Matthew Solomonson <ggmatt@gmail.com>
2016-10-27 11:36:24 -0400

    reorganize server

--- server.js
+++ server.js
@@ -2,15 +2,19 @@ import express from 'express'
 import { MongoClient } from 'mongodb'
 
 const app = express()
-
-app.listen(8080, () => console.log('Listening on 8080'))
+let db
 
 MongoClient.connect(process.env.MONGO_URL, (error, database) => {
   if (error) throw error
-  database.collection('variants')
+  db = database
+  app.listen(8080, () => console.log('Listening on 8080'))
+})
+
+app.get('/data/variants', (request, response) => {
+  db.collection('variants')
     .find({ genes: 'ENSG00000169174' })
-    .toArray((error, data) => {
+    .toArray((error, variants) => {
       if (error) throw error
-      console.log(data)
+      response.json(variants)
     })
 })
