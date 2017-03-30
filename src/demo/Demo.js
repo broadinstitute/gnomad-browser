50422682a934057e02ed8aca089a3686cc8f3b2a
Matthew Solomonson <ggmatt@gmail.com>
2017-03-30 09:49:44 -0400

    Add react router/navigation menu

--- src/demo/Demo.js
+++ src/demo/Demo.js
@@ -1,16 +1,64 @@
 import React from 'react'
+
+import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom'
+
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import Drawer from 'material-ui/Drawer'
+import MenuItem from 'material-ui/MenuItem'
+import RaisedButton from 'material-ui/RaisedButton'
 
 import RegionViewerDemo from '../RegionViewer/RegionViewer.example'
 
 import css from './styles.css'
 
+class DrawerSimpleExample extends React.Component {
+
+  state = { open: false }
+
+  handleToggle = () => this.setState({ open: !this.state.open })
+
+  render() {
+    return (
+      <div>
+        <RaisedButton
+          label="Open menu"
+          onTouchTap={this.handleToggle}
+        />
+        <Drawer open={this.state.open}>
+          <MenuItem onClick={this.handleToggle}>Close menu</MenuItem>
+          <NavLink
+            className={css.navlink}
+            activeClassName={css.active}
+            exact
+            to={'/'}
+          >
+            <MenuItem>Home</MenuItem>
+          </NavLink>
+          <NavLink
+            className={css.navlink}
+            activeClassName={css.active}
+            exact
+            to={'/region-table'}
+          >
+            <MenuItem>Region table</MenuItem>
+          </NavLink>
+        </Drawer>
+      </div>
+    )
+  }
+}
+
 const Demo = () =>
-  <div className={css.demoArea}>
-    <h1>Demo area</h1>
-    <RegionViewerDemo />
-  </div>
+  <Router>
+    <div>
+      <DrawerSimpleExample />
+      <div className={css.demoArea}>
+        <Route exact path={'/'} render={() => <h1>gnomAD component demos</h1>} />
+        <Route exact path={'/region-table'} component={RegionViewerDemo} />
+      </div>
+    </div>
+  </Router>
 
 const muiTheme = getMuiTheme({
   palette: {
