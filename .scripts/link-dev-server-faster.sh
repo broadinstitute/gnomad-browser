cbfcd08ef4aebf5518b43388e65024452f1ef701
Matthew Solomonson <ggmatt@gmail.com>
2017-06-16 20:33:34 -0400

    Update project dependency on change

--- .scripts/link-dev-server-faster.sh
+++ .scripts/link-dev-server-faster.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
+IFS=$'\n' read -d '' -r -a projects <$(dirname $0)/PROJECTS
+
 PACKAGE=$1
 
 cd packages/lens-dev-server;
 echo "> Copying local dependency $PACKAGE to node_modules";
 rm -rf ./node_modules/lens-test/lib;
 cp -r ../$PACKAGE/lib ./node_modules/lens-test/lib;
+cd ..;
+for project in "${projects[@]}"; do
+  if [ "$project" != "lens-dev-server" ]; then \
+    cd $project;
+    rm -rf ./node_modules/$PACKAGE/lib;
+    cp -r ../$PACKAGE/lib ./node_modules/$PACKAGE/lib;
+  fi
+done
 echo "Done copying";
 echo "";
