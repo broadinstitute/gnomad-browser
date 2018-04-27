d7141012622fe3faa6de3505594ffb8c1ac1ab95
Nick Watts <nwatts@broadinstitute.org>
2018-04-27 11:40:05 -0400

    Add ES mapping for regional constraint regional missense data

--- projects/gnomad/data/export_regional_constraint_regional_missense.js
+++ projects/gnomad/data/export_regional_constraint_regional_missense.js
@@ -1,14 +1,16 @@
 import { loadCsvToElastic } from '@broad/api/utilities/elasticsearch'
+import regionalMissenseMapping from './regional_missense_mapping.json'
 
 const filePath = '../../../resources/fordist_constraint_official_regional_missense_cleaned_metrics_nosynoutliers.txt'
 
 const config = {
   address: '23.236.50.46:9200',
-  dropPreviousIndex: false,
+  dropPreviousIndex: true,
   filePath,
   delimiter: '\t',
   indexName: 'regional_missense',
   typeName: 'region',
+  mapping: regionalMissenseMapping,
 }
 
 loadCsvToElastic(config)
