b420e4933267b08ce8d94a6ed590e436cb903c35
Nick Watts <nwatts@broadinstitute.org>
2018-05-17 12:46:47 -0400

    Resolve ESLint error in export-pdf example

--- packages/export-pdf/src/example/index.js
+++ packages/export-pdf/src/example/index.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 import React from 'react'
 
 import { generateComponentAsPDF } from '../index'
@@ -6,6 +7,10 @@ const TestComponent = ({ message }) => {
   return (
     <h1>This is a test! {message}</h1>
   )
+}
+
+TestComponent.propTypes = {
+  message: PropTypes.string.isRequired,
 }
 
 const options = {
