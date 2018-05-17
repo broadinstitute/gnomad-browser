d07c13baa6cc6692fca534f200166f9b909de745
Nick Watts <nwatts@broadinstitute.org>
2018-05-17 11:51:38 -0400

    Remove empty constraint package

--- packages/constraint/src/index.js
+++
@@ -1,7 +0,0 @@
-import React, { PropTypes } from 'react'
-import R from 'ramda'
-import { scaleLinear, scaleBand } from 'd3-scale'
-import { max, min, range } from 'd3-array'
-
-import css from './styles.css'
-
