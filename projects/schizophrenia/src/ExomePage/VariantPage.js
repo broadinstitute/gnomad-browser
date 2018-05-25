0fc0cbfd2119d1f7588c302dbd9fded76b088df3
Nick Watts <nwatts@broadinstitute.org>
2018-05-25 14:14:17 -0400

    Update variant details
    Co-authored-by: Matthew Solomonson <ggmatt@gmail.com>

--- projects/schizophrenia/src/ExomePage/VariantPage.js
+++ projects/schizophrenia/src/ExomePage/VariantPage.js
@@ -43,11 +43,7 @@ const VariantTitle = styled.h1`
 `
 
 const VariantDetails = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: flex-start;
-  justify-content: space-between;
-  width: 1050px;
+  width: 50%;
 `
 
 const VariantAttributes = styled.div`
@@ -61,6 +57,19 @@ const VariantAttributes = styled.div`
 
 const VariantAttribute = styled.div`
   margin-bottom: 2px;
+`
+
+const TranscriptAttributes = styled.dl`
+  margin-top: 0;
+  dt, dd {
+    display: inline;
+  }
+`
+
+const Link = styled.a`
+  color: rgba(70, 130, 180, 1);
+  cursor: pointer;
+  text-decoration: none;
 `
 
 const schizophreniaVariantDetailQuery = gql`
@@ -99,19 +108,97 @@ const withQuery = graphql(schizophreniaVariantDetailQuery, {
   },
 })
 
+function formatConsequence(abbreviation) {
+  if (!abbreviation) {
+    return ''
+  }
+  switch (abbreviation) {
+    case 'mis':
+      return 'Missense'
+    case 'ns':
+      return 'Inframe indel'
+    case 'syn':
+      return 'Synonymous'
+    case 'splice':
+      return 'Splice region'
+    case 'lof':
+      return 'Loss of function'
+    default:
+      return abbreviation
+  }
+}
+
+function formatPolyPhen(abbreviation) {
+  if (!abbreviation) {
+    return ''
+  }
+  switch (abbreviation) {
+    case 'D':
+      return 'Damaging'
+    case 'P':
+      return 'Possibly damaging'
+    case 'B':
+      return 'Benign'
+    default:
+      return abbreviation
+  }
+}
+
 const Variant = ({ variant, data: { loading, groups } }) => {
   if (!variant || loading) {
     return <VariantContainer />
   }
+
+  const transcriptHGVSc = {}
+  if (variant.hgvsc) {
+    variant.hgvsc.split(',').forEach(s => {
+      const [tID, hgvsc] = s.split(':')
+      transcriptHGVSc[tID] = hgvsc
+    })
+  }
+
+  const transcriptHGVSp = {}
+  if (variant.hgvsp) {
+    variant.hgvsp.split(',').forEach(s => {
+      const [tID, hgvsp] = s.split(':')
+      transcriptHGVSp[tID] = hgvsp
+    })
+  }
+
   console.log(groups)
   return (
     <VariantContainer>
       <VariantTitle>{variant.variant_id}</VariantTitle>
       <SideBySide>
         <VariantDetails>
+          <Link href={`http://gnomad.broadinstitute.org/variant/${variant.variant_id}`}>View in gnomAD</Link>
+
           <VariantAttributes>
+            <h2>Statistics</h2>
             <VariantAttribute>
-              <strong>Consequence:</strong> {variant.consequence}
+              <strong>Cases:</strong> {variant.ac_case} / {variant.an_case} ({Number(variant.af_case.toPrecision(4)).toExponential()})
+            </VariantAttribute>
+            <VariantAttribute>
+              <strong>Controls:</strong> {variant.ac_ctrl} / {variant.an_ctrl}  ({Number(variant.af_ctrl.toPrecision(4)).toExponential()})
+            </VariantAttribute>
+            <VariantAttribute>
+              <strong>N analysis groups:</strong> {variant.n_analysis_groups}
+            </VariantAttribute>
+            <VariantAttribute>
+              <strong>N denovos:</strong> {variant.ac_denovo}
+            </VariantAttribute>
+          </VariantAttributes>
+
+          <VariantAttributes>
+            <h2>Annotations</h2>
+            <VariantAttribute>
+              <strong>HGVSc:</strong> {variant.hgvsc_canonical}
+            </VariantAttribute>
+            <VariantAttribute>
+              <strong>HGVSp:</strong> {variant.hgvsp_canonical}
+            </VariantAttribute>
+            <VariantAttribute>
+              <strong>Consequence:</strong> {formatConsequence(variant.consequence)}
             </VariantAttribute>
             <VariantAttribute>
               <strong>MPC:</strong> {variant.mpc}
@@ -120,33 +207,86 @@ const Variant = ({ variant, data: { loading, groups } }) => {
               <strong>CADD:</strong> {variant.cadd}
             </VariantAttribute>
             <VariantAttribute>
-              <strong>Cases:</strong> {variant.ac_case} / {variant.an_case}
+              <strong>PolyPhen:</strong> {formatPolyPhen(variant.polyphen)}
             </VariantAttribute>
             <VariantAttribute>
-              <strong>Controls:</strong> {variant.ac_ctrl} / {variant.an_ctrl}
+              <strong>Flags:</strong> {variant.flags}
+            </VariantAttribute>
+            <VariantAttribute>
+              <strong>Source:</strong> {variant.source}
+            </VariantAttribute>
+            <VariantAttribute>
+              <strong>In analysis:</strong> {variant.in_analysis}
+            </VariantAttribute>
+            <VariantAttribute>
+              <strong>Comment:</strong> {variant.comment}
             </VariantAttribute>
           </VariantAttributes>
+
+          {variant.pval_meta !== null && (
+            <VariantAttributes>
+              <h2>Analysis</h2>
+              <VariantAttribute>
+                <strong>Meta P-Value:</strong> {Number(variant.pval_meta.toPrecision(3)).toExponential()}
+              </VariantAttribute>
+              <VariantAttribute>
+                <strong>Estimate:</strong> {Number(variant.estimate.toPrecision(3)).toExponential()}
+              </VariantAttribute>
+              <VariantAttribute>
+                <strong>SE:</strong> {variant.se}
+              </VariantAttribute>
+              <VariantAttribute>
+                <strong>Qp:</strong> {variant.qp}
+              </VariantAttribute>
+              <VariantAttribute>
+                <strong>I2:</strong> {variant.i2}
+              </VariantAttribute>
+            </VariantAttributes>
+          )}
+
+          {variant.transcript_id && (
+            <VariantAttributes>
+              <h2>Transcripts</h2>
+              {variant.transcript_id.split(',').map(tID => (
+                <VariantAttribute key={tID}>
+                  <strong>{tID}</strong>
+                  <TranscriptAttributes>
+                    <div>
+                      <dt>HGVSc</dt><dd>{transcriptHGVSc[tID]}</dd>
+                    </div>
+                    <div>
+                      <dt>HGVSp</dt><dd>{transcriptHGVSp[tID]}</dd>
+                    </div>
+                  </TranscriptAttributes>
+                </VariantAttribute>
+              ))}
+            </VariantAttributes>
+          )}
         </VariantDetails>
-        <Table>
-          <TableRows>
-            <TableHeader>
-              <TableCell width={'200px'}>Group</TableCell>
-              <TableCell width={'80px'}>Cases</TableCell>
-              <TableCell width={'80px'}>Controls</TableCell>
-              <TableCell width={'80px'}>P-value</TableCell>
-              <TableCell width={'80px'}>Beta</TableCell>
-            </TableHeader>
-            {fromJS(groups).map(group => (
-              <TableRow>
-                <TableCell width={'200px'}>{group.get('group')}</TableCell>
-                <TableCell width={'80px'}>{group.get('ac_case')}</TableCell>
-                <TableCell width={'80px'}>{group.get('ac_ctrl')}</TableCell>
-                <TableCell width={'80px'}>{group.get('pval')}</TableCell>
-                <TableCell width={'80px'}>{group.get('beta')}</TableCell>
-              </TableRow>
-            ))}
-          </TableRows>
-        </Table>
+
+        <div>
+          <h2>Groups</h2>
+          <Table>
+            <TableRows>
+              <TableHeader>
+                <TableCell width={'200px'}>Group</TableCell>
+                <TableCell width={'80px'}>Cases</TableCell>
+                <TableCell width={'80px'}>Controls</TableCell>
+                <TableCell width={'80px'}>P-value</TableCell>
+                <TableCell width={'80px'}>Beta</TableCell>
+              </TableHeader>
+              {fromJS(groups).map(group => (
+                <TableRow key={group.get('group')}>
+                  <TableCell width={'200px'}>{group.get('group')}</TableCell>
+                  <TableCell width={'80px'}>{group.get('ac_case')}</TableCell>
+                  <TableCell width={'80px'}>{group.get('ac_ctrl')}</TableCell>
+                  <TableCell width={'80px'}>{group.get('pval')}</TableCell>
+                  <TableCell width={'80px'}>{group.get('beta')}</TableCell>
+                </TableRow>
+              ))}
+            </TableRows>
+          </Table>
+        </div>
       </SideBySide>
     </VariantContainer>
   )
