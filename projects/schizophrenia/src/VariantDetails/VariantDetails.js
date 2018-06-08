eac18c2e1ddff8d42134b2aa5e5fa9765b976eb5
Nick Watts <nwatts@broadinstitute.org>
2018-06-08 10:35:51 -0400

    [SCHEMA] Split analysis groups table out of variant details component

--- projects/schizophrenia/src/VariantDetails/VariantDetails.js
+++ projects/schizophrenia/src/VariantDetails/VariantDetails.js
@@ -1,45 +1,26 @@
-import React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
+import React from 'react'
 import { connect } from 'react-redux'
-import gql from 'graphql-tag'
-import { graphql, compose } from 'react-apollo'
-import { singleVariantData, focusedVariant } from '@broad/redux-variants'
-import { fromJS } from 'immutable'
-import {
-  Table,
-  VerticalTextLabels,
-  TableVerticalLabel,
-  VerticalLabelText,
-  TableRows,
-  TableRow,
-  TableHeader,
-  TableCell,
-  TableTitleColumn,
-} from '@broad/ui'
+import styled from 'styled-components'
+
+import { singleVariantData } from '@broad/redux-variants'
+
+import AnalysisGroupsTable from './AnalysisGroupsTable'
+
 
 const VariantContainer = styled.div`
   display: flex;
   flex-direction: column;
-  width: 80%;
+  margin: 30px 50px 100px 0;
   min-height: 300px;
-  ${'' /* margin-left: 50px; */}
-  margin-top: 30px;
-  margin-right: 50px;
-  margin-bottom: 100px;
+  width: 80%;
 `
 
 const SideBySide = styled.div`
+  align-items: space-between;
   display: flex;
   flex-direction: row;
   width: 100%;
-  align-items: space-between;
-
-  ${'' /* s: space-between; */}
-`
-
-const VariantTitle = styled.h1`
-
 `
 
 const VariantDetails = styled.div`
@@ -47,12 +28,11 @@ const VariantDetails = styled.div`
 `
 
 const VariantAttributes = styled.div`
-  display: flex;
-  font-size: 16px;
-  flex-direction: column;
   align-items: space-between;
-  margin-top: 10px;
-  margin-bottom: 10px;
+  display: flex;
+  flex-direction: column;
+  font-size: 16px;
+  margin: 10px 0;
 `
 
 const VariantAttribute = styled.div`
@@ -72,41 +52,6 @@ const Link = styled.a`
   text-decoration: none;
 `
 
-const schizophreniaVariantDetailQuery = gql`
-  query VariantDetailTable(
-    $variantQuery: String,
-  ) {
-    groups: schzGroups(variant_id: $variantQuery) {
-      pos
-      xpos
-      pval
-      ac_case
-      contig
-      beta
-      variant_id
-      an_ctrl
-      an_case
-      group
-      ac_ctrl
-      allele_freq
-    }
-  }
-`
-
-const withQuery = graphql(schizophreniaVariantDetailQuery, {
-  options: ({ variant }) => {
-    let variantQuery
-    if (!variant) {
-      variantQuery = ''
-    } else {
-      variantQuery = variant.variant_id
-    }
-    return ({
-      variables: { variantQuery },
-      errorPolicy: 'ignore'
-    })
-  },
-})
 
 function formatConsequence(abbreviation) {
   if (!abbreviation) {
@@ -144,14 +89,14 @@ function formatPolyPhen(abbreviation) {
   }
 }
 
-const Variant = ({ variant, data: { loading, groups } }) => {
-  if (!variant || loading) {
-    return <VariantContainer />
+const Variant = ({ variant }) => {
+  if (!variant) {
+    return null
   }
 
   const transcriptHGVSc = {}
   if (variant.hgvsc) {
-    variant.hgvsc.split(',').forEach(s => {
+    variant.hgvsc.split(',').forEach((s) => {
       const [tID, hgvsc] = s.split(':')
       transcriptHGVSc[tID] = hgvsc
     })
@@ -159,16 +104,15 @@ const Variant = ({ variant, data: { loading, groups } }) => {
 
   const transcriptHGVSp = {}
   if (variant.hgvsp) {
-    variant.hgvsp.split(',').forEach(s => {
+    variant.hgvsp.split(',').forEach((s) => {
       const [tID, hgvsp] = s.split(':')
       transcriptHGVSp[tID] = hgvsp
     })
   }
 
-  console.log(groups)
   return (
     <VariantContainer>
-      <VariantTitle>{variant.variant_id}</VariantTitle>
+      <h1>{variant.variant_id}</h1>
       <SideBySide>
         <VariantDetails>
           <Link href={`http://gnomad.broadinstitute.org/variant/${variant.variant_id}`}>View in gnomAD</Link>
@@ -264,38 +208,19 @@ const Variant = ({ variant, data: { loading, groups } }) => {
           )}
         </VariantDetails>
 
-        <div>
-          <h2>Groups</h2>
-          <Table>
-            <TableRows>
-              <TableHeader>
-                <TableCell width={'200px'}>Group</TableCell>
-                <TableCell width={'80px'}>Cases</TableCell>
-                <TableCell width={'80px'}>Controls</TableCell>
-                <TableCell width={'80px'}>P-value</TableCell>
-                <TableCell width={'80px'}>Beta</TableCell>
-              </TableHeader>
-              {fromJS(groups).map(group => (
-                <TableRow key={group.get('group')}>
-                  <TableCell width={'200px'}>{group.get('group')}</TableCell>
-                  <TableCell width={'80px'}>{group.get('ac_case')}</TableCell>
-                  <TableCell width={'80px'}>{group.get('ac_ctrl')}</TableCell>
-                  <TableCell width={'80px'}>{group.get('pval')}</TableCell>
-                  <TableCell width={'80px'}>{group.get('beta')}</TableCell>
-                </TableRow>
-              ))}
-            </TableRows>
-          </Table>
-        </div>
+        <AnalysisGroupsTable variantId={variant.variant_id} />
+
       </SideBySide>
     </VariantContainer>
   )
 }
+
 Variant.propTypes = {
-  variant: PropTypes.object.isRequired,
+  variant: PropTypes.object,
 }
 
-export default compose(
-  connect(state => ({ variant: singleVariantData(state) })),
-  withQuery
-)(Variant)
+Variant.defaultProps = {
+  variant: null,
+}
+
+export default connect(state => ({ variant: singleVariantData(state) }))(Variant)
