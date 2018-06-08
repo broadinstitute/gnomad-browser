b29b9a159c5471484f405cc59b55f51fffe3abbe
Nick Watts <nwatts@broadinstitute.org>
2018-06-08 15:25:55 -0400

    [SCHEMA] Add allele frequency columns to analysis groups table

--- projects/schizophrenia/src/VariantDetails/AnalysisGroupsTable.js
+++ projects/schizophrenia/src/VariantDetails/AnalysisGroupsTable.js
@@ -12,24 +12,47 @@ import {
 } from '@broad/ui'
 
 
+function formatExponential(number) {
+  return Number(number.toPrecision(4)).toExponential()
+}
+
+
 export function BaseAnalysisGroupsTable({ groups }) {
   return (
     <Table>
       <TableRows>
         <TableHeader>
           <TableCell width={'200px'}>Group</TableCell>
-          <TableCell width={'80px'}>Cases</TableCell>
-          <TableCell width={'80px'}>Controls</TableCell>
-          <TableCell width={'80px'}>P-value</TableCell>
-          <TableCell width={'80px'}>Beta</TableCell>
+          <TableCell width={'60px'}>AC Case</TableCell>
+          <TableCell width={'60px'}>AN Case</TableCell>
+          <TableCell width={'60px'}>AC Ctrl</TableCell>
+          <TableCell width={'60px'}>AN Ctrl</TableCell>
+          <TableCell width={'60px'}>AF Case</TableCell>
+          <TableCell width={'60px'}>AF Ctrl</TableCell>
+          <TableCell width={'60px'}>P-value</TableCell>
+          <TableCell width={'60px'}>Beta</TableCell>
         </TableHeader>
         {groups.map(group => (
           <TableRow key={group.group}>
             <TableCell width={'200px'}>{group.group}</TableCell>
-            <TableCell width={'80px'}>{group.ac_case}</TableCell>
-            <TableCell width={'80px'}>{group.ac_ctrl}</TableCell>
-            <TableCell width={'80px'}>{group.pval}</TableCell>
-            <TableCell width={'80px'}>{group.beta}</TableCell>
+            <TableCell width={'60px'}>{group.ac_case}</TableCell>
+            <TableCell width={'60px'}>{group.an_case}</TableCell>
+            <TableCell width={'60px'}>{group.ac_ctrl}</TableCell>
+            <TableCell width={'60px'}>{group.an_ctrl}</TableCell>
+            <TableCell width={'60px'}>
+              {group.an_case === 0
+                ? 0
+                : formatExponential(group.ac_case / group.an_case)
+              }
+            </TableCell>
+            <TableCell width={'60px'}>
+              {group.an_ctrl === 0
+                ? 0
+                : formatExponential(group.ac_ctrl / group.an_ctrl)
+              }
+            </TableCell>
+            <TableCell width={'60px'}>{group.pval}</TableCell>
+            <TableCell width={'60px'}>{group.beta}</TableCell>
           </TableRow>
         ))}
       </TableRows>
@@ -41,9 +64,11 @@ BaseAnalysisGroupsTable.propTypes = {
   groups: PropTypes.arrayOf(PropTypes.shape({
     group: PropTypes.string.isRequired,
     ac_case: PropTypes.number.isRequired,
+    an_case: PropTypes.number.isRequired,
     ac_ctrl: PropTypes.number.isRequired,
-    pval: PropTypes.number.isRequired,
-    beta: PropTypes.number.isRequired,
+    an_ctrl: PropTypes.number.isRequired,
+    pval: PropTypes.number,
+    beta: PropTypes.number,
   })).isRequired,
 }
 
