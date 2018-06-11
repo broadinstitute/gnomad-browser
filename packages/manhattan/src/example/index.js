45bacda8ad46daf16c5fb3cb7bcd6dd082c7933f
Nick Watts <nwatts@broadinstitute.org>
2018-06-11 14:58:34 -0400

    [manhattan] Use string type for chromosome prop

--- packages/manhattan/src/example/index.js
+++ packages/manhattan/src/example/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import ManhattanPlot from '../index'
 
 import data from '@resources/gwas-eg.json'  // eslint-disable-line
+
+import ManhattanPlot from '../index'
+
+
+const plotData = data.map(d => ({ ...d, chromosome: `${d.chromosome}` }))
 
 const ManhattanPlotExample = () => {
   return (
     <div>
-      <ManhattanPlot data={data} onClickPoint={console.log} />
+      <ManhattanPlot data={plotData} onClickPoint={console.log} />
     </div>
   )
 }
