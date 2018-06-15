5cc3c48aabf6e2f6078899d8c27493f3a625d413
Nick Watts <nwatts@broadinstitute.org>
2018-06-15 14:32:49 -0400

    [track-transcript] Remove TranscriptTrack wrapper
    Merge its functionality into connect arguments

--- packages/track-transcript/src/TranscriptConnected.js
+++ packages/track-transcript/src/TranscriptConnected.js
@@ -1,7 +1,5 @@
-import React from 'react'
-import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { TranscriptTrack } from './index'
+import { bindActionCreators } from 'redux'
 
 import {
   currentGene,
@@ -17,69 +15,10 @@ import {
   actions as geneActions,
 } from '@broad/redux-genes'
 
-const TranscriptConnected = ({
-  ownProps,
-  transcripts,
-  currentTranscript,
-  canonicalTranscript,
-  transcriptsGrouped,
-  setCurrentTranscript,
-  currentExon,
-  setCurrentExon,
-  currentTissue,
-  setCurrentTissue,
-  tissueStats,
-  strand,
-  currentGene,
-  transcriptFanOut,
-  toggleTranscriptFanOut,
-}) => {
-  return (
-    <TranscriptTrack
-      transcripts={transcripts}
-      currentTranscript={currentTranscript}
-      transcriptsGrouped={transcriptsGrouped}
-      onTranscriptNameClick={setCurrentTranscript}
-      currentExon={currentExon}
-      onExonClick={setCurrentExon}
-      currentTissue={currentTissue}
-      tissueStats={tissueStats}
-      onTissueChange={setCurrentTissue}
-      currentGene={currentGene}
-      canonicalTranscript={canonicalTranscript}
-      strand={strand}
-      transcriptFanOut={transcriptFanOut}
-      transcriptButtonOnClick={toggleTranscriptFanOut}
-      {...ownProps}
-    />
-  )
-}
-TranscriptConnected.propTypes = {
-  ownProps: PropTypes.object.isRequired,
-  currentGene: PropTypes.string,
-  currentTissue: PropTypes.string,
-  currentTranscript: PropTypes.string,
-  canonicalTranscript: PropTypes.string,
-  currentExon: PropTypes.string,
-  strand: PropTypes.string,
-  transcripts: PropTypes.array.isRequired,
-  transcriptsGrouped: PropTypes.object.isRequired,
-  tissueStats: PropTypes.any.isRequired,
-  setCurrentTissue: PropTypes.func.isRequired,
-  setCurrentTranscript: PropTypes.func.isRequired,
-  setCurrentExon: PropTypes.func.isRequired,
-}
+import TranscriptTrack from './TranscriptTrack'
 
-TranscriptConnected.defaultProps = {
-  currentTissue: null,
-  currentTranscript: null,
-  canonicalTranscript: null,
-  currentExon: null,
-  strand: null,
-}
 
-const mapStateToProps = (state, ownProps) => ({
-  ownProps,
+const mapStateToProps = state => ({
   currentGene: currentGene(state),
   currentTissue: currentTissue(state),
   currentTranscript: currentTranscript(state),
@@ -92,4 +31,11 @@ const mapStateToProps = (state, ownProps) => ({
   strand: strand(state),
 })
 
-export default connect(mapStateToProps, geneActions)(TranscriptConnected)
+const mapDispatchToProps = dispatch => ({
+  onExonClick: bindActionCreators(geneActions.setCurrentExon, dispatch),
+  onTissueChange: bindActionCreators(geneActions.setCurrentTissue, dispatch),
+  onTranscriptNameClick: bindActionCreators(geneActions.setCurrentTranscript, dispatch),
+  transcriptButtonOnClick: bindActionCreators(geneActions.toggleTranscriptFanOut, dispatch),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(TranscriptTrack)
