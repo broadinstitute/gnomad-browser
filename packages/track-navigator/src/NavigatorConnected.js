9962767410ee2d6a5f5c9f0792af88fa97b5e156
Nick Watts <nwatts@broadinstitute.org>
2018-07-02 15:18:42 -0400

    [track-navigator] Remove extra wrapper in connected track

--- packages/track-navigator/src/NavigatorConnected.js
+++ packages/track-navigator/src/NavigatorConnected.js
@@ -1,67 +1,26 @@
-/* eslint-disable no-shadow */
-/* eslint-disable import/no-unresolved */
-/* eslint-disable import/extensions */
-
-import React from 'react'
-import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
+import {
+  finalFilteredVariants,
+  hoveredVariant,
+  types as variantActionTypes,
+  variantSortKey,
+} from '@broad/redux-variants'
 import {
   actions as tableActions,
   currentTableIndex,
   currentTableScrollData,
 } from '@broad/table'
 
-import {
-  hoveredVariant,
-  variantSortKey,
-  finalFilteredVariants,
-  types as variantActionTypes
-} from '@broad/redux-variants'
-
 import NavigatorTrack from './Navigator'
 
-const Navigator = ({
-  currentTableIndex,
-  currentTableScrollData,
-  hoveredVariant,
-  onNavigatorClick,
-  variants,
-  variantSortKey,
-  ownProps,
-}) => {
-  return (
-    <NavigatorTrack
-      title={''}
-      height={60}
-      onNavigatorClick={onNavigatorClick}
-      scrollSync={currentTableIndex}
-      currentTableScrollData={currentTableScrollData}
-      variants={variants}
-      hoveredVariant={hoveredVariant}
-      variantSortKey={variantSortKey}
-      {...ownProps}
-    />
-  )
-}
-Navigator.propTypes = {
-  currentTableIndex: PropTypes.number.isRequired,
-  currentTableScrollData: PropTypes.object.isRequired,
-  hoveredVariant: PropTypes.string.isRequired,
-  onNavigatorClick: PropTypes.func.isRequired,
-  variants: PropTypes.any.isRequired,
-  variantSortKey: PropTypes.string.isRequired,
-  ownProps: PropTypes.object.isRequired,
-}
 
-const mapStateToProps = (state, ownProps) => ({
-  currentTableIndex: currentTableIndex(state),
+const mapStateToProps = state => ({
   currentTableScrollData: currentTableScrollData(state),
   hoveredVariant: hoveredVariant(state),
-  variantSortKey: variantSortKey(state),
-  // variants: visibleVariants(state),
+  scrollSync: currentTableIndex(state),
   variants: finalFilteredVariants(state),
-  ownProps,
+  variantSortKey: variantSortKey(state),
 })
 
 
@@ -72,4 +31,5 @@ const mapDispatchToProps = dispatch => ({
   }
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(Navigator)
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavigatorTrack)
