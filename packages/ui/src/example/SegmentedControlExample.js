08f63834cc612653980f9af40a64de212e0c105a
Nick Watts <nwatts@broadinstitute.org>
2018-07-11 14:31:37 -0400

    [ui] Add segmented control component

---
+++ packages/ui/src/example/SegmentedControlExample.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+import { SegmentedControl } from '..'
+
+
+export default class SegmentedControlExample extends Component {
+  state = {
+    options: [
+      { value: 'foo' },
+      { value: 'bar' },
+      { value: 'baz' },
+    ],
+    value: 'foo',
+  }
+
+  onChange = (value) => {
+    this.setState({ value })
+  }
+
+  render() {
+    return (
+      <SegmentedControl
+        id="segmented-control-example"
+        options={this.state.options}
+        value={this.state.value}
+        onChange={this.onChange}
+      />
+    )
+  }
+}
