525b4088ac39dc43c31c9264444d22451d33e418
Nick Watts <nwatts@broadinstitute.org>
2018-07-23 11:28:46 -0400

    [table] Remove unused action

--- packages/table/src/tableRedux.js
+++ packages/table/src/tableRedux.js
@@ -4,16 +4,12 @@ import keymirror from 'keymirror'
 export const types = keymirror({
   SET_CURRENT_TABLE_INDEX: null,
   SET_CURRENT_TABLE_SCROLL_DATA: null,
-  SET_VISIBLE_IN_TABLE: null,
 })
 
 export const actions = {
   setCurrentTableIndex: tableIndex => ({
     type: types.SET_CURRENT_TABLE_INDEX,
     tableIndex,
-    // meta: {
-    //   throttle: true,
-    // }
   }),
 
   setCurrentTableScrollData: tableScrollData => (dispatch) => {
@@ -21,13 +17,6 @@ export const actions = {
       type: types.SET_CURRENT_TABLE_SCROLL_DATA,
       tableScrollData,
     }))
-  },
-
-  setVisibleInTable: (range) => {
-    return {
-      type: types.SET_VISIBLE_IN_TABLE,
-      range,
-    }
   },
 }
 
@@ -38,20 +27,12 @@ const actionHandlers = {
   [types.SET_CURRENT_TABLE_SCROLL_DATA] (state, { tableScrollData }) {
     return state.set('currentTableScrollData', tableScrollData)
   },
-  [types.SET_VISIBLE_IN_TABLE] (state, { range }) {
-    const [min, max] = state.get('visibleInTable')
-    if (min < 0 || max < 0) {
-      return state.set('visibleInTable', [0, 15])
-    }
-    return state.set('visibleInTable', range)
-  },
 }
 
 export default function createTableReducer () {
   const State = Immutable.Record({
     currentTableIndex: 0,
     currentTableScrollData: { scrollHeight: 1, scrollTop: 2 },
-    visibleInTable: [0, 15],
   })
   function reducer (state = new State(), action: Object): State {
     const { type } = action
