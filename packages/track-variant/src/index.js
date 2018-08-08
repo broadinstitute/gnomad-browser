d1316380f385bd42ad1981f1c830a0d1a6dae04a
Nick Watts <nwatts@broadinstitute.org>
2018-08-08 16:57:42 -0400

    [track-variant] Add stacked variants plot

--- packages/track-variant/src/index.js
+++ packages/track-variant/src/index.js
@@ -1,3 +1,4 @@
+export { StackedVariantsPlot } from './StackedVariantsPlot'
 export { VariantAlleleFrequencyPlot } from './VariantAlleleFrequencyPlot'
 export { VariantAlleleFrequencyTrack } from './VariantAlleleFrequencyTrack'
 export { VariantPositionTrack } from './VariantPositionTrack'
