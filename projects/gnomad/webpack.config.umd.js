53b9ba12326e7c1a17cd4c4ae8b9c08700e661fb
Nick Watts <nwatts@broadinstitute.org>
2018-09-06 15:09:15 -0400

    [gnomAD] Update browser build/deployment scripts

--- projects/gnomad/webpack.config.umd.js
+++ projects/gnomad/webpack.config.umd.js
@@ -1,16 +1,16 @@
 const path = require('path')
 
 const webpack = require('webpack')
+
 const config = require('../../webpack.config')
 
 const umdConfig = {
   devtool: 'source-map',
   entry: {
-    genepage: ['babel-polyfill', './src/index.umd.js']
+    genepage: ['babel-polyfill', './src/index.umd.js'],
   },
   output: {
-    path: '/Users/msolomon/Projects/exacg/exac_gnomad_private/static',
-    // path: path.resolve(__dirname, 'dist/umd'),
+    path: path.resolve(__dirname, 'dist/umd'),
     filename: '[name]-1517593459.js',
     libraryTarget: 'umd',
     library: 'ReactGnomad',
