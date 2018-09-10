223594467b89cdb3c12be32263b4d71d50d4da48
Nick Watts <nwatts@broadinstitute.org>
2018-09-10 16:13:11 -0400

    [ui] Add combobox component

---
+++ packages/ui/src/example/ComboboxExample.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+import { Combobox } from '..'
+
+export default class ComboboxExample extends Component {
+  state = {
+    value: 'foo',
+  }
+
+  render() {
+    return (
+      <Combobox
+        options={['foo', 'bar', 'baz', 'qux'].map(s => ({ label: s, value: s }))}
+        onChange={value => {
+          this.setState({ value })
+        }}
+        value={this.state.value}
+      />
+    )
+  }
+}
--- packages/ui/src/example/index.js
+++ packages/ui/src/example/index.js
@@ -14,6 +14,7 @@ import {
   TextButton,
 } from '..'
 import CheckboxExample from './CheckboxExample'
+import ComboboxExample from './ComboboxExample'
 import ConsequenceCategoriesControlExample from './ConsequenceCategoriesControlExample'
 import InfoModalExample from './InfoModalExample'
 import SegmentedControlExample from './SegmentedControlExample'
@@ -50,6 +51,11 @@ const UiExample = () => (
     <Section>
       <SectionHeading>Checkbox</SectionHeading>
       <CheckboxExample />
+    </Section>
+
+    <Section>
+      <SectionHeading>Combobox</SectionHeading>
+      <ComboboxExample />
     </Section>
 
     <Section>
