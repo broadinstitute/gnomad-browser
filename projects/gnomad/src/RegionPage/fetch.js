eb872130847cb3db901c729e944a47d46d7c5c5a
Nick Watts <nwatts@broadinstitute.org>
2018-10-07 22:37:44 -0400

    [gnomAD] Fetch ExAC variants using updated API

--- projects/gnomad/src/RegionPage/fetch.js
+++ projects/gnomad/src/RegionPage/fetch.js
@@ -82,20 +82,21 @@ export const fetchRegion = regionId => {
       lcr
       segdup
     }
-    exacVariants {
-      variant_id
-      rsid
-      pos
-      xpos
+    exacVariants: variants(dataset:exac) {
+      allele_count: ac
+      hemi_count: ac_hemi
+      hom_count: ac_hom
+      allele_num: an
+      allele_freq: af
+      consequence
+      filters
+      flags
       hgvsc
       hgvsp
-      allele_count
-      allele_freq
-      allele_num
-      filters
-      hom_count
-      consequence
-      lof
+      pos
+      rsid
+      variant_id: variantId
+      xpos
     }
   }
 
