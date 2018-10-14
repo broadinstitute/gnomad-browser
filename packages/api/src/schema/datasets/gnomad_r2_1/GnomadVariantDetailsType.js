ccd59cce6c9cf58102859938d3b92a34f0ab7827
Nick Watts <nwatts@broadinstitute.org>
2018-10-14 14:07:09 -0400

    [api] Add API for fetching gnomAD 2.1 variant details

---
+++ packages/api/src/schema/datasets/gnomad_r2_1/GnomadVariantDetailsType.js
@@ -0,0 +1,162 @@
+import {
+  GraphQLFloat,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLString,
+} from 'graphql'
+
+import { VariantInterface } from '../../types/variant'
+import { resolveReads, ReadsType } from '../shared/reads'
+import { TranscriptConsequenceType } from '../shared/transcriptConsequence'
+
+const HistogramType = new GraphQLObjectType({
+  name: 'Histogram',
+  fields: {
+    bin_edges: { type: new GraphQLList(GraphQLFloat) },
+    bin_freq: { type: new GraphQLList(GraphQLFloat) },
+    n_larger: { type: GraphQLInt },
+    n_smaller: { type: GraphQLInt },
+  },
+})
+
+const GnomadSubpopulationType = new GraphQLObjectType({
+  name: 'GnomadVariantSubpopulation',
+  fields: {
+    id: { type: new GraphQLNonNull(GraphQLString) },
+    ac: { type: new GraphQLNonNull(GraphQLInt) },
+    an: { type: new GraphQLNonNull(GraphQLInt) },
+  },
+})
+
+const GnomadPopulationType = new GraphQLObjectType({
+  name: 'GnomadVariantPopulation',
+  fields: {
+    id: { type: new GraphQLNonNull(GraphQLString) },
+    ac: { type: new GraphQLNonNull(GraphQLInt) },
+    an: { type: new GraphQLNonNull(GraphQLInt) },
+    ac_hemi: { type: new GraphQLNonNull(GraphQLInt) },
+    ac_hom: { type: new GraphQLNonNull(GraphQLInt) },
+    subpopulations: { type: new GraphQLList(GnomadSubpopulationType) },
+  },
+})
+
+const GnomadVariantQualityMetricsType = new GraphQLObjectType({
+  name: 'GnomadVariantQualityMetrics',
+  fields: {
+    genotypeDepth: {
+      type: new GraphQLObjectType({
+        name: 'GnomadVariantGenotypeDepth',
+        fields: {
+          all: { type: HistogramType },
+          alt: { type: HistogramType },
+        },
+      }),
+    },
+    genotypeQuality: {
+      type: new GraphQLObjectType({
+        name: 'GnomadVariantGenotypeQuality',
+        fields: {
+          all: { type: HistogramType },
+          alt: { type: HistogramType },
+        },
+      }),
+    },
+    siteQualityMetrics: {
+      type: new GraphQLObjectType({
+        name: 'GnomadVariantSiteQualityMetrics',
+        fields: {
+          BaseQRankSum: { type: GraphQLFloat },
+          ClippingRankSum: { type: GraphQLFloat },
+          DP: { type: GraphQLInt },
+          FS: { type: GraphQLFloat },
+          InbreedingCoeff: { type: GraphQLFloat },
+          MQ: { type: GraphQLFloat },
+          MQRankSum: { type: GraphQLFloat },
+          QD: { type: GraphQLFloat },
+          ReadPosRankSum: { type: GraphQLFloat },
+          SiteQuality: { type: GraphQLFloat },
+          SOR: { type: GraphQLFloat },
+          VQSLOD: { type: GraphQLFloat },
+        },
+      }),
+    },
+  },
+})
+
+const GnomadVariantDetailsType = new GraphQLObjectType({
+  name: 'GnomadVariantDetails',
+  interfaces: [VariantInterface],
+  fields: {
+    // variant interface fields
+    alt: { type: new GraphQLNonNull(GraphQLString) },
+    chrom: { type: new GraphQLNonNull(GraphQLString) },
+    pos: { type: new GraphQLNonNull(GraphQLInt) },
+    ref: { type: new GraphQLNonNull(GraphQLString) },
+    variantId: { type: new GraphQLNonNull(GraphQLString) },
+    xpos: { type: new GraphQLNonNull(GraphQLFloat) },
+    // gnomAD specific fields
+    colocatedVariants: { type: new GraphQLList(GraphQLString) },
+    exome: {
+      type: new GraphQLObjectType({
+        name: 'GnomadVariantDetailsExomeData',
+        fields: {
+          ac: { type: GraphQLInt },
+          an: { type: GraphQLInt },
+          ac_hemi: { type: GraphQLInt },
+          ac_hom: { type: GraphQLInt },
+          filters: { type: new GraphQLList(GraphQLString) },
+          populations: { type: new GraphQLList(GnomadPopulationType) },
+          qualityMetrics: { type: GnomadVariantQualityMetricsType },
+          reads: {
+            type: ReadsType,
+            resolve: async obj => {
+              if (!process.env.READS_DIR) {
+                return null
+              }
+              try {
+                return await resolveReads(process.env.READS_DIR, 'combined_bams_exomes', obj)
+              } catch (err) {
+                throw Error('Unable to load reads data')
+              }
+            },
+          },
+        },
+      }),
+    },
+    flags: { type: new GraphQLList(GraphQLString) },
+    genome: {
+      type: new GraphQLObjectType({
+        name: 'GnomadVariantDetailsGenomeData',
+        fields: {
+          ac: { type: GraphQLInt },
+          an: { type: GraphQLInt },
+          ac_hemi: { type: GraphQLInt },
+          ac_hom: { type: GraphQLInt },
+          filters: { type: new GraphQLList(GraphQLString) },
+          populations: { type: new GraphQLList(GnomadPopulationType) },
+          qualityMetrics: { type: GnomadVariantQualityMetricsType },
+          reads: {
+            type: ReadsType,
+            resolve: async obj => {
+              if (!process.env.READS_DIR) {
+                return null
+              }
+              try {
+                return await resolveReads(process.env.READS_DIR, 'combined_bams_genomes', obj)
+              } catch (err) {
+                throw Error('Unable to load reads data')
+              }
+            },
+          },
+        },
+      }),
+    },
+    rsid: { type: GraphQLString },
+    sortedTranscriptConsequences: { type: new GraphQLList(TranscriptConsequenceType) },
+  },
+  isTypeOf: variantData => variantData.gqlType === 'GnomadVariantDetails',
+})
+
+export default GnomadVariantDetailsType
---
+++ packages/api/src/schema/datasets/gnomad_r2_1/fetchGnomadVariantDetails.js
@@ -0,0 +1,211 @@
+const formatHistogram = histogramData => ({
+  bin_edges: histogramData.bin_edges.split('|').map(s => Number(s)),
+  bin_freq: histogramData.bin_freq.split('|').map(s => Number(s)),
+  n_larger: histogramData.n_larger,
+  n_smaller: histogramData.n_smaller,
+})
+
+const POPULATIONS = ['afr', 'amr', 'asj', 'eas', 'fin', 'nfe', 'oth', 'sas']
+
+const SUBPOPULATIONS = {
+  afr: ['female', 'male'],
+  amr: ['female', 'male'],
+  asj: ['female', 'male'],
+  eas: ['female', 'male', 'jpn', 'kor', 'oea'],
+  fin: ['female', 'male'],
+  nfe: ['female', 'male', 'bgr', 'est', 'nwe', 'onf', 'seu', 'swe'],
+  oth: ['female', 'male'],
+  sas: ['female', 'male'],
+}
+
+const formatPopulations = variantData =>
+  POPULATIONS.map(popId => ({
+    id: popId.toUpperCase(),
+    ac: (variantData.AC_adj[popId] || {}).total || 0,
+    an: (variantData.AN_adj[popId] || {}).total || 0,
+    ac_hemi: variantData.nonpar ? (variantData.AC_adj[popId] || {}).male || 0 : 0,
+    ac_hom: (variantData.nhomalt_adj[popId] || {}).total || 0,
+    subpopulations: SUBPOPULATIONS[popId].map(subPopId => ({
+      id: subPopId.toUpperCase(),
+      ac: (variantData.AC_adj[popId] || {})[subPopId] || 0,
+      an: (variantData.AN_adj[popId] || {})[subPopId] || 0,
+    })),
+  }))
+
+const fetchGnomadVariantData = async (ctx, variantId, subset) => {
+  const requests = [
+    { index: 'gnomad_exomes_2_1', subset },
+    // All genome samples are non_cancer, so separate non-cancer numbers are not stored
+    { index: 'gnomad_genomes_2_1', subset: subset === 'non_cancer' ? 'gnomad' : subset },
+  ]
+
+  const [exomeData, genomeData] = await Promise.all(
+    requests.map(({ index, subset: requestSubset }) =>
+      ctx.database.elastic
+        .search({
+          index,
+          type: 'variant',
+          _source: [
+            requestSubset,
+            'allele_info',
+            'alt',
+            'chrom',
+            'dp_hist_all',
+            'dp_hist_alt',
+            'filters',
+            'flags',
+            'gnomad_age_hist_het',
+            'gnomad_age_hist_hom',
+            'gq_hist_all',
+            'gq_hist_alt',
+            'nonpar',
+            'pos',
+            'qual',
+            'ref',
+            'rsid',
+            'sortedTranscriptConsequences',
+            'variant_id',
+            'xpos',
+          ],
+          body: {
+            query: {
+              bool: {
+                filter: [
+                  { term: { variant_id: variantId } },
+                  { range: { [`${requestSubset}.AC_adj.total`]: { gt: 0 } } },
+                ],
+              },
+            },
+          },
+          size: 1,
+        })
+        .then(response => response.hits.hits[0])
+        // eslint-disable-next-line no-underscore-dangle
+        .then(doc => (doc ? { ...doc._source, ...doc._source[requestSubset] } : undefined))
+    )
+  )
+
+  return {
+    exomeData,
+    genomeData,
+  }
+}
+
+const fetchColocatedVariants = async (ctx, variantId, subset) => {
+  const parts = variantId.split('-')
+  const chrom = parts[0]
+  const pos = Number(parts[1])
+
+  const response = await ctx.database.elastic.search({
+    index: ['gnomad_exomes_2_1', 'gnomad_genomes_2_1'],
+    type: 'variant',
+    _source: ['variant_id'],
+    body: {
+      query: {
+        bool: {
+          filter: [
+            { term: { chrom } },
+            { term: { pos } },
+            { range: { [`${subset}.AC_adj.total`]: { gt: 0 } } },
+          ],
+        },
+      },
+    },
+  })
+
+  return response.hits.hits
+    .map(doc => doc._source.variantId) // eslint-disable-line
+    .filter(otherVariantId => otherVariantId !== variantId)
+    .sort()
+    .filter(
+      (otherVariantId, index, allOtherVariantIds) =>
+        otherVariantId !== allOtherVariantIds[index + 1]
+    )
+}
+
+const fetchGnomadVariantDetails = async (ctx, variantId, subset) => {
+  const [{ exomeData, genomeData }, colocatedVariants] = await Promise.all([
+    fetchGnomadVariantData(ctx, variantId, subset),
+    fetchColocatedVariants(ctx, variantId, subset),
+  ])
+
+  if (!exomeData && !genomeData) {
+    throw Error('Variant not found')
+  }
+
+  const sharedData = exomeData || genomeData
+
+  const sharedVariantFields = {
+    alt: sharedData.alt,
+    chrom: sharedData.chrom,
+    pos: sharedData.pos,
+    ref: sharedData.ref,
+    variantId: sharedData.variant_id,
+    xpos: sharedData.xpos,
+  }
+
+  return {
+    gqlType: 'GnomadVariantDetails',
+    // variant interface fields
+    ...sharedVariantFields,
+    // gnomAD specific fields
+    colocatedVariants,
+    exome: exomeData
+      ? {
+          // Include variant fields so that the reads data resolver can access them.
+          ...sharedVariantFields,
+          ac: exomeData.AC_adj.total,
+          an: exomeData.AN_adj.total,
+          ac_hemi: exomeData.nonpar ? exomeData.AC_adj.male : 0,
+          ac_hom: exomeData.nhomalt_adj.total,
+          filters: exomeData.filters,
+          populations: formatPopulations(exomeData),
+          qualityMetrics: {
+            genotypeDepth: {
+              all: formatHistogram(exomeData.dp_hist_all),
+              alt: formatHistogram(exomeData.dp_hist_alt),
+            },
+            genotypeQuality: {
+              all: formatHistogram(exomeData.gq_hist_all),
+              alt: formatHistogram(exomeData.gq_hist_alt),
+            },
+            siteQualityMetrics: {
+              ...exomeData.allele_info,
+              SiteQuality: exomeData.qual,
+            },
+          },
+        }
+      : null,
+    flags: ['lcr', 'segdup', 'lc_lof', 'lof_flag'].filter(flag => sharedData.flags[flag]),
+    genome: genomeData
+      ? {
+          // Include variant fields so that the reads data resolver can access them.
+          ...sharedVariantFields,
+          ac: genomeData.AC_adj.total,
+          an: genomeData.AN_adj.total,
+          ac_hemi: genomeData.nonpar ? genomeData.AC_adj.male : 0,
+          ac_hom: genomeData.nhomalt_adj.total,
+          filters: genomeData.filters,
+          populations: formatPopulations(genomeData),
+          qualityMetrics: {
+            genotypeDepth: {
+              all: formatHistogram(genomeData.dp_hist_all),
+              alt: formatHistogram(genomeData.dp_hist_alt),
+            },
+            genotypeQuality: {
+              all: formatHistogram(genomeData.gq_hist_all),
+              alt: formatHistogram(genomeData.gq_hist_alt),
+            },
+            siteQualityMetrics: {
+              ...genomeData.allele_info,
+              SiteQuality: genomeData.qual,
+            },
+          },
+        }
+      : null,
+    rsid: sharedData.rsid,
+    sortedTranscriptConsequences: sharedData.sortedTranscriptConsequences,
+  }
+}
+
+export default fetchGnomadVariantDetails
