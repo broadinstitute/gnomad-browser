291dc52cbe23dddcbbfbc6cc666688ec6eaa3218
Nick Watts <nwatts@broadinstitute.org>
2018-10-15 11:09:04 -0400

    [gnomAD] Expand populations table to show subpopulations

--- projects/gnomad/src/VariantPage/PopulationsTable.js
+++ projects/gnomad/src/VariantPage/PopulationsTable.js
@@ -2,6 +2,8 @@ import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import styled from 'styled-components'
 
+import { TextButton } from '@broad/ui'
+
 const Table = styled.table`
   border-collapse: collapse;
   border-spacing: 0;
@@ -52,6 +54,10 @@ const Table = styled.table`
   }
 `
 
+const TogglePopulationButton = TextButton.extend`
+  text-align: left;
+`
+
 export class PopulationsTable extends Component {
   static propTypes = {
     populations: PropTypes.arrayOf(
@@ -61,6 +67,14 @@ export class PopulationsTable extends Component {
         an: PropTypes.number.isRequired,
         ac_hemi: PropTypes.number.isRequired,
         ac_hom: PropTypes.number.isRequired,
+        subpopulations: PropTypes.arrayOf(
+          PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            ac: PropTypes.number.isRequired,
+            an: PropTypes.number.isRequired,
+            ac_hom: PropTypes.number.isRequired,
+          })
+        ),
       })
     ).isRequired,
     showHemizygotes: PropTypes.bool,
@@ -75,6 +89,7 @@ export class PopulationsTable extends Component {
   state = {
     sortBy: 'af',
     sortAscending: false,
+    expandedPopulations: {},
   }
 
   setSortBy(sortBy) {
@@ -84,17 +99,39 @@ export class PopulationsTable extends Component {
     }))
   }
 
-  renderColumnHeader(key, label) {
+  togglePopulationExpanded(populationName) {
+    this.setState(state => ({
+      ...state,
+      expandedPopulations: {
+        ...state.expandedPopulations,
+        [populationName]: !state.expandedPopulations[populationName],
+      },
+    }))
+  }
+
+  renderColumnHeader(key, label, colSpan = undefined) {
     let ariaSortAttr = 'none'
     if (this.state.sortBy === key) {
       ariaSortAttr = this.state.sortAscending ? 'ascending' : 'descending'
     }
 
     return (
-      <th aria-sort={ariaSortAttr} role="columnheader">
+      <th colSpan={colSpan} aria-sort={ariaSortAttr} role="columnheader">
         <span role="button" tabIndex="0" onClick={() => this.setSortBy(key)}>
           {label}
         </span>
+      </th>
+    )
+  }
+
+  renderPopulationRowHeader(pop) {
+    const colSpan = this.state.expandedPopulations[pop.name] ? 1 : 2
+    const rowSpan = this.state.expandedPopulations[pop.name] ? pop.subpopulations.length + 1 : 1
+    return (
+      <th colSpan={colSpan} rowSpan={rowSpan} role="rowheader">
+        <TogglePopulationButton onClick={() => this.togglePopulationExpanded(pop.name)}>
+          {pop.name}
+        </TogglePopulationButton>
       </th>
     )
   }
@@ -104,6 +141,26 @@ export class PopulationsTable extends Component {
       .map(pop => ({
         ...pop,
         af: pop.an !== 0 ? pop.ac / pop.an : 0,
+        subpopulations: pop.subpopulations
+          .map(subPop => ({
+            ...subPop,
+            af: subPop.an !== 0 ? subPop.ac / subPop.an : 0,
+          }))
+          .sort((a, b) => {
+            // Sort male/female subpopulations to bottom of list
+            if ((a.id === 'MALE' || a.id === 'FEMALE') && (b.id !== 'MALE' && b.id !== 'FEMALE')) {
+              return 1
+            }
+            if ((b.id === 'MALE' || b.id === 'FEMALE') && (a.id !== 'MALE' && a.id !== 'FEMALE')) {
+              return -1
+            }
+
+            const [subPop1, subPop2] = this.state.sortAscending ? [a, b] : [b, a]
+
+            return this.state.sortBy === 'name'
+              ? subPop1.name.localeCompare(subPop2.name)
+              : subPop1[this.state.sortBy] - subPop2[this.state.sortBy]
+          }),
       }))
       .sort((a, b) => {
         const [pop1, pop2] = this.state.sortAscending ? [a, b] : [b, a]
@@ -126,26 +183,39 @@ export class PopulationsTable extends Component {
       <Table role="grid">
         <thead>
           <tr role="row">
-            {this.renderColumnHeader('name', 'Population')}
+            {this.renderColumnHeader('name', 'Population', 2)}
             {this.renderColumnHeader('ac', 'Allele Count')}
             {this.renderColumnHeader('an', 'Allele Number')}
             {showHomozygotes && this.renderColumnHeader('ac_hom', 'Number of Homozygotes')}
-            {showHemizygotes && this.renderColumnHeader('ac_hemi', 'Number of Hemigzygotes')}
+            {showHemizygotes && this.renderColumnHeader('ac_hemi', 'Number of Hemizygotes')}
             {this.renderColumnHeader('af', 'Allele Frequency')}
           </tr>
         </thead>
-        <tbody>
-          {populations.map(pop => (
+        {populations.map(pop => (
+          <tbody key={pop.name}>
             <tr key={pop.name} role="row">
-              <th role="rowheader">{pop.name}</th>
+              {this.renderPopulationRowHeader(pop)}
+              {this.state.expandedPopulations[pop.name] && <td role="gridcell">Overall</td>}
               <td role="gridcell">{pop.ac}</td>
               <td role="gridcell">{pop.an}</td>
               {showHomozygotes && <td role="gridcell">{pop.ac_hom}</td>}
               {showHemizygotes && <td role="gridcell">{pop.ac_hemi}</td>}
               <td role="gridcell">{pop.af.toPrecision(4)}</td>
             </tr>
-          ))}
-        </tbody>
+            {pop.subpopulations &&
+              this.state.expandedPopulations[pop.name] &&
+              pop.subpopulations.map(subPop => (
+                <tr key={`${pop.name}-${subPop.name}`} role="row">
+                  <td role="gridcell">{subPop.name}</td>
+                  <td role="gridcell">{subPop.ac}</td>
+                  <td role="gridcell">{subPop.an}</td>
+                  {showHomozygotes && <td role="gridcell">{subPop.ac_hom}</td>}
+                  {showHemizygotes && <td role="gridcell">&mdash;</td>}
+                  <td role="gridcell">{subPop.af.toPrecision(4)}</td>
+                </tr>
+              ))}
+          </tbody>
+        ))}
         <tfoot>
           <tr role="row">
             <th role="rowheader">Total</th>
