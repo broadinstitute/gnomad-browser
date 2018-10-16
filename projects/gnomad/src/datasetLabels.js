9b92fd7f0c5d405203cce8757f7b6def96b9b152
Nick Watts <nwatts@broadinstitute.org>
2018-10-16 16:02:03 -0400

    [gnomAD] Change from r to v in version labels

--- projects/gnomad/src/datasetLabels.js
+++ projects/gnomad/src/datasetLabels.js
@@ -1,11 +1,11 @@
 const datasetLabels = {
   exac: 'ExAC',
-  gnomad_r2_0_2: 'gnomAD r2.0.2',
-  gnomad_r2_1: 'gnomAD r2.1',
-  gnomad_r2_1_controls: 'gnomAD r2.1 (controls)',
-  gnomad_r2_1_non_cancer: 'gnomAD r2.1 (non-cancer)',
-  gnomad_r2_1_non_neuro: 'gnomAD r2.1 (non-neuro)',
-  gnomad_r2_1_non_topmed: 'gnomAD r2.1 (non-TOPMed)',
+  gnomad_r2_0_2: 'gnomAD v2.0.2',
+  gnomad_r2_1: 'gnomAD v2.1',
+  gnomad_r2_1_controls: 'gnomAD v2.1 (controls)',
+  gnomad_r2_1_non_cancer: 'gnomAD v2.1 (non-cancer)',
+  gnomad_r2_1_non_neuro: 'gnomAD v2.1 (non-neuro)',
+  gnomad_r2_1_non_topmed: 'gnomAD v2.1 (non-TOPMed)',
 }
 
 export default datasetLabels
