1a7f74c37e6d93ca82ac9a4c2720bf5d9881eefd
Nick Watts <nwatts@broadinstitute.org>
2018-10-16 15:23:36 -0400

    [gnomAD] Style nav bar and menu for small screens

--- projects/gnomad/src/NavBar.js
+++ projects/gnomad/src/NavBar.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
+
+import { Button } from '@broad/ui'
 
 import Searchbox from './Searchbox'
 
@@ -9,12 +11,27 @@ const NavBarWrapper = styled.div`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
+  overflow: hidden;
   box-sizing: border-box;
   width: 100%;
   padding: 0.5em 30px;
   margin-bottom: 20px;
   background-color: black;
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.23);
+
+  @media (max-width: 900px) {
+    flex-direction: column;
+  }
+`
+
+const LogoWrapper = styled.div`
+  @media (max-width: 900px) {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+  }
 `
 
 const Logo = styled.div`
@@ -23,12 +40,35 @@ const Logo = styled.div`
   font-weight: bold;
 `
 
+const ToggleMenuButton = Button.extend`
+  border: 1px solid #fafafa;
+  background: black;
+  color: white;
+
+  @media (min-width: 900px) {
+    display: none;
+  }
+`
+
 const Menu = styled.ul`
   display: flex;
   flex-direction: row;
+  overflow: hidden;
   padding: 0;
   margin: 0;
   list-style-type: none;
+
+  @media (max-width: 900px) {
+    flex-direction: column;
+    width: 100%;
+    height: ${props => (props.isExpanded ? 'auto' : 0)};
+
+    a {
+      display: inline-block;
+      width: 100%;
+      padding: 1em 0;
+    }
+  }
 `
 
 const MenuLink = styled(Link)`
@@ -43,33 +83,48 @@ const MenuExternalLink = styled.a`
   text-decoration: none;
 `
 
-const NavBar = () => (
-  <NavBarWrapper>
-    <MenuLink to="/">
-      <Logo>gnomAD browser</Logo>
-    </MenuLink>
-    <Searchbox id="navbar-search" width="320px" />
-    <Menu>
-      <li>
-        <MenuLink to="/about">About</MenuLink>
-      </li>
-      <li>
-        <MenuLink to="/downloads">Downloads</MenuLink>
-      </li>
-      <li>
-        <MenuLink to="/terms">Terms</MenuLink>
-      </li>
-      <li>
-        <MenuLink to="/contact">Contact</MenuLink>
-      </li>
-      <li>
-        <MenuExternalLink href="https://macarthurlab.org/jobs/">Jobs</MenuExternalLink>
-      </li>
-      <li>
-        <MenuLink to="/faq">FAQ</MenuLink>
-      </li>
-    </Menu>
-  </NavBarWrapper>
-)
+class NavBar extends Component {
+  state = {
+    menuExpanded: false,
+  }
+
+  toggleMenu = () => {
+    this.setState(state => ({ ...state, menuExpanded: !state.menuExpanded }))
+  }
+
+  render() {
+    return (
+      <NavBarWrapper>
+        <LogoWrapper>
+          <MenuLink to="/">
+            <Logo>gnomAD browser</Logo>
+          </MenuLink>
+          <ToggleMenuButton onClick={this.toggleMenu}>☰</ToggleMenuButton>
+        </LogoWrapper>
+        <Searchbox id="navbar-search" width="320px" />
+        <Menu isExpanded={this.state.menuExpanded}>
+          <li>
+            <MenuLink to="/about">About</MenuLink>
+          </li>
+          <li>
+            <MenuLink to="/downloads">Downloads</MenuLink>
+          </li>
+          <li>
+            <MenuLink to="/terms">Terms</MenuLink>
+          </li>
+          <li>
+            <MenuLink to="/contact">Contact</MenuLink>
+          </li>
+          <li>
+            <MenuExternalLink href="https://macarthurlab.org/jobs/">Jobs</MenuExternalLink>
+          </li>
+          <li>
+            <MenuLink to="/faq">FAQ</MenuLink>
+          </li>
+        </Menu>
+      </NavBarWrapper>
+    )
+  }
+}
 
 export default NavBar
