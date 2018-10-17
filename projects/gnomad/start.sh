291b4a32668217d9ab3783debb4caabf5acb3a2a
Nick Watts <nwatts@broadinstitute.org>
2018-10-17 09:45:59 -0400

    [gnomAD] Update default API URL

--- projects/gnomad/start.sh
+++ projects/gnomad/start.sh
@@ -8,6 +8,6 @@ cd $PROJECT_DIR
 export PATH=$PATH:$PROJECT_DIR/../../node_modules/.bin
 
 export NODE_ENV="development"
-export GNOMAD_API_URL=${GNOMAD_API_URL:-"http://gnomad-api2.broadinstitute.org"}
+export GNOMAD_API_URL=${GNOMAD_API_URL:-"http://gnomad-api.broadinstitute.org"}
 
 webpack-dev-server --config=./config/webpack.config.client.js --hot
