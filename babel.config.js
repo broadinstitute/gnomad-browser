4db0166ed6a86c697daee6a5ae83f106f2eb23eb
Nick Watts <nwatts@broadinstitute.org>
2018-10-26 17:19:33 -0400

    [ui] Import SVG using Babel instead of webpack
    So that consuming the ui package does not require using
    webpack and svg-inline-loader

--- babel.config.js
+++ babel.config.js
@@ -17,6 +17,7 @@ module.exports = {
   plugins: [
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-proposal-class-properties',
+    'inline-react-svg',
     'styled-components',
     [
       'module-resolver',
