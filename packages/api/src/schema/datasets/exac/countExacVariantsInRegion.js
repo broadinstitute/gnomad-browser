68919d8b42cded0e705a72ec97f41995a6ccec45
Nick Watts <nwatts@broadinstitute.org>
2018-10-31 15:06:05 -0400

    [gnomAD] Remove padding when querying variants by region

--- packages/api/src/schema/datasets/exac/countExacVariantsInRegion.js
+++ packages/api/src/schema/datasets/exac/countExacVariantsInRegion.js
@@ -1,21 +1,21 @@
 const countExacVariantsByRegion = async (ctx, { chrom, start, stop }) => {
-  const padding = 75
-  const rangeQuery = {
-    range: {
-      pos: {
-        gte: start - padding,
-        lte: stop + padding,
-      },
-    },
-  }
-
   const response = await ctx.database.elastic.count({
     index: 'exac_v1_variants',
     type: 'variant',
     body: {
       query: {
         bool: {
-          filter: [{ term: { chrom } }, rangeQuery],
+          filter: [
+            { term: { chrom } },
+            {
+              range: {
+                pos: {
+                  gte: start,
+                  lte: stop,
+                },
+              },
+            },
+          ],
         },
       },
     },
