0e9887feb41fcea9ffc2594839507963a8f70acf
Nick Watts <nwatts@broadinstitute.org>
2018-11-16 16:02:49 -0500

    [exome-results] Remove dependency on Immutable

--- projects/exome-results-browsers/src/client/Main.js
+++ projects/exome-results-browsers/src/client/Main.js
@@ -1,7 +1,6 @@
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
-import { List } from 'immutable'
 import React from 'react'
 import { ApolloProvider } from 'react-apollo'
 import { Provider } from 'react-redux'
@@ -83,7 +82,6 @@ const appSettings = {
       transcript_id: null,
       variant_id: null,
       xpos: null,
-      datasets: new List(['variants']),
     },
   },
 }
