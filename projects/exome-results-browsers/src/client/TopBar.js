3c242834b5e26be081fded29314bd79bfec7fe45
Nick Watts <nwatts@broadinstitute.org>
2018-11-16 17:50:22 -0500

    [exome-results] Make top bar responsive
    Resolves #348

--- projects/exome-results-browsers/src/client/TopBar.js
+++ projects/exome-results-browsers/src/client/TopBar.js
@@ -1,94 +1,148 @@
 import PropTypes from 'prop-types'
-import React from 'react'
-import { Link, withRouter } from 'react-router-dom'
+import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
 import styled from 'styled-components'
 
-const TopBarContainer = styled.div`
+import { Button } from '@broad/ui'
+
+import Link from './Link'
+
+const TopBarWrapper = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  height: 60px;
-  padding: 0 40px;
-  border-bottom: 1px solid #000;
-  background-color: ${BROWSER_CONFIG.navBarColor || '#0a79bf'};
+  padding: 15px 30px;
+  background-color: ${BROWSER_CONFIG.navBarColor || '#000'};
+
+  @media (max-width: 900px) {
+    flex-direction: column;
+  }
+
+  ${Link} {
+    padding: 0.5em;
+    color: white;
+    text-decoration: none;
+  }
 `
 
-const Logo = styled.div`
-  color: white;
-  font-size: 24px;
+const TitleWrapper = styled.div`
+  color: #fff;
+
+  @media (max-width: 900px) {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+  }
+`
+
+const Title = styled.span`
+  font-size: 1.5em;
 `
 
 const SearchInput = styled.input`
   width: 275px;
-  padding: 0.375em 0.5em;
+  padding: 0.375em 0;
   background-color: white;
   font-size: 14px;
 `
 
+const ToggleMenuButton = Button.extend`
+  border: 1px solid #fafafa;
+  background: transparent;
+  color: inherit;
+
+  @media (min-width: 900px) {
+    display: none;
+  }
+`
+
 const Menu = styled.ul`
+  display: flex;
+  flex-direction: row;
+  overflow: hidden;
   padding: 0;
   margin: 0;
+  list-style-type: none;
+
+  @media (max-width: 900px) {
+    flex-direction: column;
+    width: 100%;
+    height: ${props => (props.isExpanded ? 'auto' : 0)};
+
+    ${Link} {
+      display: inline-block;
+      width: 100%;
+      padding: 1em 0;
+    }
+  }
 `
 
-const MenuItem = styled.li`
-  display: inline-block;
-  list-style: none;
-  margin-left: 20px;
-  font-size: 18px;
-`
+class TopBar extends Component {
+  static propTypes = {
+    history: PropTypes.shape({
+      push: PropTypes.func.isRequired,
+    }).isRequired,
+  }
 
-const StyledLink = styled(Link)`
-  color: white;
-  text-decoration: none;
-`
+  state = {
+    isMenuExpanded: false,
+  }
 
-const TopBar = ({ history }) => (
-  <TopBarContainer>
-    <StyledLink to={'/'}>
-      <Logo>{BROWSER_CONFIG.navBarTitle}</Logo>
-    </StyledLink>
+  toggleMenu = () => {
+    this.setState(state => ({ ...state, isMenuExpanded: !state.isMenuExpanded }))
+  }
 
-    <form
-      onSubmit={event => {
-        event.preventDefault()
-        const geneName = event.target.elements[0].value
-        history.push(`/gene/${geneName}`)
-      }}
-    >
-      <SearchInput type="text" name="search" placeholder="Search genes" list="genes" />
-      <datalist id="genes">
-        <option value="PCSK9" />
-        <option value="ZNF658" />
-        <option value="MYH9" />
-        <option value="FMR1" />
-        <option value="BRCA2" />
-        <option value="CFTR" />
-        <option value="FBN1" />
-        <option value="TP53" />
-        <option value="SCN5A" />
-        <option value="MYH7" />
-        <option value="MYBPC3" />
-        <option value="ARSF" />
-        <option value="CD33" />
-        <option value="DMD" />
-        <option value="TTN" />
-        <option value="USH2A" />
-      </datalist>
-    </form>
+  render() {
+    const { isMenuExpanded } = this.state
+    return (
+      <TopBarWrapper>
+        <TitleWrapper>
+          <Link to="/">
+            <Title>{BROWSER_CONFIG.navBarTitle}</Title>
+          </Link>
+          <ToggleMenuButton onClick={this.toggleMenu}>☰</ToggleMenuButton>
+        </TitleWrapper>
 
-    <Menu>
-      <MenuItem>
-        <StyledLink to={'/results'}>Results</StyledLink>
-      </MenuItem>
-    </Menu>
-  </TopBarContainer>
-)
+        <form
+          onSubmit={event => {
+            const { history } = this.props
+            event.preventDefault()
+            const geneName = event.target.elements[0].value
+            history.push(`/gene/${geneName}`)
+          }}
+        >
+          <SearchInput type="text" name="search" placeholder="Search genes" list="genes" />
+          <datalist id="genes">
+            <option value="PCSK9" />
+            <option value="ZNF658" />
+            <option value="MYH9" />
+            <option value="FMR1" />
+            <option value="BRCA2" />
+            <option value="CFTR" />
+            <option value="FBN1" />
+            <option value="TP53" />
+            <option value="SCN5A" />
+            <option value="MYH7" />
+            <option value="MYBPC3" />
+            <option value="ARSF" />
+            <option value="CD33" />
+            <option value="DMD" />
+            <option value="TTN" />
+            <option value="USH2A" />
+          </datalist>
+        </form>
 
-TopBar.propTypes = {
-  history: PropTypes.shape({
-    push: PropTypes.func.isRequired,
-  }).isRequired,
+        <Menu isExpanded={isMenuExpanded}>
+          <li>
+            <Link to="/results">Results</Link>
+          </li>
+        </Menu>
+      </TopBarWrapper>
+    )
+  }
 }
 
 export default withRouter(TopBar)
