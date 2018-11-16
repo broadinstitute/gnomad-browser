dff83bcc3969fa0094534bede7c04741ad613482
Nick Watts <nwatts@broadinstitute.org>
2018-11-16 16:53:02 -0500

    [exome-results] Remove field aliases for analysis groups

--- projects/exome-results-browsers/src/server/schema/analysis/analysisGroup.js
+++ projects/exome-results-browsers/src/server/schema/analysis/analysisGroup.js
@@ -7,35 +7,15 @@ export const AnalysisGroupType = new GraphQLObjectType({
     ac_case: { type: GraphQLInt },
     ac_ctrl: { type: GraphQLInt },
     af: { type: GraphQLFloat },
-    allele_freq: {
-      type: GraphQLFloat,
-      resolve: obj => obj.af,
-    },
     af_case: { type: GraphQLFloat },
     af_ctrl: { type: GraphQLFloat },
     an: { type: GraphQLInt },
     an_case: { type: GraphQLInt },
     an_ctrl: { type: GraphQLInt },
     analysis_group: { type: GraphQLString },
-    beta: {
-      type: GraphQLFloat,
-      resolve: obj => obj.se,
-    },
-    contig: { type: GraphQLString },
     est: { type: GraphQLFloat },
-    group: {
-      type: GraphQLString,
-      resolve: obj => obj.analysis_group,
-    },
     p: { type: GraphQLFloat },
-    pval: {
-      type: GraphQLFloat,
-      resolve: obj => obj.p,
-    },
-    pos: { type: GraphQLInt },
     se: { type: GraphQLFloat },
-    variant_id: { type: GraphQLString },
-    xpos: { type: GraphQLFloat },
   },
 })
 
@@ -43,7 +23,7 @@ export const fetchAnalysisGroupsForVariant = async (ctx, variantId) => {
   const response = await ctx.database.elastic.search({
     index: BROWSER_CONFIG.elasticsearch.analysisGroups.index,
     type: BROWSER_CONFIG.elasticsearch.analysisGroups.type,
-    size: 4000,
+    size: 100,
     body: {
       query: {
         match: {
@@ -52,5 +32,5 @@ export const fetchAnalysisGroupsForVariant = async (ctx, variantId) => {
       },
     },
   })
-  return response.hits.hits.map(h => h._source) // eslint-disable-line no-underscore-dangle
+  return response.hits.hits.map(hit => hit._source) // eslint-disable-line no-underscore-dangle
 }
