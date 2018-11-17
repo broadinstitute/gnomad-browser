2cc778714ed7e079773159be3acab33de08639c2
Nick Watts <nwatts@broadinstitute.org>
2018-11-17 11:16:08 -0500

    [gnomAD] Add additional MNV information to variant details

--- packages/api/src/schema/datasets/gnomad_r2_1/gnomadMultiNucleotideVariants.js
+++ packages/api/src/schema/datasets/gnomad_r2_1/gnomadMultiNucleotideVariants.js
@@ -1,4 +1,24 @@
+import { GraphQLInt, GraphQLObjectType, GraphQLString } from 'graphql'
+
 import { fetchAllSearchResults } from '../../../utilities/elasticsearch'
+
+export const GnomadMultiNucleotideVariantType = new GraphQLObjectType({
+  name: 'GnomadMultiNucleotideVariant',
+  fields: {
+    ac: { type: GraphQLInt },
+    category: { type: GraphQLString },
+    mnvAminoAcidChange: { type: GraphQLString },
+    mnvCodonChange: { type: GraphQLString },
+    mnvConsequence: { type: GraphQLString },
+    otherVariantId: { type: GraphQLString },
+    otherAminoAcidChange: { type: GraphQLString },
+    otherCodonChange: { type: GraphQLString },
+    otherConsequence: { type: GraphQLString },
+    snvAminoAcidChange: { type: GraphQLString },
+    snvConsequence: { type: GraphQLString },
+    snvCodonChange: { type: GraphQLString },
+  },
+})
 
 export const fetchGnomadMNVsByIntervals = async (ctx, intervals) => {
   const rangeQueries = intervals.map(region => ({
@@ -56,7 +76,21 @@ export const fetchGnomadMNVsByVariantId = async (ctx, variantId) => {
   const response = await ctx.database.elastic.search({
     index: 'gnomad_exome_mnvs_2_1',
     type: 'mnv',
-    _source: ['AC_mnv', 'category', 'snp1_variant_id', 'snp2_variant_id'],
+    _source: [
+      'AC_mnv',
+      'category',
+      'mnv_amino_acids',
+      'mnv_codons',
+      'mnv_consequence',
+      'snp1_amino_acids',
+      'snp1_codons',
+      'snp1_consequence',
+      'snp1_variant_id',
+      'snp2_amino_acids',
+      'snp2_codons',
+      'snp2_consequence',
+      'snp2_variant_id',
+    ],
     body: {
       query: {
         bool: {
@@ -76,11 +110,29 @@ export const fetchGnomadMNVsByVariantId = async (ctx, variantId) => {
   return response.hits.hits.map(hit => {
     const mnv = hit._source // eslint-disable-line no-underscore-dangle
 
-    const otherVariant = mnv.snp1_variant_id === variantId ? 'snp2' : 'snp1'
+    let thisVariant
+    let otherVariant
+    if (variantId === mnv.snp1_variant_id) {
+      thisVariant = 'snp1'
+      otherVariant = 'snp2'
+    } else {
+      thisVariant = 'snp2'
+      otherVariant = 'snp1'
+    }
+
     return {
       ac: mnv.AC_mnv,
       category: mnv.category,
+      mnvAminoAcidChange: mnv.mnv_amino_acids.replace('/', ' → '),
+      mnvCodonChange: mnv.mnv_codons.toUpperCase().replace('/', ' → '),
+      mnvConsequence: mnv.mnv_consequence,
       otherVariantId: mnv[`${otherVariant}_variant_id`],
+      otherAminoAcidChange: mnv[`${otherVariant}_amino_acids`].replace('/', ' → '),
+      otherCodonChange: mnv[`${otherVariant}_codons`].toUpperCase().replace('/', ' → '),
+      otherConsequence: mnv[`${otherVariant}_consequence`],
+      snvAminoAcidChange: mnv[`${thisVariant}_amino_acids`].replace('/', ' → '),
+      snvCodonChange: mnv[`${thisVariant}_codons`].toUpperCase().replace('/', ' → '),
+      snvConsequence: mnv[`${thisVariant}_consequence`],
     }
   })
 }
