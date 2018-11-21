6ec31b812ab57be9f32d9aa491286c10103dff90
Nick Watts <nwatts@broadinstitute.org>
2018-11-21 14:55:44 -0500

    [exome-results] Allow defining display order of analysis groups
    Resolves #344

---
+++ projects/exome-results-browsers/src/client/sortByGroup.js
@@ -0,0 +1,19 @@
+const groupOrder = BROWSER_CONFIG.analysisGroups.order
+
+let sortFunction
+if (groupOrder) {
+  const groupIndex = groupOrder.reduce(
+    (acc, group, i) => ({
+      ...acc,
+      [group]: i,
+    }),
+    {}
+  )
+  sortFunction = (groupA, groupB) => groupIndex[groupA] - groupIndex[groupB]
+} else {
+  sortFunction = (groupA, groupB) => groupA.localeCompare(groupB)
+}
+
+const sortByGroup = (list, key = 'analysis_group') => list.sort((a, b) => sortFunction(a[key], b[key]))
+
+export default sortByGroup
