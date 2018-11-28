a1f513aeef8f4186e20cb34cba03322abde8390e
Nick Watts <nwatts@broadinstitute.org>
2018-11-28 17:45:09 -0500

    [exome-results] Remove import from source file

--- projects/exome-results-browsers/src/client/Main.js
+++ projects/exome-results-browsers/src/client/Main.js
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { injectGlobal } from 'styled-components'
 
-import createGenePageStore from '@broad/gene-page/src/store/store'
+import { createGenePageStore } from '@broad/gene-page'
 import { actions as userInterfaceActions } from '@broad/ui'
 import { getLabelForConsequenceTerm, registerConsequences } from '@broad/utilities'
 
