a35df642255629806e3a86ae0c378ab3795bfd67
Nick Watts <nwatts@broadinstitute.org>
2018-11-29 22:35:55 -0500

    [exome-results] Move gene result search out of table component

--- projects/exome-results-browsers/src/client/GeneResultsPage/GeneResultsPage.js
+++ projects/exome-results-browsers/src/client/GeneResultsPage/GeneResultsPage.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { Component } from 'react'
+import styled from 'styled-components'
 
-import { Page, PageHeading } from '@broad/ui'
+import { Page, PageHeading, Search } from '@broad/ui'
 
 import browserConfig from '@browser/config'
 
@@ -25,33 +26,64 @@ const geneResultsQuery = `
   }
 `
 
-const GeneResultsPage = () => (
-  <Page>
-    <PageHeading>{browserConfig.geneResults.resultsPageHeading}</PageHeading>
-    <Query query={geneResultsQuery}>
-      {({ data, error, loading }) => {
-        if (loading) {
-          return <StatusMessage>Loading gene results...</StatusMessage>
-        }
+const ControlSection = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 1em;
+`
 
-        if (error) {
-          return <StatusMessage>Unable to load gene results</StatusMessage>
-        }
+class GeneResultsPage extends Component {
+  state = {
+    searchText: '',
+  }
 
-        const shapedResults = data.geneResults.map(result => {
-          const resultCopy = { ...result }
-          result.categories.forEach(c => {
-            resultCopy[`xcase_${c.id}`] = c.xcase
-            resultCopy[`xctrl_${c.id}`] = c.xctrl
-            resultCopy[`pval_${c.id}`] = c.pval
-          })
-          return resultCopy
-        })
+  render() {
+    return (
+      <Page>
+        <PageHeading>{browserConfig.geneResults.resultsPageHeading}</PageHeading>
+        <Query query={geneResultsQuery}>
+          {({ data, error, loading }) => {
+            if (loading) {
+              return <StatusMessage>Loading gene results...</StatusMessage>
+            }
 
-        return <GeneResultsTable results={shapedResults} />
-      }}
-    </Query>
-  </Page>
-)
+            if (error) {
+              return <StatusMessage>Unable to load gene results</StatusMessage>
+            }
+
+            const { searchText } = this.state
+            const filteredResults = data.geneResults.filter(result =>
+              (result.gene_name || '').includes(searchText)
+            )
+
+            const shapedResults = filteredResults.map(result => {
+              const resultCopy = { ...result }
+              result.categories.forEach(c => {
+                resultCopy[`xcase_${c.id}`] = c.xcase
+                resultCopy[`xctrl_${c.id}`] = c.xctrl
+                resultCopy[`pval_${c.id}`] = c.pval
+              })
+              return resultCopy
+            })
+
+            return (
+              <div>
+                <ControlSection>
+                  <Search
+                    placeholder="Search results by gene"
+                    onChange={value => {
+                      this.setState({ searchText: value.toUpperCase() })
+                    }}
+                  />
+                </ControlSection>
+                <GeneResultsTable results={shapedResults} />
+              </div>
+            )
+          }}
+        </Query>
+      </Page>
+    )
+  }
+}
 
 export default GeneResultsPage
