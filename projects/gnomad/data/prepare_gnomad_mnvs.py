dc2f596d48b7cac5abb1040eb750070c5d959aee
Nick Watts <nwatts@broadinstitute.org>
2018-12-03 15:45:25 -0500

    [gnomAD] Fix MNV loading script
    Do not rely on order of codons in codons fields.
    
    Resolves #361

--- projects/gnomad/data/prepare_gnomad_mnvs.py
+++ projects/gnomad/data/prepare_gnomad_mnvs.py
@@ -31,9 +31,13 @@ ds = hl.read_table(args.input_url)
 # Prepare #
 ###########
 
-# Codons are stored as ref_codon/alt_codon
+# Codons are stored as codon1/codon2
 def index_of_change(codons_str):
     return hl.bind(lambda codons: hl.range(0, 3).find(lambda i: codons[0][i] != codons[1][i]), codons_str.split("/"))
+
+
+def swap_codons(codons_str):
+    return hl.bind(lambda codons: codons[1] + "/" + codons[0], codons_str.split("/"))
 
 
 # Convert the string "None" to null
@@ -48,39 +52,55 @@ def none_to_null(value):
 # Both SNPs may have the same ref and alt, so we can't tell whether snp1 comes first
 # by matching the first ref/alt to the changed base in snp1_codons.
 # We also can't tell if snp1 comes first by looking only at the index of the changed
-# base in snp1_codons. If snp1_codons shows a change in the second base, we have to
-# compare that the index of the changed base in snp2_codons to tell whether snp2 comes
-# before or after snp1.
+# base in snp1's codons, since the second base in the codon could contain either the
+# first or second change.
 #
-# We can map the row's locus to an index in the mnv_codons field by finding the index
-# of the first changed base in the MNV's ref/alt codons.
-# Then, from the snp1_codons/snp2_codons fields, we can find the index of snp1 and snp2
-# within the MNV's codon.
-# Combining those two pieces of information, we can determine the positions of both SNPs.
-def snp_variant_id(ds, snp_number):
+# We can map the row's locus to an index in the codons by finding the index of the
+# first changed base in the MNV's codons.
+# Combining that with the index of the change in the snp1/snp2 codons, we can
+# determine the position of snp1 and snp2 relative to the locus.
+def snp_variant_id(snp_number):
     snp_codons = ds[f"snp{snp_number}_codons"]
     return hl.bind(
-        lambda index_of_change_in_codon, offset_locus_from_codon_start: hl.bind(
+        lambda snp_offset_from_position: hl.bind(
             lambda contig, position, ref, alt: contig + "-" + hl.str(position) + "-" + ref + "-" + alt,
             ds.locus.contig,
-            ds.locus.position + index_of_change_in_codon - offset_locus_from_codon_start,
-            snp_codons[index_of_change_in_codon],
-            snp_codons[index_of_change_in_codon + 4],
+            ds.locus.position + snp_offset_from_position,
+            ds.refs[snp_offset_from_position],
+            ds.alts[snp_offset_from_position],
         ),
-        index_of_change(snp_codons),
-        index_of_change(ds.mnv_codons),
+        index_of_change(snp_codons) - index_of_change(ds.mnv_codons),
     )
+
+
+def snp_ref(snp_number):
+    snp_codons = ds[f"snp{snp_number}_codons"]
+    return ds.refs[index_of_change(snp_codons) - index_of_change(ds.mnv_codons)]
 
 
 ds = ds.annotate(
     chrom=ds.locus.contig,
     pos=ds.locus.position,
     xpos=get_expr_for_xpos(ds),
-    snp1_variant_id=snp_variant_id(ds, 1),
-    snp2_variant_id=snp_variant_id(ds, 2),
+    snp1_variant_id=snp_variant_id(1),
+    snp2_variant_id=snp_variant_id(2),
     snp1_lof=none_to_null(ds.snp1_lof),
     snp2_lof=none_to_null(ds.snp2_lof),
     mnv_lof=none_to_null(ds.mnv_lof),
+    # In the Hail table, the order of the codons is based on the order of transcription.
+    # For the browser, order them ref codon first then alt codon.
+    snp1_codons=hl.cond(
+        ds.snp1_codons[index_of_change(ds.snp1_codons)] != snp_ref(1), swap_codons(ds.snp1_codons), ds.snp1_codons
+    ),
+    snp2_codons=hl.cond(
+        ds.snp2_codons[index_of_change(ds.snp2_codons)] != snp_ref(2), swap_codons(ds.snp2_codons), ds.snp2_codons
+    ),
+    mnv_codons=hl.bind(
+        lambda offset: hl.cond(
+            ds.mnv_codons[offset : offset + ds.refs.length()] != ds.refs, swap_codons(ds.mnv_codons), ds.mnv_codons
+        ),
+        index_of_change(ds.mnv_codons),
+    ),
 )
 
 ds = ds.transmute(category=ds.categ)
