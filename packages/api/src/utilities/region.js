042e0a3683dc07c07437ccd9253e9200da9bec3a
Nick Watts <nwatts@broadinstitute.org>
2018-12-05 13:59:10 -0500

    [gnomAD] Add region utility functions

--- packages/api/src/utilities/region.js
+++ packages/api/src/utilities/region.js
@@ -1,3 +1,12 @@
+export const extendRegions = (amount, regions) =>
+  regions.map(({ start, stop, xstart, xstop, ...rest }) => ({
+    ...rest,
+    start: start - amount,
+    stop: stop + amount,
+    xstart: xstart - amount,
+    xstop: xstop + amount,
+  }))
+
 /**
  * Create a minimal representation of a set of regions.
  * xstart and xstop are inclusive.
@@ -10,7 +19,7 @@
  * @param {number} sortedRegions[].xstart
  * @param {number} sortedRegions[].xstop
  */
-export const mergeOverlappingRegions = (sortedRegions) => {
+export const mergeOverlappingRegions = sortedRegions => {
   if (sortedRegions.length === 0) {
     return []
   }
@@ -22,7 +31,7 @@ export const mergeOverlappingRegions = (sortedRegions) => {
   for (let i = 1; i < sortedRegions.length; i += 1) {
     const nextRegion = sortedRegions[i]
 
-    if (nextRegion.xstart <= (previousRegion.xstop + 1)) {
+    if (nextRegion.xstart <= previousRegion.xstop + 1) {
       if (nextRegion.xstop > previousRegion.xstop) {
         previousRegion.stop = nextRegion.stop
         previousRegion.xstop = nextRegion.xstop
@@ -35,3 +44,6 @@ export const mergeOverlappingRegions = (sortedRegions) => {
 
   return mergedRegions
 }
+
+export const totalRegionSize = regions =>
+  regions.reduce((acc, { start, stop }) => acc + stop - start, 0)
