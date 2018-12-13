3de80c33beaff5e55c410b5bd573dd1f74e17ddf
Nick Watts <nwatts@broadinstitute.org>
2018-12-13 09:57:06 -0500

    [epi25] Rename EE group

--- projects/exome-results-browsers/browsers/epi25/data/load_gene_results_to_es.py
+++ projects/exome-results-browsers/browsers/epi25/data/load_gene_results_to_es.py
@@ -9,7 +9,7 @@ from hail_scripts.v01.utils.elasticsearch_client import ElasticsearchClient
 p = argparse.ArgumentParser()
 p.add_argument("--host", help="Elasticsearch host or IP", required=True)
 p.add_argument("--port", help="Elasticsearch port", default=9200, type=int)
-p.add_argument("--num-shards", help="Number of elasticsearch shards", default=1, type=int)
+p.add_argument("--num-shards", help="Number of elasticsearch shards", default=2, type=int)
 p.add_argument("--block-size", help="Elasticsearch block size to use when exporting", default=1000, type=int)
 args = p.parse_args()
 
@@ -40,11 +40,13 @@ kt = hc.import_table(
     }
 )
 
+kt = kt.annotate('analysis_group = if (analysis_group == "EE") "DEE" else analysis_group')
+
 es = ElasticsearchClient(args.host, args.port)
 
 es.export_kt_to_elasticsearch(
     kt,
-    index_name="epi_exome_gene_results_181107",
+    index_name="epi25_gene_results_181107",
     index_type_name="result",
     block_size=args.block_size,
     num_shards=args.num_shards,
