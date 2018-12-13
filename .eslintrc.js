3b2a3a58ba73b703789b8237b347c030c0dea77b
Nick Watts <nwatts@broadinstitute.org>
2018-12-13 12:00:07 -0500

    Consolidate ESLint config, ignore imports from aliases

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,21 +14,39 @@ module.exports = {
   rules: {
     'prettier/prettier': 'error',
     'no-console': 0,
-    'func-names': 0,
+    'func-names': ['warn', 'as-needed'],
     'react/jsx-filename-extension': ['error', { extensions: ['.js'] }],
-    'no-unused-vars': ['error', {
-      ignoreRestSiblings: true,
-      varsIgnorePattern: '',
-    }],
     'import/prefer-default-export': 0,
-    // 'import/no-extraneous-dependencies': ['error', {'devDependencies': ['**/*.test.js', '**/*.spec.js', '**/*.example.js']}],
     'import/no-extraneous-dependencies': 0,
   },
   overrides: [
     {
+      // Set environment for tests
       files: ['**/*spec.js', '**/*test.js'],
       env: {
         jest: true,
+      },
+    },
+    {
+      // Set environment for server-side code
+      files: ['packages/api/**/*.js', 'projects/*/src/server/**/*.js'],
+      env: {
+        browser: false,
+        node: true,
+      },
+    },
+    {
+      // Allow importing from resources only in package examples
+      files: ['packages/**/example/*.js'],
+      rules: {
+        'import/no-unresolved': ['error', { ignore: ['^@resources\/'] }],
+      },
+    },
+    {
+      // Ignore @browser webpack alias in exome-results-browsers
+      files: ['projects/exome-results-browsers/**/*.js'],
+      rules: {
+        'import/no-unresolved': ['error', { ignore: ['^@browser\/'] }],
       },
     },
   ],
