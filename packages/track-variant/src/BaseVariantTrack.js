3b00ab2f45ba1d346af2a177a6773dd27c7278d0
Nick Watts <nwatts@broadinstitute.org>
2018-12-13 10:38:15 -0500

    [track-variant] Vertically center track titles

--- packages/track-variant/src/BaseVariantTrack.js
+++ packages/track-variant/src/BaseVariantTrack.js
@@ -2,26 +2,23 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import styled from 'styled-components'
 
-
 const TrackContainer = styled.div`
   display: flex;
+  flex-direction: row;
+  align-items: stretch;
   margin-top: 5px;
 `
-
 
 const LeftPanel = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100%;
   justify-content: center;
   width: ${props => props.width}px;
 `
 
-
 const CenterPanel = styled.div`
   width: ${props => props.width}px;
 `
-
 
 export const BaseVariantTrack = ({
   children,
@@ -31,7 +28,7 @@ export const BaseVariantTrack = ({
 }) => (
   <TrackContainer>
     <LeftPanel width={leftPanelWidth}>
-      {title.split('\n').map(s => (<span key={s}>{s}</span>))}
+      {title.split('\n').map(s => <span key={s}>{s}</span>)}
     </LeftPanel>
     <CenterPanel width={width}>
       {children}
