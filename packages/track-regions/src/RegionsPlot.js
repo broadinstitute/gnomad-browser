f1121c5ad7c8f0fcd9f267f9758b78f81e70d683
Nick Watts <nwatts@broadinstitute.org>
2018-12-18 15:45:36 -0500

    [transcript] Allow downloading transcripts plot
    Relates to #226

--- packages/track-regions/src/RegionsPlot.js
+++ packages/track-regions/src/RegionsPlot.js
@@ -1,8 +1,16 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-export const RegionsPlot = ({ height, regions, regionAttributes, regionKey, width, xScale }) => (
-  <svg width={width} height={height}>
+export const RegionsPlot = ({
+  height,
+  regions,
+  regionAttributes,
+  regionKey,
+  width,
+  xScale,
+  ...rest
+}) => (
+  <svg {...rest} width={width} height={height}>
     <line x1={0} x2={width} y1={height / 2} y2={height / 2} stroke="#bdbdbd" strokeWidth={2} />
     {regions.map(region => {
       const x1 = xScale(region.start)
