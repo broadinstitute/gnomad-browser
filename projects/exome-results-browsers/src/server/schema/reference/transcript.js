9fc75a71461dc3224aca67707304dbae75211426
Nick Watts <nwatts@broadinstitute.org>
2018-12-19 11:51:46 -0500

    [exome-results] Fetch transcript and exons from Mongo concurrently

--- projects/exome-results-browsers/src/server/schema/reference/transcript.js
+++ projects/exome-results-browsers/src/server/schema/reference/transcript.js
@@ -1,18 +1,34 @@
-import { GraphQLList, GraphQLObjectType, GraphQLString } from 'graphql'
+import { GraphQLInt, GraphQLList, GraphQLObjectType, GraphQLString } from 'graphql'
 
-import { ExonType, fetchExonsByTranscriptId } from './exon'
+const ExonType = new GraphQLObjectType({
+  name: 'Exon',
+  fields: {
+    feature_type: { type: GraphQLString },
+    start: { type: GraphQLInt },
+    stop: { type: GraphQLInt },
+  },
+})
 
 export const TranscriptType = new GraphQLObjectType({
   name: 'Transcript',
   fields: {
     transcript_id: { type: GraphQLString },
     strand: { type: GraphQLString },
-    exons: {
-      type: new GraphQLList(ExonType),
-      resolve: (obj, args, ctx) => fetchExonsByTranscriptId(ctx, obj.transcript_id),
-    },
+    exons: { type: new GraphQLList(ExonType) },
   },
 })
 
-export const fetchTranscriptById = (ctx, transcriptId) =>
-  ctx.database.mongo.collection('transcripts').findOne({ transcript_id: transcriptId })
+export const fetchTranscriptById = async (ctx, transcriptId) => {
+  const [transcript, exons] = await Promise.all([
+    ctx.database.mongo.collection('transcripts').findOne({ transcript_id: transcriptId }),
+    ctx.database.mongo
+      .collection('exons')
+      .find({ transcript_id: transcriptId })
+      .toArray(),
+  ])
+
+  return {
+    ...transcript,
+    exons,
+  }
+}
