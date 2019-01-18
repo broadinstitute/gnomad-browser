6a5df6ab97ff4e689d1fc998c9eddf9471254eb8
Nick Watts <nwatts@broadinstitute.org>
2019-01-18 09:32:31 -0500

    [gnomAD] Only show LC LoF flag with LoF annotations
    Related to #364

--- packages/api/src/schema/datasets/exac/shapeExacVariantSummary.js
+++ packages/api/src/schema/datasets/exac/shapeExacVariantSummary.js
@@ -1,5 +1,32 @@
 import POPULATIONS from './populations'
 
+const getFlags = (variantData, transcriptConsequence) => {
+  const flags = []
+
+  if (variantData.flags.lof_flag) {
+    flags.push('lof_flag')
+  }
+
+  // ExAC variants loaded with the gnomAD 2.1 pipeline may have an LC LoF flag if they have
+  // some LoF category VEP anotations on non-protein-coding transcripts. However, other
+  // transcript consequences will be sorted above the non-coding consequences. Checking the
+  // displayed consequence's category here prevents the case where an LC LoF flag will be shown
+  // next to a missense/synonymous/other VEP annotation on the gene page.
+  // See #364.
+  const isLofOnNonCodingTranscript =
+    transcriptConsequence.lof === 'NC' ||
+    (transcriptConsequence.category === 'lof' && !transcriptConsequence.lof)
+  if (
+    variantData.flags.lc_lof &&
+    transcriptConsequence.category === 'lof' &&
+    !isLofOnNonCodingTranscript
+  ) {
+    flags.push('lc_lof')
+  }
+
+  return flags
+}
+
 const shapeExacVariantSummary = context => {
   return esHit => {
     // eslint-disable-next-line no-underscore-dangle
@@ -24,7 +51,7 @@ const shapeExacVariantSummary = context => {
       consequence_in_canonical_transcript: !!transcriptConsequence.canonical,
       datasets: ['exacVariants'],
       filters: variantData.filters,
-      flags: ['lc_lof', 'lof_flag'].filter(flag => variantData.flags[flag]),
+      flags: getFlags(variantData, transcriptConsequence),
       hgvs: transcriptConsequence.hgvs,
       hgvsc: transcriptConsequence.hgvsc ? transcriptConsequence.hgvsc.split(':')[1] : null,
       hgvsp: transcriptConsequence.hgvsp ? transcriptConsequence.hgvsp.split(':')[1] : null,
