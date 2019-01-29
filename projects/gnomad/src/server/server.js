7404312b45ef3de92da3a9225c32e8a84bc5fc3b
Nick Watts <nwatts@broadinstitute.org>
2019-01-29 17:12:29 -0500

    [gnomAD] Add option to redirect requests to HTTPS

--- projects/gnomad/src/server/server.js
+++ projects/gnomad/src/server/server.js
@@ -8,6 +8,17 @@ const port = process.env.PORT || 80
 const app = express()
 app.use(compression())
 
+// Redirect HTTP requests to HTTPS
+if (process.env.ENABLE_SSL_REDIRECT) {
+  app.use((request, response, next) => {
+    if (request.get('x-forwarded-proto') === 'http') {
+      response.redirect(`https://${request.get('host')}${request.url}`)
+    } else {
+      next()
+    }
+  })
+}
+
 const publicDir = path.resolve(__dirname, 'public')
 app.use(express.static(publicDir))
 app.get('*', (request, response) => {
