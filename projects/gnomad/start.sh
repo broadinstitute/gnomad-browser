f5fe8a57c5c04f4f7e85ee0596345860e7a7de97
Nick Watts <nwatts@broadinstitute.org>
2019-01-29 17:12:29 -0500

    [gnomAD] Add option to redirect requests to HTTPS

--- projects/gnomad/start.sh
+++ projects/gnomad/start.sh
@@ -8,6 +8,6 @@ cd $PROJECT_DIR
 export PATH=$PATH:$PROJECT_DIR/node_modules/.bin
 
 export NODE_ENV="development"
-export GNOMAD_API_URL=${GNOMAD_API_URL:-"http://gnomad-api.broadinstitute.org"}
+export GNOMAD_API_URL=${GNOMAD_API_URL:-"https://gnomad.broadinstitute.org/api"}
 
 webpack-dev-server --config=./config/webpack.config.client.js --hot --watch-poll
