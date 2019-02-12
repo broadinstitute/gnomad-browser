09c864b47864cf0f1396e47e856ba2d02d19feeb
Nick Watts <nwatts@broadinstitute.org>
2019-02-12 09:25:05 -0500

    [gnomAD] Change GeneViewer to a class component

--- projects/gnomad/src/client/GenePage/RegionViewer.js
+++ projects/gnomad/src/client/GenePage/RegionViewer.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 import { geneData, regionalConstraint } from '@broad/redux-genes'
@@ -17,94 +17,99 @@ import CoverageTrack from './CoverageTrack'
 import ClinVarTrack from './ClinVarTrack'
 import TranscriptLink from './TranscriptLink'
 
-const GeneViewer = ({
-  allVariants,
-  datasetId,
-  gene,
-  geneId,
-  isLoadingVariants, // eslint-disable-line no-shadow
-  regionalConstraint, // eslint-disable-line no-shadow
-  screenSize, // eslint-disable-line no-shadow
-}) => {
-  // Margins have to be kept in sync with styles in ui/Page.js
-  const smallScreen = screenSize.width < 900
-  const regionViewerWidth = smallScreen ? screenSize.width - 130 : screenSize.width - 290
+// eslint-disable-next-line react/prefer-stateless-function
+class GeneViewer extends Component {
+  static propTypes = {
+    allVariants: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+    datasetId: PropTypes.string.isRequired,
+    gene: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+    geneId: PropTypes.string.isRequired,
+    isLoadingVariants: PropTypes.bool.isRequired,
+    regionalConstraint: PropTypes.arrayOf(
+      PropTypes.shape({
+        genomic_end: PropTypes.number.isRequired,
+        genomic_start: PropTypes.number.isRequired,
+        region_name: PropTypes.string.isRequired,
+      })
+    ).isRequired,
+    screenSize: PropTypes.shape({ width: PropTypes.number.isRequired }).isRequired,
+  }
 
-  const geneJS = gene.toJS()
-  const { transcript, strand } = geneJS
-  const variantsReversed = allVariants.reverse()
+  render() {
+    const {
+      allVariants,
+      datasetId,
+      gene,
+      geneId,
+      isLoadingVariants, // eslint-disable-line no-shadow
+      regionalConstraint, // eslint-disable-line no-shadow
+      screenSize, // eslint-disable-line no-shadow
+    } = this.props
 
-  const { exons } = transcript
-  const hasCodingExons = exons.some(exon => exon.feature_type === 'CDS')
+    // Margins have to be kept in sync with styles in ui/Page.js
+    const smallScreen = screenSize.width < 900
+    const regionViewerWidth = smallScreen ? screenSize.width - 130 : screenSize.width - 290
 
-  return (
-    <RegionViewer
-      width={regionViewerWidth}
-      padding={75}
-      regions={geneJS.composite_transcript.exons}
-      rightPanelWidth={smallScreen ? 0 : 160}
-    >
-      {hasCodingExons && <CoverageTrack datasetId={datasetId} geneId={geneId} />}
+    const geneJS = gene.toJS()
+    const { transcript, strand } = geneJS
+    const variantsReversed = allVariants.reverse()
 
-      {hasCodingExons && (
-        <ConnectedTranscriptsTrack
-          compositeExons={geneJS.composite_transcript.exons}
-          filenameForExport={`${geneId}_transcripts`}
-          renderTranscriptId={(transcriptId, { isCanonical, isSelected }) => (
-            <TranscriptLink
-              to={`/gene/${gene.get('gene_name')}/transcript/${transcriptId}`}
-              isCanonical={isCanonical}
-              isSelected={isSelected}
-            >
-              {transcriptId}
-            </TranscriptLink>
-          )}
-        />
-      )}
+    const { exons } = transcript
+    const hasCodingExons = exons.some(exon => exon.feature_type === 'CDS')
 
-      {!hasCodingExons && (
-        <StatusMessage>
-          Coverage &amp; transcripts not shown for genes with no coding exons
-        </StatusMessage>
-      )}
+    return (
+      <RegionViewer
+        width={regionViewerWidth}
+        padding={75}
+        regions={geneJS.composite_transcript.exons}
+        rightPanelWidth={smallScreen ? 0 : 160}
+      >
+        {hasCodingExons && <CoverageTrack datasetId={datasetId} geneId={geneId} />}
 
-      {!isLoadingVariants && <ClinVarTrack variants={gene.get('clinvar_variants').toJS()} />}
-
-      {!isLoadingVariants &&
-        regionalConstraint.length > 0 &&
-        datasetId === 'exac' && (
-          <RegionalConstraintTrack
-            height={15}
-            regionalConstraintData={regionalConstraint}
-            strand={strand}
+        {hasCodingExons && (
+          <ConnectedTranscriptsTrack
+            compositeExons={geneJS.composite_transcript.exons}
+            filenameForExport={`${geneId}_transcripts`}
+            renderTranscriptId={(transcriptId, { isCanonical, isSelected }) => (
+              <TranscriptLink
+                to={`/gene/${gene.get('gene_name')}/transcript/${transcriptId}`}
+                isCanonical={isCanonical}
+                isSelected={isSelected}
+              >
+                {transcriptId}
+              </TranscriptLink>
+            )}
           />
         )}
 
-      {!isLoadingVariants && (
-        <VariantAlleleFrequencyTrack
-          title={`${datasetLabels[datasetId]}\n(${allVariants.size})`}
-          variants={variantsReversed.toJS()}
-        />
-      )}
-      {!isLoadingVariants && <NavigatorTrackConnected title="Viewing in table" />}
-    </RegionViewer>
-  )
-}
+        {!hasCodingExons && (
+          <StatusMessage>
+            Coverage &amp; transcripts not shown for genes with no coding exons
+          </StatusMessage>
+        )}
 
-GeneViewer.propTypes = {
-  allVariants: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
-  datasetId: PropTypes.string.isRequired,
-  gene: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
-  geneId: PropTypes.string.isRequired,
-  isLoadingVariants: PropTypes.bool.isRequired,
-  regionalConstraint: PropTypes.arrayOf(
-    PropTypes.shape({
-      genomic_end: PropTypes.number.isRequired,
-      genomic_start: PropTypes.number.isRequired,
-      region_name: PropTypes.string.isRequired,
-    })
-  ).isRequired,
-  screenSize: PropTypes.shape({ width: PropTypes.number.isRequired }).isRequired,
+        {!isLoadingVariants && <ClinVarTrack variants={gene.get('clinvar_variants').toJS()} />}
+
+        {!isLoadingVariants &&
+          regionalConstraint.length > 0 &&
+          datasetId === 'exac' && (
+            <RegionalConstraintTrack
+              height={15}
+              regionalConstraintData={regionalConstraint}
+              strand={strand}
+            />
+          )}
+
+        {!isLoadingVariants && (
+          <VariantAlleleFrequencyTrack
+            title={`${datasetLabels[datasetId]}\n(${allVariants.size})`}
+            variants={variantsReversed.toJS()}
+          />
+        )}
+        {!isLoadingVariants && <NavigatorTrackConnected title="Viewing in table" />}
+      </RegionViewer>
+    )
+  }
 }
 
 export default connect(state => ({
