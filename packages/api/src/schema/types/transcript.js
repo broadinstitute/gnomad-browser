79a1a61fa30c2fee721a7d1ebcbb1ea610d754c8
Nick Watts <nwatts@broadinstitute.org>
2019-02-15 10:26:09 -0500

    [gnomAD] Filter duplicate exons from composite transcript
    Remove the "exon" feature type regions which duplicate
    "CDS" and "UTR" regions.

--- packages/api/src/schema/types/transcript.js
+++ packages/api/src/schema/types/transcript.js
@@ -155,15 +155,37 @@ export const fetchCompositeTranscriptByGene = async (ctx, gene) => {
   const allExons = await lookupExonsByGeneId(ctx.database.gnomad, gene.gene_id)
   const sortedExons = allExons.sort((r1, r2) => r1.start - r2.start)
 
-  const cdsExons = mergeOverlappingRegions(sortedExons.filter(exon => exon.feature_type === 'CDS'))
-  const utrExons = mergeOverlappingRegions(sortedExons.filter(exon => exon.feature_type === 'UTR'))
-  const otherExons = mergeOverlappingRegions(
-    sortedExons.filter(exon => exon.feature_type === 'exon')
+  const cdsExons = allExons.filter(exon => exon.feature_type === 'CDS')
+  const utrExons = allExons.filter(exon => exon.feature_type === 'UTR')
+
+  const cdsCompositeExons = mergeOverlappingRegions(cdsExons)
+  const utrCompositeExons = mergeOverlappingRegions(utrExons)
+
+  /**
+   * There are 3 feature types in the exons collection: "CDS", "UTR", and "exon".
+   * There are "exon" regions that cover the "CDS" and "UTR" regions and also
+   * some (non-coding) transcripts that contain only "exon" regions.
+   * This filters the "exon" regions to only those that are in non-coding transcripts.
+   *
+   * This makes the UI for selecting visible regions easier, since it can filter
+   * on "CDS" or "UTR" feature type without having to also filter out the "exon" regions
+   * that duplicate the "CDS" and "UTR" regions.
+   */
+  const codingTranscripts = new Set(
+    allExons
+      .filter(exon => exon.feature_type === 'CDS' || exon.feature_type === 'UTR')
+      .map(exon => exon.transcript_id)
   )
+
+  const nonCodingTranscriptExons = sortedExons.filter(
+    exon => !codingTranscripts.has(exon.transcript_id)
+  )
+
+  const nonCodingTranscriptCompositeExons = mergeOverlappingRegions(nonCodingTranscriptExons)
 
   return {
     gene_id: gene.gene_id,
     chrom: gene.chrom,
-    exons: [...otherExons, ...cdsExons, ...utrExons],
+    exons: [...cdsCompositeExons, ...utrCompositeExons, ...nonCodingTranscriptCompositeExons],
   }
 }
