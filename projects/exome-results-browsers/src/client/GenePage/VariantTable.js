f2c6faa4d9ac6dd167a9a2785c0e1e69bad5b63c
Nick Watts <nwatts@broadinstitute.org>
2019-02-19 14:35:46 -0500

    [exome-results] Separate variant table column definitions

--- projects/exome-results-browsers/src/client/GenePage/VariantTable.js
+++ projects/exome-results-browsers/src/client/GenePage/VariantTable.js
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
-import Highlighter from 'react-highlight-words'
 import { connect } from 'react-redux'
 import styled from 'styled-components'
 
@@ -12,10 +11,9 @@ import {
   variantSortAscending,
 } from '@broad/redux-variants'
 import { actions as tableActions, currentTableIndex } from '@broad/table'
-import { Grid, TextButton } from '@broad/ui'
-import { getCategoryFromConsequence, getLabelForConsequenceTerm } from '@broad/utilities'
+import { Grid } from '@broad/ui'
 
-import browserConfig from '@browser/config'
+import columns from './variantTableColumns'
 
 const NoVariants = styled.div`
   display: flex;
@@ -27,55 +25,6 @@ const NoVariants = styled.div`
   font-size: 20px;
   font-weight: bold;
 `
-
-const VariantIdButton = TextButton.extend`
-  overflow: hidden;
-  text-align: left;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-`
-
-const categoryColors = {
-  lof: '#DD2C00',
-  missense: 'orange',
-  synonymous: '#2E7D32',
-  other: '#424242',
-}
-
-const getConsequenceColor = consequenceTerm => {
-  if (!consequenceTerm) {
-    return 'gray'
-  }
-  const category = getCategoryFromConsequence(consequenceTerm) || 'other'
-  return categoryColors[category]
-}
-
-const getConsequenceName = consequenceTerm =>
-  consequenceTerm ? getLabelForConsequenceTerm(consequenceTerm) : 'N/A'
-
-const renderNumberCell = (row, key) => {
-  const number = row[key]
-  if (number === null || number === undefined) {
-    return ''
-  }
-  const truncated = Number(number.toPrecision(3))
-  if (truncated === 0) {
-    return '0'
-  }
-  return truncated
-}
-
-const renderExponentialNumberCell = (row, key) => {
-  const number = row[key]
-  if (number === null || number === undefined) {
-    return ''
-  }
-  const truncated = Number(number.toPrecision(3))
-  if (truncated === 0) {
-    return '0'
-  }
-  return truncated.toExponential()
-}
 
 class VariantTable extends Component {
   grid = null
@@ -111,142 +60,9 @@ class VariantTable extends Component {
       return <NoVariants height={500}>No variants found</NoVariants>
     }
 
-    const highlightWords = highlightText.split(/\s+/)
-    const columns = [
-      {
-        key: 'variant_id',
-        heading: 'Variant ID',
-        tooltip: 'Chromosome-position-reference-alternate',
-        isRowHeader: true,
-        isSortable: true,
-        minWidth: 130,
-        grow: 2,
-        render: row => (
-          <VariantIdButton onClick={() => setFocusedVariant(row.variant_id)} tabIndex={-1}>
-            <Highlighter searchWords={highlightWords} textToHighlight={row.variant_id} />
-          </VariantIdButton>
-        ),
-      },
-      {
-        key: 'hgvsc_canonical',
-        heading: 'HGVSc',
-        tooltip: 'HGVS coding sequence',
-        minWidth: 140,
-        grow: 2,
-        isSortable: true,
-        render: row => (
-          <Highlighter
-            className="grid-cell-content"
-            searchWords={highlightWords}
-            textToHighlight={row.hgvsc_canonical || ''}
-          />
-        ),
-      },
-      {
-        key: 'hgvsp_canonical',
-        heading: 'HGVSp',
-        tooltip: 'HGVS protein sequence',
-        isSortable: true,
-        minWidth: 140,
-        grow: 2,
-        render: row => (
-          <Highlighter
-            className="grid-cell-content"
-            searchWords={highlightWords}
-            textToHighlight={row.hgvsp_canonical || ''}
-          />
-        ),
-      },
-      {
-        key: 'consequence',
-        heading: 'Consequence',
-        tooltip: 'Predicted functional consequence',
-        isSortable: true,
-        minWidth: 110,
-        render: row => (
-          <span
-            className="grid-cell-content"
-            style={{ color: getConsequenceColor(row.consequence) }}
-          >
-            <Highlighter
-              searchWords={highlightWords}
-              textToHighlight={getConsequenceName(row.consequence)}
-            />
-          </span>
-        ),
-      },
-      {
-        key: 'ac_case',
-        heading: 'AC Case',
-        tooltip: 'Allele count in cases',
-        isSortable: true,
-        minWidth: 60,
-      },
-      {
-        key: 'an_case',
-        heading: 'AN Case',
-        tooltip: 'Allele number in cases',
-        isSortable: true,
-        minWidth: 60,
-      },
-      {
-        key: 'ac_ctrl',
-        heading: 'AC Control',
-        tooltip: 'Allele count in controls',
-        isSortable: true,
-        minWidth: 60,
-      },
-      {
-        key: 'an_ctrl',
-        heading: 'AN Control',
-        tooltip: 'Allele number in controls',
-        isSortable: true,
-        minWidth: 60,
-      },
-      {
-        key: 'af_case',
-        heading: 'AF Case',
-        tooltip: 'Allele frequency in cases',
-        isSortable: true,
-        minWidth: 70,
-        render: renderExponentialNumberCell,
-      },
-      {
-        key: 'af_ctrl',
-        heading: 'AF Control',
-        tooltip: 'Allele frequency in controls',
-        isSortable: true,
-        minWidth: 70,
-        render: renderExponentialNumberCell,
-      },
-      {
-        key: 'estimate',
-        heading: 'Estimate',
-        tooltip: browserConfig.variantTable.tooltips.estimate,
-        isSortable: true,
-        minWidth: 60,
-        render: renderNumberCell,
-      },
-      {
-        key: 'pval_meta',
-        heading: 'P-Val',
-        tooltip: browserConfig.variantTable.tooltips.pval_meta,
-        isSortable: true,
-        minWidth: 60,
-        render: renderNumberCell,
-      },
-      {
-        key: 'in_analysis',
-        heading: 'In Analysis',
-        tooltip: browserConfig.variantTable.tooltips.in_analysis,
-        isSortable: true,
-        minWidth: 60,
-        render: (row, key) => row[key] && 'yes',
-      },
-    ]
-
     return (
       <Grid
+        cellData={{ highlightWords: highlightText.split(/\s+/), setFocusedVariant }}
         columns={columns}
         data={variants.toJS()}
         numRowsRendered={20}
---
+++ projects/exome-results-browsers/src/client/GenePage/variantTableColumns.js
@@ -0,0 +1,185 @@
+import React from 'react'
+import Highlighter from 'react-highlight-words'
+
+import { TextButton } from '@broad/ui'
+import { getCategoryFromConsequence, getLabelForConsequenceTerm } from '@broad/utilities'
+
+import browserConfig from '@browser/config'
+
+const VariantIdButton = TextButton.extend`
+  overflow: hidden;
+  text-align: left;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`
+
+const categoryColors = {
+  lof: '#DD2C00',
+  missense: 'orange',
+  synonymous: '#2E7D32',
+  other: '#424242',
+}
+
+const getConsequenceColor = consequenceTerm => {
+  if (!consequenceTerm) {
+    return 'gray'
+  }
+  const category = getCategoryFromConsequence(consequenceTerm) || 'other'
+  return categoryColors[category]
+}
+
+const getConsequenceName = consequenceTerm =>
+  consequenceTerm ? getLabelForConsequenceTerm(consequenceTerm) : 'N/A'
+
+const renderNumberCell = (row, key) => {
+  const number = row[key]
+  if (number === null || number === undefined) {
+    return ''
+  }
+  const truncated = Number(number.toPrecision(3))
+  if (truncated === 0) {
+    return '0'
+  }
+  return truncated
+}
+
+const renderExponentialNumberCell = (row, key) => {
+  const number = row[key]
+  if (number === null || number === undefined) {
+    return ''
+  }
+  const truncated = Number(number.toPrecision(3))
+  if (truncated === 0) {
+    return '0'
+  }
+  return truncated.toExponential()
+}
+
+const columns = [
+  {
+    key: 'variant_id',
+    heading: 'Variant ID',
+    tooltip: 'Chromosome-position-reference-alternate',
+    isRowHeader: true,
+    isSortable: true,
+    minWidth: 130,
+    grow: 2,
+    render: (row, key, { highlightWords, setFocusedVariant }) => (
+      <VariantIdButton onClick={() => setFocusedVariant(row[key])} tabIndex={-1}>
+        <Highlighter searchWords={highlightWords} textToHighlight={row[key]} />
+      </VariantIdButton>
+    ),
+  },
+  {
+    key: 'hgvsc_canonical',
+    heading: 'HGVSc',
+    tooltip: 'HGVS coding sequence',
+    minWidth: 140,
+    grow: 2,
+    isSortable: true,
+    render: (row, key, { highlightWords }) => (
+      <Highlighter
+        className="grid-cell-content"
+        searchWords={highlightWords}
+        textToHighlight={row[key] || ''}
+      />
+    ),
+  },
+  {
+    key: 'hgvsp_canonical',
+    heading: 'HGVSp',
+    tooltip: 'HGVS protein sequence',
+    isSortable: true,
+    minWidth: 140,
+    grow: 2,
+    render: (row, key, { highlightWords }) => (
+      <Highlighter
+        className="grid-cell-content"
+        searchWords={highlightWords}
+        textToHighlight={row[key] || ''}
+      />
+    ),
+  },
+  {
+    key: 'consequence',
+    heading: 'Consequence',
+    tooltip: 'Predicted functional consequence',
+    isSortable: true,
+    minWidth: 110,
+    render: (row, key, { highlightWords }) => (
+      <span className="grid-cell-content" style={{ color: getConsequenceColor(row[key]) }}>
+        <Highlighter searchWords={highlightWords} textToHighlight={getConsequenceName(row[key])} />
+      </span>
+    ),
+  },
+  {
+    key: 'ac_case',
+    heading: 'AC Case',
+    tooltip: 'Allele count in cases',
+    isSortable: true,
+    minWidth: 60,
+  },
+  {
+    key: 'an_case',
+    heading: 'AN Case',
+    tooltip: 'Allele number in cases',
+    isSortable: true,
+    minWidth: 60,
+  },
+  {
+    key: 'ac_ctrl',
+    heading: 'AC Control',
+    tooltip: 'Allele count in controls',
+    isSortable: true,
+    minWidth: 60,
+  },
+  {
+    key: 'an_ctrl',
+    heading: 'AN Control',
+    tooltip: 'Allele number in controls',
+    isSortable: true,
+    minWidth: 60,
+  },
+  {
+    key: 'af_case',
+    heading: 'AF Case',
+    tooltip: 'Allele frequency in cases',
+    isSortable: true,
+    minWidth: 70,
+    render: renderExponentialNumberCell,
+  },
+  {
+    key: 'af_ctrl',
+    heading: 'AF Control',
+    tooltip: 'Allele frequency in controls',
+    isSortable: true,
+    minWidth: 70,
+    render: renderExponentialNumberCell,
+  },
+  {
+    key: 'estimate',
+    heading: 'Estimate',
+    tooltip: browserConfig.variantTable.tooltips.estimate,
+    isSortable: true,
+    minWidth: 60,
+    render: renderNumberCell,
+  },
+  {
+    key: 'pval_meta',
+    heading: 'P-Val',
+    tooltip: browserConfig.variantTable.tooltips.pval_meta,
+    isSortable: true,
+    minWidth: 65,
+    render: renderNumberCell,
+  },
+  {
+    key: 'in_analysis',
+    heading: 'In Analysis',
+    tooltip: browserConfig.variantTable.tooltips.in_analysis,
+    isSortable: true,
+    minWidth: 60,
+    render: (row, key) => row[key] && 'yes',
+  },
+]
+
+export default columns
