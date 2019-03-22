0154f60564bab79cfc5f29bcf24b54d497d96260
Nick Watts <nwatts@broadinstitute.org>
2019-03-22 16:03:22 -0400

    [gnomAD] Remove unused code

--- projects/gnomad/src/client/RegionPage/StructuralVariantsInRegion.js
+++ projects/gnomad/src/client/RegionPage/StructuralVariantsInRegion.js
@@ -41,16 +41,6 @@ const StructuralVariantsInRegion = ({ region, ...rest }) => {
           return <StatusMessage>Failed to load variants</StatusMessage>
         }
 
-        // const variants = data.variants.map(v => ({
-        //   ...v,
-        //   alts: v.alt,
-        //   ac: v.ac.total,
-        //   an: v.an.total,
-        //   af: v.af.total,
-        //   n_homalt: v.n_homalt.total,
-        //   consequence: v.worst_consequence,
-        // }))
-
         return (
           <StructuralVariants
             {...rest}
