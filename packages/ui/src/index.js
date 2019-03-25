9c27c327a7944bb6353b109cc7be3366222df5e9
Nick Watts <nwatts@broadinstitute.org>
2019-03-25 11:04:42 -0400

    [ui] Remove unused component

--- packages/ui/src/index.js
+++ packages/ui/src/index.js
@@ -6,8 +6,6 @@ export {
 
 export { BaseButton, Button, PrimaryButton, TextButton } from './Button'
 
-export { ClassicExacButton } from './classicExac/button'
-
 export { CategoryFilterControl } from './CategoryFilterControl'
 
 export { ConsequenceCategoriesControl } from './ConsequenceCategoriesControl'
