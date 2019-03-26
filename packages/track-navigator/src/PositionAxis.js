1ba55748e4ed35281038ec2e389ebd4c6f68af0d
Nick Watts <nwatts@broadinstitute.org>
2019-03-26 09:51:34 -0400

    [navigator] Adjust number of axis ticks based on width
    Resolves #47

--- packages/track-navigator/src/PositionAxis.js
+++ packages/track-navigator/src/PositionAxis.js
@@ -3,26 +3,34 @@ import React from 'react'
 
 const PositionAxis = ({ scalePosition, width }) => {
   const height = 15
-  const numIntervals = 10
+  const numIntervals = Math.min(10, Math.floor(width / 90))
 
   const tickInterval = width / numIntervals
-  const tickPositions = [...Array(numIntervals - 1)].map((_, i) => tickInterval * (i + 1))
-
-  const ticks = tickPositions.map(x => (
-    <g key={x}>
-      <line x1={x} y1={height} x2={x} y2={height - 5} stroke="black" strokeWidth={1} />
-      <text x={x} y={height - 7} textAnchor="center" style={{ fontSize: '10px' }}>
-        {scalePosition.invert(x)}
-      </text>
-    </g>
-  ))
+  const ticks = [...Array(numIntervals - 1)].map((_, i) => tickInterval * (i + 1))
 
   return (
     <svg height={height} width={width}>
       <line x1={0} y1={height} x2={width} y2={height} stroke="black" strokeWidth={2} />
-      <line x1={0} y1={height - 7} x2={0} y2={height} stroke="black" strokeWidth={2} />
-      <line x1={width} y1={height - 7} x2={width} y2={height} stroke="black" strokeWidth={2} />
-      {ticks}
+      <g>
+        <line x1={0} y1={height} x2={0} y2={height - 5} stroke="black" strokeWidth={2} />
+        <text x={0} y={height - 7} textAnchor="start" style={{ fontSize: '10px' }}>
+          {scalePosition.invert(0).toLocaleString()}
+        </text>
+      </g>
+      {ticks.map(x => (
+        <g key={x}>
+          <line x1={x} y1={height} x2={x} y2={height - 5} stroke="black" strokeWidth={1} />
+          <text x={x} y={height - 7} textAnchor="middle" style={{ fontSize: '10px' }}>
+            {scalePosition.invert(x).toLocaleString()}
+          </text>
+        </g>
+      ))}
+      <g>
+        <line x1={width} y1={height} x2={width} y2={height - 5} stroke="black" strokeWidth={2} />
+        <text x={width} y={height - 7} textAnchor="end" style={{ fontSize: '10px' }}>
+          {scalePosition.invert(width).toLocaleString()}
+        </text>
+      </g>
     </svg>
   )
 }
