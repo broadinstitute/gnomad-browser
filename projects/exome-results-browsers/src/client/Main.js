d5a6d7d010c6fa7a3179201c03b94f0a64860eab
Nick Watts <nwatts@broadinstitute.org>
2019-04-16 13:54:29 -0400

    [exome-results] Remove Redux

--- projects/exome-results-browsers/src/client/Main.js
+++ projects/exome-results-browsers/src/client/Main.js
@@ -1,13 +1,7 @@
 import React from 'react'
-import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { applyMiddleware, combineReducers, createStore } from 'redux'
-import createDebounce from 'redux-debounced'
-import { createLogger } from 'redux-logger'
-import thunk from 'redux-thunk'
 import { createGlobalStyle } from 'styled-components'
 
-import { actions as userInterfaceActions, createUserInterfaceReducer } from '@broad/ui'
 import { registerConsequences } from '@broad/utilities'
 
 import browserConfig from '@browser/config'
@@ -27,28 +21,12 @@ document.title = browserConfig.pageTitle
 
 registerConsequences(browserConfig.consequences)
 
-const rootReducer = combineReducers({
-  ui: createUserInterfaceReducer(),
-})
-
-const store = createStore(
-  rootReducer,
-  undefined,
-  applyMiddleware(createDebounce(), thunk, createLogger())
-)
-
-window.addEventListener('resize', () =>
-  store.dispatch(userInterfaceActions.setScreenSize(window.innerHeight, window.innerWidth))
-)
-
 const Main = () => (
   <React.Fragment>
     <GlobalStyles />
-    <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>
+    <Router>
+      <App />
+    </Router>
   </React.Fragment>
 )
 
