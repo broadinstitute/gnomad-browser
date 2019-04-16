66fa0a98ab26a6dd32f1d3e912863fa98f01a06d
Nick Watts <nwatts@broadinstitute.org>
2019-04-16 13:37:25 -0400

    [exome-results] Remove dependency on redux-variants package

---
+++ projects/exome-results-browsers/src/client/GenePage/sortVariants.js
@@ -0,0 +1,47 @@
+function isEmpty(val) {
+  return val === undefined || val === null || val === ''
+}
+
+const makeComparator = (key, fn) => {
+  const comparator = (v1, v2) => fn(v1, v2)
+  comparator.key = key
+  return comparator
+}
+
+const makeStringComparator = key => makeComparator(key, (v1, v2) => v1.localeCompare(v2))
+
+const makeNumericComparator = key => makeComparator(key, (v1, v2) => v1 - v2)
+
+const comparators = {
+  variant_id: makeNumericComparator('pos'),
+  hgvsc_canonical: makeStringComparator('hgvsc_canonical'),
+  hgvsp_canonical: makeStringComparator('hgvsp_canonical'),
+  consequence: makeStringComparator('consequence'),
+  ac_case: makeNumericComparator('ac_case'),
+  an_case: makeNumericComparator('an_case'),
+  ac_ctrl: makeNumericComparator('ac_ctrl'),
+  an_ctrl: makeNumericComparator('an_ctrl'),
+  af_case: makeNumericComparator('af_case'),
+  af_ctrl: makeNumericComparator('af_ctrl'),
+  estimate: makeNumericComparator('estimate'),
+  pval_meta: makeNumericComparator('pval_meta'),
+  in_analysis: makeNumericComparator('in_analysis'),
+}
+
+const sortVariants = (variants, { sortKey, sortOrder }) => {
+  const baseComparator = comparators[sortKey]
+  const comparator = sortOrder === 'ascending' ? baseComparator : (a, b) => baseComparator(b, a)
+
+  return [...variants].sort((v1, v2) => {
+    // Always sort null values to end of list
+    if (isEmpty(v1[baseComparator.key])) {
+      return 1
+    }
+    if (isEmpty(v2[baseComparator.key])) {
+      return -1
+    }
+    return comparator(v1[baseComparator.key], v2[baseComparator.key])
+  })
+}
+
+export default sortVariants
--- projects/exome-results-browsers/src/client/Main.js
+++ projects/exome-results-browsers/src/client/Main.js
@@ -7,9 +7,8 @@ import { createLogger } from 'redux-logger'
 import thunk from 'redux-thunk'
 import { createGlobalStyle } from 'styled-components'
 
-import { createVariantReducer } from '@broad/redux-variants'
 import { actions as userInterfaceActions, createUserInterfaceReducer } from '@broad/ui'
-import { getLabelForConsequenceTerm, registerConsequences } from '@broad/utilities'
+import { registerConsequences } from '@broad/utilities'
 
 import browserConfig from '@browser/config'
 
@@ -26,79 +25,10 @@ const GlobalStyles = createGlobalStyle`
 
 document.title = browserConfig.pageTitle
 
-const variantDatasets = {
-  variants: {
-    id: null,
-    ac: null,
-    ac_case: null,
-    ac_ctrl: null,
-    ac_denovo: null,
-    af: null,
-    allele_freq: null,
-    af_case: null,
-    af_ctrl: null,
-    an: null,
-    an_case: null,
-    an_ctrl: null,
-    cadd: null,
-    canonical_transcript_id: null,
-    chrom: null,
-    comment: null,
-    consequence: null,
-    csq_analysis: null,
-    csq_canonical: null,
-    csq_worst: null,
-    estimate: null,
-    flags: null,
-    gene_id: null,
-    gene_name: null,
-    hgvsc: null,
-    hgvsc_canonical: null,
-    hgvsp: null,
-    hgvsp_canonical: null,
-    i2: null,
-    in_analysis: null,
-    mpc: null,
-    n_analysis_groups: null,
-    polyphen: null,
-    pos: null,
-    pval_meta: null,
-    qp: null,
-    se: null,
-    source: null,
-    transcript_id: null,
-    variant_id: null,
-    xpos: null,
-  },
-}
-
 registerConsequences(browserConfig.consequences)
 
 const rootReducer = combineReducers({
   ui: createUserInterfaceReducer(),
-  variants: createVariantReducer({
-    projectDefaults: {
-      startingVariant: null,
-      startingVariantDataset: 'variants',
-      startingIndelFilter: true,
-      startingQcFilter: false,
-      startingSnpFilter: true,
-    },
-    variantDatasets,
-    variantSearchPredicate(variant, query) {
-      return (
-        variant
-          .get('variant_id')
-          .toLowerCase()
-          .includes(query) ||
-        (variant.get('hgvsc_canonical') || '').toLowerCase().includes(query) ||
-        (variant.get('hgvsp_canonical') || '').toLowerCase().includes(query) ||
-        getLabelForConsequenceTerm(variant.get('consequence') || '')
-          .toLowerCase()
-          .includes(query)
-      )
-    },
-  }),
 })
 
 const store = createStore(
