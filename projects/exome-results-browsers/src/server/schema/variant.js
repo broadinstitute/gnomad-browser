652e8bda95e3cd599e3359953ce1b7264b8044ac
Nick Watts <nwatts@broadinstitute.org>
2019-04-23 10:15:09 -0400

    [exome-results] Merge analysis group and variant modules

--- projects/exome-results-browsers/src/server/schema/variant.js
+++ projects/exome-results-browsers/src/server/schema/variant.js
@@ -10,6 +10,7 @@ import {
 import browserConfig from '@browser/config'
 
 import { fetchAllSearchResults } from '../utilities/elasticsearch'
+import { UserVisibleError } from '../utilities/errors'
 
 export const VariantType = new GraphQLObjectType({
   name: 'Variant',
@@ -127,4 +128,60 @@ export const fetchVariantsByGeneId = async (ctx, geneId, analysisGroup) => {
       se: doc.groups[analysisGroup].se,
     }
   })
+}
+
+const VariantResultType = new GraphQLObjectType({
+  name: 'VariantResult',
+  fields: {
+    ac: { type: GraphQLInt },
+    ac_case: { type: GraphQLInt },
+    ac_ctrl: { type: GraphQLInt },
+    af: { type: GraphQLFloat },
+    af_case: { type: GraphQLFloat },
+    af_ctrl: { type: GraphQLFloat },
+    an: { type: GraphQLInt },
+    an_case: { type: GraphQLInt },
+    an_ctrl: { type: GraphQLInt },
+    analysis_group: { type: GraphQLString },
+    est: { type: GraphQLFloat },
+    p: { type: GraphQLFloat },
+    se: { type: GraphQLFloat },
+  },
+})
+
+export const VariantDetailsType = new GraphQLObjectType({
+  name: 'VariantDetail',
+  fields: {
+    results: { type: new GraphQLList(VariantResultType) },
+  },
+})
+
+export const fetchVariantDetails = async (ctx, variantId) => {
+  const response = await ctx.database.elastic.search({
+    index: browserConfig.elasticsearch.variants.index,
+    type: browserConfig.elasticsearch.variants.type,
+    size: 1,
+    body: {
+      query: {
+        match: {
+          variant_id: variantId,
+        },
+      },
+    },
+  })
+
+  if (!response.hits.hits.length) {
+    throw new UserVisibleError('Variant not found')
+  }
+
+  const doc = response.hits.hits[0]._source // eslint-disable-line no-underscore-dangle
+
+  const results = Object.entries(doc.groups)
+    .filter(entry => entry[1] && Object.entries(entry[1]).length !== 0)
+    .map(([groupName, groupData]) => ({
+      analysis_group: groupName,
+      ...groupData,
+    }))
+
+  return { results }
 }
