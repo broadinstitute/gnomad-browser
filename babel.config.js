8db6be718380b8f4bbe350ecf9fa076f1c5bc41b
Nick Watts <nwatts@broadinstitute.org>
2019-04-26 14:27:07 -0400

    Replace deprecated Babel polyfill

--- babel.config.js
+++ babel.config.js
@@ -2,20 +2,23 @@ const path = require('path')
 
 module.exports = {
   presets: [
-    '@babel/preset-react',
     [
       '@babel/preset-env',
       {
-        targets: {
-          browsers: ['last 2 versions', 'ie >= 10'],
-        },
+        targets: ['last 2 versions', 'ie >= 10'],
         useBuiltIns: 'entry',
         corejs: 3,
+        exclude: ['transform-typeof-symbol'],
+      },
+    ],
+    [
+      '@babel/preset-react',
+      {
+        useBuiltIns: false,
       },
     ],
   ],
   plugins: [
-    '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-proposal-class-properties',
     'inline-react-svg',
     'react-hot-loader/babel',
