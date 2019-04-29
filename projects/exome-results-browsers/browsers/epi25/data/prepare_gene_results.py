37ecd32bc6ad08483bd7bd3b5071d191997a230c
Nick Watts <nwatts@broadinstitute.org>
2019-04-29 15:37:08 -0400

    [exome-results] Annotate gene results with chrom/position

--- projects/exome-results-browsers/browsers/epi25/data/prepare_gene_results.py
+++ projects/exome-results-browsers/browsers/epi25/data/prepare_gene_results.py
@@ -4,6 +4,7 @@ import hail as hl
 
 p = argparse.ArgumentParser()
 p.add_argument("--input-url", required=True)
+p.add_argument("--genes-url", required=True)
 p.add_argument("--output-url", required=True)
 args = p.parse_args()
 
@@ -42,5 +43,9 @@ ds = ds.annotate(
 
 # "Meta" p-val was carried over from SCHEMA's data format but isn't descriptive of Epi25
 ds = ds.rename({"pval_meta": "pval", "description": "gene_description"})
+
+genes = hl.read_table(args.genes_url)
+genes = genes.key_by("gene_id")
+ds = ds.annotate(chrom=genes[ds.gene_id].chrom, pos=genes[ds.gene_id].start)
 
 ds.write(args.output_url)
--- projects/exome-results-browsers/browsers/schema/data/prepare_gene_results.py
+++ projects/exome-results-browsers/browsers/schema/data/prepare_gene_results.py
@@ -4,6 +4,7 @@ import hail as hl
 
 p = argparse.ArgumentParser()
 p.add_argument("--input-url", required=True)
+p.add_argument("--genes-url", required=True)
 p.add_argument("--output-url", required=True)
 args = p.parse_args()
 
@@ -12,5 +13,9 @@ hl.init(log="/tmp/hail.log")
 ds = hl.read_table(args.input_url)
 
 ds = ds.annotate(analysis_group="meta")
+
+genes = hl.read_table(args.genes_url)
+genes = genes.key_by("gene_id")
+ds = ds.annotate(chrom=genes[ds.gene_id].chrom, pos=genes[ds.gene_id].start)
 
 ds.write(args.output_url)
