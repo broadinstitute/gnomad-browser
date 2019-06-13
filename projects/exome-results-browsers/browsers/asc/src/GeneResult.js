38fee38f44805c91b55ad1ae222b6b747ba4ba5d
Nick Watts <nwatts@broadinstitute.org>
2019-06-13 12:57:17 -0400

    [ASC] Add full labels for analysis groups

--- projects/exome-results-browsers/browsers/asc/src/GeneResult.js
+++ projects/exome-results-browsers/browsers/asc/src/GeneResult.js
@@ -20,32 +20,34 @@ const GeneResultsTable = ({ geneResult }) => (
       </thead>
       <tbody>
         <tr>
-          <th scope="row">De Novo PTV</th>
+          <th scope="row">De novo protein-truncating variants</th>
           <td>{geneResult.xcase_dn_ptv === null ? '—' : geneResult.xcase_dn_ptv}</td>
           <td>{geneResult.xcont_dn_ptv === null ? '—' : geneResult.xcont_dn_ptv}</td>
         </tr>
         <tr>
-          <th scope="row">De Novo MisA</th>
+          <th scope="row">De novo missense variants with MPC 1-2</th>
           <td>{geneResult.xcase_dn_misa === null ? '—' : geneResult.xcase_dn_misa}</td>
           <td>{geneResult.xcont_dn_misa === null ? '—' : geneResult.xcont_dn_misa}</td>
         </tr>
         <tr>
-          <th scope="row">De Novo MisB</th>
+          <th scope="row">De novo missense variants with MPC &ge; 2</th>
           <td>{geneResult.xcase_dn_misb === null ? '—' : geneResult.xcase_dn_misb}</td>
           <td>{geneResult.xcont_dn_misb === null ? '—' : geneResult.xcont_dn_misb}</td>
         </tr>
         <tr>
-          <th scope="row">DBS PTV</th>
+          <th scope="row">
+            Protein-truncating variants in iPSYCH (&quot;Danish Blood Spot&quot;) cohort
+          </th>
           <td>{geneResult.xcase_dbs_ptv === null ? '—' : geneResult.xcase_dbs_ptv}</td>
           <td>{geneResult.xcont_dbs_ptv === null ? '—' : geneResult.xcont_dbs_ptv}</td>
         </tr>
         <tr>
-          <th scope="row">SWE PTV</th>
+          <th scope="row">Protein-truncating variants in Swedish cohort</th>
           <td>{geneResult.xcase_swe_ptv === null ? '—' : geneResult.xcase_swe_ptv}</td>
           <td>{geneResult.xcont_swe_ptv === null ? '—' : geneResult.xcont_swe_ptv}</td>
         </tr>
         <tr>
-          <th scope="row">TUT</th>
+          <th scope="row">Protein-truncating variants transmitted/not transmitted to probands</th>
           <td>{geneResult.xcase_tut === null ? '—' : geneResult.xcase_tut}</td>
           <td>{geneResult.xcont_tut === null ? '—' : geneResult.xcont_tut}</td>
         </tr>
