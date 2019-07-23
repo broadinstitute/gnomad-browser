c9abda0564f34a83b5cf0dc27d03a016d72ad1d5
Nick Watts <nwatts@broadinstitute.org>
2019-07-23 16:04:14 -0400

    [gnomAD] Add FAQ entries

---
+++ projects/gnomad/src/client/ageDistribution.js
@@ -0,0 +1,16 @@
+const ageDistribution = {
+  exome: {
+    binEdges: [30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80],
+    binValues: [3423, 4546, 8487, 10355, 12693, 11933, 10534, 8882, 5991, 4136],
+    nSmaller: 2547,
+    nLarger: 1935,
+  },
+  genome: {
+    binEdges: [30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80],
+    binValues: [623, 663, 923, 1363, 1666, 1254, 1001, 640, 362, 154],
+    nSmaller: 2522,
+    nLarger: 70,
+  },
+}
+
+export default ageDistribution
