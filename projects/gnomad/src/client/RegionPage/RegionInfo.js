20cc5eb646ff215a379f0ce36c0b6d208a1b25dd
Nick Watts <nwatts@broadinstitute.org>
2019-07-29 19:31:28 -0400

    [gnomAD] Add UCSC link to region page

--- projects/gnomad/src/client/RegionPage/RegionInfo.js
+++ projects/gnomad/src/client/RegionPage/RegionInfo.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import { ExternalLink } from '@broad/ui'
+
 import AttributeList from '../AttributeList'
 
 const RegionInfo = ({ region }) => {
-  const { start, stop } = region
+  const { chrom, start, stop } = region
+  const ucscUrl = `http://genome.ucsc.edu/cgi-bin/hgTracks?db=hg19&position=chr${chrom}%3A${start}-${stop}`
+
   return (
     <AttributeList labelWidth={120}>
       <AttributeList.Item label="Region size">
         {(stop - start + 1).toLocaleString()} BP
+      </AttributeList.Item>
+      <AttributeList.Item label="UCSC Browser">
+        <ExternalLink href={ucscUrl}>{`${chrom}:${start}-${stop}`}</ExternalLink>
       </AttributeList.Item>
     </AttributeList>
   )
@@ -16,6 +23,7 @@ const RegionInfo = ({ region }) => {
 
 RegionInfo.propTypes = {
   region: PropTypes.shape({
+    chrom: PropTypes.string.isRequired,
     start: PropTypes.number.isRequired,
     stop: PropTypes.number.isRequired,
   }).isRequired,
