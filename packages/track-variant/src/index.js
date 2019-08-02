b39543cd9b87a6538069656185b1d6bb82a14989
Nick Watts <nwatts@broadinstitute.org>
2019-08-02 14:27:09 -0400

    [track-variant] Remove alternate version of variant track

--- packages/track-variant/src/index.js
+++ packages/track-variant/src/index.js
@@ -1,3 +1,2 @@
 export { VariantAlleleFrequencyPlot } from './VariantAlleleFrequencyPlot'
 export { VariantAlleleFrequencyTrack } from './VariantAlleleFrequencyTrack'
-export { VariantPositionTrack } from './VariantPositionTrack'
