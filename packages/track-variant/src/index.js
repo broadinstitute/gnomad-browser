1ef9ea98a992cfedc47722403f6e0b21897745d7
Nick Watts <nwatts@broadinstitute.org>
2019-08-02 14:07:00 -0400

    [track-variant] Remove stacked variant plot from package

--- packages/track-variant/src/index.js
+++ packages/track-variant/src/index.js
@@ -1,4 +1,3 @@
-export { StackedVariantsPlot } from './StackedVariantsPlot'
 export { VariantAlleleFrequencyPlot } from './VariantAlleleFrequencyPlot'
 export { VariantAlleleFrequencyTrack } from './VariantAlleleFrequencyTrack'
 export { VariantPositionTrack } from './VariantPositionTrack'
