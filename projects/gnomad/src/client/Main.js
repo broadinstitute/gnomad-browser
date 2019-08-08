2fc119c598118a2258166c515ef120c0ce88fad0
Nick Watts <nwatts@broadinstitute.org>
2019-08-08 10:22:00 -0400

    [gnomAD] Use new transcripts track, remove redux-genes
    Resolves #275

--- projects/gnomad/src/client/Main.js
+++ projects/gnomad/src/client/Main.js
@@ -6,7 +6,6 @@ import thunk from 'redux-thunk'
 import { createLogger } from 'redux-logger'
 
 import { createHelpReducer } from '@broad/help'
-import { createGeneReducer } from '@broad/redux-genes'
 import { actions as userInterfaceActions, createUserInterfaceReducer } from '@broad/ui'
 
 // eslint-disable-next-line import/no-webpack-loader-syntax,import/no-unresolved
@@ -16,19 +15,7 @@ import App from './routes'
 
 import toc from './help/toc.json'
 
-const appSettings = {
-  variantDatasets: {
-    exac: {},
-    gnomad_r2_1: {},
-    gnomad_r2_1_controls: {},
-    gnomad_r2_1_non_cancer: {},
-    gnomad_r2_1_non_neuro: {},
-    gnomad_r2_1_non_topmed: {},
-  },
-}
-
 const rootReducer = combineReducers({
-  genes: createGeneReducer(appSettings),
   help: createHelpReducer({
     topics: helpTopics,
     toc: toc.toc,
