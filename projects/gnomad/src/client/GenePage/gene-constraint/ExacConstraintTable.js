e9aa705bb118d9d0e0dcad83ab27493633c2f3ff
Nick Watts <nwatts@broadinstitute.org>
2019-08-19 11:10:11 -0400

    [gnomAD] Refactor options for constraint number rendering

--- projects/gnomad/src/client/GenePage/gene-constraint/ExacConstraintTable.js
+++ projects/gnomad/src/client/GenePage/gene-constraint/ExacConstraintTable.js
@@ -21,7 +21,12 @@ const ExacConstraintTable = ({ constraint }) => (
         <td>{renderRoundedNumber(constraint.exp_syn)}</td>
         <td>{constraint.n_syn}</td>
         <td>
-          Z = {renderRoundedNumber(constraint.syn_z, 2, 3, constraint.syn_z > 3.71 && '#ff2600')}
+          Z ={' '}
+          {renderRoundedNumber(constraint.syn_z, {
+            precision: 2,
+            tooltipPrecision: 3,
+            highlightColor: constraint.syn_z > 3.71 ? '#ff2600' : null,
+          })}
         </td>
       </tr>
       <tr>
@@ -29,7 +34,12 @@ const ExacConstraintTable = ({ constraint }) => (
         <td>{renderRoundedNumber(constraint.exp_mis)}</td>
         <td>{constraint.n_mis}</td>
         <td>
-          Z = {renderRoundedNumber(constraint.mis_z, 2, 3, constraint.mis_z > 3.09 && '#ff9300')}
+          Z ={' '}
+          {renderRoundedNumber(constraint.mis_z, {
+            precision: 2,
+            tooltipPrecision: 3,
+            highlightColor: constraint.mis_z > 3.09 ? '#ff9300' : null,
+          })}
         </td>
       </tr>
       <tr>
@@ -37,7 +47,12 @@ const ExacConstraintTable = ({ constraint }) => (
         <td>{renderRoundedNumber(constraint.exp_lof)}</td>
         <td>{constraint.n_lof}</td>
         <td>
-          pLI = {renderRoundedNumber(constraint.pLI, 2, 3, constraint.pLI > 0.9 && '#ff9300')}
+          pLI ={' '}
+          {renderRoundedNumber(constraint.pLI, {
+            precision: 2,
+            tooltipPrecision: 3,
+            highlightColor: constraint.pLI > 0.9 ? '#ff9300' : null,
+          })}
         </td>
       </tr>
     </tbody>
