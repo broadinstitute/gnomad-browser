748d9ac70181c31d2cda94bae07035fcee3ad6c9
Nick Watts <nwatts@broadinstitute.org>
2019-08-21 12:14:26 -0400

    [gnomAD] Allow query syntax and validation errors to be returned

--- projects/gnomad-api/src/server.js
+++ projects/gnomad-api/src/server.js
@@ -51,6 +51,20 @@ app.use(cors())
           },
         },
         customFormatErrorFn: error => {
+          // graphql-js doesn't distinguish between different error types, so this is the
+          // only way to determine what errors come from query validation (and thus should
+          // be shown to the user)
+          // See https://github.com/graphql/graphql-js/issues/1847
+          const isQueryValidationError =
+            (error.message.startsWith('Syntax Error') &&
+              error.stack.includes('graphql/error/syntaxError')) ||
+            (error.message.startsWith('Cannot query field') &&
+              error.stack.includes('graphql/validation/rules'))
+
+          if (isQueryValidationError) {
+            return { message: error.message, locations: error.locations }
+          }
+
           const isUserVisible = error.extensions && error.extensions.isUserVisible
 
           // User visible errors (such as variant not found) are expected to occur during
