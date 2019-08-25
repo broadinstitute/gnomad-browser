07b4cb19ead907999ce51f853ed24b6980fd539a
Nick Watts <nwatts@broadinstitute.org>
2019-08-25 18:39:22 -0400

    [gnomAD] Separate constraint query from constraint table

--- projects/gnomad/src/client/GenePage/gene-constraint/GnomadConstraintTable.js
+++ projects/gnomad/src/client/GenePage/gene-constraint/GnomadConstraintTable.js
@@ -28,34 +28,6 @@ const OEMetrics = styled.span`
   white-space: nowrap;
 `
 
-const constraintQuery = `
-  query GnomadConstraint($transcriptId: String!) {
-    transcript(transcript_id: $transcriptId) {
-      gnomad_constraint {
-        exp_lof
-        exp_mis
-        exp_syn
-        obs_lof
-        obs_mis
-        obs_syn
-        oe_lof
-        oe_lof_lower
-        oe_lof_upper
-        oe_mis
-        oe_mis_lower
-        oe_mis_upper
-        oe_syn
-        oe_syn_lower
-        oe_syn_upper
-        lof_z
-        mis_z
-        syn_z
-        pLI
-      }
-    }
-  }
-`
-
 const Graph = ({ value, lower, upper, color }) => {
   const width = 60
   const xPadding = 13
@@ -141,7 +113,139 @@ const renderOEGraph = (constraint, category, color) => {
   )
 }
 
-const GnomadConstraintTable = ({ transcriptId }) => (
+const GnomadConstraintTable = ({ constraint }) => {
+  let lofHighlightColor = null
+  if (constraint.oe_lof_upper !== null) {
+    if (constraint.oe_lof_upper < 0.33) {
+      lofHighlightColor = '#ff2600'
+    } else if (constraint.oe_lof_upper < 0.66) {
+      lofHighlightColor = '#ff9300'
+    } else if (constraint.oe_lof_upper < 1) {
+      lofHighlightColor = '#ffc000'
+    }
+  }
+
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <th scope="col">Category</th>
+          <th scope="col">
+            <TooltipAnchor tooltip="Expected variant counts were predicted using a depth corrected probability of mutation for each gene. More details can be found in section 4.1 of the supplement in Lek et al. Note that the expected variant counts for bases with a median depth <1 were removed from the totals.">
+              <TooltipHint>Exp. SNVs</TooltipHint>
+            </TooltipAnchor>
+          </th>
+          <th scope="col">
+            <TooltipAnchor tooltip="Includes single nucleotide changes that occurred in the canonical transcript that were found at a frequency of <0.1%, passed all filters, and at sites with a median depth ≥1. The counts represent the number of unique variants and not the allele count of these variants.">
+              <TooltipHint>Obs. SNVs</TooltipHint>
+            </TooltipAnchor>
+          </th>
+          <th scope="col">Constraint metrics</th>
+          <th />
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">Synonymous</th>
+          <td>{renderRoundedNumber(constraint.exp_syn)}</td>
+          <td>{constraint.obs_syn === null ? '—' : constraint.obs_syn}</td>
+          <td>
+            Z ={' '}
+            {renderRoundedNumber(constraint.syn_z, {
+              precision: 2,
+              tooltipPrecision: 3,
+              highlightColor: constraint.syn_z > 3.71 ? '#ff2600' : null,
+            })}
+            <br />
+            {renderOEMetrics(constraint, 'syn')}
+          </td>
+          <td>{renderOEGraph(constraint, 'syn')}</td>
+        </tr>
+        <tr>
+          <th scope="row">Missense</th>
+          <td>{renderRoundedNumber(constraint.exp_mis)}</td>
+          <td>{constraint.obs_mis === null ? '—' : constraint.obs_mis}</td>
+          <td>
+            Z ={' '}
+            {renderRoundedNumber(constraint.mis_z, {
+              precision: 2,
+              tooltipPrecision: 3,
+              highlightColor: constraint.mis_z > 3.09 ? '#ff9300' : null,
+            })}
+            <br />
+            {renderOEMetrics(constraint, 'mis')}
+          </td>
+          <td>{renderOEGraph(constraint, 'mis')}</td>
+        </tr>
+        <tr>
+          <th scope="row">pLoF</th>
+          <td>{renderRoundedNumber(constraint.exp_lof)}</td>
+          <td>{constraint.obs_lof === null ? '—' : constraint.obs_lof}</td>
+          <td>
+            pLI = {renderRoundedNumber(constraint.pLI, { precision: 2, tooltipPrecision: 3 })}
+            <br />
+            {renderOEMetrics(constraint, 'lof', lofHighlightColor)}
+          </td>
+          <td>{renderOEGraph(constraint, 'lof', lofHighlightColor)}</td>
+        </tr>
+      </tbody>
+    </Table>
+  )
+}
+
+GnomadConstraintTable.propTypes = {
+  constraint: PropTypes.shape({
+    exp_lof: PropTypes.number,
+    exp_mis: PropTypes.number,
+    exp_syn: PropTypes.number,
+    obs_lof: PropTypes.number,
+    obs_mis: PropTypes.number,
+    obs_syn: PropTypes.number,
+    oe_lof: PropTypes.number,
+    oe_lof_lower: PropTypes.number,
+    oe_lof_upper: PropTypes.number,
+    oe_mis: PropTypes.number,
+    oe_mis_lower: PropTypes.number,
+    oe_mis_upper: PropTypes.number,
+    oe_syn: PropTypes.number,
+    oe_syn_lower: PropTypes.number,
+    oe_syn_upper: PropTypes.number,
+    lof_z: PropTypes.number,
+    mis_z: PropTypes.number,
+    syn_z: PropTypes.number,
+    pLI: PropTypes.number,
+  }).isRequired,
+}
+
+const constraintQuery = `
+  query GnomadConstraint($transcriptId: String!) {
+    transcript(transcript_id: $transcriptId) {
+      gnomad_constraint {
+        exp_lof
+        exp_mis
+        exp_syn
+        obs_lof
+        obs_mis
+        obs_syn
+        oe_lof
+        oe_lof_lower
+        oe_lof_upper
+        oe_mis
+        oe_mis_lower
+        oe_mis_upper
+        oe_syn
+        oe_syn_lower
+        oe_syn_upper
+        lof_z
+        mis_z
+        syn_z
+        pLI
+      }
+    }
+  }
+`
+
+const ConnectedGnomadConstraintTable = ({ transcriptId }) => (
   <Query query={constraintQuery} variables={{ transcriptId }}>
     {({ data, error, loading }) => {
       if (loading) {
@@ -157,89 +261,13 @@ const GnomadConstraintTable = ({ transcriptId }) => (
 
       const constraint = data.transcript.gnomad_constraint
 
-      let lofHighlightColor = null
-      if (constraint.oe_lof_upper !== null) {
-        if (constraint.oe_lof_upper < 0.33) {
-          lofHighlightColor = '#ff2600'
-        } else if (constraint.oe_lof_upper < 0.66) {
-          lofHighlightColor = '#ff9300'
-        } else if (constraint.oe_lof_upper < 1) {
-          lofHighlightColor = '#ffc000'
-        }
-      }
-
-      return (
-        <Table>
-          <thead>
-            <tr>
-              <th scope="col">Category</th>
-              <th scope="col">
-                <TooltipAnchor tooltip="Expected variant counts were predicted using a depth corrected probability of mutation for each gene. More details can be found in section 4.1 of the supplement in Lek et al. Note that the expected variant counts for bases with a median depth <1 were removed from the totals.">
-                  <TooltipHint>Exp. SNVs</TooltipHint>
-                </TooltipAnchor>
-              </th>
-              <th scope="col">
-                <TooltipAnchor tooltip="Includes single nucleotide changes that occurred in the canonical transcript that were found at a frequency of <0.1%, passed all filters, and at sites with a median depth ≥1. The counts represent the number of unique variants and not the allele count of these variants.">
-                  <TooltipHint>Obs. SNVs</TooltipHint>
-                </TooltipAnchor>
-              </th>
-              <th scope="col">Constraint metrics</th>
-              <th />
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">Synonymous</th>
-              <td>{renderRoundedNumber(constraint.exp_syn)}</td>
-              <td>{constraint.obs_syn === null ? '—' : constraint.obs_syn}</td>
-              <td>
-                Z ={' '}
-                {renderRoundedNumber(constraint.syn_z, {
-                  precision: 2,
-                  tooltipPrecision: 3,
-                  highlightColor: constraint.syn_z > 3.71 ? '#ff2600' : null,
-                })}
-                <br />
-                {renderOEMetrics(constraint, 'syn')}
-              </td>
-              <td>{renderOEGraph(constraint, 'syn')}</td>
-            </tr>
-            <tr>
-              <th scope="row">Missense</th>
-              <td>{renderRoundedNumber(constraint.exp_mis)}</td>
-              <td>{constraint.obs_mis === null ? '—' : constraint.obs_mis}</td>
-              <td>
-                Z ={' '}
-                {renderRoundedNumber(constraint.mis_z, {
-                  precision: 2,
-                  tooltipPrecision: 3,
-                  highlightColor: constraint.mis_z > 3.09 ? '#ff9300' : null,
-                })}
-                <br />
-                {renderOEMetrics(constraint, 'mis')}
-              </td>
-              <td>{renderOEGraph(constraint, 'mis')}</td>
-            </tr>
-            <tr>
-              <th scope="row">pLoF</th>
-              <td>{renderRoundedNumber(constraint.exp_lof)}</td>
-              <td>{constraint.obs_lof === null ? '—' : constraint.obs_lof}</td>
-              <td>
-                pLI = {renderRoundedNumber(constraint.pLI, { precision: 2, tooltipPrecision: 3 })}
-                <br />
-                {renderOEMetrics(constraint, 'lof', lofHighlightColor)}
-              </td>
-              <td>{renderOEGraph(constraint, 'lof', lofHighlightColor)}</td>
-            </tr>
-          </tbody>
-        </Table>
-      )
+      return <GnomadConstraintTable constraint={constraint} />
     }}
   </Query>
 )
 
-GnomadConstraintTable.propTypes = {
+ConnectedGnomadConstraintTable.propTypes = {
   transcriptId: PropTypes.string.isRequired,
 }
 
-export default GnomadConstraintTable
+export default ConnectedGnomadConstraintTable
