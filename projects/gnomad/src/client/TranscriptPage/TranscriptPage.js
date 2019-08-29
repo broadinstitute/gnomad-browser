763c364893c5f54c5ce15866aa23dbc15e723124
Nick Watts <nwatts@broadinstitute.org>
2019-08-29 10:43:54 -0400

    [gnomAD] Add separate transcript page
    Resolves #286

---
+++ projects/gnomad/src/client/TranscriptPage/TranscriptPage.js
@@ -0,0 +1,365 @@
+import { mean } from 'd3-array'
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+import styled from 'styled-components'
+
+import { QuestionMark } from '@broad/help'
+import { RegionViewer } from '@broad/region-viewer'
+import { TranscriptsTrackWithTissueExpression } from '@broad/track-transcripts'
+import { ExternalLink } from '@broad/ui'
+
+import DocumentTitle from '../DocumentTitle'
+import GnomadPageHeading from '../GnomadPageHeading'
+import StatusMessage from '../StatusMessage'
+import { TrackPage, TrackPageSection } from '../TrackPage'
+import GeneConstraint from '../GenePage/gene-constraint/GeneConstraint'
+import GeneCoverageTrack from '../GenePage/GeneCoverageTrack'
+import GeneInfo from '../GenePage/GeneInfo'
+import RegionalConstraintTrack from '../GenePage/RegionalConstraintTrack'
+import TissueExpressionTrack from '../GenePage/TissueExpressionTrack'
+import TranscriptLink from '../GenePage/TranscriptLink'
+
+import VariantsInTranscript from './VariantsInTranscript'
+
+const GeneFullName = styled.span`
+  font-size: 0.75em;
+  font-weight: 400;
+`
+
+const GeneInfoColumnWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  @media (max-width: 1200px) {
+    flex-direction: column;
+    align-items: center;
+  }
+
+  /* Matches responsive styles in AttributeList */
+  @media (max-width: 600px) {
+    align-items: stretch;
+  }
+`
+
+const ControlPanel = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+  width: ${props => props.width}px;
+  margin-left: ${props => props.marginLeft}px;
+`
+
+const Legend = styled.ul`
+  display: flex;
+  flex-direction: row;
+  padding: 0;
+  margin: 0.5em 0;
+  list-style-type: none;
+`
+
+const LegendItemWrapper = styled.li`
+  display: flex;
+  align-items: stretch;
+  margin-left: 1em;
+`
+
+const Label = styled.label`
+  user-select: none;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+
+const CheckboxInput = styled.input.attrs({ type: 'checkbox' })`
+  margin-right: 0.5em;
+`
+
+const LegendSwatch = styled.span`
+  display: flex;
+  align-items: center;
+  width: 16px;
+  margin-left: 0.5em;
+
+  &::before {
+    content: '';
+    display: inline-block;
+    width: 16px;
+    height: ${props => props.height}px;
+    background: ${props => props.color};
+  }
+`
+
+const transcriptFeatureAttributes = {
+  exon: {
+    fill: '#bdbdbd',
+    height: 4,
+  },
+  CDS: {
+    fill: '#424242',
+    height: 10,
+  },
+  UTR: {
+    fill: '#424242',
+    height: 4,
+  },
+}
+
+const sortTranscripts = (transcripts, canonicalTranscriptId) =>
+  transcripts.sort((t1, t2) => {
+    // Sort transcripts by isCanonical, mean expression, transcript ID
+    if (t1.transcript_id === canonicalTranscriptId) {
+      return -1
+    }
+    if (t2.transcript_id === canonicalTranscriptId) {
+      return 1
+    }
+
+    const t1Mean = mean(Object.values(t1.gtex_tissue_expression))
+    const t2Mean = mean(Object.values(t2.gtex_tissue_expression))
+
+    if (t1Mean === t2Mean) {
+      return t1.transcript_id.localeCompare(t2.transcript_id)
+    }
+
+    return t2Mean - t1Mean
+  })
+
+class TranscriptPage extends Component {
+  static propTypes = {
+    datasetId: PropTypes.string.isRequired,
+    transcript: PropTypes.shape({
+      transcript_id: PropTypes.string.isRequired,
+      chrom: PropTypes.string.isRequired,
+      start: PropTypes.number.isRequired,
+      stop: PropTypes.number.isRequired,
+      strand: PropTypes.string.isRequired,
+      exons: PropTypes.arrayOf(
+        PropTypes.shape({
+          feature_type: PropTypes.string.isRequired,
+          start: PropTypes.number.isRequired,
+          stop: PropTypes.number.isRequired,
+        })
+      ).isRequired,
+      gene: PropTypes.shape({
+        full_gene_name: PropTypes.string.isRequired,
+        gene_id: PropTypes.string.isRequired,
+        gene_name: PropTypes.string.isRequired,
+        exons: PropTypes.arrayOf(
+          PropTypes.shape({
+            feature_type: PropTypes.string.isRequired,
+            start: PropTypes.number.isRequired,
+            stop: PropTypes.number.isRequired,
+          })
+        ).isRequired,
+      }).isRequired,
+    }).isRequired,
+    width: PropTypes.number.isRequired,
+  }
+
+  constructor(props) {
+    super(props)
+
+    const { transcript } = props
+    const hasCDS = transcript.gene.exons.some(exon => exon.feature_type === 'CDS')
+
+    this.state = {
+      includeNonCodingTranscripts: !hasCDS,
+      includeUTRs: false,
+    }
+  }
+
+  render() {
+    const { datasetId, transcript, width } = this.props
+    const { includeUTRs, includeNonCodingTranscripts } = this.state
+    const { gene } = transcript
+
+    if (datasetId === 'gnomad_sv_r2') {
+      return <Redirect to={`/gene/${gene.gene_id}?dataset=gnomad_sv_r2`} />
+    }
+
+    const smallScreen = width < 900
+
+    // Subtract 30px for padding on Page component
+    const regionViewerWidth = width - 30
+
+    const hasNonCodingTranscripts = gene.transcripts.some(
+      tx => !tx.exons.some(exon => exon.feature_type === 'CDS')
+    )
+
+    const cdsCompositeExons = gene.exons.filter(exon => exon.feature_type === 'CDS')
+    const hasCodingExons = cdsCompositeExons.length > 0
+
+    const regionViewerRegions = gene.exons.filter(
+      exon =>
+        exon.feature_type === 'CDS' ||
+        (exon.feature_type === 'UTR' && includeUTRs) ||
+        (exon.feature_type === 'exon' && includeNonCodingTranscripts)
+    )
+
+    return (
+      <TrackPage>
+        <TrackPageSection>
+          <DocumentTitle title={gene.gene_name} />
+          <GnomadPageHeading
+            datasetOptions={{ includeStructuralVariants: false }}
+            selectedDataset={datasetId}
+          >
+            {gene.gene_name} <GeneFullName>{gene.full_gene_name}</GeneFullName>
+          </GnomadPageHeading>
+          <GeneInfoColumnWrapper>
+            <GeneInfo gene={gene} />
+            <div>
+              <h2>
+                Gene Constraint <QuestionMark topic="gene-constraint" />
+              </h2>
+              <GeneConstraint
+                datasetId={datasetId}
+                gene={gene}
+                selectedTranscriptId={transcript.transcript_id}
+              />
+            </div>
+          </GeneInfoColumnWrapper>
+        </TrackPageSection>
+        <RegionViewer
+          leftPanelWidth={100}
+          width={regionViewerWidth}
+          padding={75}
+          regions={regionViewerRegions}
+          rightPanelWidth={smallScreen ? 0 : 160}
+        >
+          {hasCodingExons && (
+            <GeneCoverageTrack
+              datasetId={datasetId}
+              geneId={gene.gene_id}
+              showExomeCoverage={datasetId !== 'gnomad_sv_r2'}
+            />
+          )}
+
+          <ControlPanel marginLeft={100} width={regionViewerWidth - 100 - (smallScreen ? 0 : 160)}>
+            Include:
+            <Legend>
+              <LegendItemWrapper>
+                <Label htmlFor="include-cds-regions">
+                  <CheckboxInput
+                    checked={hasCodingExons}
+                    disabled
+                    id="include-cds-regions"
+                    onChange={() => {}}
+                  />
+                  CDS
+                  <LegendSwatch
+                    color={transcriptFeatureAttributes.CDS.fill}
+                    height={transcriptFeatureAttributes.CDS.height}
+                  />
+                </Label>
+              </LegendItemWrapper>
+
+              <LegendItemWrapper>
+                <Label htmlFor="include-utr-regions">
+                  <CheckboxInput
+                    checked={includeUTRs}
+                    disabled={!gene.exons.some(exon => exon.feature_type === 'UTR')}
+                    id="include-utr-regions"
+                    onChange={e => {
+                      this.setState({ includeUTRs: e.target.checked })
+                    }}
+                  />
+                  UTR
+                  <LegendSwatch
+                    color={transcriptFeatureAttributes.UTR.fill}
+                    height={transcriptFeatureAttributes.UTR.height}
+                  />
+                </Label>
+              </LegendItemWrapper>
+
+              <LegendItemWrapper>
+                <Label htmlFor="include-nc-transcripts">
+                  <CheckboxInput
+                    checked={includeNonCodingTranscripts}
+                    disabled={!hasNonCodingTranscripts}
+                    id="include-nc-transcripts"
+                    onChange={e => {
+                      this.setState({ includeNonCodingTranscripts: e.target.checked })
+                    }}
+                  />
+                  Non-coding transcript
+                  <LegendSwatch
+                    color={transcriptFeatureAttributes.exon.fill}
+                    height={transcriptFeatureAttributes.exon.height}
+                  />
+                </Label>
+              </LegendItemWrapper>
+            </Legend>
+          </ControlPanel>
+
+          {hasCodingExons && (
+            <TranscriptsTrackWithTissueExpression
+              activeTranscript={{
+                exons: gene.exons,
+                strand: gene.strand,
+              }}
+              exportFilename={`${gene.gene_id}_transcripts`}
+              expressionLabel={
+                <span>
+                  <ExternalLink href={`http://www.gtexportal.org/home/gene/${gene.gene_name}`}>
+                    Isoform expression
+                  </ExternalLink>
+                  <QuestionMark topic="gtex" />
+                </span>
+              }
+              renderTranscriptLeftPanel={({ transcript: trackTranscript }) => (
+                <TranscriptLink
+                  to={`/gene/${gene.gene_id}/transcript/${trackTranscript.transcript_id}`}
+                  isCanonical={trackTranscript.transcript_id === gene.canonical_transcript}
+                  isSelected={trackTranscript.transcript_id === transcript.transcript_id}
+                >
+                  {trackTranscript.transcript_id}
+                </TranscriptLink>
+              )}
+              showNonCodingTranscripts={includeNonCodingTranscripts}
+              showUTRs={includeUTRs}
+              transcripts={sortTranscripts(
+                gene.transcripts.map(otherTranscript => ({
+                  ...otherTranscript,
+                  exons: otherTranscript.exons.some(exon => exon.feature_type !== 'exon')
+                    ? otherTranscript.exons.filter(exon => exon.feature_type !== 'exon')
+                    : otherTranscript.exons,
+                })),
+                gene.canonical_transcript
+              )}
+            />
+          )}
+
+          {!hasCodingExons && (
+            <StatusMessage>
+              Coverage &amp; transcripts not shown for genes with no coding exons
+            </StatusMessage>
+          )}
+
+          {hasCodingExons && (
+            <TissueExpressionTrack exons={cdsCompositeExons} expressionRegions={gene.pext} />
+          )}
+
+          {gene.exac_regional_missense_constraint_regions.length > 0 && datasetId === 'exac' && (
+            <RegionalConstraintTrack
+              height={15}
+              regions={gene.exac_regional_missense_constraint_regions}
+            />
+          )}
+
+          <VariantsInTranscript
+            datasetId={datasetId}
+            gene={gene}
+            transcriptId={transcript.transcript_id}
+            width={regionViewerWidth}
+          />
+        </RegionViewer>
+      </TrackPage>
+    )
+  }
+}
+
+export default TranscriptPage
---
+++ projects/gnomad/src/client/TranscriptPage/TranscriptPageContainer.js
@@ -0,0 +1,225 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+import Query from '../Query'
+import StatusMessage from '../StatusMessage'
+import { withWindowSize } from '../windowSize'
+import TranscriptPage from './TranscriptPage'
+
+const AutosizedTranscriptPage = withWindowSize(TranscriptPage)
+
+const query = `
+query Transcript($transcriptId: String!) {
+  transcript(transcript_id: $transcriptId) {
+    transcript_id
+    chrom
+    strand
+    start
+    stop
+    exons {
+      feature_type
+      start
+      stop
+    }
+    gene {
+      gene_id
+      gene_name
+      full_gene_name
+      canonical_transcript
+      omim_accession
+      chrom
+      start
+      stop
+      strand
+      exons {
+        feature_type
+        start
+        stop
+      }
+      exac_constraint {
+        exp_syn
+        n_syn
+        syn_z
+        exp_mis
+        n_mis
+        mis_z
+        exp_lof
+        n_lof
+        lof_z
+        pLI
+      }
+      transcripts {
+        transcript_id
+        strand
+        exons {
+          feature_type
+          start
+          stop
+        }
+        gtex_tissue_expression: gtex_tissue_tpms_by_transcript {
+          adipose_subcutaneous: adiposeSubcutaneous
+          adipose_visceral_omentum: adiposeVisceralOmentum
+          adrenal_gland: adrenalGland
+          artery_aorta: arteryAorta
+          artery_coronary: arteryCoronary
+          artery_tibial: arteryTibial
+          bladder: bladder
+          brain_amygdala: brainAmygdala
+          brain_anterior_cingulate_cortex_ba24: brainAnteriorcingulatecortexBa24
+          brain_caudate_basal_ganglia: brainCaudateBasalganglia
+          brain_cerebellar_hemisphere: brainCerebellarhemisphere
+          brain_cerebellum: brainCerebellum
+          brain_cortex: brainCortex
+          brain_frontal_cortex_ba9: brainFrontalcortexBa9
+          brain_hippocampus: brainHippocampus
+          brain_hypothalamus: brainHypothalamus
+          brain_nucleus_accumbens_basal_ganglia: brainNucleusaccumbensBasalganglia
+          brain_putamen_basal_ganglia: brainPutamenBasalganglia
+          brain_spinal_cord_cervical_c_1: brainSpinalcordCervicalc1
+          brain_substantia_nigra: brainSubstantianigra
+          breast_mammary_tissue: breastMammarytissue
+          cells_ebv_transformed_lymphocytes: cellsEbvTransformedlymphocytes
+          cells_transformed_fibroblasts: cellsTransformedfibroblasts
+          cervix_ectocervix: cervixEctocervix
+          cervix_endocervix: cervixEndocervix
+          colon_sigmoid: colonSigmoid
+          colon_transverse: colonTransverse
+          esophagus_gastroesophageal_junction: esophagusGastroesophagealjunction
+          esophagus_mucosa: esophagusMucosa
+          esophagus_muscularis: esophagusMuscularis
+          fallopian_tube: fallopianTube
+          heart_atrial_appendage: heartAtrialappendage
+          heart_left_ventricle: heartLeftventricle
+          kidney_cortex: kidneyCortex
+          liver: liver
+          lung: lung
+          minor_salivary_gland: minorSalivaryGland
+          muscle_skeletal: muscleSkeletal
+          nerve_tibial: nerveTibial
+          ovary: ovary
+          pancreas: pancreas
+          pituitary: pituitary
+          prostate: prostate
+          skin_not_sun_exposed_suprapubic: skinNotsunexposedSuprapubic
+          skin_sun_exposed_lower_leg: skinSunexposedLowerleg
+          small_intestine_terminal_ileum: smallIntestineTerminalileum
+          spleen: spleen
+          stomach: stomach
+          testis: testis
+          thyroid: thyroid
+          uterus: uterus
+          vagina: vagina
+          whole_blood: wholeBlood
+        }
+      }
+      pext {
+        start
+        stop
+        mean
+        tissues {
+          adipose_subcutaneous
+          adipose_visceral_omentum
+          adrenal_gland
+          artery_aorta
+          artery_coronary
+          artery_tibial
+          bladder
+          brain_amygdala
+          brain_anterior_cingulate_cortex_ba24
+          brain_caudate_basal_ganglia
+          brain_cerebellar_hemisphere
+          brain_cerebellum
+          brain_cortex
+          brain_frontal_cortex_ba9
+          brain_hippocampus
+          brain_hypothalamus
+          brain_nucleus_accumbens_basal_ganglia
+          brain_putamen_basal_ganglia
+          brain_spinal_cord_cervical_c_1
+          brain_substantia_nigra
+          breast_mammary_tissue
+          cells_ebv_transformed_lymphocytes
+          cells_transformed_fibroblasts
+          cervix_ectocervix
+          cervix_endocervix
+          colon_sigmoid
+          colon_transverse
+          esophagus_gastroesophageal_junction
+          esophagus_mucosa
+          esophagus_muscularis
+          fallopian_tube
+          heart_atrial_appendage
+          heart_left_ventricle
+          kidney_cortex
+          liver
+          lung
+          minor_salivary_gland
+          muscle_skeletal
+          nerve_tibial
+          ovary
+          pancreas
+          pituitary
+          prostate
+          skin_not_sun_exposed_suprapubic
+          skin_sun_exposed_lower_leg
+          small_intestine_terminal_ileum
+          spleen
+          stomach
+          testis
+          thyroid
+          uterus
+          vagina
+          whole_blood
+        }
+      }
+      exac_regional_missense_constraint_regions {
+        start
+        stop
+        obs_mis
+        exp_mis
+        obs_exp
+        chisq_diff_null
+      }
+    }
+  }
+}
+`
+
+const TranscriptPageContainer = ({ datasetId, transcriptId, ...otherProps }) => (
+  <Query query={query} variables={{ transcriptId }}>
+    {({ data, error, graphQLErrors, loading }) => {
+      if (loading) {
+        return <StatusMessage>Loading transcript...</StatusMessage>
+      }
+
+      if (error || !data) {
+        return <StatusMessage>Unable to load transcript</StatusMessage>
+      }
+
+      if (!data.transcript) {
+        return (
+          <StatusMessage>
+            {graphQLErrors
+              ? graphQLErrors.map(e => e.message).join(', ')
+              : 'Unable to load transcript'}
+          </StatusMessage>
+        )
+      }
+
+      return (
+        <AutosizedTranscriptPage
+          {...otherProps}
+          datasetId={datasetId}
+          transcript={data.transcript}
+        />
+      )
+    }}
+  </Query>
+)
+
+TranscriptPageContainer.propTypes = {
+  datasetId: PropTypes.string.isRequired,
+  transcriptId: PropTypes.string.isRequired,
+}
+
+export default TranscriptPageContainer
