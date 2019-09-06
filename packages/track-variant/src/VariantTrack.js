c256fe6b0bb1eb453e24ee4f9cbf17f7ab03c7a0
Nick Watts <nwatts@broadinstitute.org>
2019-09-06 13:40:29 -0400

    [track-variant] Forward all props from variant track to plot

--- packages/track-variant/src/VariantTrack.js
+++ packages/track-variant/src/VariantTrack.js
@@ -5,33 +5,18 @@ import { Track } from '@broad/region-viewer'
 
 import { VariantPlot } from './VariantPlot'
 
-export const VariantTrack = ({ height, title, variants }) => (
+export const VariantTrack = ({ title, ...otherProps }) => (
   <Track title={title}>
     {({ scalePosition, width }) => (
-      <VariantPlot
-        height={height}
-        scalePosition={scalePosition}
-        variants={variants}
-        width={width}
-      />
+      <VariantPlot {...otherProps} scalePosition={scalePosition} width={width} />
     )}
   </Track>
 )
 
 VariantTrack.propTypes = {
-  height: PropTypes.number,
   title: PropTypes.string,
-  variants: PropTypes.arrayOf(
-    PropTypes.shape({
-      allele_freq: PropTypes.number,
-      consequence: PropTypes.string,
-      pos: PropTypes.number.isRequired,
-      variant_id: PropTypes.string.isRequired,
-    })
-  ).isRequired,
 }
 
 VariantTrack.defaultProps = {
-  height: 60,
   title: '',
 }
