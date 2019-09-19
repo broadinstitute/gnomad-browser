89710599b5af59c93c0b8b44346e09e41ae67506
Nick Watts <nwatts@broadinstitute.org>
2019-09-19 10:21:40 -0400

    [exome-results] Improve API error handling
    * Return response for syntax/validation errors
    * Do not log user visible errors

--- projects/exome-results-browsers/src/server/server.js
+++ projects/exome-results-browsers/src/server/server.js
@@ -43,11 +43,29 @@ app.use(compression())
         },
       },
       customFormatErrorFn: error => {
-        console.log(error)
-        const message =
-          error.extensions && error.extensions.isUserVisible
-            ? error.message
-            : 'An unknown error occurred'
+        // graphql-js doesn't distinguish between different error types, so this is the
+        // only way to determine what errors come from query validation (and thus should
+        // be shown to the user)
+        // See https://github.com/graphql/graphql-js/issues/1847
+        const isQueryValidationError =
+          (error.message.startsWith('Syntax Error') &&
+            error.stack.includes('graphql/error/syntaxError')) ||
+          (error.message.startsWith('Cannot query field') &&
+            error.stack.includes('graphql/validation/rules'))
+
+        if (isQueryValidationError) {
+          return { message: error.message, locations: error.locations }
+        }
+
+        const isUserVisible = error.extensions && error.extensions.isUserVisible
+
+        // User visible errors (such as variant not found) are expected to occur during
+        // normal use of the browser and don't need to be logged.
+        if (!isUserVisible) {
+          console.log(error)
+        }
+
+        const message = isUserVisible ? error.message : 'An unknown error occurred'
         return { message }
       },
     })
