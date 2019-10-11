471b95270490cf459ae15fa38f700529aa95e0be
Nick Watts <nwatts@broadinstitute.org>
2019-10-11 16:00:53 -0400

    Split downloads page into multiple tabs

--- projects/gnomad/src/client/downloads/GnomadV2Downloads.js
+++ projects/gnomad/src/client/downloads/GnomadV2Downloads.js
@@ -1,38 +1,14 @@
-import PropTypes from 'prop-types'
 import React from 'react'
-import styled from 'styled-components'
 
-import { PageHeading, ExternalLink, Link, List, ListItem } from '@broad/ui'
+import { ExternalLink, Link, List, ListItem } from '@broad/ui'
 
-import { withAnchor } from './AnchorLink'
-import DocumentTitle from './DocumentTitle'
-import InfoPage from './InfoPage'
-
-const FileList = styled(List)`
-  li {
-    line-height: 1.25;
-  }
-`
-
-const SectionTitle = withAnchor(styled.h2``)
-
-const ColumnsWrapper = styled.div`
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: space-between;
-`
-
-const Column = styled.div`
-  flex-basis: calc(50% - 25px);
-
-  @media (max-width: 900px) {
-    flex-basis: 100%;
-  }
-
-  > h3 {
-    margin-top: 0;
-  }
-`
+import {
+  ChromosomeVcfLinks,
+  Column,
+  ColumnsWrapper,
+  FileList,
+  SectionTitle,
+} from './downloadsPageStyles'
 
 const exomeChromosomeVcfs = [
   { chrom: '1', size: '5.77 GiB', md5: '9817acdf1d9600efb3355e4cb4b7ee1f' },
@@ -61,33 +37,6 @@ const exomeChromosomeVcfs = [
   { chrom: 'Y', size: '15.66 MiB', md5: 'fa412becb3aba27b9acf2529bb76b895' },
 ]
 
-const liftoverExomeChromosomeVcfs = [
-  { chrom: '1', size: '8.4 GiB', md5: '7de10568689b50697288c7743ac342e0' },
-  { chrom: '2', size: '6.1 GiB', md5: '9daadf80734f3fc5894e31c37d5edd5e' },
-  { chrom: '3', size: '4.78 GiB', md5: '693a09cb4c70cd21f8b0aa3e63f672ef' },
-  { chrom: '4', size: '3.16 GiB', md5: 'd7b7d709857a648a1cec2262cbe72ba0' },
-  { chrom: '5', size: '3.66 GiB', md5: 'd2544f5a7664909c7500b3e5b6e72eed' },
-  { chrom: '6', size: '4.12 GiB', md5: 'e32aa5ef1948b406b398898c49b439d3' },
-  { chrom: '7', size: '4.17 GiB', md5: '5e8dbec42f3e7ff9a3f023358137cc41' },
-  { chrom: '8', size: '3.08 GiB', md5: '454d9efe12cdda104032ed78e3769d68' },
-  { chrom: '9', size: '3.49 GiB', md5: '13f24aac134162a215024ab9f34d9e4b' },
-  { chrom: '10', size: '3.25 GiB', md5: 'f895398b0932c14abce5025d97484bba' },
-  { chrom: '11', size: '5.23 GiB', md5: '998b6c79c19422e983183371130d9a56' },
-  { chrom: '12', size: '4.45 GiB', md5: '46527606732eb288454b1ed22723ba4c' },
-  { chrom: '13', size: '1.39 GiB', md5: '15adaca2b92c43788dae049e0e053808' },
-  { chrom: '14', size: '2.92 GiB', md5: '20eccd8bc17f90e14701e1148cfd2ea9' },
-  { chrom: '15', size: '3.02 GiB', md5: '8bf4b97fa2bbc5e26909eeccee99125a' },
-  { chrom: '16', size: '4.39 GiB', md5: 'ffb631cf9ddc234b6e6053e28c9f9a02' },
-  { chrom: '17', size: '5.22 GiB', md5: 'e21098ef8e41b9963a774fbe3dcbbdba' },
-  { chrom: '18', size: '1.25 GiB', md5: '655c3f6f63cc7ca14e3d1b307abb550e' },
-  { chrom: '19', size: '6.2 GiB', md5: '2587da8ee4e7cd28b453d72c11c074b1' },
-  { chrom: '20', size: '2.1 GiB', md5: 'db9dbd66a56428b41b9bc324abf59fb5' },
-  { chrom: '21', size: '948.41 MiB', md5: 'ecccf042a895703ab305ab81c9738855' },
-  { chrom: '22', size: '2.07 GiB', md5: 'e96e113e26b0ad95a5f2789622b9224c' },
-  { chrom: 'X', size: '1.9 GiB', md5: 'b2d62ceecb20b6240ab2047fa0b59de9' },
-  { chrom: 'Y', size: '24.14 MiB', md5: '7a3bf626380a108be514991ad21df866' },
-]
-
 const genomeChromosomeVcfs = [
   { chrom: '1', size: '35.83 GiB', md5: 'b81951f2b7cb2dbef63932677b58f57e' },
   { chrom: '2', size: '38.21 GiB', md5: 'f7db44e30f5be3378486c07494aa035f' },
@@ -114,79 +63,8 @@ const genomeChromosomeVcfs = [
   { chrom: 'X', size: '17.8 GiB', md5: '77d25e87e94d360eb2acedaaedaf3df1' },
 ]
 
-const liftoverGenomeChromosomeVcfs = [
-  { chrom: '1', size: '57.78 GiB', md5: '33ad7883e83ea091bc77b0e19c62ae10' },
-  { chrom: '2', size: '61.9 GiB', md5: '721dd923eb5152ab7a80b55a918706eb' },
-  { chrom: '3', size: '50.7 GiB', md5: '45b1db20bd90e71259ad1d3fbcc88f74' },
-  { chrom: '4', size: '49.13 GiB', md5: '60823a1b5d3eed9569373c7ae6882d12' },
-  { chrom: '5', size: '45.89 GiB', md5: 'f3fe0fb5f4d86be2c6ecbc7f666c5a09' },
-  { chrom: '6', size: '42.87 GiB', md5: '4eaaaff9d38b765d161c00104fbb6402' },
-  { chrom: '7', size: '41.94 GiB', md5: 'fda487fc1c2c4c8a13d86a9c6c70587e' },
-  { chrom: '8', size: '39.7 GiB', md5: 'bd482c5fa45049d038646bfd20743004' },
-  { chrom: '9', size: '32.33 GiB', md5: '2fbec174a4e704f3defd52a5fdc25391' },
-  { chrom: '10', size: '34.29 GiB', md5: '91c0d14635a17593b3fdbcdf91eb8b70' },
-  { chrom: '11', size: '35.26 GiB', md5: 'fb19e2a020fd4349e4a2f3192dc4ac36' },
-  { chrom: '12', size: '34.22 GiB', md5: '788f21f5f5b7d68d00323eddce1b8a59' },
-  { chrom: '13', size: '24.26 GiB', md5: '2258c0edee6994c28fb35e4d7c37ce39' },
-  { chrom: '14', size: '23.5 GiB', md5: 'c991f7509e2f9cc1bc021e5a0e7f2d3e' },
-  { chrom: '15', size: '22.14 GiB', md5: 'a9cd0171649fc0020e927a55222e20d5' },
-  { chrom: '16', size: '24.82 GiB', md5: '292ac43732f3bf3dce6f360ad3290acc' },
-  { chrom: '17', size: '21.55 GiB', md5: '943ac20b17c0e3a737b3bc9ef457def4' },
-  { chrom: '18', size: '19.53 GiB', md5: '33bf64c3cf4f9c77b2b903969032e699' },
-  { chrom: '19', size: '17.71 GiB', md5: '21b57b0ee18225bc95cb494de784ed7a' },
-  { chrom: '20', size: '15.58 GiB', md5: '5f49605670c7fb8b7cb2186c4327327b' },
-  { chrom: '21', size: '9.87 GiB', md5: '585b7fd4161b09943a70693b3019091f' },
-  { chrom: '22', size: '10.33 GiB', md5: '711fc7674901ce7ff26c53ff55a78f7f' },
-  { chrom: 'X', size: '27.76 GiB', md5: '381a63cba98b123acdf8b716b1ebc4e3' },
-]
-
-const ChromosomeVcfLinks = ({ chrom, md5, size, url }) => {
-  const vcfUrl = url(chrom)
-  const tabixUrl = `${vcfUrl}.tbi`
-
-  return (
-    <React.Fragment>
-      <ExternalLink href={vcfUrl}>{`chr${chrom} sites VCF`}</ExternalLink>{' '}
-      <ExternalLink href={tabixUrl}>(.tbi)</ExternalLink>
-      <br />
-      <span>
-        {size}, MD5:&nbsp;{md5}
-      </span>
-    </React.Fragment>
-  )
-}
-
-ChromosomeVcfLinks.propTypes = {
-  chrom: PropTypes.string.isRequired,
-  md5: PropTypes.string.isRequired,
-  size: PropTypes.string.isRequired,
-  url: PropTypes.func.isRequired,
-}
-
 export default () => (
-  <InfoPage>
-    <DocumentTitle title="Downloads" />
-    <PageHeading>Downloads</PageHeading>
-    <p>
-      gnomAD is available for download in VCF and Hail Table (.ht) formats. The variant dataset
-      files below contain all subsets (non-neuro, non-cancer, controls-only, and non-TOPMed). The
-      files can be downloaded in parallel on the command line with{' '}
-      <ExternalLink href="https://cloud.google.com/storage/docs/gsutil">gsutil</ExternalLink>.
-    </p>
-
-    <p>
-      For example, after installing gsutil, start navigating with this command:{' '}
-      <code>gsutil ls gs://gnomad-public/release/2.1.1</code>
-    </p>
-
-    <p>
-      To work efficiently with gnomAD, we recommend using{' '}
-      <ExternalLink href="https://Hail.is/">Hail 0.2</ExternalLink> and our{' '}
-      <ExternalLink href="https://github.com/macarthur-lab/gnomad_hail">
-        Hail utilities for gnomAD
-      </ExternalLink>
-      .
-    </p>
+  <React.Fragment>
     <p>
       <strong>Update, March 6, 2019: gnomAD 2.1.1 released</strong>{' '}
       <ExternalLink href="https://storage.googleapis.com/gnomad-public/release/2.1.1/README.txt">
@@ -199,9 +77,6 @@ export default () => (
       <List>
         <ListItem>
           <Link href="#variants">Variants</Link>
-        </ListItem>
-        <ListItem>
-          <Link href="#variants-grch38-liftover">Variants (GRCh38 liftover)</Link>
         </ListItem>
         <ListItem>
           <Link href="#coverage">Coverage</Link>
@@ -298,86 +173,6 @@ export default () => (
                   {...vcf}
                   url={chrom =>
                     `https://storage.googleapis.com/gnomad-public/release/2.1.1/vcf/genomes/gnomad.genomes.r2.1.1.sites.${chrom}.vcf.bgz`
-                  }
-                />
-              </ListItem>
-            ))}
-          </FileList>
-        </Column>
-      </ColumnsWrapper>
-    </section>
-
-    <section>
-      <SectionTitle id="variants-grch38-liftover">Variants (GRCh38 liftover)</SectionTitle>
-      <ColumnsWrapper>
-        <Column>
-          <h3>Exomes</h3>
-          <FileList>
-            <ListItem>
-              <ExternalLink href="https://console.cloud.google.com/storage/browser/gnomad-public/release/2.1.1/liftover_grch38/ht/exomes">
-                Sites Hail Table
-              </ExternalLink>
-              <br />
-              gs://gnomad-public/release/2.1.1/
-              <wbr />
-              liftover_grch38/ht/exomes/
-              <wbr />
-              gnomad.exomes.r2.1.1.sites.liftover_grch38.ht
-            </ListItem>
-
-            <ListItem>
-              <ExternalLink href="https://storage.googleapis.com/gnomad-public/release/2.1.1/liftover_grch38/vcf/exomes/gnomad.exomes.r2.1.1.sites.liftover_grch38.vcf.bgz">
-                All chromosomes VCF
-              </ExternalLink>{' '}
-              <ExternalLink href="https://storage.googleapis.com/gnomad-public/release/2.1.1/liftover_grch38/vcf/exomes/gnomad.exomes.r2.1.1.sites.liftover_grch38.vcf.bgz.tbi">
-                (.tbi)
-              </ExternalLink>
-              <br />
-              <span>85.31 GiB, MD5: cff8d0cfed50adc9211d1feaed2d4ca7</span>
-            </ListItem>
-            {liftoverExomeChromosomeVcfs.map(vcf => (
-              <ListItem key={vcf.chrom}>
-                <ChromosomeVcfLinks
-                  {...vcf}
-                  url={chrom =>
-                    `https://storage.googleapis.com/gnomad-public/release/2.1.1/liftover_grch38/vcf/exomes/gnomad.exomes.r2.1.1.sites.${chrom}.liftover_grch38.vcf.bgz`
-                  }
-                />
-              </ListItem>
-            ))}
-          </FileList>
-        </Column>
-
-        <Column>
-          <h3>Genomes</h3>
-          <FileList>
-            <ListItem>
-              <ExternalLink href="https://console.cloud.google.com/storage/gnomad-public/release/2.1.1/liftover_grch38/ht/genomes">
-                Sites Hail Table
-              </ExternalLink>
-              <br />
-              gs://gnomad-public/release/2.1.1/
-              <wbr />
-              liftover_grch38/ht/genomes/
-              <wbr />
-              gnomad.genomes.r2.1.1.sites.liftover_grch38.ht
-            </ListItem>
-            <ListItem>
-              <ExternalLink href="https://storage.googleapis.com/gnomad-public/release/2.1.1/liftover_grch38/vcf/genomes/gnomad.genomes.r2.1.1.sites.liftover_grch38.vcf.bgz">
-                All chromosomes VCF
-              </ExternalLink>{' '}
-              <ExternalLink href="https://storage.googleapis.com/gnomad-public/release/2.1.1/liftover_grch38/vcf/genomes/gnomad.genomes.r2.1.1.sites.liftover_grch38.vcf.bgz.tbi">
-                (.tbi)
-              </ExternalLink>
-              <br />
-              <span>743.06 GiB, MD5: 83de3d5b52669f714e810d4fcf047c18</span>
-            </ListItem>
-            {liftoverGenomeChromosomeVcfs.map(vcf => (
-              <ListItem key={vcf.chrom}>
-                <ChromosomeVcfLinks
-                  {...vcf}
-                  url={chrom =>
-                    `https://storage.googleapis.com/gnomad-public/release/2.1.1/liftover_grch38/vcf/genomes/gnomad.genomes.r2.1.1.sites.${chrom}.liftover_grch38.vcf.bgz`
                   }
                 />
               </ListItem>
@@ -538,5 +333,5 @@ export default () => (
         </ListItem>
       </FileList>
     </section>
-  </InfoPage>
+  </React.Fragment>
 )
---
+++ projects/gnomad/src/client/downloads/GnomadV3Downloads.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default () => (
+  <React.Fragment>
+    <section>
+      <h2>Variants</h2>
+    </section>
+  </React.Fragment>
+)
