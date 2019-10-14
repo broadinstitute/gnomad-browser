a33fc88b2478474d7cbeafcd48a0c697fcaac2f3
Nick Watts <nwatts@broadinstitute.org>
2019-10-13 21:07:57 -0400

    Add logging module
    Format logs for StackDriver

---
+++ projects/gnomad-api/src/utilities/logging.js
@@ -0,0 +1,56 @@
+// https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry#logseverity
+const Severity = {
+  DEFAULT: 'DEFAULT',
+  DEBUG: 'DEBUG',
+  INFO: 'INFO',
+  NOTICE: 'NOTICE',
+  WARNING: 'WARNING',
+  ERROR: 'ERROR',
+  CRITICAL: 'CRITICAL',
+  ALERT: 'ALERT',
+  EMERGENCY: 'EMERGENCY',
+}
+
+// TODO: Configure this with environment variables
+
+class Logger {
+  // eslint-disable-next-line class-methods-use-this
+  formatRecord(obj) {
+    // https://cloud.google.com/error-reporting/docs/formatting-error-messages#json_representation
+    const record = {}
+
+    if (typeof obj === 'string') {
+      record.message = obj
+    }
+
+    if (obj.message) {
+      record.message = obj.message
+    }
+
+    if (obj.stack) {
+      record.stack_trace = obj.stack
+    }
+
+    return record
+  }
+
+  info(obj) {
+    const record = this.formatRecord(obj)
+    record.severity = Severity.INFO
+    console.log(JSON.stringify(record))
+  }
+
+  warn(obj) {
+    const record = this.formatRecord(obj)
+    record.severity = Severity.WARNING
+    console.log(JSON.stringify(record))
+  }
+
+  error(obj) {
+    const record = this.formatRecord(obj)
+    record.severity = Severity.ERROR
+    console.log(JSON.stringify(record))
+  }
+}
+
+export default new Logger()
