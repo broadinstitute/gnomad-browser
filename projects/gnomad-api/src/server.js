34791b07165a4b3a2c256a087724034a3b9c9011
Nick Watts <nwatts@broadinstitute.org>
2019-10-14 11:14:32 -0400

    Remove unnecessary IIFE
    Not required after async Mongo connection was removed

--- projects/gnomad-api/src/server.js
+++ projects/gnomad-api/src/server.js
@@ -26,71 +26,64 @@ if (JSON.parse(process.env.ENABLE_SSL_REDIRECT || 'false')) {
   })
 }
 
-// eslint-disable-line prettier/prettier
-;(async () => {
-  try {
-    const elastic = new elasticsearch.Client({
-      apiVersion: '5.5',
-      host: process.env.ELASTICSEARCH_URL,
-    })
+const elastic = new elasticsearch.Client({
+  apiVersion: '5.5',
+  host: process.env.ELASTICSEARCH_URL,
+})
 
-    const redis = new Redis({
-      host: process.env.REDIS_HOST,
-      port: 6379,
-    })
+const redis = new Redis({
+  host: process.env.REDIS_HOST,
+  port: 6379,
+})
 
-    app.get('/', (request, response) => {
-      response.send('ok')
-    })
+app.get('/', (request, response) => {
+  response.send('ok')
+})
 
-    app.use(
-      [/^\/api\/?$/],
-      graphQLHTTP({
-        schema: gnomadSchema,
-        graphiql: true,
-        context: {
-          database: {
-            elastic,
-            redis,
-          },
-        },
-        customFormatErrorFn: error => {
-          // graphql-js doesn't distinguish between different error types, so this is the
-          // only way to determine what errors come from query validation (and thus should
-          // be shown to the user)
-          // See https://github.com/graphql/graphql-js/issues/1847
-          const isQueryValidationError =
-            (error.message.startsWith('Syntax Error') &&
-              error.stack.includes('graphql/error/syntaxError')) ||
-            (error.message.startsWith('Cannot query field') &&
-              error.stack.includes('graphql/validation/rules'))
+app.use(
+  [/^\/api\/?$/],
+  graphQLHTTP({
+    schema: gnomadSchema,
+    graphiql: true,
+    context: {
+      database: {
+        elastic,
+        redis,
+      },
+    },
+    customFormatErrorFn: error => {
+      // graphql-js doesn't distinguish between different error types, so this is the
+      // only way to determine what errors come from query validation (and thus should
+      // be shown to the user)
+      // See https://github.com/graphql/graphql-js/issues/1847
+      const isQueryValidationError =
+        (error.message.startsWith('Syntax Error') &&
+          error.stack.includes('graphql/error/syntaxError')) ||
+        (error.message.startsWith('Cannot query field') &&
+          error.stack.includes('graphql/validation/rules'))
 
-          if (isQueryValidationError) {
-            return { message: error.message, locations: error.locations }
-          }
+      if (isQueryValidationError) {
+        return { message: error.message, locations: error.locations }
+      }
 
-          const isUserVisible = error.extensions && error.extensions.isUserVisible
+      const isUserVisible = error.extensions && error.extensions.isUserVisible
 
-          // User visible errors (such as variant not found) are expected to occur during
-          // normal use of the browser and don't need to be logged.
-          if (!isUserVisible) {
-            logger.warn(error)
-          }
+      // User visible errors (such as variant not found) are expected to occur during
+      // normal use of the browser and don't need to be logged.
+      if (!isUserVisible) {
+        logger.warn(error)
+      }
 
-          const message = isUserVisible ? error.message : 'An unknown error occurred'
-          return { message }
-        },
-      })
-    )
+      const message = isUserVisible ? error.message : 'An unknown error occurred'
+      return { message }
+    },
+  })
+)
 
-    if (process.env.READS_DIR) {
-      app.use(['/reads', '/api/reads'], serveStatic(process.env.READS_DIR, { acceptRanges: true }))
-    }
+if (process.env.READS_DIR) {
+  app.use(['/reads', '/api/reads'], serveStatic(process.env.READS_DIR, { acceptRanges: true }))
+}
 
-    app.listen(process.env.GRAPHQL_PORT, () => {
-      logger.info(`Listening on ${process.env.GRAPHQL_PORT}`)
-    })
-  } catch (error) {
-    logger.error(error)
-  }
-})()
+app.listen(process.env.GRAPHQL_PORT, () => {
+  logger.info(`Listening on ${process.env.GRAPHQL_PORT}`)
+})
