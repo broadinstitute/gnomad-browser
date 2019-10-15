99a47a858055d54faddd0f63c194a66a8791d762
Nick Watts <nwatts@broadinstitute.org>
2019-10-15 10:05:05 -0400

    [SCHEMA] Update script for loading variant results
    Add n de novos to AC case

--- projects/exome-results-browsers/browsers/schema/data/prepare_variant_results.py
+++ projects/exome-results-browsers/browsers/schema/data/prepare_variant_results.py
@@ -2,73 +2,64 @@ import argparse
 
 import hail as hl
 
-from hail_scripts.v02.utils.computed_fields import (
-    get_expr_for_alt_allele,
-    get_expr_for_contig,
-    get_expr_for_ref_allele,
-    get_expr_for_variant_id,
-    get_expr_for_xpos,
-)
+from data_utils.computed_fields import variant_id, x_position
 
 
-p = argparse.ArgumentParser()
-p.add_argument("--variant-results-url", required=True)
-p.add_argument("--variant-annotations-url", required=True)
-p.add_argument("--output-url", required=True)
-args = p.parse_args()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--results", required=True)
+    parser.add_argument("--annotations", required=True)
+    parser.add_argument("--output", required=True)
+    args = parser.parse_args()
 
+    hl.init(log="/tmp/hail.log")
 
-hl.init(log="/tmp/hail.log")
-
-variants = hl.read_table(args.variant_annotations_url)
-variants = variants.annotate(
-    variant_id=get_expr_for_variant_id(variants),
-    chrom=get_expr_for_contig(variants.locus),
-    pos=variants.locus.position,
-    xpos=get_expr_for_xpos(variants.locus),
-    alt=get_expr_for_alt_allele(variants),
-    ref=get_expr_for_ref_allele(variants),
-)
-variants = variants.transmute(
-    transcript_id=hl.delimit(variants.transcript_id, ","),
-    hgvsc=hl.delimit(
-        variants.hgvsc.keys().map(lambda k: k + ":" + variants.hgvsc[k]), ","
-    ),
-    hgvsp=hl.delimit(
-        variants.hgvsp.keys().map(lambda k: k + ":" + variants.hgvsp[k]), ","
-    ),
-)
-variants = variants.annotate(flags="PASS")
-variants = variants.drop("v")
-
-results = hl.read_table(args.variant_results_url)
-results = results.annotate(
-    analysis_group=results.analysis_group.lower()
-    .replace("[^a-z0-9]+", "_")
-    .replace("_+$", "")
-)
-results = results.drop("v")
-
-variants = variants.filter(hl.is_defined(results[variants.key]))
-
-analysis_groups = results.aggregate(hl.agg.collect_as_set(results.analysis_group))
-result_fields = [f for f in results.row_value.dtype.fields if f != "analysis_group"]
-
-variants = variants.annotate(groups=hl.struct())
-for group in analysis_groups:
-    group_results = results.filter(results.analysis_group == group).drop(
-        "analysis_group"
-    )
+    variants = hl.read_table(args.annotations)
     variants = variants.annotate(
-        groups=variants.groups.annotate(**{group: group_results[variants.key]})
+        variant_id=variant_id(variants.locus, variants.alleles),
+        chrom=variants.locus.contig,
+        pos=variants.locus.position,
+        xpos=x_position(variants.locus),
+        alt=variants.alleles[1],
+        ref=variants.alleles[0],
     )
 
-# The latest (2019/04/15) SCHEMA dataset moved the source and in_analysis field from variant level to group level
-# in_analysis is the same for all groups within a variant, but source is not
-variants = variants.annotate(
-    in_analysis=variants.groups.meta.in_analysis, source=variants.groups.meta.source
-)
+    variants = variants.transmute(
+        transcript_id=hl.delimit(variants.transcript_id, ","),
+        hgvsc=hl.delimit(variants.hgvsc.keys().map(lambda k: k + ":" + variants.hgvsc[k]), ","),
+        hgvsp=hl.delimit(variants.hgvsp.keys().map(lambda k: k + ":" + variants.hgvsp[k]), ","),
+    )
 
-variants = variants.key_by().drop("locus", "alleles")
+    variants = variants.annotate(flags="PASS")
+    variants = variants.drop("v")
 
-variants.write(args.output_url)
+    results = hl.read_table(args.results)
+    results = results.annotate(
+        analysis_group=results.analysis_group.lower().replace("[^a-z0-9]+", "_").replace("_+$", "")
+    )
+    results = results.drop("v")
+
+    # Add n_denovos to AC_case
+    results = results.annotate(ac_case=results.ac_case + results.n_denovos)
+    results = results.annotate(af_case=hl.cond(results.an_case == 0, 0, results.ac_case / results.an_case))
+
+    variants = variants.filter(hl.is_defined(results[variants.key]))
+
+    analysis_groups = results.aggregate(hl.agg.collect_as_set(results.analysis_group))
+
+    variants = variants.annotate(groups=hl.struct())
+    for group in analysis_groups:
+        group_results = results.filter(results.analysis_group == group).drop("analysis_group")
+        variants = variants.annotate(groups=variants.groups.annotate(**{group: group_results[variants.key]}))
+
+    # The latest (2019/04/15) SCHEMA dataset moved the source and in_analysis field from variant level to group level
+    # in_analysis is the same for all groups within a variant, but source is not
+    variants = variants.annotate(in_analysis=variants.groups.meta.in_analysis, source=variants.groups.meta.source)
+
+    variants = variants.key_by().drop("locus", "alleles")
+
+    variants.write(args.output)
+
+
+if __name__ == "__main__":
+    main()
