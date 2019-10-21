6dd12a5d6fefeb15ae02b28623ff313023e920cb
Nick Watts <nwatts@broadinstitute.org>
2019-10-21 16:53:49 -0400

    Return ExAC variant details in same format as gnomAD
    Relates to #490

--- projects/gnomad/src/client/VariantPage/VariantDetailsQuery.js
+++ projects/gnomad/src/client/VariantPage/VariantDetailsQuery.js
@@ -4,12 +4,11 @@ import React from 'react'
 import { referenceGenomeForDataset } from '../datasets'
 import Query from '../Query'
 
-import exacVariantQuery from './queries/exacVariantQuery'
 import gnomadVariantQuery from './queries/gnomadVariantQuery'
 
 export const VariantDetailsQuery = ({ children, datasetId, variantId }) => (
   <Query
-    query={datasetId === 'exac' ? exacVariantQuery : gnomadVariantQuery}
+    query={gnomadVariantQuery}
     variables={{ datasetId, variantId, referenceGenome: referenceGenomeForDataset(datasetId) }}
   >
     {children}
