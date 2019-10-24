8b2a2edc0bd301f6444031861b56cd07683e1aad
Nick Watts <nwatts@broadinstitute.org>
2019-10-24 15:06:39 -0400

    Fix typo on home page

--- projects/gnomad/src/client/HomePage.js
+++ projects/gnomad/src/client/HomePage.js
@@ -64,8 +64,8 @@ export default () => (
       community.
     </p>
     <p>
-      The v2 data set (GRCh37/h19) provided on this website spans 125,748 exome sequences and 15,708
-      whole-genome sequences from unrelated individuals sequenced as part of various
+      The v2 data set (GRCh37/hg19) provided on this website spans 125,748 exome sequences and
+      15,708 whole-genome sequences from unrelated individuals sequenced as part of various
       disease-specific and population genetic studies. The v3 data set (GRCh38) spans 71,702
       genomes, selected as in v2. The gnomAD Principal Investigators and groups that have
       contributed data to the current release are listed{' '}
