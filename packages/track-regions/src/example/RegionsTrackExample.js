ccd64fee5c5f020d56c49c2a41ed156ef4e50e15
Nick Watts <nwatts@broadinstitute.org>
2019-11-18 14:11:39 -0500

    Remove Redux from track examples

--- packages/track-regions/src/example/RegionsTrackExample.js
+++ packages/track-regions/src/example/RegionsTrackExample.js
@@ -1,45 +1,26 @@
 import React from 'react'
-import { connect, Provider } from 'react-redux'
-import { applyMiddleware, combineReducers, createStore } from 'redux'
-import { createLogger } from 'redux-logger'
-import thunk from 'redux-thunk'
 
-import { createGeneReducer, transcripts } from '@broad/redux-genes'
 import GeneViewer from '@broad/region-viewer/src/example/GeneViewer'
 
 import { RegionsTrack } from '..'
 
-const logger = createLogger()
-
-const store = createStore(
-  combineReducers({
-    genes: createGeneReducer({
-      variantDatasets: {},
-    }),
-  }),
-  applyMiddleware(thunk, logger)
-)
-
-const TranscriptRegionsTrack = connect(state => ({ geneTranscripts: transcripts(state) }))(
-  ({ geneTranscripts, ...rest }) => (
-    <div>
-      {geneTranscripts.map(transcript => (
-        <RegionsTrack
-          key={transcript.transcript_id}
-          {...rest}
-          regions={transcript.exons.filter(e => e.feature_type === 'UTR')}
-        />
-      ))}
-    </div>
-  )
-)
-
 const RegionsTrackExample = () => (
-  <Provider store={store}>
-    <GeneViewer geneName="PCSK9" width={1000}>
-      <TranscriptRegionsTrack height={10} />
-    </GeneViewer>
-  </Provider>
+  <GeneViewer geneSymbol="PCSK9" width={1000}>
+    {gene => {
+      return (
+        <React.Fragment>
+          {gene.transcripts.map(transcript => (
+            <div key={transcript.transcript_id} style={{ marginBottom: '5px' }}>
+              <RegionsTrack
+                height={10}
+                regions={transcript.exons.filter(e => e.feature_type === 'UTR')}
+              />
+            </div>
+          ))}
+        </React.Fragment>
+      )
+    }}
+  </GeneViewer>
 )
 
 export default RegionsTrackExample
