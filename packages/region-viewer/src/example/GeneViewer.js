100ea416d4b553b825acef42cad1cf315876600a
Nick Watts <nwatts@broadinstitute.org>
2019-11-18 14:11:39 -0500

    Remove Redux from track examples

--- packages/region-viewer/src/example/GeneViewer.js
+++ packages/region-viewer/src/example/GeneViewer.js
@@ -1,157 +1,59 @@
-import fetch from 'graphql-fetch'
 import PropTypes from 'prop-types'
-import React, { PureComponent } from 'react'
-import { connect } from 'react-redux'
-
-import { actions as geneActions, geneData } from '@broad/redux-genes'
+import React from 'react'
 
 import { RegionViewer } from '..'
 
-const API_URL = 'http://gnomad-api.broadinstitute.org/'
+import Query from './Query'
 
-const fetchGeneData = geneName => {
-  const query = `{
-    gene(gene_name: "${geneName}") {
+const GENE_QUERY = `
+  query Gene ($geneSymbol: String!) {
+    gene(gene_symbol: $geneSymbol, reference_genome: GRCh37) {
       gene_name
-      canonical_transcript
+      canonical_transcript_id
       strand
-      transcript {
-        exons {
-          feature_type
-          start
-          stop
-          strand
-        }
+      exons {
+        feature_type
+        start
+        stop
       }
       transcripts {
-        start
         transcript_id
-        strand
-        stop
-        xstart
         chrom
-        gene_id
-        xstop
+        strand
+        start
+        stop
         exons {
-          start
-          transcript_id
           feature_type
-          strand
+          start
           stop
-          chrom
-          gene_id
-        }
-        gtex_tissue_tpms_by_transcript {
-          adiposeSubcutaneous
-          adiposeVisceralOmentum
-          adrenalGland
-          arteryAorta
-          arteryCoronary
-          arteryTibial
-          bladder
-          brainAmygdala
-          brainAnteriorcingulatecortexBa24
-          brainCaudateBasalganglia
-          brainCerebellarhemisphere
-          brainCerebellum
-          brainCortex
-          brainFrontalcortexBa9
-          brainHippocampus
-          brainHypothalamus
-          brainNucleusaccumbensBasalganglia
-          brainPutamenBasalganglia
-          brainSpinalcordCervicalc1
-          brainSubstantianigra
-          breastMammarytissue
-          cellsEbvTransformedlymphocytes
-          cellsTransformedfibroblasts
-          cervixEctocervix
-          cervixEndocervix
-          colonSigmoid
-          colonTransverse
-          esophagusGastroesophagealjunction
-          esophagusMucosa
-          esophagusMuscularis
-          fallopianTube
-          heartAtrialappendage
-          heartLeftventricle
-          kidneyCortex
-          liver
-          lung
-          minorSalivaryGland
-          muscleSkeletal
-          nerveTibial
-          ovary
-          pancreas
-          pituitary
-          prostate
-          skinNotsunexposedSuprapubic
-          skinSunexposedLowerleg
-          smallIntestineTerminalileum
-          spleen
-          stomach
-          testis
-          thyroid
-          uterus
-          vagina
-          wholeBlood
         }
       }
     }
-  }`
+  }
+`
 
-  return fetch(API_URL)(query)
+const GeneViewer = ({ children, geneSymbol, ...rest }) => (
+  <Query query={GENE_QUERY} variables={{ geneSymbol }}>
+    {({ data, error, loading }) => {
+      if (loading) {
+        return <p>Loading gene data</p>
+      }
+      if (error || !data) {
+        return <p>Failed to load gene data</p>
+      }
+
+      return (
+        <RegionViewer {...rest} padding={75} regions={data.gene.exons}>
+          {children(data.gene)}
+        </RegionViewer>
+      )
+    }}
+  </Query>
+)
+
+GeneViewer.propTypes = {
+  children: PropTypes.func.isRequired,
+  geneSymbol: PropTypes.string.isRequired,
 }
 
-class GeneViewer extends PureComponent {
-  static propTypes = {
-    geneName: PropTypes.string.isRequired,
-    loadGene: PropTypes.func.isRequired,
-  }
-
-  componentDidMount() {
-    const { loadGene } = this.props
-    loadGene()
-  }
-
-  componentDidUpdate(prevProps) {
-    const { geneName, loadGene } = this.props
-    if (geneName !== prevProps.geneName) {
-      loadGene()
-    }
-  }
-
-  render() {
-    const { children, gene, ...rest } = this.props
-
-    if (!gene) {
-      return null
-    }
-
-    const canonicalTranscriptExons = gene.toJS().transcript.exons
-
-    return (
-      <RegionViewer {...rest} padding={75} regions={canonicalTranscriptExons}>
-        {children}
-      </RegionViewer>
-    )
-  }
-}
-
-export default connect(
-  state => ({
-    gene: geneData(state),
-  }),
-  (dispatch, ownProps) => ({
-    loadGene() {
-      return dispatch(thunkDispatch => {
-        thunkDispatch(geneActions.setCurrentGene(ownProps.geneName))
-        thunkDispatch(geneActions.requestGeneData(ownProps.geneName))
-        return fetchGeneData(ownProps.geneName).then(response => {
-          thunkDispatch(geneActions.receiveGeneData(ownProps.geneName, response.data.gene))
-          return response
-        })
-      })
-    },
-  })
-)(GeneViewer)
+export default GeneViewer
--- packages/region-viewer/src/example/RegionViewerExample.js
+++ packages/region-viewer/src/example/RegionViewerExample.js
@@ -1,41 +1,25 @@
 import React from 'react'
-import { Provider } from 'react-redux'
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import { createLogger } from 'redux-logger'
-import thunk from 'redux-thunk'
-import styled from 'styled-components'
-
-import { createGeneReducer } from '@broad/redux-genes'
-import PositionTableTrack from '@broad/track-position-table'
-import { TranscriptTrackConnected } from '@broad/track-transcript'
+import TranscriptsTrack from '@broad/track-transcripts'
 
 import GeneViewer from './GeneViewer'
 
-const logger = createLogger()
-
-const Wrapper = styled.div`
-  width: 1000px;
-  margin: 50px auto;
-`
-
-const store = createStore(
-  combineReducers({
-    genes: createGeneReducer({
-      variantDatasets: {},
-    }),
-  }),
-  applyMiddleware(thunk, logger)
+const RegionViewerExample = () => (
+  <GeneViewer geneSymbol="PCSK9" width={1000}>
+    {gene => {
+      return (
+        <TranscriptsTrack
+          activeTranscript={{
+            exons: gene.exons,
+            strand: gene.strand,
+          }}
+          canonicalTranscript={gene.canonical_transcript_id}
+          transcripts={gene.transcripts}
+          showNonCodingTranscripts={false}
+          showUTRs={false}
+        />
+      )
+    }}
+  </GeneViewer>
 )
 
-const ExampleApp = () => (
-  <Provider store={store}>
-    <Wrapper>
-      <GeneViewer geneName="PCSK9" width={1000}>
-        <TranscriptTrackConnected height={12} />
-        <PositionTableTrack />
-      </GeneViewer>
-    </Wrapper>
-  </Provider>
-)
-
-export default ExampleApp
+export default RegionViewerExample
