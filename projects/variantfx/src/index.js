23331bdb698e5c522fb6cd8fe22e4d6b4d278e8b
Nick Watts <nwatts@broadinstitute.org>
2019-11-18 10:19:17 -0500

    Remove VariantFX project code

--- projects/variantfx/src/index.js
+++
@@ -1,9 +0,0 @@
-import 'core-js/stable'
-import React from 'react'
-import { render } from 'react-dom'
-
-import Main from './Main'
-
-const mount = document.getElementById('root')
-
-render(<Main />, mount)
