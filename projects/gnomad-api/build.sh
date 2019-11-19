97e139ec5d4a4d1f214340111dc9b02951423cb8
Nick Watts <nwatts@broadinstitute.org>
2019-11-19 17:13:16 -0500

    Do not modify path in start/build scripts

--- projects/gnomad-api/build.sh
+++ projects/gnomad-api/build.sh
@@ -5,10 +5,8 @@ set -eu
 PROJECT_DIR=$(dirname "${BASH_SOURCE}")
 cd $PROJECT_DIR
 
-export PATH=$PATH:$PROJECT_DIR/../../node_modules/.bin
-
 export NODE_ENV="production"
 
 rm -rf dist
 
-webpack
+yarn run webpack
