59dd02dc5324e27486623edbfcd22eccedcc1a36
Nick Watts <nwatts@broadinstitute.org>
2020-01-09 10:14:47 -0500

    Update ES query for GTEx tissue expression

--- projects/gnomad-api/src/schema/types/gtex.js
+++ projects/gnomad-api/src/schema/types/gtex.js
@@ -1,82 +1,95 @@
-import { GraphQLFloat, GraphQLObjectType, GraphQLString } from 'graphql'
+import { GraphQLFloat, GraphQLNonNull, GraphQLObjectType } from 'graphql'
 
-export const GtexTissueExpressionsType = new GraphQLObjectType({
-  name: 'GtexTissueExpressions',
+import { UserVisibleError } from '../errors'
+
+export const GtexTissueExpressionType = new GraphQLObjectType({
+  name: 'GtexTissueExpression',
   fields: {
-    adiposeSubcutaneous: { type: GraphQLFloat },
-    adiposeVisceralOmentum: { type: GraphQLFloat },
-    adrenalGland: { type: GraphQLFloat },
-    arteryAorta: { type: GraphQLFloat },
-    arteryCoronary: { type: GraphQLFloat },
-    arteryTibial: { type: GraphQLFloat },
-    bladder: { type: GraphQLFloat },
-    brainAmygdala: { type: GraphQLFloat },
-    brainAnteriorcingulatecortexBa24: { type: GraphQLFloat },
-    brainCaudateBasalganglia: { type: GraphQLFloat },
-    brainCerebellarhemisphere: { type: GraphQLFloat },
-    brainCerebellum: { type: GraphQLFloat },
-    brainCortex: { type: GraphQLFloat },
-    brainFrontalcortexBa9: { type: GraphQLFloat },
-    brainHippocampus: { type: GraphQLFloat },
-    brainHypothalamus: { type: GraphQLFloat },
-    brainNucleusaccumbensBasalganglia: { type: GraphQLFloat },
-    brainPutamenBasalganglia: { type: GraphQLFloat },
-    brainSpinalcordCervicalc1: { type: GraphQLFloat },
-    brainSubstantianigra: { type: GraphQLFloat },
-    breastMammarytissue: { type: GraphQLFloat },
-    cellsEbvTransformedlymphocytes: { type: GraphQLFloat },
-    cellsTransformedfibroblasts: { type: GraphQLFloat },
-    cervixEctocervix: { type: GraphQLFloat },
-    cervixEndocervix: { type: GraphQLFloat },
-    colonSigmoid: { type: GraphQLFloat },
-    colonTransverse: { type: GraphQLFloat },
-    esophagusGastroesophagealjunction: { type: GraphQLFloat },
-    esophagusMucosa: { type: GraphQLFloat },
-    esophagusMuscularis: { type: GraphQLFloat },
-    fallopianTube: { type: GraphQLFloat },
-    heartAtrialappendage: { type: GraphQLFloat },
-    heartLeftventricle: { type: GraphQLFloat },
-    kidneyCortex: { type: GraphQLFloat },
-    liver: { type: GraphQLFloat },
-    lung: { type: GraphQLFloat },
-    minorSalivaryGland: { type: GraphQLFloat },
-    muscleSkeletal: { type: GraphQLFloat },
-    nerveTibial: { type: GraphQLFloat },
-    ovary: { type: GraphQLFloat },
-    pancreas: { type: GraphQLFloat },
-    pituitary: { type: GraphQLFloat },
-    prostate: { type: GraphQLFloat },
-    skinNotsunexposedSuprapubic: { type: GraphQLFloat },
-    skinSunexposedLowerleg: { type: GraphQLFloat },
-    smallIntestineTerminalileum: { type: GraphQLFloat },
-    spleen: { type: GraphQLFloat },
-    stomach: { type: GraphQLFloat },
-    testis: { type: GraphQLFloat },
-    thyroid: { type: GraphQLFloat },
-    uterus: { type: GraphQLFloat },
-    vagina: { type: GraphQLFloat },
-    wholeBlood: { type: GraphQLFloat },
-    transcriptId: { type: GraphQLString },
-    geneId: { type: GraphQLString },
+    adipose_subcutaneous: { type: new GraphQLNonNull(GraphQLFloat) },
+    adipose_visceral_omentum: { type: new GraphQLNonNull(GraphQLFloat) },
+    adrenal_gland: { type: new GraphQLNonNull(GraphQLFloat) },
+    artery_aorta: { type: new GraphQLNonNull(GraphQLFloat) },
+    artery_coronary: { type: new GraphQLNonNull(GraphQLFloat) },
+    artery_tibial: { type: new GraphQLNonNull(GraphQLFloat) },
+    bladder: { type: new GraphQLNonNull(GraphQLFloat) },
+    brain_amygdala: { type: new GraphQLNonNull(GraphQLFloat) },
+    brain_anterior_cingulate_cortex_ba24: { type: new GraphQLNonNull(GraphQLFloat) },
+    brain_caudate_basal_ganglia: { type: new GraphQLNonNull(GraphQLFloat) },
+    brain_cerebellar_hemisphere: { type: new GraphQLNonNull(GraphQLFloat) },
+    brain_cerebellum: { type: new GraphQLNonNull(GraphQLFloat) },
+    brain_cortex: { type: new GraphQLNonNull(GraphQLFloat) },
+    brain_frontal_cortex_ba9: { type: new GraphQLNonNull(GraphQLFloat) },
+    brain_hippocampus: { type: new GraphQLNonNull(GraphQLFloat) },
+    brain_hypothalamus: { type: new GraphQLNonNull(GraphQLFloat) },
+    brain_nucleus_accumbens_basal_ganglia: { type: new GraphQLNonNull(GraphQLFloat) },
+    brain_putamen_basal_ganglia: { type: new GraphQLNonNull(GraphQLFloat) },
+    brain_spinal_cord_cervical_c_1: { type: new GraphQLNonNull(GraphQLFloat) },
+    brain_substantia_nigra: { type: new GraphQLNonNull(GraphQLFloat) },
+    breast_mammary_tissue: { type: new GraphQLNonNull(GraphQLFloat) },
+    cells_ebv_transformed_lymphocytes: { type: new GraphQLNonNull(GraphQLFloat) },
+    cells_transformed_fibroblasts: { type: new GraphQLNonNull(GraphQLFloat) },
+    cervix_ectocervix: { type: new GraphQLNonNull(GraphQLFloat) },
+    cervix_endocervix: { type: new GraphQLNonNull(GraphQLFloat) },
+    colon_sigmoid: { type: new GraphQLNonNull(GraphQLFloat) },
+    colon_transverse: { type: new GraphQLNonNull(GraphQLFloat) },
+    esophagus_gastroesophageal_junction: { type: new GraphQLNonNull(GraphQLFloat) },
+    esophagus_mucosa: { type: new GraphQLNonNull(GraphQLFloat) },
+    esophagus_muscularis: { type: new GraphQLNonNull(GraphQLFloat) },
+    fallopian_tube: { type: new GraphQLNonNull(GraphQLFloat) },
+    heart_atrial_appendage: { type: new GraphQLNonNull(GraphQLFloat) },
+    heart_left_ventricle: { type: new GraphQLNonNull(GraphQLFloat) },
+    kidney_cortex: { type: new GraphQLNonNull(GraphQLFloat) },
+    liver: { type: new GraphQLNonNull(GraphQLFloat) },
+    lung: { type: new GraphQLNonNull(GraphQLFloat) },
+    minor_salivary_gland: { type: new GraphQLNonNull(GraphQLFloat) },
+    muscle_skeletal: { type: new GraphQLNonNull(GraphQLFloat) },
+    nerve_tibial: { type: new GraphQLNonNull(GraphQLFloat) },
+    ovary: { type: new GraphQLNonNull(GraphQLFloat) },
+    pancreas: { type: new GraphQLNonNull(GraphQLFloat) },
+    pituitary: { type: new GraphQLNonNull(GraphQLFloat) },
+    prostate: { type: new GraphQLNonNull(GraphQLFloat) },
+    skin_not_sun_exposed_suprapubic: { type: new GraphQLNonNull(GraphQLFloat) },
+    skin_sun_exposed_lower_leg: { type: new GraphQLNonNull(GraphQLFloat) },
+    small_intestine_terminal_ileum: { type: new GraphQLNonNull(GraphQLFloat) },
+    spleen: { type: new GraphQLNonNull(GraphQLFloat) },
+    stomach: { type: new GraphQLNonNull(GraphQLFloat) },
+    testis: { type: new GraphQLNonNull(GraphQLFloat) },
+    thyroid: { type: new GraphQLNonNull(GraphQLFloat) },
+    uterus: { type: new GraphQLNonNull(GraphQLFloat) },
+    vagina: { type: new GraphQLNonNull(GraphQLFloat) },
+    whole_blood: { type: new GraphQLNonNull(GraphQLFloat) },
   },
 })
 
-export const fetchGtexTissueExpressionsByTranscript = async (ctx, transcriptId) => {
-  const response = await ctx.database.elastic.search({
-    index: 'gtex_tissue_tpms_by_transcript',
-    type: 'tissue_tpms',
-    size: 1,
-    body: {
-      query: {
-        bool: {
-          filter: {
-            term: { transcriptId },
-          },
-        },
-      },
-    },
-  })
+const fetchTissueExpressionDocument = async (ctx, index, transcriptId) => {
+  try {
+    const response = await ctx.database.elastic.get({
+      index,
+      type: 'documents',
+      id: transcriptId,
+    })
 
-  const doc = response.hits.hits[0]
-  return doc ? doc._source : null // eslint-disable-line no-underscore-dangle
+    return response._source
+  } catch (err) {
+    if (err.message === 'Not Found') {
+      throw new UserVisibleError('Variant not found')
+    }
+    throw err
+  }
+}
+
+export const fetchGtexTissueExpressionByTranscript = async (ctx, transcript) => {
+  if (transcript.reference_genome !== 'GRCh37') {
+    throw new UserVisibleError(
+      `Tissue expression is not available for reference genome ${transcript.reference_genome}`
+    )
+  }
+
+  const doc = await fetchTissueExpressionDocument(
+    ctx,
+    'gtex_v7_tissue_expression',
+    transcript.transcript_id
+  )
+
+  return doc.tissues
 }
--- projects/gnomad/src/client/TranscriptPage/TranscriptPageContainer.js
+++ projects/gnomad/src/client/TranscriptPage/TranscriptPageContainer.js
@@ -81,60 +81,60 @@ query Transcript($transcriptId: String!, $referenceGenome: ReferenceGenomeId!) {
           start
           stop
         }
-        gtex_tissue_expression: gtex_tissue_tpms_by_transcript {
-          adipose_subcutaneous: adiposeSubcutaneous
-          adipose_visceral_omentum: adiposeVisceralOmentum
-          adrenal_gland: adrenalGland
-          artery_aorta: arteryAorta
-          artery_coronary: arteryCoronary
-          artery_tibial: arteryTibial
-          bladder: bladder
-          brain_amygdala: brainAmygdala
-          brain_anterior_cingulate_cortex_ba24: brainAnteriorcingulatecortexBa24
-          brain_caudate_basal_ganglia: brainCaudateBasalganglia
-          brain_cerebellar_hemisphere: brainCerebellarhemisphere
-          brain_cerebellum: brainCerebellum
-          brain_cortex: brainCortex
-          brain_frontal_cortex_ba9: brainFrontalcortexBa9
-          brain_hippocampus: brainHippocampus
-          brain_hypothalamus: brainHypothalamus
-          brain_nucleus_accumbens_basal_ganglia: brainNucleusaccumbensBasalganglia
-          brain_putamen_basal_ganglia: brainPutamenBasalganglia
-          brain_spinal_cord_cervical_c_1: brainSpinalcordCervicalc1
-          brain_substantia_nigra: brainSubstantianigra
-          breast_mammary_tissue: breastMammarytissue
-          cells_ebv_transformed_lymphocytes: cellsEbvTransformedlymphocytes
-          cells_transformed_fibroblasts: cellsTransformedfibroblasts
-          cervix_ectocervix: cervixEctocervix
-          cervix_endocervix: cervixEndocervix
-          colon_sigmoid: colonSigmoid
-          colon_transverse: colonTransverse
-          esophagus_gastroesophageal_junction: esophagusGastroesophagealjunction
-          esophagus_mucosa: esophagusMucosa
-          esophagus_muscularis: esophagusMuscularis
-          fallopian_tube: fallopianTube
-          heart_atrial_appendage: heartAtrialappendage
-          heart_left_ventricle: heartLeftventricle
-          kidney_cortex: kidneyCortex
-          liver: liver
-          lung: lung
-          minor_salivary_gland: minorSalivaryGland
-          muscle_skeletal: muscleSkeletal
-          nerve_tibial: nerveTibial
-          ovary: ovary
-          pancreas: pancreas
-          pituitary: pituitary
-          prostate: prostate
-          skin_not_sun_exposed_suprapubic: skinNotsunexposedSuprapubic
-          skin_sun_exposed_lower_leg: skinSunexposedLowerleg
-          small_intestine_terminal_ileum: smallIntestineTerminalileum
-          spleen: spleen
-          stomach: stomach
-          testis: testis
-          thyroid: thyroid
-          uterus: uterus
-          vagina: vagina
-          whole_blood: wholeBlood
+        gtex_tissue_expression {
+          adipose_subcutaneous
+          adipose_visceral_omentum
+          adrenal_gland
+          artery_aorta
+          artery_coronary
+          artery_tibial
+          bladder
+          brain_amygdala
+          brain_anterior_cingulate_cortex_ba24
+          brain_caudate_basal_ganglia
+          brain_cerebellar_hemisphere
+          brain_cerebellum
+          brain_cortex
+          brain_frontal_cortex_ba9
+          brain_hippocampus
+          brain_hypothalamus
+          brain_nucleus_accumbens_basal_ganglia
+          brain_putamen_basal_ganglia
+          brain_spinal_cord_cervical_c_1
+          brain_substantia_nigra
+          breast_mammary_tissue
+          cells_ebv_transformed_lymphocytes
+          cells_transformed_fibroblasts
+          cervix_ectocervix
+          cervix_endocervix
+          colon_sigmoid
+          colon_transverse
+          esophagus_gastroesophageal_junction
+          esophagus_mucosa
+          esophagus_muscularis
+          fallopian_tube
+          heart_atrial_appendage
+          heart_left_ventricle
+          kidney_cortex
+          liver
+          lung
+          minor_salivary_gland
+          muscle_skeletal
+          nerve_tibial
+          ovary
+          pancreas
+          pituitary
+          prostate
+          skin_not_sun_exposed_suprapubic
+          skin_sun_exposed_lower_leg
+          small_intestine_terminal_ileum
+          spleen
+          stomach
+          testis
+          thyroid
+          uterus
+          vagina
+          whole_blood
         }
       }
       pext {
