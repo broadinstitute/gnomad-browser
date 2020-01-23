3abd4315527fceaaa01002587a7f69875fe680dc
Nick Watts <nwatts@broadinstitute.org>
2020-01-23 15:39:33 -0500

    Generalize argument for configuring ES Hadoop connector

--- data/export_hail_table_to_elasticsearch.py
+++ data/export_hail_table_to_elasticsearch.py
@@ -10,9 +10,16 @@ def main():
     parser.add_argument("table_url", help="URL of Hail table to export")
     parser.add_argument("host", help="Elasticsearch host or IP")
     parser.add_argument("index_name", help="Elasticsearch index name")
-    parser.add_argument("--es-nodes-wan", help="Elasticsearch configuration: if true, connector skips network discovery", default=False, type=bool) 
     parser.add_argument("--block-size", help="Elasticsearch block size to use when exporting", default=200, type=int)
     parser.add_argument("--disable-fields", help="Disable a field in Elasticsearch", action="append", default=[])
+    parser.add_argument(
+        "--es-config",
+        help="Configuration for Elasticsearch Hadoop connector",
+        action="append",
+        default=[],
+        nargs=2,
+        metavar=("property", "value"),
+    )
     parser.add_argument("--id-field", help="Field to use as Elasticsearch document ID", default=None)
     parser.add_argument("--num-shards", help="Number of elasticsearch shards", default=1, type=int)
     parser.add_argument("--port", help="Elasticsearch port", default=9200, type=int)
@@ -37,7 +44,7 @@ def main():
         num_shards=args.num_shards,
         port=args.port,
         verbose=True,
-        es_config={'es.nodes.wan.only': 'true' if args.es_nodes_wan else 'false' }
+        es_config=dict(args.es_config),
     )
 
 
