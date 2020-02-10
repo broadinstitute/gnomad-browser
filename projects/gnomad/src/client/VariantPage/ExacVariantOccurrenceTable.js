dc631430de6e0df3818b91220914f4c3f435965e
Nick Watts <nwatts@broadinstitute.org>
2020-02-10 12:08:16 -0500

    Add asterisk in table for variants with low allele number
    Resolves #551

--- projects/gnomad/src/client/VariantPage/ExacVariantOccurrenceTable.js
+++ projects/gnomad/src/client/VariantPage/ExacVariantOccurrenceTable.js
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 
 import { Badge } from '@broad/ui'
 
+import sampleCounts from '../dataset-constants/sampleCounts'
 import QCFilter from '../QCFilter'
 
 const Table = styled.table`
@@ -29,54 +30,69 @@ const Table = styled.table`
   }
 `
 
-const ExacVariantOccurrenceTable = ({ variant }) => (
-  <div>
-    <Table>
-      <tbody>
-        <tr>
-          <td />
-          <th scope="col">Exomes</th>
-        </tr>
-        <tr>
-          <th scope="row">Filter</th>
-          <td>
-            {variant.exome.filters.length === 0 ? (
-              <Badge level="success">Pass</Badge>
-            ) : (
-              variant.exome.filters.map(filter => <QCFilter key={filter} filter={filter} />)
-            )}
-          </td>
-        </tr>
-        <tr>
-          <th scope="row">Allele Count</th>
-          <td>{variant.exome.ac}</td>
-        </tr>
-        <tr>
-          <th scope="row">Allele Number</th>
-          <td>{variant.exome.an}</td>
-        </tr>
-        <tr>
-          <th scope="row">Allele Frequency</th>
-          <td>
-            {variant.exome.an === 0 ? 0 : (variant.exome.ac / variant.exome.an).toPrecision(4)}
-          </td>
-        </tr>
-        {variant.chrom !== 'Y' && (
+const ExacVariantOccurrenceTable = ({ variant }) => {
+  // Display a warning if a variant's AN is < 50% of the max AN.
+  // Max AN is 2 * sample count, so 50% max AN is equal to sample count.
+  const hasLowAlleleNumber = variant.exome.an < sampleCounts.exac.total
+
+  return (
+    <div>
+      <Table>
+        <tbody>
           <tr>
-            <th scope="row">Number of homozygotes</th>
-            <td>{variant.exome.ac_hom}</td>
+            <td />
+            <th scope="col">Exomes</th>
           </tr>
-        )}
-        {(variant.chrom === 'X' || variant.chrom === 'Y') && (
           <tr>
-            <th scope="row">Number of hemizygotes</th>
-            <td>{variant.exome.ac_hemi}</td>
+            <th scope="row">Filter</th>
+            <td>
+              {variant.exome.filters.length === 0 ? (
+                <Badge level="success">Pass</Badge>
+              ) : (
+                variant.exome.filters.map(filter => <QCFilter key={filter} filter={filter} />)
+              )}
+            </td>
           </tr>
-        )}
-      </tbody>
-    </Table>
-  </div>
-)
+          <tr>
+            <th scope="row">Allele Count</th>
+            <td>{variant.exome.ac}</td>
+          </tr>
+          <tr>
+            <th scope="row">Allele Number</th>
+            <td>
+              {variant.exome.an}
+              {hasLowAlleleNumber && ' *'}
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">Allele Frequency</th>
+            <td>
+              {variant.exome.an === 0 ? 0 : (variant.exome.ac / variant.exome.an).toPrecision(4)}
+            </td>
+          </tr>
+          {variant.chrom !== 'Y' && (
+            <tr>
+              <th scope="row">Number of homozygotes</th>
+              <td>{variant.exome.ac_hom}</td>
+            </tr>
+          )}
+          {(variant.chrom === 'X' || variant.chrom === 'Y') && (
+            <tr>
+              <th scope="row">Number of hemizygotes</th>
+              <td>{variant.exome.ac_hemi}</td>
+            </tr>
+          )}
+        </tbody>
+      </Table>
+      {hasLowAlleleNumber && (
+        <p>
+          <Badge level="warning">Warning</Badge> This variant is covered in fewer than 50% of
+          individuals in ExAC. Allele frequency estimates may not be reliable.
+        </p>
+      )}
+    </div>
+  )
+}
 
 ExacVariantOccurrenceTable.propTypes = {
   variant: PropTypes.shape({
@@ -87,7 +103,7 @@ ExacVariantOccurrenceTable.propTypes = {
       an: PropTypes.number.isRequired,
       ac_hom: PropTypes.number.isRequired,
       ac_hemi: PropTypes.number,
-    }),
+    }).isRequired,
   }).isRequired,
 }
 
