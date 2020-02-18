a1e68ffbee0072f4f4688e9d5ea5c41213900aea
Nick Watts <nwatts@broadinstitute.org>
2020-02-18 13:08:08 -0500

    Fix hook on downloads page

--- projects/gnomad/src/client/downloads/DownloadsPage.js
+++ projects/gnomad/src/client/downloads/DownloadsPage.js
@@ -24,7 +24,7 @@ const DownloadsPage = ({ location }) => {
     } else if (location.hash.startsWith('#exac-')) {
       setActiveTab('exac')
     }
-  }, location.hash)
+  }, [location.hash])
 
   return (
     <InfoPage>
