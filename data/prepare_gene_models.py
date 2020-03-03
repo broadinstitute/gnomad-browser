8af1e63baff7b22f163627b033dc68b8041ac1c9
Nick Watts <nwatts@broadinstitute.org>
2020-03-03 14:35:07 -0500

    Remove position offsets in gene models
    Resolves #528

--- data/prepare_gene_models.py
+++ data/prepare_gene_models.py
@@ -33,8 +33,8 @@ def get_exons(gencode):
         gene_id=exons.gene_id.split("\\.")[0],
         chrom=exons.interval.start.seqname[3:],
         strand=exons.strand,
-        start=exons.interval.start.position + 1,
-        stop=exons.interval.end.position + 1,
+        start=exons.interval.start.position,
+        stop=exons.interval.end.position,
     )
 
     return exons
@@ -55,8 +55,8 @@ def get_genes(gencode):
         gene_symbol=genes.gene_name,
         chrom=genes.interval.start.seqname[3:],
         strand=genes.strand,
-        start=genes.interval.start.position + 1,
-        stop=genes.interval.end.position + 1,
+        start=genes.interval.start.position,
+        stop=genes.interval.end.position,
     )
 
     genes = genes.annotate(xstart=xpos(genes.chrom, genes.start), xstop=xpos(genes.chrom, genes.stop))
@@ -115,8 +115,8 @@ def get_transcripts(gencode):
         gene_id=transcripts.gene_id.split("\\.")[0],
         chrom=transcripts.interval.start.seqname[3:],
         strand=transcripts.strand,
-        start=transcripts.interval.start.position + 1,
-        stop=transcripts.interval.end.position + 1,
+        start=transcripts.interval.start.position,
+        stop=transcripts.interval.end.position,
     )
 
     transcripts = transcripts.annotate(
