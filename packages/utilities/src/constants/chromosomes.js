36736894eb4b9872577e1182f2536a350a7e5a58
Nick Watts <nwatts@broadinstitute.org>
2020-03-10 16:32:20 -0400

    [utilities] Remove chromosomes list

--- packages/utilities/src/constants/chromosomes.js
+++
@@ -1,5 +0,0 @@
-export const HUMAN_AUTOSOMES = Array.from(new Array(22), (x, i) => `${i + 1}`)
-
-export const HUMAN_CHROMOSOMES = [...HUMAN_AUTOSOMES, 'X', 'Y']
-
-export const HUMAN_CHROMOSOMES_WITH_MITO = [...HUMAN_CHROMOSOMES, 'M']
--- packages/utilities/src/index.js
+++ packages/utilities/src/index.js
@@ -4,10 +4,4 @@ export {
   registerConsequences,
 } from './consequences'
 
-export {
-  HUMAN_AUTOSOMES,
-  HUMAN_CHROMOSOMES,
-  HUMAN_CHROMOSOMES_WITH_MITO,
-} from './constants/chromosomes'
-
 export { isRegionId, normalizeRegionId, isVariantId, normalizeVariantId } from './identifiers'
