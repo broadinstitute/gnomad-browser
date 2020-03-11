5f3a711bcd4acf2fc59e70ecdd667071de16b513
Nick Watts <nwatts@broadinstitute.org>
2020-03-11 12:03:08 -0400

    [track-variant] Set variant fill color using prop

--- projects/gnomad/src/client/VariantList/VariantTrack.js
+++ projects/gnomad/src/client/VariantList/VariantTrack.js
@@ -1,10 +1,24 @@
+import { transparentize } from 'polished'
 import React, { PureComponent } from 'react'
 
 import BaseVariantTrack from '@gnomad/track-variant'
+import { getCategoryFromConsequence } from '@gnomad/utilities'
+
+const consequenceCategoryColors = {
+  lof: transparentize(0.3, '#FF583F'),
+  missense: transparentize(0.3, '#F0C94D'),
+  synonymous: transparentize(0.3, 'green'),
+  other: transparentize(0.3, '#757575'),
+}
+
+const variantColor = variant => {
+  const category = getCategoryFromConsequence(variant.consequence) || 'other'
+  return consequenceCategoryColors[category]
+}
 
 class VariantTrack extends PureComponent {
   render() {
-    return <BaseVariantTrack {...this.props} />
+    return <BaseVariantTrack variantColor={variantColor} {...this.props} />
   }
 }
 
