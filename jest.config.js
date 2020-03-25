d276855ecda47a8b4453ba269e69ae392e85c1eb
Nick Watts <nwatts@broadinstitute.org>
2020-03-25 15:59:00 -0400

    Add React Testing Library

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,28 @@
 // https://jestjs.io/docs/en/configuration.html
 
+const fs = require('fs')
+const path = require('path')
+
+const packages = fs
+  .readdirSync(path.join(__dirname, 'packages'), { withFileTypes: true })
+  .filter(dirent => dirent.isDirectory())
+  .map(dirent => dirent.name)
+
 module.exports = {
-  projects: ['<rootDir>/packages/*', '<rootDir>/projects/gnomad', '<rootDir>/projects/gnomad-api'],
+  projects: [
+    ...packages.map(pkg => ({
+      displayName: pkg,
+      testMatch: [`<rootDir>/packages/${pkg}/**/*.spec.js`],
+      setupFilesAfterEnv: ['@testing-library/jest-dom'],
+    })),
+    {
+      displayName: 'gnomad',
+      testMatch: ['<rootDir>/projects/gnomad/**/*.spec.js'],
+      setupFilesAfterEnv: ['@testing-library/jest-dom'],
+    },
+    {
+      displayName: 'gnomad-api',
+      testMatch: ['<rootDir>/projects/gnomad-api/**/*.spec.js'],
+    },
+  ],
 }
