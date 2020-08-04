6f00cae4777f0569e9633332e776c76e34d319de
Nick Watts <nwatts@broadinstitute.org>
2020-08-04 16:20:10 -0400

    Remove 'gcloud docker' from deploy scripts

--- projects/gnomad-api/deploy/deploy-image.sh
+++ projects/gnomad-api/deploy/deploy-image.sh
@@ -17,8 +17,8 @@ source "../../cluster/config.sh"
 IMAGE_NAME="gcr.io/${GCLOUD_PROJECT}/gnomad-api"
 
 # Push to container registry
-gcloud docker -- push ${IMAGE_NAME}:${DEPLOY_TAG}
-gcloud docker -- push ${IMAGE_NAME}:latest
+docker push ${IMAGE_NAME}:${DEPLOY_TAG}
+docker push ${IMAGE_NAME}:latest
 
 # Update API deployment
 kubectl set image deployment/gnomad-api app=${IMAGE_NAME}:${DEPLOY_TAG}
