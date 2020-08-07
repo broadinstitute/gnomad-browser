0364b65dd3352e90915cbb95182ef30311199661
Nick Watts <nwatts@broadinstitute.org>
2020-08-07 14:59:25 -0400

    Add FAQ entry for contributing data

--- projects/gnomad/src/client/FAQPage.js
+++ projects/gnomad/src/client/FAQPage.js
@@ -139,6 +139,36 @@ export default () => (
         </p>
       </Answer>
 
+      <Question id="can-i-contribute-data-to-gnomad">Can I contribute data to gnomAD?</Question>
+      <Answer>
+        <p>
+          Please <ExternalLink href="mailto:exomeconsortium@gmail.com">contact us</ExternalLink> if
+          you are a study investigator and are interested in contributing exome/genome sequencing
+          data to the gnomAD resource. We are particularly interested in increasing the diversity
+          included in gnomAD to improve the resource. However, please be aware that there are a
+          number of challenges associated with incorporating external datasets into gnomAD. To
+          produce the high quality, unified gnomAD dataset, we are unable to directly add externally
+          generated VCFs to the gnomAD dataset and need to process all CRAM/BAM files in a uniform
+          manner at the Broad Institute. This requires data reprocessing, including regulatory
+          review, transfer of raw data to the Broad Institute, remapping of raw data, and generation
+          of a VCF. We have limited funding for data reprocessing for including externally sequenced
+          samples in gnomAD and therefore we prioritize the most valuable datasets for reprocessing.
+        </p>
+
+        <p>
+          Please also note that given the importance of gnomAD to the clinical genetics community to
+          diagnose patients with monogenic disease, we do not include individuals recruited for
+          severe pediatric disease (e.g., cystic fibrosis and autism) as well as their first-degree
+          relatives, allowing the database to largely represent the general population.
+        </p>
+
+        <p>
+          All investigators who contribute data to gnomAD join the gnomAD Consortium. See our{' '}
+          <ExternalLink href="/mou">Memorandum of Understanding</ExternalLink> for more details on
+          Consortium membership.
+        </p>
+      </Answer>
+
       <Question id="why-is-a-particular-variant-found-in-some-versions-of-exac-gnomad-but-not-others">
         Why is a particular variant found in some versions of ExAC/gnomAD but not others?
       </Question>
