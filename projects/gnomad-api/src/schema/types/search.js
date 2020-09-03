ee55c6ade79065c94060867ec28c0f89a671a224
Nick Watts <nwatts@broadinstitute.org>
2020-09-03 17:28:06 -0400

    Update variant and region search
    * Accept additional variant ID formats
    * Return multiple options for a position
    * Use same logic in API and browser

--- projects/gnomad-api/src/schema/types/search.js
+++ projects/gnomad-api/src/schema/types/search.js
@@ -1,5 +1,8 @@
 import { GraphQLNonNull, GraphQLObjectType, GraphQLString } from 'graphql'
 import { flatMap, uniqBy } from 'lodash'
+
+import { normalizeVariantId, parseRegionId } from '@gnomad/identifiers'
+
 import datasetsConfig from '../datasets/datasetsConfig'
 import { UserVisibleError } from '../errors'
 
@@ -11,66 +14,6 @@ export const SearchResultType = new GraphQLObjectType({
   },
 })
 
-const REGION_ID_REGEX = /^(chr)?(\d+|x|y|m|mt)[-:]([0-9,]+)([-:]([0-9,]+)?)?$/i
-
-export const isRegionId = str => {
-  const match = REGION_ID_REGEX.exec(str)
-  if (!match) {
-    return false
-  }
-
-  const chrom = match[2].toLowerCase()
-  const chromNumber = Number(chrom)
-  if (!Number.isNaN(chromNumber) && (chromNumber < 1 || chromNumber > 22)) {
-    return false
-  }
-
-  const start = Number(match[3])
-  const end = Number(match[5])
-
-  if (end && end < start) {
-    return false
-  }
-
-  return true
-}
-
-export const normalizeRegionId = regionId => {
-  const parts = regionId.split(/[-:]/)
-  const chrom = parts[0].toUpperCase().replace(/^CHR/, '')
-  let start = Number(parts[1].replace(/,/g, ''))
-  let end
-
-  if (parts[2]) {
-    end = Number(parts[2].replace(/,/g, ''))
-  } else {
-    end = start + 20
-    start = Math.max(start - 20, 0)
-  }
-
-  return `${chrom}-${start}-${end}`
-}
-
-const VARIANT_ID_REGEX = /^(chr)?(\d+|x|y|m|mt)[-:]([0-9,]+)[-:]([acgt]+)[-:]([acgt]+)$/i
-
-export const isVariantId = str => {
-  const match = VARIANT_ID_REGEX.exec(str)
-  if (!match) {
-    return false
-  }
-
-  const chrom = match[2].toLowerCase()
-  const chromNumber = Number(chrom)
-  if (!Number.isNaN(chromNumber) && (chromNumber < 1 || chromNumber > 22)) {
-    return false
-  }
-
-  return true
-}
-
-export const normalizeVariantId = variantId =>
-  variantId.toUpperCase().replace(/,/g, '').replace(/:/g, '-').replace(/^CHR/, '')
-
 export const resolveSearchResults = async (ctx, dataset, query) => {
   if (dataset !== 'gnomad_r2_1' && dataset !== 'gnomad_r3') {
     throw new UserVisibleError(
@@ -78,7 +21,7 @@ export const resolveSearchResults = async (ctx, dataset, query) => {
     )
   }
 
-  if (isVariantId(query)) {
+  try {
     const variantId = normalizeVariantId(query)
     return [
       {
@@ -86,17 +29,31 @@ export const resolveSearchResults = async (ctx, dataset, query) => {
         url: `/variant/${variantId}?dataset=${dataset}`,
       },
     ]
-  }
+  } catch (err) {} // eslint-disable-line no-empty
 
-  if (isRegionId(query)) {
-    const regionId = normalizeRegionId(query)
-    return [
+  try {
+    const { chrom, start, stop } = parseRegionId(query)
+    const regionId = `${chrom}-${start}-${stop}`
+
+    const results = [
       {
         label: regionId,
         url: `/region/${regionId}?dataset=${dataset}`,
       },
     ]
-  }
+
+    // If a position is entered, return options for a 40 base region centered
+    // at the position and the position as a one base region.
+    if (/^(chr)?(\d+|x|y|m|mt)[-:.]([\d,]+)$/i.test(query)) {
+      const windowRegionId = `${chrom}-${Math.max(1, start - 20)}-${stop + 20}`
+      results.unshift({
+        label: windowRegionId,
+        url: `/region/${windowRegionId}?dataset=${dataset}`,
+      })
+    }
+
+    return results
+  } catch (err) {} // eslint-disable-line no-empty
 
   const upperCaseQuery = query.toUpperCase()
 
