e0b819e58ba82b5e232b60cd9949a00f5ff6ee11
Nick Watts <nwatts@broadinstitute.org>
2020-09-14 12:11:33 -0400

    Add instructions for creating BED file for transcripts in IGV.js

--- reads/README.md
+++ reads/README.md
@@ -6,3 +6,18 @@ The individual read visualizations are based on "mini-BAMs" containing data for 
 The code for generating the "mini-BAMs" is located in the [broadinstitute/gnomad-readviz](https://github.com/broadinstitute/gnomad-readviz) repository.
 
 Tracks containing the reference sequence and transcripts (based on Gencode data) are also shown.
+
+## Reference data
+
+### Transcripts
+
+The read data component uses a BED format [annotation track](https://github.com/igvteam/igv.js/wiki/Annotation-Track)
+to display transcripts.
+
+To convert a Gencode GTF file to BED format, use the `gtf2bed.py` script. An index file is also needed to avoid loading
+the entire BED file in the browser. 
+
+```
+python ./reference-data/gtf2bed.py /path/to/gencode.gtf /path/to/gencode.bed.bgz
+tabix -p bed /path/to/gencode.bed.bgz
+```
---
+++ reads/reference-data/gtf2bed.py
@@ -0,0 +1,90 @@
+import argparse
+import os
+
+import hail as hl
+
+
+def gtf2bed(gtf: hl.Table) -> hl.Table:
+    transcripts = gtf.filter(gtf.feature == "transcript")
+    transcripts = transcripts.select(
+        transcript_id=transcripts.transcript_id,
+        chrom=transcripts.interval.start.seqname,
+        strand=transcripts.strand,
+        start=transcripts.interval.start.position - 1,
+        stop=transcripts.interval.end.position - 1,
+    )
+
+    exons = gtf.filter(hl.set(["exon", "CDS", "UTR"]).contains(gtf.feature))
+    exons = exons.select(
+        transcript_id=exons.transcript_id,
+        feature_type=exons.feature,
+        start=exons.interval.start.position - 1,
+        stop=exons.interval.end.position - 1,
+    )
+
+    transcript_exons = exons.group_by(exons.transcript_id).aggregate(exons=hl.agg.collect(exons.row_value))
+    transcript_exons = transcript_exons.annotate(exons=hl.sorted(transcript_exons.exons, lambda exon: exon.start))
+
+    transcripts = transcripts.annotate(**transcript_exons[transcripts.transcript_id])
+
+    ds = transcripts.select(
+        chrom=transcripts.chrom,
+        chromStart=transcripts.start,
+        chromEnd=transcripts.stop,
+        name=transcripts.transcript_id,
+        score=0,
+        strand=transcripts.strand,
+        thickStart=hl.rbind(
+            transcripts.exons.filter(lambda exon: exon.feature_type == "CDS"),
+            lambda cds_exons: hl.if_else(hl.len(cds_exons) > 0, cds_exons.head().start, transcripts.start,),
+        ),
+        thickEnd=hl.rbind(
+            transcripts.exons.filter(lambda exon: exon.feature_type == "CDS"),
+            lambda cds_exons: hl.if_else(hl.len(cds_exons) > 0, hl.reversed(cds_exons).head().stop, transcripts.start,),
+        ),
+        itemRgb="0",
+        blockCount=hl.len(transcripts.exons),
+        blockSizes=hl.delimit(
+            transcripts.exons.filter(lambda exon: exon.feature_type == "exon").map(
+                lambda exon: exon.stop - exon.start + 1
+            ),
+            ",",
+        ),
+        blockStarts=hl.delimit(
+            transcripts.exons.filter(lambda exon: exon.feature_type == "exon").map(
+                lambda exon: exon.start - transcripts.start
+            ),
+            ",",
+        ),
+    )
+
+    ds = ds.order_by(
+        hl.rbind(
+            ds.chrom.replace("^chr", ""),
+            lambda chrom: hl.case()
+            .when(chrom == "X", 23)
+            .when(chrom == "Y", 24)
+            .when(chrom[0] == "M", 25)
+            .default(hl.int(chrom)),
+        ),
+        ds.chromStart,
+    ).drop("interval")
+
+    return ds
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("gtf", help="Path to GTF file")
+    parser.add_argument("output", help="Path to output BED file")
+    args = parser.parse_args()
+
+    hl.init(log=os.devnull)
+
+    ds = hl.experimental.import_gtf(args.gtf)
+    ds = gtf2bed(ds)
+    ds.export(args.output, header=False)
+
+
+if __name__ == "__main__":
+    main()
