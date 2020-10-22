8742f66d428091f9bdf43a9a708b0fe4b542fa2f
Nick Watts <nwatts@broadinstitute.org>
2020-10-22 14:03:37 -0400

    Add expression for compressed variant ID
    Used to compress variant IDs into the 512 byte limit for
    Elasticsearch document IDs

--- data-pipeline/src/data_pipeline/data_types/variant/__init__.py
+++ data-pipeline/src/data_pipeline/data_types/variant/__init__.py
@@ -1,2 +1,2 @@
 from .transcript_consequence.annotate_transcript_consequences import annotate_transcript_consequences
-from .variant_id import variant_id, variant_ids
+from .variant_id import variant_id, variant_ids, compressed_variant_id
--- data-pipeline/src/data_pipeline/data_types/variant/variant_id.py
+++ data-pipeline/src/data_pipeline/data_types/variant/variant_id.py
@@ -1,3 +1,5 @@
+import string
+
 import hail as hl
 
 from data_pipeline.data_types.locus import normalized_contig
@@ -35,3 +37,58 @@ def variant_ids(
         )
 
     return alleles[1:].map(compute_variant_id)
+
+
+_ENCODED_ALLELE_CHARACTERS = hl.array(list(string.ascii_uppercase + string.ascii_lowercase + string.digits + "-_"))
+
+
+def _grouped(arr: hl.expr.ArrayExpression, group_size: hl.expr.Int32Expression) -> hl.expr.ArrayExpression:
+    return hl.range(0, hl.len(arr), group_size).map(lambda i: arr[i : i + group_size])
+
+
+def _encode_allele(allele: hl.expr.StringExpression) -> hl.expr.StringExpression:
+    return hl.delimit(
+        _grouped(
+            # Convert string to array
+            allele.split("")[:-1]
+            # Convert letters to numbers
+            .map(lambda letter: hl.switch(letter).when("A", 0).when("C", 1).when("G", 2).when("T", 3).or_missing()),
+            3,  # Group into sets of 3
+        )
+        # Ensure each group has 3 elements
+        .map(lambda g: g.extend(hl.range(3 - hl.len(g)).map(lambda _: 0)))
+        # Bit shift and add group elements
+        .map(lambda g: g[0] * 16 + g[1] * 4 + g[2])
+        # Convert to letters
+        .map(lambda n: _ENCODED_ALLELE_CHARACTERS[n]),
+        "",
+    )
+
+
+def compressed_variant_id(locus: hl.expr.LocusExpression, alleles: hl.expr.ArrayExpression) -> hl.expr.StringExpression:
+    return hl.rbind(
+        hl.len(alleles[0]),
+        hl.len(alleles[1]),
+        lambda ref_len, alt_len: hl.case()
+        .when(
+            ref_len > alt_len,
+            normalized_contig(locus.contig)
+            + "-"
+            + hl.str(locus.position)
+            + "d"
+            + hl.str(ref_len - alt_len)
+            + "-"
+            + alleles[1],
+        )
+        .when(
+            ref_len < alt_len,
+            normalized_contig(locus.contig)
+            + "-"
+            + hl.str(locus.position)
+            + "i"
+            + hl.str(alt_len - ref_len)
+            + "-"
+            + _encode_allele(alleles[1]),
+        )
+        .default(variant_id(locus, alleles)),
+    )
