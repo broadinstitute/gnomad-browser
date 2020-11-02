767d292c524a6b9f769d9a158537c47d4e944632
Nick Watts <nwatts@broadinstitute.org>
2020-11-02 14:33:52 -0500

    Return null instead of empty objects for gene fields
    Resolves #636

--- graphql-api/src/graphql/resolvers/gene-fields.js
+++ graphql-api/src/graphql/resolvers/gene-fields.js
@@ -1,35 +1,18 @@
-const { UserVisibleError } = require('../../errors')
+const { isEmpty } = require('lodash')
 
-const GRCh37GeneFieldResolver = (field, label) => {
+const nullifyEmptyObject = (field) => {
   return (obj) => {
-    const referenceGenome = obj.reference_genome
-    if (referenceGenome !== 'GRCh37') {
-      throw new UserVisibleError(`${label} is not available on ${referenceGenome}`)
-    }
-
-    if (!obj[field]) {
-      throw new UserVisibleError(`${label} is not available for this gene`)
-    }
-
-    return obj[field]
+    const value = obj[field]
+    return isEmpty(value) ? null : value
   }
 }
 
 module.exports = {
   Gene: {
-    // Elasticsearch may return an empty object instead of null
-    mane_select_transcript: (obj) => {
-      if (obj.mane_select_transcript && obj.mane_select_transcript.ensembl_id) {
-        return obj.mane_select_transcript
-      }
-      return null
-    },
-    pext: GRCh37GeneFieldResolver('pext', 'pext'),
-    gnomad_constraint: GRCh37GeneFieldResolver('gnomad_constraint', 'gnomAD v2 constraint'),
-    exac_constraint: GRCh37GeneFieldResolver('exac_constraint', 'ExAC constraint'),
-    exac_regional_missense_constraint_regions: GRCh37GeneFieldResolver(
-      'exac_regional_missense_constraint_regions',
-      'ExAC regional missense constraint'
-    ),
+    // Elasticsearch documents may contain an empty object instead of null
+    mane_select_transcript: nullifyEmptyObject('mane_select_transcript'),
+    pext: nullifyEmptyObject('pext'),
+    gnomad_constraint: nullifyEmptyObject('gnomad_constraint'),
+    exac_constraint: nullifyEmptyObject('exac_constraint'),
   },
 }
