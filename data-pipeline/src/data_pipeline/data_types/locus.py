7d8b7cb0d8001854e079fea9a4ab894e1772efe7
Nick Watts <nwatts@broadinstitute.org>
2020-11-18 13:58:00 -0500

    Normalize MT contig to M in data pipelines

--- data-pipeline/src/data_pipeline/data_types/locus.py
+++ data-pipeline/src/data_pipeline/data_types/locus.py
@@ -2,13 +2,13 @@ import hail as hl
 
 
 def normalized_contig(contig: hl.expr.StringExpression) -> hl.expr.StringExpression:
-    return contig.replace("^chr", "")
+    return hl.rbind(hl.str(contig).replace("^chr", ""), lambda c: hl.if_else(c == "MT", "M", c))
 
 
 def contig_number(contig: hl.expr.StringExpression) -> hl.expr.Int32Expression:
     return hl.bind(
         lambda contig: (
-            hl.case().when(contig == "X", 23).when(contig == "Y", 24).when(contig[0] == "M", 25).default(hl.int(contig))
+            hl.case().when(contig == "X", 23).when(contig == "Y", 24).when(contig == "M", 25).default(hl.int(contig))
         ),
         normalized_contig(contig),
     )
