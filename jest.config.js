660dcf079368f854de917b0cff38db15d02b2e25
Nick Watts <nwatts@broadinstitute.org>
2020-11-23 12:53:28 -0500

    Mock static files in browser tests

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,10 @@ module.exports = {
   projects: [
     {
       displayName: 'browser',
+      moduleNameMapper: {
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+          '<rootDir>/tests/__mocks__/fileMock.js',
+      },
       testMatch: ['<rootDir>/browser/**/*.spec.js'],
       setupFilesAfterEnv: ['@testing-library/jest-dom'],
     },
