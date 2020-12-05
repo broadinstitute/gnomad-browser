521cd9b0752cc01e9411e231e1cb7c3060a17b1d
Nick Watts <nwatts@broadinstitute.org>
2020-12-05 17:12:27 -0500

    Resolve GraphQL errors from fields of transcript's gene

--- graphql-api/src/graphql/resolvers/gene-fields.js
+++ graphql-api/src/graphql/resolvers/gene-fields.js
@@ -15,4 +15,11 @@ module.exports = {
     gnomad_constraint: nullifyEmptyObject('gnomad_constraint'),
     exac_constraint: nullifyEmptyObject('exac_constraint'),
   },
+  TranscriptGene: {
+    // Elasticsearch documents may contain an empty object instead of null
+    mane_select_transcript: nullifyEmptyObject('mane_select_transcript'),
+    pext: nullifyEmptyObject('pext'),
+    gnomad_constraint: nullifyEmptyObject('gnomad_constraint'),
+    exac_constraint: nullifyEmptyObject('exac_constraint'),
+  },
 }
