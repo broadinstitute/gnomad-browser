483cb0f374678d5e5a5c5bd46803b1da7e8e0ac6
Nick Watts <nwatts@broadinstitute.org>
2020-12-14 15:04:36 -0500

    Add color codes for in silico predictors
    Resolves #626

--- browser/src/VariantPage/VariantInSilicoPredictors.js
+++ browser/src/VariantPage/VariantInSilicoPredictors.js
@@ -1,14 +1,33 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import styled from 'styled-components'
 
 import { List, ListItem } from '@gnomad/ui'
 
-const PREDICTOR_LABELS = {
-  cadd: 'CADD',
-  revel: 'REVEL',
-  primate_ai: 'PrimateAI',
-  splice_ai: 'SpliceAI',
+const PREDICTORS = {
+  cadd: { label: 'CADD', warningThreshold: 10, dangerThreshold: 20 },
+  revel: { label: 'REVEL', warningThreshold: 0.5, dangerThreshold: 0.75 },
+  primate_ai: { label: 'PrimateAI', warningThreshold: 0.5, dangerThreshold: 0.7 },
+  splice_ai: { label: 'SpliceAI', warningThreshold: 0.5, dangerThreshold: 0.8 },
 }
+
+const Marker = styled.span`
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  margin-right: 0.5em;
+
+  &::before {
+    content: '';
+    display: inline-block;
+    box-sizing: border-box;
+    width: 10px;
+    height: 10px;
+    border: 1px solid #000;
+    border-radius: 5px;
+    background: ${props => props.color};
+  }
+`
 
 const VariantInSilicoPredictors = ({ variant }) => {
   return (
@@ -18,11 +37,35 @@ const VariantInSilicoPredictors = ({ variant }) => {
         annotations.
       </p>
       <List>
-        {variant.in_silico_predictors.map(({ id, value }) => (
-          <ListItem key={id}>
-            {PREDICTOR_LABELS[id]}: {value}
-          </ListItem>
-        ))}
+        {variant.in_silico_predictors.map(({ id, value }) => {
+          const predictor = PREDICTORS[id]
+
+          let color = null
+          const parsedValue = parseFloat(value)
+          if (!Number.isNaN(parsedValue)) {
+            if (parsedValue >= predictor.dangerThreshold) {
+              color = '#FF583F'
+            } else if (parsedValue >= predictor.warningThreshold) {
+              color = '#F0C94D'
+            } else {
+              color = 'green'
+            }
+          }
+
+          if (predictor) {
+            return (
+              <ListItem key={id}>
+                {color && <Marker color={color} />}
+                {predictor.label}: {value}
+              </ListItem>
+            )
+          }
+          return (
+            <ListItem key={id}>
+              {id}: {value}
+            </ListItem>
+          )
+        })}
       </List>
     </div>
   )
