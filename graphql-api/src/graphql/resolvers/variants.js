b072b708c9e549a754455e948a3742bb2d022743
Nick Watts <nwatts@broadinstitute.org>
2020-12-17 16:05:01 -0500

    Set limit on region size for variant queries

--- graphql-api/src/graphql/resolvers/variants.js
+++ graphql-api/src/graphql/resolvers/variants.js
@@ -55,6 +55,10 @@ const resolveVariantsInRegion = async (obj, args, ctx) => {
     throw new UserVisibleError('Dataset is required')
   }
 
+  if (obj.stop - obj.start >= 2.5e6) {
+    throw new UserVisibleError('Select a smaller region to view variants')
+  }
+
   const numVariantsInRegion = await countVariantsInRegion(ctx.esClient, dataset, obj)
   if (numVariantsInRegion > 30000) {
     throw new UserVisibleError(
