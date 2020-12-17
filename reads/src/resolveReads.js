65a9f1637d2281f687781f918d7ed0fb3a668c46
Nick Watts <nwatts@broadinstitute.org>
2020-12-17 11:05:46 -0500

    Upgrade sqlite library in reads API

--- reads/src/resolveReads.js
+++ reads/src/resolveReads.js
@@ -1,13 +1,17 @@
 const path = require('path')
 
 const sqlite = require('sqlite')
+const sqlite3 = require('sqlite3')
 
 const ZYGOSITY_CATEGORIES = ['het', 'hom', 'hemi']
 
 const resolveReads = async ({ readsDirectory, publicPath, meta }, { alt, chrom, pos, ref }) => {
   const dbPath = path.join(readsDirectory, `all_variants_${meta}.chr${chrom}.db`)
 
-  const db = await sqlite.open(dbPath)
+  const db = await sqlite.open({
+    filename: dbPath,
+    driver: sqlite3.Database,
+  })
   const rows = await db.all(
     'select combined_bamout_id, read_group_id, zygosity from variants where chrom = ? and pos = ? and ref = ? and alt = ?',
     chrom,
