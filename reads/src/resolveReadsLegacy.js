08e6443c4af679b2e75a8e169f356b45502b6b8c
Nick Watts <nwatts@broadinstitute.org>
2020-12-17 11:05:46 -0500

    Upgrade sqlite library in reads API

--- reads/src/resolveReadsLegacy.js
+++ reads/src/resolveReadsLegacy.js
@@ -1,6 +1,7 @@
 const path = require('path')
 
 const sqlite = require('sqlite')
+const sqlite3 = require('sqlite3')
 
 const resolveReadsLegacy = async ({ readsDirectory, publicPath }, { alt, chrom, pos, ref }) => {
   const dbPath = path.join(
@@ -9,7 +10,10 @@ const resolveReadsLegacy = async ({ readsDirectory, publicPath }, { alt, chrom, 
     `combined_chr${chrom}_${`${pos % 1000}`.padStart(3, '0')}.db`
   )
 
-  const db = await sqlite.open(dbPath)
+  const db = await sqlite.open({
+    filename: dbPath,
+    driver: sqlite3.Database,
+  })
   const rows = await db.all(
     'select n_expected_samples, n_available_samples, het_or_hom_or_hemi from t where chrom = ? and pos = ? and ref = ? and alt = ?',
     chrom,
