648ffdbd45a67fa264bfd0dfc7b5bf78f8b92fb7
Nick Watts <nwatts@broadinstitute.org>
2021-01-06 09:35:13 -0500

    Convert variant tables into functional components

--- browser/src/MitochondrialVariantList/MitochondrialVariantsTable.js
+++ browser/src/MitochondrialVariantList/MitochondrialVariantsTable.js
@@ -1,51 +1,49 @@
 import PropTypes from 'prop-types'
-import React, { PureComponent } from 'react'
+import React, { memo } from 'react'
 
 import { Grid } from '@gnomad/ui'
 
 import MitochondrialVariantPropType from './MitochondrialVariantPropType'
 import { getColumns } from './mitochondrialVariantTableColumns'
 
-class MitochondrialVariantsTable extends PureComponent {
-  static propTypes = {
-    context: PropTypes.oneOf(['gene', 'region', 'transcript']).isRequired,
-    forwardedRef: PropTypes.oneOfType([
-      PropTypes.func,
-      PropTypes.shape({ current: PropTypes.any }), // eslint-disable-line react/forbid-prop-types
-    ]).isRequired,
-    numRowsRendered: PropTypes.number.isRequired,
-    onHoverVariant: PropTypes.func.isRequired,
-    variants: PropTypes.arrayOf(MitochondrialVariantPropType).isRequired,
-    width: PropTypes.number.isRequired,
-  }
-
-  render() {
-    const {
-      context,
-      forwardedRef,
-      numRowsRendered,
-      onHoverVariant,
-      variants,
-      width,
-      ...rest
-    } = this.props
-
-    return (
-      <Grid
-        ref={forwardedRef}
-        {...rest}
-        columns={getColumns({ context, width })}
-        data={variants}
-        numRowsRendered={numRowsRendered}
-        onHoverRow={rowIndex => {
-          onHoverVariant(rowIndex === null ? null : variants[rowIndex].variant_id)
-        }}
-        rowKey={variant => variant.variant_id}
-      />
-    )
-  }
+const MitochondrialVariantsTable = ({
+  context,
+  forwardedRef,
+  numRowsRendered,
+  onHoverVariant,
+  variants,
+  width,
+  ...rest
+}) => {
+  return (
+    <Grid
+      ref={forwardedRef}
+      {...rest}
+      columns={getColumns({ context, width })}
+      data={variants}
+      numRowsRendered={numRowsRendered}
+      onHoverRow={rowIndex => {
+        onHoverVariant(rowIndex === null ? null : variants[rowIndex].variant_id)
+      }}
+      rowKey={variant => variant.variant_id}
+    />
+  )
 }
 
+MitochondrialVariantsTable.propTypes = {
+  context: PropTypes.oneOf(['gene', 'region', 'transcript']).isRequired,
+  forwardedRef: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.shape({ current: PropTypes.any }), // eslint-disable-line react/forbid-prop-types
+  ]).isRequired,
+  numRowsRendered: PropTypes.number.isRequired,
+  onHoverVariant: PropTypes.func.isRequired,
+  variants: PropTypes.arrayOf(MitochondrialVariantPropType).isRequired,
+  width: PropTypes.number.isRequired,
+}
+
+const MemoizedMitochondrialVariantsTable = memo(MitochondrialVariantsTable)
+
 export default React.forwardRef((props, ref) => (
-  <MitochondrialVariantsTable {...props} forwardedRef={ref} />
+  <MemoizedMitochondrialVariantsTable {...props} forwardedRef={ref} />
 ))
--- browser/src/VariantList/VariantTable.js
+++ browser/src/VariantList/VariantTable.js
@@ -1,82 +1,63 @@
 import PropTypes from 'prop-types'
-import React, { PureComponent } from 'react'
+import React, { forwardRef, memo } from 'react'
 
 import { Grid } from '@gnomad/ui'
 
-class VariantTable extends PureComponent {
-  static propTypes = {
-    columns: PropTypes.arrayOf(PropTypes.object).isRequired, // eslint-disable-line react/forbid-prop-types
-    highlightText: PropTypes.string,
-    highlightedVariantId: PropTypes.string,
-    onVisibleRowsChange: PropTypes.func,
-    onHoverVariant: PropTypes.func,
-    onRequestSort: PropTypes.func,
-    rowIndexLastClickedInNavigator: PropTypes.number,
-    sortKey: PropTypes.string.isRequired,
-    sortOrder: PropTypes.oneOf(['ascending', 'descending']).isRequired,
-    variants: PropTypes.arrayOf(PropTypes.object).isRequired, // eslint-disable-line react/forbid-prop-types
-  }
-
-  static defaultProps = {
-    highlightText: '',
-    highlightedVariantId: null,
-    onVisibleRowsChange: () => {},
-    onHoverVariant: () => {},
-    onRequestSort: () => {},
-    rowIndexLastClickedInNavigator: null,
-  }
-
-  constructor(props) {
-    super(props)
-
-    this.grid = React.createRef()
-  }
-
-  componentDidUpdate(prevProps) {
-    const { rowIndexLastClickedInNavigator } = this.props
-    if (rowIndexLastClickedInNavigator !== prevProps.rowIndexLastClickedInNavigator) {
-      if (this.grid.current) {
-        this.grid.current.scrollToDataRow(rowIndexLastClickedInNavigator)
+const VariantTable = ({
+  columns,
+  forwardedRef,
+  highlightText,
+  highlightedVariantId,
+  onVisibleRowsChange,
+  onHoverVariant,
+  onRequestSort,
+  variants,
+  sortKey,
+  sortOrder,
+}) => {
+  return (
+    <Grid
+      cellData={{ highlightWords: highlightText.split(',').map(s => s.trim()) }}
+      columns={columns}
+      data={variants}
+      numRowsRendered={20}
+      onHoverRow={rowIndex => {
+        onHoverVariant(rowIndex === null ? null : variants[rowIndex].variant_id)
+      }}
+      onRequestSort={onRequestSort}
+      onVisibleRowsChange={onVisibleRowsChange}
+      ref={forwardedRef}
+      rowKey={variant => variant.variant_id}
+      shouldHighlightRow={
+        highlightedVariantId ? variant => variant.variant_id === highlightedVariantId : () => false
       }
-    }
-  }
-
-  render() {
-    const {
-      columns,
-      highlightText,
-      highlightedVariantId,
-      onVisibleRowsChange,
-      onHoverVariant,
-      onRequestSort,
-      variants,
-      sortKey,
-      sortOrder,
-    } = this.props
-
-    return (
-      <Grid
-        cellData={{ highlightWords: highlightText.split(',').map(s => s.trim()) }}
-        columns={columns}
-        data={variants}
-        numRowsRendered={20}
-        onHoverRow={rowIndex => {
-          onHoverVariant(rowIndex === null ? null : variants[rowIndex].variant_id)
-        }}
-        onRequestSort={onRequestSort}
-        onVisibleRowsChange={onVisibleRowsChange}
-        ref={this.grid}
-        rowKey={variant => variant.variant_id}
-        shouldHighlightRow={
-          highlightedVariantId
-            ? variant => variant.variant_id === highlightedVariantId
-            : () => false
-        }
-        sortKey={sortKey}
-        sortOrder={sortOrder}
-      />
-    )
-  }
+      sortKey={sortKey}
+      sortOrder={sortOrder}
+    />
+  )
 }
 
-export default VariantTable
+VariantTable.propTypes = {
+  columns: PropTypes.arrayOf(PropTypes.object).isRequired, // eslint-disable-line react/forbid-prop-types
+  forwardedRef: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+  highlightText: PropTypes.string,
+  highlightedVariantId: PropTypes.string,
+  onVisibleRowsChange: PropTypes.func,
+  onHoverVariant: PropTypes.func,
+  onRequestSort: PropTypes.func,
+  sortKey: PropTypes.string.isRequired,
+  sortOrder: PropTypes.oneOf(['ascending', 'descending']).isRequired,
+  variants: PropTypes.arrayOf(PropTypes.object).isRequired, // eslint-disable-line react/forbid-prop-types
+}
+
+VariantTable.defaultProps = {
+  highlightText: '',
+  highlightedVariantId: null,
+  onVisibleRowsChange: () => {},
+  onHoverVariant: () => {},
+  onRequestSort: () => {},
+}
+
+const MemoizedVariantTable = memo(VariantTable)
+
+export default forwardRef((props, ref) => <MemoizedVariantTable {...props} forwardedRef={ref} />)
