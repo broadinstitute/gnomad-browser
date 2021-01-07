0b46fbf4ced2b0945434a56784f0f99af42f1cdb
Nick Watts <nwatts@broadinstitute.org>
2021-01-07 10:40:24 -0500

    Configure webpack to use same browserslist config as Babel

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,18 @@
+const fs = require('fs')
+const path = require('path')
+
+// Workaround for https://github.com/babel/babel/issues/10965
+const browserslist = fs
+  .readFileSync(path.resolve(__dirname, 'browser/.browserslistrc'), { encoding: 'utf8' })
+  .split('\n')
+  .filter(Boolean)
+
 module.exports = {
   presets: [
     [
       '@babel/preset-env',
       {
-        targets: ['last 2 versions', 'ie >= 10'],
+        targets: browserslist,
         useBuiltIns: 'entry',
         corejs: 3,
         exclude: ['transform-typeof-symbol'],
