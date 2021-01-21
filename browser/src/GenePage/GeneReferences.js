feb48a4f4d34156a2faba253a616faad03934204
Nick Watts <nwatts@broadinstitute.org>
2021-01-21 10:05:06 -0500

    Add GenCC to gene references

--- browser/src/GenePage/GeneReferences.js
+++ browser/src/GenePage/GeneReferences.js
@@ -70,20 +70,25 @@ const GeneReferences = ({ gene }) => {
               </ExternalLink>
             </ListItem>
             {hgncId && (
-              <ListItem>
-                <ExternalLink href={`https://search.clinicalgenome.org/kb/genes/${hgncId}`}>
-                  ClinGen
-                </ExternalLink>
-              </ListItem>
-            )}
-            {hgncId && (
-              <ListItem>
-                <ExternalLink
-                  href={`https://www.genenames.org/data/gene-symbol-report/#!/hgnc_id/${hgncId}`}
-                >
-                  HGNC
-                </ExternalLink>
-              </ListItem>
+              <>
+                <ListItem>
+                  <ExternalLink href={`https://search.clinicalgenome.org/kb/genes/${hgncId}`}>
+                    ClinGen
+                  </ExternalLink>
+                </ListItem>
+                <ListItem>
+                  <ExternalLink href={`https://search.thegencc.org/genes/${hgncId}`}>
+                    Gene Curation Coalition (GenCC)
+                  </ExternalLink>
+                </ListItem>
+                <ListItem>
+                  <ExternalLink
+                    href={`https://www.genenames.org/data/gene-symbol-report/#!/hgnc_id/${hgncId}`}
+                  >
+                    HGNC
+                  </ExternalLink>
+                </ListItem>
+              </>
             )}
           </List>
         </Modal>
