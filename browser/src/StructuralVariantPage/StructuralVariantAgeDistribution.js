776c9a67af1c4516ec3431b53f1820c7c88f83d4
Nick Watts <nwatts@broadinstitute.org>
2021-02-03 15:13:54 -0500

    Add overall age distribution for structural variants
    Related to #692

--- browser/src/StructuralVariantPage/StructuralVariantAgeDistribution.js
+++ browser/src/StructuralVariantPage/StructuralVariantAgeDistribution.js
@@ -4,9 +4,17 @@ import styled from 'styled-components'
 
 import { Checkbox } from '@gnomad/ui'
 
+import overallAgeDistribution from '../dataset-constants/gnomad_sv_r2_1/ageDistribution.json'
+import Legend, { StripedSwatch } from '../Legend'
 import StackedHistogram from '../StackedHistogram'
 import ControlSection from '../VariantPage/ControlSection'
 import StructuralVariantDetailPropType from './StructuralVariantDetailPropType'
+
+const LegendWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 1em;
+`
 
 const CheckboxWrapper = styled.div`
   label {
@@ -18,6 +26,8 @@ const CheckboxWrapper = styled.div`
 const StructuralVariantAgeDistribution = ({ variant }) => {
   const [includeHeterozygotes, setIncludeHeterozygotes] = useState(true)
   const [includeHomozygotes, setIncludeHomozygotes] = useState(true)
+
+  const [showAllIndividuals, setShowAllIndividuals] = useState(true)
 
   // Only show histogram if there is data to show
   const isAgeDataAvailable = [
@@ -55,20 +65,48 @@ const StructuralVariantAgeDistribution = ({ variant }) => {
     ],
   ]
 
+  const secondaryValues = [
+    [overallAgeDistribution.n_smaller],
+    ...overallAgeDistribution.bin_freq.map(n => [n]),
+    [overallAgeDistribution.n_larger],
+  ]
+
+  const series = [{ label: 'Variant carriers', color: '#73ab3d' }]
+  if (showAllIndividuals) {
+    series.push({
+      label: 'All individuals',
+      swatch: <StripedSwatch id="age-distribution-legend-swatch" color="#73ab3d" />,
+    })
+  }
+
   return (
     <div>
+      <LegendWrapper>
+        <Legend series={series} />
+      </LegendWrapper>
+
       <StackedHistogram
         id="age-distribution-plot"
         bins={bins}
         values={values}
+        secondaryValues={showAllIndividuals ? secondaryValues : null}
         xLabel="Age"
         yLabel="Variant carriers"
+        secondaryYLabel="All individuals"
         barColors={['#73ab3d']}
-        formatTooltip={(bin, variantCarriersInBin) => {
+        formatTooltip={(bin, variantCarriersInBin, allIndividualsInBin) => {
           const nVariantCarriers = sum(variantCarriersInBin)
-          return `${nVariantCarriers.toLocaleString()} variant carrier${
+          let tooltipText = `${nVariantCarriers.toLocaleString()} variant carrier${
             nVariantCarriers !== 1 ? 's' : ''
-          } are in the ${bin} age range`
+          }`
+          if (showAllIndividuals && allIndividualsInBin) {
+            const nTotalIndividuals = sum(allIndividualsInBin)
+            tooltipText += ` and ${nTotalIndividuals.toLocaleString()} total individual${
+              nTotalIndividuals !== 1 ? 's' : ''
+            }`
+          }
+          tooltipText += ` are in the ${bin} age range`
+          return tooltipText
         }}
       />
 
@@ -85,6 +123,12 @@ const StructuralVariantAgeDistribution = ({ variant }) => {
             id="sv-age-distribution-include-homozygotes"
             label="Include homozygous variant carriers"
             onChange={setIncludeHomozygotes}
+          />
+          <Checkbox
+            checked={showAllIndividuals}
+            id="age-distribution-show-all-individuals"
+            label="Compare to all individuals"
+            onChange={setShowAllIndividuals}
           />
         </CheckboxWrapper>
       </ControlSection>
