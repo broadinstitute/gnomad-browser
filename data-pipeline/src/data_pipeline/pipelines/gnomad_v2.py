e64509fe29cd63c692716608365beb040f200c07
Nick Watts <nwatts@broadinstitute.org>
2021-03-03 17:52:59 -0500

    Add new LoF curation results

--- data-pipeline/src/data_pipeline/pipelines/gnomad_v2.py
+++ data-pipeline/src/data_pipeline/pipelines/gnomad_v2.py
@@ -100,7 +100,10 @@ pipeline.add_task(
     "/gnomad_v2/gnomad_v2_lof_curation_results.ht",
     {"genes_path": genes_pipeline.get_task("prepare_grch37_genes")},
     {
+        # If a result for a variant/gene pair is present in more than one file, the result in the first file in this list takes precedence.
         "curation_result_paths": [
+            "gs://gnomad-public/truth-sets/source/lof-curation/metabolic_conditions_genes_curation_results.csv",
+            "gs://gnomad-public/truth-sets/source/lof-curation/haploinsufficient_genes_curation_results.csv",
             "gs://gnomad-public/truth-sets/source/lof-curation/AP4_curation_results.csv",
             "gs://gnomad-public/truth-sets/source/lof-curation/FIG4_curation_results.csv",
             "gs://gnomad-public/truth-sets/source/lof-curation/lysosomal_storage_disease_genes_curation_results.csv",
