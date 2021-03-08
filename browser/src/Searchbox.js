ab6190617d55262e0ed9a0a10f9f93ce093c50c6
Nick Watts <nwatts@broadinstitute.org>
2021-03-08 18:04:30 -0500

    Resolve some searches on client side
    Handle variant IDs, regions, and gene/transcript IDs on
    client side.

--- browser/src/Searchbox.js
+++ browser/src/Searchbox.js
@@ -3,7 +3,16 @@ import React, { useEffect, useRef, useState } from 'react'
 import { withRouter } from 'react-router-dom'
 import styled from 'styled-components'
 
+import {
+  isRegionId,
+  isVariantId,
+  normalizeVariantId,
+  parseRegionId,
+  isRsId,
+} from '@gnomad/identifiers'
 import { Searchbox, Select } from '@gnomad/ui'
+
+import { referenceGenomeForDataset } from './datasets'
 
 const Wrapper = styled.div`
   display: flex;
@@ -24,31 +33,130 @@ const Wrapper = styled.div`
   }
 `
 
-const searchQuery = `
-query Search($dataset: DatasetId!, $query: String!) {
-  searchResults(dataset: $dataset, query: $query) {
-    label
-    value: url
-  }
-}
-`
+export const fetchSearchResults = (dataset, query) => {
+  // ==============================================================================================
+  // Variants
+  // ==============================================================================================
 
-const fetchSearchResults = (dataset, query) =>
-  fetch('/api/', {
+  if (isVariantId(query)) {
+    const variantId = normalizeVariantId(query)
+    return Promise.resolve([
+      {
+        label: variantId,
+        value: `/variant/${variantId}?dataset=${dataset}`,
+      },
+    ])
+  }
+
+  if (isRsId(query)) {
+    const rsId = query
+    return Promise.resolve([
+      {
+        label: rsId,
+        value: `/variant/${rsId}?dataset=${dataset}`,
+      },
+    ])
+  }
+
+  // ==============================================================================================
+  // Region
+  // ==============================================================================================
+
+  if (isRegionId(query)) {
+    const { chrom, start, stop } = parseRegionId(query)
+    const regionId = `${chrom}-${start}-${stop}`
+    const results = [
+      {
+        label: regionId,
+        value: `/region/${regionId}?dataset=${dataset}`,
+      },
+    ]
+
+    // If a position is entered, return options for a 40 base region centered
+    // at the position and the position as a one base region.
+    if (start === stop) {
+      const windowRegionId = `${chrom}-${Math.max(1, start - 20)}-${stop + 20}`
+      results.unshift({
+        label: windowRegionId,
+        value: `/region/${windowRegionId}?dataset=${dataset}`,
+      })
+    }
+
+    return Promise.resolve(results)
+  }
+
+  // ==============================================================================================
+  // Gene ID
+  // ==============================================================================================
+
+  const upperCaseQuery = query.toUpperCase()
+
+  if (/^ENSG\d{11}$/.test(upperCaseQuery)) {
+    const geneId = upperCaseQuery
+    return Promise.resolve([
+      {
+        label: geneId,
+        value: `/gene/${geneId}?dataset=${dataset}`,
+      },
+    ])
+  }
+
+  // ==============================================================================================
+  // Transcript ID
+  // ==============================================================================================
+
+  if (/^ENST\d{11}$/.test(upperCaseQuery)) {
+    const transcriptId = upperCaseQuery
+    return Promise.resolve([
+      {
+        label: transcriptId,
+        value: `/transcript/${transcriptId}?dataset=${dataset}`,
+      },
+    ])
+  }
+
+  // ==============================================================================================
+  // Gene symbol
+  // ==============================================================================================
+
+  return fetch('/api/', {
     body: JSON.stringify({
-      query: searchQuery,
-      variables: { dataset, query },
+      query: `
+        query GeneSearch($query: String!, $referenceGenome: ReferenceGenomeId!) {
+          gene_search(query: $query, reference_genome: $referenceGenome) {
+            ensembl_id
+            symbol
+          }
+        }
+      `,
+      variables: { query, referenceGenome: referenceGenomeForDataset(dataset) },
     }),
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
   })
     .then(response => response.json())
     .then(response => {
-      if (!response.data.searchResults) {
+      if (!response.data.gene_search) {
         throw new Error('Unable to retrieve search results')
       }
-      return response.data.searchResults
+
+      const genes = response.data.gene_search
+
+      const geneSymbolCounts = {}
+      genes.forEach(gene => {
+        if (geneSymbolCounts[gene.symbol] === undefined) {
+          geneSymbolCounts[gene.symbol] = 0
+        }
+        geneSymbolCounts[gene.symbol] += 1
+      })
+
+      return genes.map(gene => ({
+        label:
+          geneSymbolCounts[gene.symbol] > 1 ? `${gene.symbol} (${gene.ensembl_id})` : gene.symbol,
+        value: `/gene/${gene.ensembl_id}?dataset=${dataset}`,
+      }))
     })
+}
 
 const getDefaultSearchDataset = selectedDataset => {
   if (selectedDataset) {
