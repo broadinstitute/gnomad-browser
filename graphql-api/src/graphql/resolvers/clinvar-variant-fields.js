56c8a9dbcc4cea0e01fa8180593885675f1fe988
Nick Watts <nwatts@broadinstitute.org>
2021-03-16 11:23:35 -0400

    Fix GraphQL error related to gnomAD data for ClinVar variants
    Empty objects in ES caused "cannot return null for non-nullable
    field" errors

--- graphql-api/src/graphql/resolvers/clinvar-variant-fields.js
+++ graphql-api/src/graphql/resolvers/clinvar-variant-fields.js
@@ -1,5 +1,18 @@
+const { isEmpty } = require('lodash')
+
+const nullifyEmptyObject = (field) => {
+  return (obj) => {
+    const value = obj[field]
+    return isEmpty(value) ? null : value
+  }
+}
+
 module.exports = {
   ClinVarVariant: {
     gnomad: (variant) => (variant.gnomad.exome || variant.gnomad.genome ? variant.gnomad : null),
+  },
+  ClinVarVariantGnomadData: {
+    exome: nullifyEmptyObject('exome'),
+    genome: nullifyEmptyObject('genome'),
   },
 }
