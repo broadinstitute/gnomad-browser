58c2f730c0579076db6c31379883248c9c641b5f
Nick Watts <nwatts@broadinstitute.org>
2021-03-19 14:30:40 -0400

    Update dataset versions in browser text

--- browser/help/faq/technical-details/what-genome-build-is-the-gnomad-data-based-on.md
+++ browser/help/faq/technical-details/what-genome-build-is-the-gnomad-data-based-on.md
@@ -2,4 +2,4 @@
 question: 'What genome build is the gnomAD data based on?'
 ---
 
-gnomAD v2 is based on [GRCh37/hg19](ftp://ftp.broadinstitute.org/pub/seq/references/Homo_sapiens_assembly19.fasta). gnomAD v3.1 is based on GRCh38.
+gnomAD v2 is based on [GRCh37/hg19](ftp://ftp.broadinstitute.org/pub/seq/references/Homo_sapiens_assembly19.fasta). gnomAD v3 is based on GRCh38.
--- browser/src/HomePage.js
+++ browser/src/HomePage.js
@@ -43,8 +43,8 @@ export default () => (
       <SubHeading>genome aggregation database</SubHeading>
       <Searchbox width="100%" />
       <p>
-        Please note that gnomAD v2.1.1 and v3.1 have substantially different but overlapping sample
-        compositions and are on different genome builds. For more information, see{' '}
+        Please note that gnomAD v2.1.1 and v3.1.1 have substantially different but overlapping
+        sample compositions and are on different genome builds. For more information, see{' '}
         <Link to="/help/should-i-switch-to-the-latest-version-of-gnomad">
           &quot;Should I switch to the latest version of gnomAD?&quot;
         </Link>
