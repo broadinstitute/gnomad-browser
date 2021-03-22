d77a06c86688aba3b2288e11edf55925f4bd4dfc
Nick Watts <nwatts@broadinstitute.org>
2021-03-22 16:38:24 -0400

    Fix rsids field for gnomAD v2 and ExAC variants
    Resolves #743

--- graphql-api/src/graphql/resolvers/variant-fields.js
+++ graphql-api/src/graphql/resolvers/variant-fields.js
@@ -1,10 +1,12 @@
 module.exports = {
   Variant: {
-    // Workaround. gnomAD v2/ExAC variants with no rsID have a set containing null in the rsids field.
+    // Workaround for #743 (https://github.com/broadinstitute/gnomad-browser/issues/743)
+    // gnomAD v2/ExAC variants with no rsID have a set containing null in the rsids field.
     rsids: (obj) => (obj.rsids || []).filter((rsid) => rsid !== null),
   },
   VariantDetails: {
-    // Workaround. gnomAD v2/ExAC variants with no rsID have a set containing null in the rsids field.
+    // Workaround for #743 (https://github.com/broadinstitute/gnomad-browser/issues/743)
+    // gnomAD v2/ExAC variants with no rsID have a set containing null in the rsids field.
     rsids: (obj) => (obj.rsids || []).filter((rsid) => rsid !== null),
   },
 }
