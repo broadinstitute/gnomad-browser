d7807e3ddedcacfe9d9d92b4a239d5f222b386a0
Nick Watts <nwatts@broadinstitute.org>
2021-03-23 15:42:05 -0400

    If an rsID search returns multiple variants, prompt to select one
    Resolves #738

--- browser/src/VariantPageRouter.js
+++ browser/src/VariantPageRouter.js
@@ -1,10 +1,15 @@
 import PropTypes from 'prop-types'
+import queryString from 'query-string'
 import React, { lazy } from 'react'
+import { Redirect } from 'react-router-dom'
 
 import { isVariantId, normalizeVariantId, isRsId } from '@gnomad/identifiers'
-import { Page, PageHeading } from '@gnomad/ui'
+import { List, ListItem, Page, PageHeading } from '@gnomad/ui'
 
+import { labelForDataset } from './datasets'
 import DocumentTitle from './DocumentTitle'
+import Link from './Link'
+import Query from './Query'
 
 const MitochondrialVariantPage = lazy(() =>
   import('./MitochondrialVariantPage/MitochondrialVariantPage')
@@ -12,6 +17,75 @@ const MitochondrialVariantPage = lazy(() =>
 const MNVPage = lazy(() => import('./MNVPage/MNVPage'))
 const StructuralVariantPage = lazy(() => import('./StructuralVariantPage/StructuralVariantPage'))
 const VariantPage = lazy(() => import('./VariantPage/VariantPage'))
+
+const VARIANT_SEARCH_QUERY = `
+query VariantSearch($query: String!, $datasetId: DatasetId!) {
+  variant_search(query: $query, dataset: $datasetId) {
+    variant_id
+  }
+}
+`
+
+const VariantSearchPage = ({ datasetId, query }) => {
+  return (
+    <Page>
+      <DocumentTitle title={`${query} | ${labelForDataset(datasetId)}`} />
+      <PageHeading>{query}</PageHeading>
+
+      <Query
+        query={VARIANT_SEARCH_QUERY}
+        variables={{ datasetId, query }}
+        loadingMessage="Searching variants"
+        errorMessage="Unable to complete search"
+        success={data => data.variant_search}
+      >
+        {({ data }) => {
+          const matchingVariants = data.variant_search.map(result => result.variant_id)
+
+          if (matchingVariants.length === 0) {
+            return <p>No matching variants found.</p>
+          }
+
+          if (matchingVariants.length === 1) {
+            return (
+              <Redirect
+                to={{
+                  pathname: `/variant/${matchingVariants[0]}`,
+                  search: queryString.stringify({ dataset: datasetId }),
+                }}
+              />
+            )
+          }
+
+          return (
+            <>
+              <p style={{ fontSize: '16px' }}>Multiple variants found:</p>
+              <List>
+                {matchingVariants.map(variantId => (
+                  <ListItem key={variantId}>
+                    <Link
+                      to={{
+                        pathname: `/variant/${variantId}`,
+                        search: queryString.stringify({ dataset: datasetId }),
+                      }}
+                    >
+                      {variantId}
+                    </Link>
+                  </ListItem>
+                ))}
+              </List>
+            </>
+          )
+        }}
+      </Query>
+    </Page>
+  )
+}
+
+VariantSearchPage.propTypes = {
+  datasetId: PropTypes.string.isRequired,
+  query: PropTypes.string.isRequired,
+}
 
 const VariantPageRouter = ({ datasetId, variantId }) => {
   if (datasetId.startsWith('gnomad_sv')) {
@@ -33,7 +107,7 @@ const VariantPageRouter = ({ datasetId, variantId }) => {
   }
 
   if (isRsId(variantId)) {
-    return <VariantPage datasetId={datasetId} rsId={variantId} />
+    return <VariantSearchPage datasetId={datasetId} query={variantId} />
   }
 
   return (
