1443cdfd3681bf2341f522c034ad498f231a3741
Nick Watts <nwatts@broadinstitute.org>
2021-04-01 10:26:20 -0400

    Add note about descriptions of fields in constraint files

--- browser/src/downloads/GnomadV2Downloads.js
+++ browser/src/downloads/GnomadV2Downloads.js
@@ -306,7 +306,12 @@ export default () => (
         <ExternalLink href="https://doi.org/10.1038/s41586-020-2308-7">
           <em>The mutational constraint spectrum quantified from variation in 141,456 humans.</em>{' '}
           Nature 581, 434–443 (2020).
+        </ExternalLink>{' '}
+        Descriptions of the fields in these files can be found on pages 74-77 of the{' '}
+        <ExternalLink href="https://www.nature.com/articles/s41586-020-2308-7#Sec12">
+          Supplementary Information
         </ExternalLink>
+        .
       </p>
       <FileList>
         <ListItem>
