89df5a964834fb265819ea35da0fc97e84f2cb28
Nick Watts <nwatts@broadinstitute.org>
2021-05-05 18:11:40 -0400

    Switch to ClinVar weekly release

--- data-pipeline/src/data_pipeline/datasets/clinvar.py
+++ data-pipeline/src/data_pipeline/datasets/clinvar.py
@@ -14,9 +14,7 @@ from data_pipeline.data_types.locus import normalized_contig
 from data_pipeline.data_types.variant import variant_id
 
 
-CLINVAR_XML_URL = (
-    "https://ftp.ncbi.nlm.nih.gov/pub/clinvar/xml/clinvar_variation/ClinVarVariationRelease_00-latest.xml.gz"
-)
+CLINVAR_XML_URL = "https://ftp.ncbi.nlm.nih.gov/pub/clinvar/xml/clinvar_variation/weekly_release/ClinVarVariationRelease_00-latest_weekly.xml.gz"
 
 
 CLINVAR_GOLD_STARS = {
