b796b3894640041d327ef799bc1ba9a50a164739
Nick Watts <nwatts@broadinstitute.org>
2021-06-02 09:50:13 -0400

    Move co-occurrence variant IDs form to a separate module

--- browser/src/VariantCooccurrencePage/VariantCooccurrencePage.js
+++ browser/src/VariantCooccurrencePage/VariantCooccurrencePage.js
@@ -1,14 +1,12 @@
 import PropTypes from 'prop-types'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import styled from 'styled-components'
 
-import { isVariantId } from '@gnomad/identifiers'
-import { Badge, Input, List, ListItem, Page, PrimaryButton } from '@gnomad/ui'
+import { Badge, List, ListItem, Page } from '@gnomad/ui'
 
 import { GNOMAD_POPULATION_NAMES } from '@gnomad/dataset-metadata/gnomadPopulations'
 
-import { referenceGenomeForDataset } from '../datasets'
 import DocumentTitle from '../DocumentTitle'
 import GnomadPageHeading from '../GnomadPageHeading'
 import Link from '../Link'
@@ -20,6 +18,7 @@ import CooccurrenceDataPropType from './CooccurrenceDataPropType'
 import VariantCooccurrenceDetailsTable from './VariantCooccurrenceDetailsTable'
 import VariantCooccurrenceHaplotypeCountsTable from './VariantCooccurrenceHaplotypeCountsTable'
 import VariantCooccurrenceSummaryTable from './VariantCooccurrenceSummaryTable'
+import VariantCooccurrenceVariantIdsForm from './VariantCooccurrenceVariantIdsForm'
 
 const Section = styled.section`
   width: 100%;
@@ -141,119 +140,6 @@ VariantCoocurrence.propTypes = {
   cooccurrenceData: CooccurrenceDataPropType.isRequired,
 }
 
-const InputGroup = styled.div`
-  margin-bottom: 1em;
-`
-
-const FormValidationMessage = styled.span`
-  display: inline-block;
-  margin-top: 0.5em;
-  color: #ff583f;
-`
-
-const SubmitButton = styled(PrimaryButton).attrs({ type: 'submit' })``
-
-const VariantCoocurrenceForm = ({ datasetId, onSubmit }) => {
-  const [variant1Id, setVariant1Id] = useState('')
-  const [variant2Id, setVariant2Id] = useState('')
-  const [submitted, setSubmitted] = useState(false)
-
-  useEffect(() => {
-    setSubmitted(false)
-  }, [variant1Id, variant2Id])
-
-  let variant1ValidationError = null
-  if (variant1Id) {
-    if (!isVariantId(variant1Id)) {
-      variant1ValidationError =
-        'Variants must be specified as chromosome-position-reference-alternate'
-    } else if (variant2Id && variant1Id === variant2Id) {
-      variant1ValidationError = 'Two different variants must be provided'
-    }
-  }
-  const isVariant1Invalid = Boolean(variant1ValidationError)
-
-  let variant2ValidationError = null
-  if (variant2Id) {
-    if (!isVariantId(variant2Id)) {
-      variant2ValidationError =
-        'Variants must be specified as chromosome-position-reference-alternate'
-    } else if (variant1Id && variant2Id === variant1Id) {
-      variant2ValidationError = 'Two different variants must be provided'
-    }
-  }
-  const isVariant2Invalid = Boolean(variant2ValidationError)
-
-  return (
-    <form
-      onSubmit={e => {
-        e.preventDefault()
-
-        setSubmitted(true)
-        onSubmit([variant1Id, variant2Id])
-      }}
-    >
-      <InputGroup>
-        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-        <label htmlFor="cooccurrence-variant1" style={{ display: 'block' }}>
-          Variant 1 (required)
-          <Input
-            aria-describedby={isVariant1Invalid ? 'cooccurrence-variant1-error' : undefined}
-            id="cooccurrence-variant1"
-            placeholder={`chromosome-position-reference-alternate (${referenceGenomeForDataset(
-              datasetId
-            )})`}
-            required
-            value={variant1Id}
-            onChange={e => {
-              setVariant1Id(e.target.value)
-            }}
-          />
-        </label>
-        {isVariant1Invalid && (
-          <FormValidationMessage id="cooccurrence-variant1-error">
-            {variant1ValidationError}
-          </FormValidationMessage>
-        )}
-      </InputGroup>
-      <InputGroup>
-        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-        <label htmlFor="cooccurrence-variant2" style={{ display: 'block' }}>
-          Variant 2 (required)
-          <Input
-            aria-describedby={isVariant2Invalid ? 'cooccurrence-variant2-error' : undefined}
-            id="cooccurrence-variant2"
-            placeholder={`chromosome-position-reference-alternate (${referenceGenomeForDataset(
-              datasetId
-            )})`}
-            required
-            value={variant2Id}
-            onChange={e => {
-              setVariant2Id(e.target.value)
-            }}
-          />
-        </label>
-        {isVariant2Invalid && (
-          <FormValidationMessage id="cooccurrence-variant2-error">
-            {variant2ValidationError}
-          </FormValidationMessage>
-        )}
-      </InputGroup>
-
-      <SubmitButton
-        disabled={!variant1Id || !variant2Id || isVariant1Invalid || isVariant2Invalid || submitted}
-      >
-        Submit
-      </SubmitButton>
-    </form>
-  )
-}
-
-VariantCoocurrenceForm.propTypes = {
-  datasetId: PropTypes.string.isRequired,
-  onSubmit: PropTypes.func.isRequired,
-}
-
 const query = `
 query VariantCooccurrence($variants: [String!]!, $variant1: String!, $variant2: String, $datasetId: DatasetId!) {
   variant_cooccurrence(variants: $variants, dataset: $datasetId) {
@@ -343,7 +229,7 @@ const VariantCoocurrenceContainer = ({ datasetId }) => {
           <ListItem>Have a global allele frequency &le; 5%</ListItem>
         </List>
 
-        <VariantCoocurrenceForm datasetId={datasetId} onSubmit={setVariantIds} />
+        <VariantCooccurrenceVariantIdsForm datasetId={datasetId} onSubmit={setVariantIds} />
       </Section>
       {variantIds && (
         <Query
