d7fdfe2d72ea3521cb34ab0f8ef681e9688250d0
Nick Watts <nwatts@broadinstitute.org>
2021-06-09 16:01:23 -0400

    Allow sorting variants by LoF curation verdict
    Resolves #774

--- browser/src/VariantList/sortUtilities.js
+++ browser/src/VariantList/sortUtilities.js
@@ -1,13 +1,24 @@
 export const isEmpty = val => val === undefined || val === null || val === ''
 
 export const makeCompareFunction = (key, fn) => (v1, v2) => {
-  if (isEmpty(v1[key])) {
+  let key1
+  let key2
+
+  if (typeof key === 'function') {
+    key1 = key(v1)
+    key2 = key(v2)
+  } else {
+    key1 = v1[key]
+    key2 = v2[key]
+  }
+
+  if (isEmpty(key1)) {
     return 1
   }
-  if (isEmpty(v2[key])) {
+  if (isEmpty(key2)) {
     return -1
   }
-  return fn(v1[key], v2[key])
+  return fn(key1, key2)
 }
 
 export const makeStringCompareFunction = key =>
