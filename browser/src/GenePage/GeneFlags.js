2acb91d7b8ab9c7f947820fc45c8a6c78cc27793
Nick Watts <nwatts@broadinstitute.org>
2021-06-14 13:25:53 -0400

    Reword warning for CHIP-associated genes

--- browser/src/GenePage/GeneFlags.js
+++ browser/src/GenePage/GeneFlags.js
@@ -8,13 +8,14 @@ const GeneFlags = ({ gene }) => {
     <>
       {gene.flags.includes('chip') && (
         <p>
-          <Badge level="warning">Note</Badge> This gene shows evidence of{' '}
+          <Badge level="warning">Note</Badge> Analysis of allele balance and age data indicates that
+          this gene shows evidence of{' '}
           <ExternalLink href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8050831/">
             clonal hematopoiesis of indeterminate potential (CHIP)
           </ExternalLink>
-          . The presence of somatic variants should be taken into account when interpreting the
-          penetrance, pathogenicity, and frequency of potential germline variants. For more
-          information, see pages 37-40 of{' '}
+          . The potential presence of somatic variants should be taken into account when
+          interpreting the penetrance, pathogenicity, and frequency of assumed germline variants.
+          For more information, see pages 37-40 of{' '}
           <ExternalLink href="https://static-content.springer.com/esm/art%3A10.1038%2Fs41586-020-2308-7/MediaObjects/41586_2020_2308_MOESM1_ESM.pdf">
             supplementary information
           </ExternalLink>{' '}
