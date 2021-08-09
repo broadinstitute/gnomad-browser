ddd3a275d29a283cf72a779c11c89accbfe4b3d3
Jkmcameron <jkmcameron@gmail.com>
2021-08-09 03:04:03 -0400

    Made the necessary additions so that Structural Variants can be looked up through the search bar.
    search.spec.js and Searchbox.spec.js seemingly contain unit tests and perhaps a simple search for a structural variant should be added to this list of tests? I'm not familiar enough with Javascript/Node.JS and not familiar enough with the code base to tinker with that.

--- graphql-api/src/queries/search.js
+++ graphql-api/src/queries/search.js
@@ -10,6 +10,41 @@ const { DATASET_REFERENCE_GENOMES } = require('../datasets')
 
 const fetchSearchResults = async (esClient, datasetId, query) => {
   // ==============================================================================================
+  // Structural Variants
+  // ==============================================================================================
+
+  const STRUCTURAL_VARIANT_ID_REGEX = /^(MCNV|INS|DEL)_(\d+|X|Y)_([1-9][0-9]*)$/i
+
+  const isStructuralVariantId = str => {
+    const match = STRUCTURAL_VARIANT_ID_REGEX.exec(str)
+    if (!match) {
+      return false
+    }
+
+    const chrom = match[2]
+    const chromNumber = Number(chrom)
+    if (!Number.isNaN(chromNumber) && (chromNumber < 1 || chromNumber > 22)) {
+      return false
+    }
+
+    const id = Number(match[3])
+    if (id > 1e9) {
+      return false
+    }
+
+    return true
+  }
+
+  if (isStructuralVariantId(query)) {
+    const structuralVariantId = query.toUpperCase()
+    return Promise.resolve([
+      {
+        label: structuralVariantId,
+        value: `/variant/${structuralVariantId}?dataset=${datasetId}`,
+      },
+    ])
+  }
+  // ==============================================================================================
   // Variants
   // ==============================================================================================
 
