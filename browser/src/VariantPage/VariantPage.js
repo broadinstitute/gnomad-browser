daab71bc26a64d5606484f833d110c093e61803c
Nick Watts <nwatts@broadinstitute.org>
2021-08-20 07:58:11 -0400

    Move variant page title component to a separate file

--- browser/src/VariantPage/VariantPage.js
+++ browser/src/VariantPage/VariantPage.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import styled from 'styled-components'
 
-import { Badge, Button, ExternalLink, Page, TooltipAnchor } from '@gnomad/ui'
+import { Badge, Button, ExternalLink, Page } from '@gnomad/ui'
 
 import { labelForDataset, referenceGenomeForDataset } from '../datasets'
 import Delayed from '../Delayed'
@@ -23,6 +23,7 @@ import VariantNotFound from './VariantNotFound'
 import { GnomadVariantOccurrenceTable } from './VariantOccurrenceTable'
 import VariantInSilicoPredictors from './VariantInSilicoPredictors'
 import VariantLoFCurationResults from './VariantLoFCurationResults'
+import VariantPageTitle from './VariantPageTitle'
 import VariantPopulationFrequencies from './VariantPopulationFrequencies'
 import VariantRelatedVariants, { variantHasRelatedVariants } from './VariantRelatedVariants'
 import VariantSiteQualityMetrics from './VariantSiteQualityMetrics'
@@ -173,74 +174,6 @@ VariantPageContent.propTypes = {
     in_silico_predictors: PropTypes.arrayOf(PropTypes.object), // eslint-disable-line react/forbid-prop-types
     transcript_consequences: PropTypes.arrayOf(PropTypes.object), // eslint-disable-line react/forbid-prop-types
   }).isRequired,
-}
-
-const TitleWrapper = styled.span`
-  display: inline-flex;
-  flex-direction: row;
-  align-items: center;
-  overflow: hidden;
-  max-width: 100%;
-
-  @media (max-width: 900px) {
-    flex-direction: column;
-  }
-`
-
-const Separator = styled.span`
-  @media (max-width: 900px) {
-    display: none;
-  }
-`
-
-const TitleAlleles = styled.span`
-  flex-shrink: 1;
-  overflow: hidden;
-  max-width: 100%;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-`
-
-const VariantPageTitle = ({ datasetId, variantId }) => {
-  const [chrom, pos, ref, alt] = variantId.split('-')
-
-  let variantDescription = 'Variant'
-  if (ref.length === 1 && alt.length === 1) {
-    variantDescription = 'Single nucleotide variant'
-  }
-  if (ref.length < alt.length) {
-    const insertionLength = alt.length - ref.length
-    variantDescription = `Insertion (${insertionLength} base${insertionLength > 1 ? 's' : ''})`
-  }
-  if (ref.length > alt.length) {
-    const deletionLength = ref.length - alt.length
-    variantDescription = `Deletion (${deletionLength} base${deletionLength > 1 ? 's' : ''})`
-  }
-
-  return (
-    <TitleWrapper>
-      <span>{variantDescription}</span>
-      <Separator style={{ width: '1ch' }}>:</Separator>
-      <TooltipAnchor tooltip={variantId}>
-        <TitleWrapper>
-          <span>
-            {chrom}-{pos}
-          </span>
-          <Separator>-</Separator>
-          <TitleAlleles>
-            {ref}-{alt}
-          </TitleAlleles>
-        </TitleWrapper>
-      </TooltipAnchor>
-      <Separator> </Separator>
-      <span>({referenceGenomeForDataset(datasetId)})</span>
-    </TitleWrapper>
-  )
-}
-
-VariantPageTitle.propTypes = {
-  datasetId: PropTypes.string.isRequired,
-  variantId: PropTypes.string.isRequired,
 }
 
 const variantQuery = `
