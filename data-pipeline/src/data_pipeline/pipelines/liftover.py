22675a1053a1805ea85ba8d4324652de28c5dff6
Nick Watts <nwatts@broadinstitute.org>
2021-08-20 15:13:52 -0400

    Update liftover data pipeline to use public Hail Tables
    Resolves #788

--- data-pipeline/src/data_pipeline/pipelines/liftover.py
+++ data-pipeline/src/data_pipeline/pipelines/liftover.py
@@ -1,5 +1,4 @@
 import hail as hl
-from hail.genetics import reference_genome
 
 from data_pipeline.data_types.variant import variant_id
 
@@ -13,19 +12,34 @@ pipeline = Pipeline()
 ###############################################
 
 
-def prepare_gnomad_v2_liftover(gnomad_v2_liftover_path, gnomad_v3_variants_path):
-    ds = hl.read_table(gnomad_v2_liftover_path)
+def prepare_gnomad_v2_liftover(
+    gnomad_v2_liftover_exomes_path, gnomad_v2_liftover_genomes_path, gnomad_v3_variants_path
+):
+    exome_liftover = hl.read_table(gnomad_v2_liftover_exomes_path)
+    exome_liftover = exome_liftover.select(
+        exome=hl.struct(original_locus=exome_liftover.original_locus, original_alleles=exome_liftover.original_alleles)
+    )
+    genome_liftover = hl.read_table(gnomad_v2_liftover_genomes_path).select("original_locus", "original_alleles")
+    genome_liftover = genome_liftover.select(
+        genome=hl.struct(
+            original_locus=genome_liftover.original_locus, original_alleles=genome_liftover.original_alleles
+        )
+    )
 
-    ds = ds.filter(hl.is_defined(ds.liftover_locus) & hl.is_defined(ds.liftover_alleles))
+    ds = exome_liftover.join(genome_liftover, "outer")
+    ds = ds.transmute(**hl.or_else(ds.exome, ds.genome))
 
+    ds = ds.select_globals()
     ds = ds.key_by()
+
+    ds = ds.rename({"locus": "liftover_locus", "alleles": "liftover_alleles"})
 
     ds = ds.select(
         source=hl.struct(
-            variant_id=variant_id(ds.locus, ds.alleles),
-            locus=ds.locus,
-            alleles=ds.alleles,
-            reference_genome=ds.locus.dtype.reference_genome.name,
+            variant_id=variant_id(ds.original_locus, ds.original_alleles),
+            locus=ds.original_locus,
+            alleles=ds.original_alleles,
+            reference_genome=ds.original_locus.dtype.reference_genome.name,
         ),
         liftover=hl.struct(
             variant_id=variant_id(ds.liftover_locus, ds.liftover_alleles),
@@ -53,7 +67,8 @@ pipeline.add_task(
     prepare_gnomad_v2_liftover,
     "/liftover.ht",
     {
-        "gnomad_v2_liftover_path": "gs://gnomad-browser/gnomad-liftover/output.ht",
+        "gnomad_v2_liftover_exomes_path": "gs://gnomad-public-requester-pays/release/2.1.1/liftover_grch38/ht/exomes/gnomad.exomes.r2.1.1.sites.liftover_grch38.ht",
+        "gnomad_v2_liftover_genomes_path": "gs://gnomad-public-requester-pays/release/2.1.1/liftover_grch38/ht/genomes/gnomad.genomes.r2.1.1.sites.liftover_grch38.ht",
         "gnomad_v3_variants_path": "gs://gcp-public-data--gnomad/release/3.1.1/ht/genomes/gnomad.genomes.v3.1.1.sites.ht",
     },
 )
