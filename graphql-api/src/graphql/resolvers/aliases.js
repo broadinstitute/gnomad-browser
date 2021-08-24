d552e5f4923fe0aa443465647e241d65b20d02fc
Nick Watts <nwatts@broadinstitute.org>
2021-08-24 10:11:22 -0400

    Add protein domains from VEP to API

--- graphql-api/src/graphql/resolvers/aliases.js
+++ graphql-api/src/graphql/resolvers/aliases.js
@@ -27,6 +27,7 @@ module.exports = {
     // Major consequence fields
     consequence: majorConsequenceFieldResolver('major_consequence'),
     consequence_in_canonical_transcript: majorConsequenceFieldResolver('canonical'),
+    domains: majorConsequenceFieldResolver('domains'),
     gene_id: majorConsequenceFieldResolver('gene_id'),
     gene_symbol: majorConsequenceFieldResolver('gene_symbol'),
     transcript_id: majorConsequenceFieldResolver('transcript_id'),
