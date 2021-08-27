5f8a83af35c9670fc2312f5b271eaaa5cf98cdb2
Nick Watts <nwatts@broadinstitute.org>
2021-08-27 10:30:50 -0400

    Update STR attribute labels

--- browser/src/ShortTandemRepeatPage/ShortTandemRepeatAttributes.js
+++ browser/src/ShortTandemRepeatPage/ShortTandemRepeatAttributes.js
@@ -35,10 +35,10 @@ const ShortTandemRepeatAttributes = ({ shortTandemRepeat }) => {
           {shortTandemRepeat.associated_disease.name}
         </ExternalLink>
       </AttributeList.Item>
-      <AttributeList.Item label="Normal threshold">
+      <AttributeList.Item label="Normal range">
         &le; {shortTandemRepeat.associated_disease.normal_threshold} repeats
       </AttributeList.Item>
-      <AttributeList.Item label="Pathogenic threshold">
+      <AttributeList.Item label="Pathogenic range">
         &ge; {shortTandemRepeat.associated_disease.pathogenic_threshold} repeats
       </AttributeList.Item>
     </AttributeList>
