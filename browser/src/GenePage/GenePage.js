3fae47e4be836ff9f6cb55167faba1c6efdee7e5
Nick Watts <nwatts@broadinstitute.org>
2021-08-31 11:20:37 -0400

    Add STRs to gene and region pages

--- browser/src/GenePage/GenePage.js
+++ browser/src/GenePage/GenePage.js
@@ -6,13 +6,14 @@ import styled from 'styled-components'
 
 import { Track } from '@gnomad/region-viewer'
 import { TranscriptPlot } from '@gnomad/track-transcripts'
-import { Button } from '@gnomad/ui'
+import { Badge, Button } from '@gnomad/ui'
 
 import ConstraintTable from '../ConstraintTable/ConstraintTable'
 import { labelForDataset } from '../datasets'
 import DocumentTitle from '../DocumentTitle'
 import GnomadPageHeading from '../GnomadPageHeading'
 import InfoButton from '../help/InfoButton'
+import Link from '../Link'
 import RegionalConstraintTrack from '../RegionalConstraintTrack'
 import RegionCoverageTrack from '../RegionPage/RegionCoverageTrack'
 import RegionViewer from '../RegionViewer/ZoomableRegionViewer'
@@ -220,6 +221,15 @@ const GenePage = ({ datasetId, gene, geneId }) => {
           <div style={{ maxWidth: '50%' }}>
             <GeneInfo gene={gene} />
             <GeneFlags gene={gene} />
+            {gene.short_tandem_repeats && gene.short_tandem_repeats.length > 0 && (
+              <p>
+                <Badge level="info">Note</Badge> This gene contains a{' '}
+                <Link to={`/short-tandem-repeat/${gene.short_tandem_repeats[0].id}`}>
+                  short tandem repeat
+                </Link>
+                .
+              </p>
+            )}
           </div>
           <div>
             <h2>Constraint {gene.chrom !== 'M' && <InfoButton topic="constraint" />}</h2>
@@ -460,6 +470,11 @@ GenePage.propTypes = {
       ).isRequired,
       flags: PropTypes.arrayOf(PropTypes.string).isRequired,
     }),
+    short_tandem_repeats: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+      })
+    ),
     // eslint-disable-next-line react/forbid-prop-types
     exac_regional_missense_constraint_regions: PropTypes.any,
   }).isRequired,
