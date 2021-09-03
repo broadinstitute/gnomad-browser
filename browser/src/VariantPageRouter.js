d4f1487c6ad6533e23f795d06deb50ba42e7ab93
Nick Watts <nwatts@broadinstitute.org>
2021-09-03 11:54:52 -0400

    Refactor variant page search for non-gnomAD IDs

--- browser/src/VariantPageRouter.js
+++ browser/src/VariantPageRouter.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
 import queryString from 'query-string'
-import React, { lazy } from 'react'
+import React, { lazy, useCallback } from 'react'
 import { Redirect } from 'react-router-dom'
 
 import { isVariantId, normalizeVariantId, isRsId } from '@gnomad/identifiers'
@@ -9,7 +9,8 @@ import { Badge, List, ListItem, Page, PageHeading } from '@gnomad/ui'
 import { labelForDataset } from './datasets'
 import DocumentTitle from './DocumentTitle'
 import Link from './Link'
-import Query from './Query'
+import useRequest from './useRequest'
+import StatusMessage from './StatusMessage'
 
 const MitochondrialVariantPage = lazy(() =>
   import('./MitochondrialVariantPage/MitochondrialVariantPage')
@@ -18,13 +19,81 @@ const MNVPage = lazy(() => import('./MNVPage/MNVPage'))
 const StructuralVariantPage = lazy(() => import('./StructuralVariantPage/StructuralVariantPage'))
 const VariantPage = lazy(() => import('./VariantPage/VariantPage'))
 
-const VARIANT_SEARCH_QUERY = `
-query VariantSearch($query: String!, $datasetId: DatasetId!) {
-  variant_search(query: $query, dataset: $datasetId) {
-    variant_id
-  }
+const fetchVariantSearchResults = (datasetId, query) => {
+  return fetch('/api/', {
+    body: JSON.stringify({
+      query: `
+        query VariantSearch($query: String!, $datasetId: DatasetId!) {
+          variant_search(query: $query, dataset: $datasetId) {
+            variant_id
+          }
+        }
+      `,
+      variables: { datasetId, query },
+    }),
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  })
+    .then(response => response.json())
+    .then(response => {
+      if (!response.data.variant_search) {
+        throw new Error('Unable to retrieve search results')
+      }
+
+      return response.data.variant_search.map(result => result.variant_id)
+    })
 }
-`
+
+const VariantSearch = ({ datasetId, query }) => {
+  const search = useCallback(() => fetchVariantSearchResults(datasetId, query), [datasetId, query])
+  const { isLoading, response: matchingVariants, error } = useRequest(search)
+
+  if (isLoading) {
+    return <StatusMessage>Searching variants</StatusMessage>
+  }
+  if (error || !matchingVariants) {
+    return <StatusMessage>Unable to complete search</StatusMessage>
+  }
+  if (matchingVariants.length === 0) {
+    return <p>No matching variants found.</p>
+  }
+
+  if (matchingVariants.length === 1) {
+    return (
+      <Redirect
+        to={{
+          pathname: `/variant/${matchingVariants[0]}`,
+          search: queryString.stringify({ dataset: datasetId }),
+        }}
+      />
+    )
+  }
+
+  return (
+    <>
+      <p style={{ fontSize: '16px' }}>Multiple matching variants found:</p>
+      <List>
+        {matchingVariants.map(variantId => (
+          <ListItem key={variantId}>
+            <Link
+              to={{
+                pathname: `/variant/${variantId}`,
+                search: queryString.stringify({ dataset: datasetId }),
+              }}
+            >
+              {variantId}
+            </Link>
+          </ListItem>
+        ))}
+      </List>
+    </>
+  )
+}
+
+VariantSearch.propTypes = {
+  datasetId: PropTypes.string.isRequired,
+  query: PropTypes.string.isRequired,
+}
 
 const VariantSearchPage = ({ datasetId, query }) => {
   return (
@@ -40,52 +109,7 @@ const VariantSearchPage = ({ datasetId, query }) => {
         </p>
       )}
 
-      <Query
-        query={VARIANT_SEARCH_QUERY}
-        variables={{ datasetId, query }}
-        loadingMessage="Searching variants"
-        errorMessage="Unable to complete search"
-        success={data => data.variant_search}
-      >
-        {({ data }) => {
-          const matchingVariants = data.variant_search.map(result => result.variant_id)
-
-          if (matchingVariants.length === 0) {
-            return <p>No matching variants found.</p>
-          }
-
-          if (matchingVariants.length === 1) {
-            return (
-              <Redirect
-                to={{
-                  pathname: `/variant/${matchingVariants[0]}`,
-                  search: queryString.stringify({ dataset: datasetId }),
-                }}
-              />
-            )
-          }
-
-          return (
-            <>
-              <p style={{ fontSize: '16px' }}>Multiple matching variants found:</p>
-              <List>
-                {matchingVariants.map(variantId => (
-                  <ListItem key={variantId}>
-                    <Link
-                      to={{
-                        pathname: `/variant/${variantId}`,
-                        search: queryString.stringify({ dataset: datasetId }),
-                      }}
-                    >
-                      {variantId}
-                    </Link>
-                  </ListItem>
-                ))}
-              </List>
-            </>
-          )
-        }}
-      </Query>
+      <VariantSearch datasetId={datasetId} query={query} />
     </Page>
   )
 }
