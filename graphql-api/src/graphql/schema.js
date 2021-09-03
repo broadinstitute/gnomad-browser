661a3d1fe6a6311110b77d98634ee57a0db4e3ba
Nick Watts <nwatts@broadinstitute.org>
2021-09-03 11:27:52 -0400

    Remove generic search API
    Replaced by gene_search and variant_search.

--- graphql-api/src/graphql/schema.js
+++ graphql-api/src/graphql/schema.js
@@ -17,7 +17,6 @@ const mitochondrialVariantResolvers = require('./resolvers/mitochondrial-variant
 const multiNucleotideVariantResolves = require('./resolvers/multi-nucleotide-variants')
 const regionResolvers = require('./resolvers/region')
 const regionFieldResolvers = require('./resolvers/region-fields')
-const searchResolvers = require('./resolvers/search')
 const structuralVariantResolvers = require('./resolvers/structural-variants')
 const transcriptResolvers = require('./resolvers/transcript')
 const transcriptFieldResolvers = require('./resolvers/transcript-fields')
@@ -44,7 +43,6 @@ const resolvers = mergeResolvers([
   multiNucleotideVariantResolves,
   regionResolvers,
   regionFieldResolvers,
-  searchResolvers,
   structuralVariantResolvers,
   transcriptResolvers,
   transcriptFieldResolvers,
