1435449a3709e89547773f17ad4b19e5bf860ce8
Nick Watts <nwatts@broadinstitute.org>
2021-10-28 11:54:31 -0400

    Remove workaround for rsIDs issue in data pipeline

--- graphql-api/src/graphql/resolvers/variant-fields.js
+++ graphql-api/src/graphql/resolvers/variant-fields.js
@@ -1,12 +1,8 @@
 module.exports = {
   Variant: {
-    // Workaround for #743 (https://github.com/broadinstitute/gnomad-browser/issues/743)
-    // gnomAD v2/ExAC variants with no rsID have a set containing null in the rsids field.
-    rsids: (obj) => (obj.rsids || []).filter((rsid) => rsid !== null),
+    rsids: (obj) => obj.rsids || [],
   },
   VariantDetails: {
-    // Workaround for #743 (https://github.com/broadinstitute/gnomad-browser/issues/743)
-    // gnomAD v2/ExAC variants with no rsID have a set containing null in the rsids field.
-    rsids: (obj) => (obj.rsids || []).filter((rsid) => rsid !== null),
+    rsids: (obj) => obj.rsids || [],
   },
 }
