9e6a3411868eef8ebf53427742a23c7993b2364e
Nick Watts <nwatts@broadinstitute.org>
2021-12-15 17:00:18 -0500

    Update pre-commit configuration
    - Add Prettier hook
    - Update pre-commit-hooks library

--- browser/src/MitochondrialVariantPage/MitochondrialVariantReferenceList.js
+++ browser/src/MitochondrialVariantPage/MitochondrialVariantReferenceList.js
@@ -6,8 +6,10 @@ import MitochondrialVariantDetailPropType from './MitochondrialVariantDetailProp
 
 const MitochondrialVariantReferenceList = ({ variant }) => {
   const { pos, ref, alt } = variant
-  /* eslint-disable-next-line prettier/prettier */
-  const ucscURL = `https://genome.ucsc.edu/cgi-bin/hgTracks?db=hg38&highlight=hg38.chrM%3A${pos}-${pos + (ref.length - 1)}&position=chrM%3A${pos - 25}-${pos + (ref.length - 1) + 25}`
+
+  const ucscURL = `https://genome.ucsc.edu/cgi-bin/hgTracks?db=hg38&highlight=hg38.chrM%3A${pos}-${
+    pos + (ref.length - 1)
+  }&position=chrM%3A${pos - 25}-${pos + (ref.length - 1) + 25}`
 
   const mitomapURL = `https://mitomap.org/cgi-bin/search_allele?variant=${encodeURIComponent(
     `${pos}${ref}>${alt}`
