fa310e3c02dc2bd239711bb0785b5f44d3b69758
Nick Watts <nwatts@broadinstitute.org>
2022-01-05 11:03:08 -0500

    Add classifications to STR genotype distribution repeat units menu

--- browser/src/ShortTandemRepeatPage/ShortTandemRepeatGenotypeDistributionRepeatUnitsSelect.js
+++ browser/src/ShortTandemRepeatPage/ShortTandemRepeatGenotypeDistributionRepeatUnitsSelect.js
@@ -8,6 +8,15 @@ const ShortTandemRepeatGenotypeDistributionRepeatUnitsSelect = ({
   value,
   onChange,
 }) => {
+  // Adjacent repeats do not have classifications for repeat units.
+  const isAdjacentRepeat = !shortTandemRepeatOrAdjacentRepeat.associated_diseases
+  const repeatUnitClassifications = isAdjacentRepeat
+    ? {}
+    : shortTandemRepeatOrAdjacentRepeat.repeat_units.reduce(
+        (acc, repeatUnit) => ({ ...acc, [repeatUnit.repeat_unit]: repeatUnit.classification }),
+        {}
+      )
+
   return (
     <label
       htmlFor={`short-tandem-repeat-${shortTandemRepeatOrAdjacentRepeat.id}-genotype-distribution-repeat-units`}
@@ -29,12 +38,23 @@ const ShortTandemRepeatGenotypeDistributionRepeatUnitsSelect = ({
             return (
               <option key={optionValue} value={optionValue}>
                 {repeatUnitDistribution.repeat_units
-                  .map(repeatUnit =>
-                    repeatUnit === shortTandemRepeatOrAdjacentRepeat.reference_repeat_unit &&
-                    shortTandemRepeatOrAdjacentRepeat.repeat_units.length > 1
-                      ? `${repeatUnit} (reference)`
-                      : repeatUnit
-                  )
+                  .map(repeatUnit => {
+                    const notes = []
+                    if (repeatUnitClassifications[repeatUnit]) {
+                      notes.push(repeatUnitClassifications[repeatUnit])
+                    }
+                    if (repeatUnit === shortTandemRepeatOrAdjacentRepeat.reference_repeat_unit) {
+                      notes.push('reference')
+                    }
+
+                    if (
+                      shortTandemRepeatOrAdjacentRepeat.repeat_units.length > 1 &&
+                      notes.length > 0
+                    ) {
+                      return `${repeatUnit} (${notes.join(', ')})`
+                    }
+                    return repeatUnit
+                  })
                   .join(' / ')}
               </option>
             )
@@ -48,6 +68,7 @@ const ShortTandemRepeatGenotypeDistributionRepeatUnitsSelect = ({
 ShortTandemRepeatGenotypeDistributionRepeatUnitsSelect.propTypes = {
   shortTandemRepeatOrAdjacentRepeat: PropTypes.shape({
     id: PropTypes.string.isRequired,
+    associated_diseases: PropTypes.arrayOf(PropTypes.object),
     reference_repeat_unit: PropTypes.string.isRequired,
     genotype_distribution: PropTypes.shape({
       repeat_units: PropTypes.arrayOf(
