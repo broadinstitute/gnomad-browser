909c2759d9a49e0b88f5a57b32ad5cd45d327553
Nick Watts <nwatts@broadinstitute.org>
2022-01-20 10:33:36 -0500

    Move STR associated diseases table to separate component

--- browser/src/ShortTandemRepeatPage/ShortTandemRepeatPage.js
+++ browser/src/ShortTandemRepeatPage/ShortTandemRepeatPage.js
@@ -3,17 +3,7 @@ import PropTypes from 'prop-types'
 import React, { useState } from 'react'
 import styled from 'styled-components'
 
-import {
-  Badge,
-  BaseTable,
-  Button,
-  ExternalLink,
-  List,
-  ListItem,
-  Modal,
-  Page,
-  Select,
-} from '@gnomad/ui'
+import { Badge, Button, ExternalLink, List, ListItem, Modal, Page, Select } from '@gnomad/ui'
 
 import { labelForDataset } from '../datasets'
 import DocumentTitle from '../DocumentTitle'
@@ -26,6 +16,7 @@ import ControlSection from '../VariantPage/ControlSection'
 
 import ShortTandemRepeatAdjacentRepeat from './ShortTandemRepeatAdjacentRepeat'
 import ShortTandemRepeatAgeDistributionPlot from './ShortTandemRepeatAgeDistributionPlot'
+import ShortTandemRepeatAssociatedDiseasesTable from './ShortTandemRepeatAssociatedDiseasesTable'
 import ShortTandemRepeatAttributes from './ShortTandemRepeatAttributes'
 import ShortTandemRepeatPopulationOptions from './ShortTandemRepeatPopulationOptions'
 import { ShortTandemRepeatPropType } from './ShortTandemRepeatPropTypes'
@@ -176,46 +167,7 @@ const ShortTandemRepeatPage = ({ datasetId, shortTandemRepeat }) => {
           Associated Diseases <InfoButton topic="str-associated-diseases" />
         </h2>
         <TableWrapper>
-          <BaseTable style={{ minWidth: '100%' }}>
-            <thead>
-              <tr>
-                <th scope="col">Disease</th>
-                <th scope="col">OMIM</th>
-                <th scope="col">Inheritance</th>
-                <th scope="col">Ranges of repeats</th>
-              </tr>
-            </thead>
-            <tbody>
-              {shortTandemRepeat.associated_diseases.map(disease => {
-                return (
-                  <tr key={disease.name}>
-                    <th scope="row">{disease.name}</th>
-                    <td>
-                      {disease.omim_id && (
-                        <ExternalLink href={`https://omim.org/entry/${disease.omim_id}`}>
-                          {disease.omim_id}
-                        </ExternalLink>
-                      )}
-                    </td>
-                    <td>{disease.inheritance_mode}</td>
-                    <td>
-                      {disease.repeat_size_classifications
-                        .map(classification => {
-                          if (classification.min === null) {
-                            return `${classification.classification} ≤ ${classification.max}`
-                          }
-                          if (classification.max === null) {
-                            return `${classification.classification} ≥ ${classification.min}`
-                          }
-                          return `${classification.classification} ${classification.min} - ${classification.max}`
-                        })
-                        .join(', ')}
-                    </td>
-                  </tr>
-                )
-              })}
-            </tbody>
-          </BaseTable>
+          <ShortTandemRepeatAssociatedDiseasesTable shortTandemRepeat={shortTandemRepeat} />
         </TableWrapper>
       </section>
 
