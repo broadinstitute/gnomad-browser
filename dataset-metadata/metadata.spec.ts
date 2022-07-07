8755fb0eb54aa1976668ceec87fa21cab62dd82c
Phil Darnowsky <pdarnows@broadinstitute.org>
2022-07-07 10:21:32 -0400

    introduce hasShortVariants metadata field

--- dataset-metadata/metadata.spec.ts
+++ dataset-metadata/metadata.spec.ts
@@ -1,5 +1,5 @@
 import { describe, test, expect } from '@jest/globals'
-import { DatasetId, isSubset, labelForDataset } from './metadata'
+import { DatasetId, isSubset, labelForDataset, hasShortVariants } from './metadata'
 
 describe.each([
   ['exac', false],
@@ -41,4 +41,26 @@ describe.each([
 ] as [DatasetId, string][])('labelForDataset(%s)', (datasetId, expectedResult) => {
   test(`Label for ${datasetId} is "${expectedResult}"`, () =>
     expect(labelForDataset(datasetId)).toEqual(expectedResult))
+})
+
+describe.each([
+  ['exac', true],
+  ['gnomad_r2_1', true],
+  ['gnomad_r2_1_controls', true],
+  ['gnomad_r2_1_non_cancer', true],
+  ['gnomad_r2_1_non_neuro', true],
+  ['gnomad_r2_1_non_topmed', true],
+  ['gnomad_r3', true],
+  ['gnomad_r3_controls_and_biobanks', true],
+  ['gnomad_r3_non_cancer', true],
+  ['gnomad_r3_non_neuro', true],
+  ['gnomad_r3_non_topmed', true],
+  ['gnomad_r3_non_v2', true],
+  ['gnomad_sv_r2_1', false],
+  ['gnomad_sv_r2_1_controls', false],
+  ['gnomad_sv_r2_1_non_neuro', false],
+] as [DatasetId, boolean][])('hasShortVariants(%s)', (datasetId, expectedResult) => {
+  const verbPhrase = expectedResult ? 'has' : 'does not have'
+  test(`${datasetId} ${verbPhrase} short variants`, () =>
+    expect(hasShortVariants(datasetId)).toEqual(expectedResult))
 })
