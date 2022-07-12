9da4902ee705fc7b0cff40b64cdb1d2bc9b73493
Phil Darnowsky <pdarnows@broadinstitute.org>
2022-07-12 14:52:22 -0400

    Set up for snapshot testing, add example test
    resolves #930

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
           '<rootDir>/tests/__mocks__/fileMock.js',
       },
-      testMatch: ['<rootDir>/browser/**/*.spec.js'],
+      testMatch: ['<rootDir>/browser/**/*.spec.js?(x)'],
       setupFilesAfterEnv: ['@testing-library/jest-dom'],
     },
     {
