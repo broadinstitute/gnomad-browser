bf2b40a39a9907859a9a1a685ce2875b10e94d38
Phil Darnowsky <pdarnows@broadinstitute.org>
2022-07-21 15:06:30 -0400

    Prepare Prettier for TypeScript
    While we're overhauling a lot of files, this is also a good opportunity to reformat code to make our usage of parens consistent.

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,18 +2,4 @@ module.exports = {
   printWidth: 100,
   semi: false,
   singleQuote: true,
-  overrides: [
-    {
-      files: 'browser/**/*.js',
-      options: {
-        arrowParens: 'avoid',
-      },
-    },
-    {
-      files: 'reads/**/*.js',
-      options: {
-        arrowParens: 'avoid',
-      },
-    },
-  ],
 }
