f30c4d9cabbe88417ec547029d2411e0169d88ac
Phil Darnowsky <pdarnows@broadinstitute.org>
2022-08-22 13:43:46 -0400

    Get rid of some dead code

--- browser/src/datasets.ts
+++ browser/src/datasets.ts
@@ -1,5 +1,3 @@
-import { datasetLabels } from '../../dataset-metadata/metadata'
-
 export const referenceGenomeForDataset = (datasetId: any) => {
   if (datasetId.startsWith('gnomad_r3')) {
     return 'GRCh38'
@@ -7,6 +5,3 @@ export const referenceGenomeForDataset = (datasetId: any) => {
 
   return 'GRCh37'
 }
-
-// @ts-expect-error TS(7053) FIXME: Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
-export const labelForDataset = (datasetId: any) => datasetLabels[datasetId] || 'Unknown'
