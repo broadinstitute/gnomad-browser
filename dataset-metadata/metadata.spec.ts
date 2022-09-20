801f8e03cfe45836f22bd3b864d2de79ab7fd578
Phil Darnowsky <pdarnows@broadinstitute.org>
2022-09-20 12:51:46 -0400

    Introduce hasExomeCoverage helper

--- dataset-metadata/metadata.spec.ts
+++ dataset-metadata/metadata.spec.ts
@@ -2,6 +2,7 @@ import { describe, test, expect } from '@jest/globals'
 import {
   DatasetId,
   hasConstraints,
+  hasExomeCoverage,
   hasShortVariants,
   hasStructuralVariants,
   isSubset,
@@ -137,4 +138,26 @@ describe.each([
 ] as [DatasetId, ReferenceGenome][])('referenceGenome(%s)', (datasetId, expectedResult) => {
   test(`${datasetId} uses reference genome ${expectedResult}`, () =>
     expect(referenceGenome(datasetId)).toEqual(expectedResult))
+})
+
+describe.each([
+  ['exac', true],
+  ['gnomad_r2_1', true],
+  ['gnomad_r2_1_controls', true],
+  ['gnomad_r2_1_non_cancer', true],
+  ['gnomad_r2_1_non_neuro', true],
+  ['gnomad_r2_1_non_topmed', true],
+  ['gnomad_r3', false],
+  ['gnomad_r3_controls_and_biobanks', false],
+  ['gnomad_r3_non_cancer', false],
+  ['gnomad_r3_non_neuro', false],
+  ['gnomad_r3_non_topmed', false],
+  ['gnomad_r3_non_v2', false],
+  ['gnomad_sv_r2_1', true],
+  ['gnomad_sv_r2_1_controls', true],
+  ['gnomad_sv_r2_1_non_neuro', true],
+] as [DatasetId, boolean][])('hasExomeCoverage(%s)', (datasetId, expectedResult) => {
+  const verbPhrase = expectedResult ? 'has' : 'does not have'
+  test(`${datasetId} ${verbPhrase} exome coverage`, () =>
+    expect(hasExomeCoverage(datasetId)).toEqual(expectedResult))
 })
--- jest.config.ts
+++ jest.config.ts
@@ -7,6 +7,8 @@ module.exports = {
       moduleNameMapper: {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
           '<rootDir>/tests/__mocks__/fileMock.js',
+        // workaround for yet another extremely cursed aspect of the JS stack
+        '^lodash-es$': 'lodash',
       },
       testMatch: ['<rootDir>/browser/**/*.spec.(js|jsx|ts|tsx)'],
       setupFilesAfterEnv: ['@testing-library/jest-dom'],
