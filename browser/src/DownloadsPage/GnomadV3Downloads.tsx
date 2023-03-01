621244279b82c91408d70e3cace97bf1d1d8bc06
Riley Grant <grantrileyh@gmail.com>
2023-03-01 14:09:29 -0600

    Update genomic constraint file locations on Downloads page

--- browser/src/DownloadsPage/GnomadV3Downloads.tsx
+++ browser/src/DownloadsPage/GnomadV3Downloads.tsx
@@ -359,40 +359,28 @@ const GnomadV3Downloads = () => (
         <ListItem>
           <GenericDownloadLinks
             label="README"
-            gcsBucket="gnomad-nc-constraint-v31-paper"
-            path="/download_files/nc_constraint_gnomad_v31_README.docx"
-            includeAWS={false}
-            includeAzure={false}
+            path="/release/3.1/secondary_analyses/genomic_constraint/nc_constraint_gnomad_v31_README.docx"
           />
         </ListItem>
         {/* @ts-expect-error TS(2745) FIXME: This JSX tag's 'children' prop expects type 'never... Remove this comment to see the full error message */}
         <ListItem>
           <GenericDownloadLinks
             label="Raw genomic constraint by 1kb regions"
-            gcsBucket="gnomad-nc-constraint-v31-paper"
-            path="/download_files/constraint_z_genome_1kb.raw.download.txt.gz"
-            includeAWS={false}
-            includeAzure={false}
+            path="/release/3.1/secondary_analyses/genomic_constraint/constraint_z_genome_1kb.raw.download.txt.gz"
           />
         </ListItem>
         {/* @ts-expect-error TS(2745) FIXME: This JSX tag's 'children' prop expects type 'never... Remove this comment to see the full error message */}
         <ListItem>
           <GenericDownloadLinks
             label="QCed genomic constraint by 1kb regions"
-            gcsBucket="gnomad-nc-constraint-v31-paper"
-            path="/download_files/constraint_z_genome_1kb.qc.download.txt.gz"
-            includeAWS={false}
-            includeAzure={false}
+            path="/release/3.1/secondary_analyses/genomic_constraint/constraint_z_genome_1kb.qc.download.txt.gz"
           />
         </ListItem>
         {/* @ts-expect-error TS(2745) FIXME: This JSX tag's 'children' prop expects type 'never... Remove this comment to see the full error message */}
         <ListItem>
           <GenericDownloadLinks
             label="Non-coding constraint for gene tissue enhancers"
-            gcsBucket="gnomad-nc-constraint-v31-paper"
-            path="/download_files/constraint_z_enh_gene_roadmaplinks.all.download.txt.gz"
-            includeAWS={false}
-            includeAzure={false}
+            path="/release/3.1/secondary_analyses/genomic_constraint/constraint_z_enh_gene_roadmaplinks.all.download.txt.gz"
           />
         </ListItem>
       </FileList>
