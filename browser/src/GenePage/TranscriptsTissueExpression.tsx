1f7d350563706665afb2b493ae9c6a1e4762a07f
Phil Darnowsky <pdarnows@broadinstitute.org>
2023-03-24 10:47:04 -0400

    Add type and factory for gtex

--- browser/src/GenePage/TranscriptsTissueExpression.tsx
+++ browser/src/GenePage/TranscriptsTissueExpression.tsx
@@ -35,6 +35,62 @@ type OwnTranscriptsTissueExpressionProps = {
   defaultSortTissuesBy?: 'alphabetical' | 'mean-expression'
 }
 
+export type GtexTissueExpression = {
+  adipose_subcutaneous: number
+  adipose_visceral_omentum: number
+  adrenal_gland: number
+  artery_aorta: number
+  artery_coronary: number
+  artery_tibial: number
+  bladder: number
+  brain_amygdala: number
+  brain_anterior_cingulate_cortex_ba24: number
+  brain_caudate_basal_ganglia: number
+  brain_cerebellar_hemisphere: number
+  brain_cerebellum: number
+  brain_cortex: number
+  brain_frontal_cortex_ba9: number
+  brain_hippocampus: number
+  brain_hypothalamus: number
+  brain_nucleus_accumbens_basal_ganglia: number
+  brain_putamen_basal_ganglia: number
+  brain_spinal_cord_cervical_c_1: number
+  brain_substantia_nigra: number
+  breast_mammary_tissue: number
+  cells_ebv_transformed_lymphocytes: number
+  cells_transformed_fibroblasts: number
+  cervix_ectocervix: number
+  cervix_endocervix: number
+  colon_sigmoid: number
+  colon_transverse: number
+  esophagus_gastroesophageal_junction: number
+  esophagus_mucosa: number
+  esophagus_muscularis: number
+  fallopian_tube: number
+  heart_atrial_appendage: number
+  heart_left_ventricle: number
+  kidney_cortex: number
+  liver: number
+  lung: number
+  minor_salivary_gland: number
+  muscle_skeletal: number
+  nerve_tibial: number
+  ovary: number
+  pancreas: number
+  pituitary: number
+  prostate: number
+  skin_not_sun_exposed_suprapubic: number
+  skin_sun_exposed_lower_leg: number
+  small_intestine_terminal_ileum: number
+  spleen: number
+  stomach: number
+  testis: number
+  thyroid: number
+  uterus: number
+  vagina: number
+  whole_blood: number
+}
+
 // @ts-expect-error TS(2456) FIXME: Type alias 'TranscriptsTissueExpressionProps' circ... Remove this comment to see the full error message
 type TranscriptsTissueExpressionProps = OwnTranscriptsTissueExpressionProps &
   typeof TranscriptsTissueExpression.defaultProps
