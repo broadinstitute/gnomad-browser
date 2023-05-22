c83dc09ef11d29c4b85af65b211830c460f72cf2
Phil Darnowsky <pdarnows@broadinstitute.org>
2023-05-22 14:33:51 -0400

    Remove dataset ID conditional from variant feedback form

--- browser/src/variantFeedback.ts
+++ browser/src/variantFeedback.ts
@@ -1,21 +1,16 @@
 import queryString from 'query-string'
+import { variantFeedbackDescription } from '@gnomad/dataset-metadata/metadata'
 
 export const variantFeedbackUrl = (variant: any, datasetId: any) => {
   if (process.env.REPORT_VARIANT_URL) {
-    const params = {}
+    const params: Record<string, string> = {}
     if (process.env.REPORT_VARIANT_VARIANT_ID_PARAMETER) {
-      // @ts-expect-error TS(7053) FIXME: Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
       params[process.env.REPORT_VARIANT_VARIANT_ID_PARAMETER] = variant.variant_id
     }
-    if (process.env.REPORT_VARIANT_DATASET_PARAMETER) {
-      if (datasetId.startsWith('gnomad_r3')) {
-        // @ts-expect-error TS(7053) FIXME: Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
-        params[process.env.REPORT_VARIANT_DATASET_PARAMETER] = 'gnomAD v3'
-      }
-      if (datasetId.startsWith('gnomad_r2') || datasetId.startsWith('gnomad_sv_r2')) {
-        // @ts-expect-error TS(7053) FIXME: Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
-        params[process.env.REPORT_VARIANT_DATASET_PARAMETER] = 'gnomAD v2'
-      }
+
+    const datasetName = variantFeedbackDescription(datasetId)
+    if (datasetName && process.env.REPORT_VARIANT_DATASET_PARAMETER) {
+      params[process.env.REPORT_VARIANT_DATASET_PARAMETER] = datasetName
     }
     return `${process.env.REPORT_VARIANT_URL}?${queryString.stringify(params)}`
   }
