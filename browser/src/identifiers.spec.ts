c9fdd4590bd3a07ef78a29dd163461e4535b7fcd
Phil Darnowsky <pdarnows@broadinstitute.org>
2023-06-21 16:18:40 -0400

    Search recognizes V3 SV ID format

--- browser/src/identifiers.spec.ts
+++ browser/src/identifiers.spec.ts
@@ -1,17 +1,37 @@
 import { describe, test, expect } from '@jest/globals'
 import { isStructuralVariantId } from './identifiers'
+import { forDatasetsMatching } from '../../tests/__helpers__/datasets'
 
 describe('isStructuralVariantId', () => {
-  const v2VariantTypes = ['BND', 'CPX', 'CTX', 'DEL', 'DUP', 'INS', 'INV', 'MCNV', 'OTH']
   const chromosomes = ['1', '12', 'X', 'Y']
-  const positions = ['3', '63', '963']
 
-  v2VariantTypes.forEach((v2VariantType) => {
-    chromosomes.forEach((chromosome) => {
-      positions.forEach((position) => {
-        const variantId = [v2VariantType, chromosome, position].join('_')
-        test(`recognizes ${variantId} as a v2 variant ID`, () => {
-          expect(isStructuralVariantId(variantId)).toEqual(true)
+  forDatasetsMatching(/gnomad_sv_r2/, 'with dataset %s', (datasetId) => {
+    const variantTypes = ['BND', 'CPX', 'CTX', 'DEL', 'DUP', 'INS', 'INV', 'MCNV', 'OTH']
+    const positions = ['3', '63', '963']
+
+    variantTypes.forEach((variantType) => {
+      chromosomes.forEach((chromosome) => {
+        positions.forEach((position) => {
+          const variantId = [variantType, chromosome, position].join('_')
+          test(`recognizes ${variantId} as a v2 variant ID`, () => {
+            expect(isStructuralVariantId(variantId, datasetId)).toEqual(true)
+          })
+        })
+      })
+    })
+  })
+
+  forDatasetsMatching(/gnomad_sv_r3/, 'with dataset %s', (datasetId) => {
+    const variantTypes = ['BND', 'CPX', 'CTX', 'DEL', 'DUP', 'INS', 'INV', 'CNV']
+    const suffixes = ['0F1E2D3C', 'DEADBEEF', '12345678']
+
+    variantTypes.forEach((variantType) => {
+      chromosomes.forEach((chromosome) => {
+        suffixes.forEach((suffix) => {
+          const variantId = [variantType, `CHR${chromosome}`, suffix].join('_')
+          test(`recognizes ${variantId} as a v3 variant ID`, () => {
+            expect(isStructuralVariantId(variantId, datasetId)).toEqual(true)
+          })
         })
       })
     })
--- browser/src/identifiers.ts
+++ browser/src/identifiers.ts
@@ -1,7 +1,17 @@
-const STRUCTURAL_VARIANT_ID_REGEX = /^(BND|CPX|CTX|DEL|DUP|INS|INV|MCNV|OTH)_(\d+|X|Y)_([1-9][0-9]*)$/i
+import { DatasetId, isV3SVs } from '@gnomad/dataset-metadata/metadata'
 
-export const isStructuralVariantId = (str: any) => {
-  const match = STRUCTURAL_VARIANT_ID_REGEX.exec(str)
+const V2_STRUCTURAL_VARIANT_ID_REGEX =
+  /^(BND|CPX|CTX|DEL|DUP|INS|INV|MCNV|OTH)_(\d+|X|Y)_([1-9][0-9]*)$/i
+
+const V3_STRUCTURAL_VARIANT_ID_REGEX =
+  /^(BND|CPX|CTX|DEL|DUP|INS|INV|CNV)_CHR(\d+|X|Y)_([0-9a-f]*)$/i
+
+export const isStructuralVariantId = (str: string, datasetId: DatasetId) => {
+  const svRegex = isV3SVs(datasetId)
+    ? V3_STRUCTURAL_VARIANT_ID_REGEX
+    : V2_STRUCTURAL_VARIANT_ID_REGEX
+
+  const match = svRegex.exec(str)
   if (!match) {
     return false
   }
@@ -12,9 +22,11 @@ export const isStructuralVariantId = (str: any) => {
     return false
   }
 
-  const id = Number(match[3])
-  if (id > 1e9) {
-    return false
+  if (!isV3SVs(datasetId)) {
+    const id = Number(match[3])
+    if (id > 1e9) {
+      return false
+    }
   }
 
   return true
