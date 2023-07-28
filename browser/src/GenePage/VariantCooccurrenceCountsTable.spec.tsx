38fa22b4f22f2f9f2318e1b9c8cf9417be45ef8f
Riley Grant <grantrileyh@gmail.com>
2023-07-28 16:35:48 -0500

    Modify coocurrence table to only render on v2 data

--- browser/src/GenePage/VariantCooccurrenceCountsTable.spec.tsx
+++ browser/src/GenePage/VariantCooccurrenceCountsTable.spec.tsx
@@ -7,6 +7,50 @@ import {
 } from '../__factories__/VariantCooccurrenceCountsPerSeverityAndAf'
 
 import VariantCooccurrenceCountsTable from './VariantCooccurrenceCountsTable'
+import { forDatasetsMatching, forDatasetsNotMatching } from '../../../tests/__helpers__/datasets'
+
+const v2Regexp = /_r2/
+
+forDatasetsNotMatching(
+  v2Regexp,
+  'VariantCoocurrenceCountsTable with non v2 dataset "%s"',
+  (datasetId) => {
+    test('has no unexpected changes and renders as placeholder text', () => {
+      const heterozygousCounts =
+        HeterozygousVariantCooccurrenceCountsPerSeverityAndAfFactory.build()
+      const homozygousCounts = HomozygousVariantCooccurrenceCountsPerSeverityAndAfFactory.build()
+      const tableContent = render(
+        <VariantCooccurrenceCountsTable
+          datasetId={datasetId}
+          heterozygous_variant_cooccurrence_counts={heterozygousCounts}
+          homozygous_variant_cooccurrence_counts={homozygousCounts}
+        />
+      )
+      const normalContentFragment = tableContent.asFragment()
+      expect(normalContentFragment.querySelectorAll('p').length).toEqual(1)
+      expect(normalContentFragment.querySelectorAll('table').length).toEqual(0)
+      expect(normalContentFragment).toMatchSnapshot()
+    })
+  }
+)
+
+forDatasetsMatching(v2Regexp, 'VariantCoocurrenceCountsTable with v2 dataset "%s"', (datasetId) => {
+  test('has no unexpected changes and renders as a table', () => {
+    const heterozygousCounts = HeterozygousVariantCooccurrenceCountsPerSeverityAndAfFactory.build()
+    const homozygousCounts = HomozygousVariantCooccurrenceCountsPerSeverityAndAfFactory.build()
+    const tableContent = render(
+      <VariantCooccurrenceCountsTable
+        datasetId={datasetId}
+        heterozygous_variant_cooccurrence_counts={heterozygousCounts}
+        homozygous_variant_cooccurrence_counts={homozygousCounts}
+      />
+    )
+    const normalContentFragment = tableContent.asFragment()
+    expect(normalContentFragment.querySelectorAll('p').length).toEqual(0)
+    expect(normalContentFragment.querySelectorAll('table').length).toEqual(2)
+    expect(normalContentFragment).toMatchSnapshot()
+  })
+})
 
 describe('VariantCooccurrenceCountsTable', () => {
   test('renders correct data into correct table cells in both regular and extended mode', () => {
@@ -14,6 +58,7 @@ describe('VariantCooccurrenceCountsTable', () => {
     const homozygousCounts = HomozygousVariantCooccurrenceCountsPerSeverityAndAfFactory.build()
     const tableContent = render(
       <VariantCooccurrenceCountsTable
+        datasetId="gnomad_r2_1"
         heterozygous_variant_cooccurrence_counts={heterozygousCounts}
         homozygous_variant_cooccurrence_counts={homozygousCounts}
       />
@@ -42,6 +87,7 @@ describe('VariantCooccurrenceCountsTable', () => {
   test('fills in missing data with zeroes', () => {
     const tableContent = render(
       <VariantCooccurrenceCountsTable
+        datasetId="gnomad_r2_1"
         heterozygous_variant_cooccurrence_counts={{}}
         homozygous_variant_cooccurrence_counts={{}}
       />
