8d052632c323d1d9f908fe2a7ff4d37a3d7f2ffa
Matthew Solomonson <ggmatt@gmail.com>
2023-09-19 14:30:24 -0400

    Add output validation/schemas for step 2

--- data-pipeline/src/data_pipeline/pipelines/gnomad_v4_coverage.py
+++ data-pipeline/src/data_pipeline/pipelines/gnomad_v4_coverage.py
@@ -3,28 +3,28 @@ from data_pipeline.pipeline import Pipeline, run_pipeline
 from data_pipeline.data_types.coverage import prepare_coverage
 
 
-pipeline = Pipeline()
+pipeline = Pipeline(name="gnomad_v4_coverage")
 
 pipeline.add_task(
-    "prepare_gnomad_v4_exome_coverage",
-    prepare_coverage,
-    "/gnomad_v4/gnomad_v4_exome_coverage.ht",
+    name="prepare_gnomad_v4_exome_coverage",
+    task_function=prepare_coverage,
+    output_path="/gnomad_v4/gnomad_v4_exome_coverage.ht",
     # Using v3 coverage as mock for now
-    {
+    inputs={
         "coverage_path": "gs://gcp-public-data--gnomad/release/3.0.1/coverage/genomes/gnomad.genomes.r3.0.1.coverage.ht",
-        "filter_intervals": ["chr1:10030-10150"],
     },
+    params={"filter_intervals": ["chr1:10030-10150"]},
 )
 
 pipeline.add_task(
-    "prepare_gnomad_v4_genome_coverage",
-    prepare_coverage,
-    "/gnomad_v4/gnomad_v4_genome_coverage.ht",
+    name="prepare_gnomad_v4_genome_coverage",
+    task_function=prepare_coverage,
+    output_path="/gnomad_v4/gnomad_v4_genome_coverage.ht",
     # Using v3 coverage as mock for now
-    {
+    inputs={
         "coverage_path": "gs://gcp-public-data--gnomad/release/3.0.1/coverage/genomes/gnomad.genomes.r3.0.1.coverage.ht",
-        "filter_intervals": ["chr1:10030-10150"],
     },
+    params={"filter_intervals": ["chr1:10030-10150"]},
 )
 
 ###############################################
