268573f9a6f849bdc2a1af614fb437b1c830afba
Matthew Solomonson <ggmatt@gmail.com>
2023-09-19 13:43:34 -0400

    Add input validation for step 1 output

--- data-pipeline/tests/v4/test_inputs.py
+++ data-pipeline/tests/v4/test_inputs.py
@@ -1,11 +1,18 @@
 from typing import List
-from cattrs import structure, transform_error
-from data_pipeline.datasets.gnomad_v4.types.initial_variant import InitialVariant
+from cattrs import structure, transform_error, structure_attrs_fromdict
 import hail as hl
 import json
 from loguru import logger
 
+from data_pipeline.pipelines.gnomad_v4_variants import (
+    pipeline as gnomad_v4_variant_pipeline,
+)
+
 from data_pipeline.datasets.gnomad_v4.types.initial_globals import Globals
+from data_pipeline.datasets.gnomad_v4.types.initial_variant import InitialVariant
+from data_pipeline.datasets.gnomad_v4.types.prepare_variants_step1 import Variant as Step1Variant
+
+step1_task = gnomad_v4_variant_pipeline.get_task("prepare_gnomad_v4_exome_variants")
 
 
 def ht_to_json(ht: hl.Table, field: str = "row"):
@@ -25,14 +32,24 @@ def ht_to_json(ht: hl.Table, field: str = "row"):
 
 
 def test_globals_input_validation():
-    ht = hl.read_table("./data/mock_v4_release.ht")
+    input_path = gnomad_v4_variant_pipeline.get_task("prepare_gnomad_v4_exome_variants").get_inputs()["input_path"]
+    ht = hl.read_table(input_path)
     result = ht_to_json(ht, "globals")[0]
     logger.info(result)
     structure(result, Globals)
 
 
-def test_variant_input_validation():
-    ht = hl.read_table("./data/mock_v4_release.ht")
+def test_validate_variant_input():
+    input_path = gnomad_v4_variant_pipeline.get_task("prepare_gnomad_v4_exome_variants").get_inputs()["input_path"]
+    ht = hl.read_table(input_path)
     # ht = ht.sample(0.1, seed=1234)
     result = ht_to_json(ht)
-    structure(result, List[InitialVariant])
+    [structure_attrs_fromdict(variant, InitialVariant) for variant in result]
+
+
+def test_validate_step1_output():
+    output_path = gnomad_v4_variant_pipeline.get_task("prepare_gnomad_v4_exome_variants").get_output_path()
+    ht = hl.read_table(output_path)
+    # ht = ht.sample(0.1, seed=1234)
+    result = ht_to_json(ht)
+    [structure_attrs_fromdict(variant, Step1Variant) for variant in result]
