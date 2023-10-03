282385ec464046e4e45f9e47db6ac642f6cf8869
Elissa Alarmani <ealarman@broadinstitute.org>
2023-10-03 10:58:20 -0400

    update singular and plural naming

--- graphql-api/src/graphql/resolvers/copy-number-variants.ts
+++ graphql-api/src/graphql/resolvers/copy-number-variants.ts
@@ -1,13 +1,13 @@
 import { UserVisibleError } from '../../errors'
 import {
-  fetchCopyNumberVariantsById,
+  fetchCopyNumberVariantById,
   fetchCopyNumberVariantsByGene,
   fetchCopyNumberVariantsByRegion,
 } from '../../queries/copy-number-variant-queries'
 
-const resolveCopyNumberVariants = async (_: any, args: any, ctx: any) => {
-  const variant = await fetchCopyNumberVariantsById(ctx.esClient, args.variantId)
-
+//TODO: args.dataset for all three resolvers
+const resolveCopyNumberVariant = async (_: any, args: any, ctx: any) => {
+  const variant = await fetchCopyNumberVariantById(ctx.esClient, args.variantId)
   if (!variant) {
     throw new UserVisibleError('Variant not found')
   }
@@ -19,13 +19,13 @@ const resolveCopyNumberVariantsInGene = (obj: any, ctx: any) => {
   return fetchCopyNumberVariantsByGene(ctx.esClient, obj)
 }
 
-const resolveCopyNumberVariantsInRegion = (obj: any, ctx: any) => {
+const resolveCopyNumberVariantsInRegion = (obj: any,  ctx: any) => {
   return fetchCopyNumberVariantsByRegion(ctx.esClient, obj)
 }
 
 const resolvers = {
   Query: {
-    copy_number_variants: resolveCopyNumberVariants,
+    copy_number_variant: resolveCopyNumberVariant,
   },
   Gene: {
     copy_number_variants: resolveCopyNumberVariantsInGene,
