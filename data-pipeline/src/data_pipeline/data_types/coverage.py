1e319fa80d1258d7706893f39ecdf18d41c38192
Matthew Solomonson <ggmatt@gmail.com>
2023-10-18 12:51:36 -0400

    Update v4 variants validation and scripts

--- data-pipeline/src/data_pipeline/data_types/coverage.py
+++ data-pipeline/src/data_pipeline/data_types/coverage.py
@@ -7,14 +7,17 @@ from data_pipeline.data_types.locus import x_position
 def prepare_coverage(coverage_path: str, filter_intervals: Optional[List[str]] = None):
     coverage = hl.read_table(coverage_path)
 
-    if filter_intervals:
-        intervals = [hl.parse_locus_interval(interval, reference_genome="GRCh38") for interval in filter_intervals]
-        coverage = hl.filter_intervals(coverage, intervals)
-
     coverage = coverage.annotate(xpos=x_position(coverage.locus))
 
+    coverage_fields = coverage.row.dtype.fields
+
+    # Specific to v4, the data is contained in a coverage_stats field and we take the first element
+    if "coverage_stats" in coverage_fields:
+        if type(coverage.coverage_stats) == hl.expr.expressions.typed_expressions.ArrayStructExpression:
+            coverage = coverage.annotate(**coverage.coverage_stats[0])
+
     # Median field name is different in v3.0.1 vs v2
-    if "median" not in coverage.row.dtype.fields:
+    if "median" not in coverage_fields:
         coverage = coverage.annotate(median=coverage.median_approx)
 
     # Drop extra fields in v3
@@ -32,5 +35,9 @@ def prepare_coverage(coverage_path: str, filter_intervals: Optional[List[str]] =
         "over_50",
         "over_100",
     )
+
+    if filter_intervals:
+        intervals = [hl.parse_locus_interval(interval, reference_genome="GRCh38") for interval in filter_intervals]
+        coverage = hl.filter_intervals(coverage, intervals)
 
     return coverage
--- data-pipeline/src/data_pipeline/datasets/gnomad_v4/types/prepare_variants_step3.py
+++ data-pipeline/src/data_pipeline/datasets/gnomad_v4/types/prepare_variants_step3.py
@@ -5,9 +5,8 @@ from data_pipeline.datasets.gnomad_v4.types.prepare_variants_step1 import Coloca
 
 from data_pipeline.datasets.gnomad_v4.types.initial_variant import (
     InSilicoPredictors,
-    Domain,
-    Rf,
-    Grpmax,
+    # Domain,
+    # Grpmax,
     Locus,
 )
 
@@ -16,7 +15,8 @@ from data_pipeline.datasets.gnomad_v4.types.initial_variant import (
 class TranscriptConsequence:
     biotype: Union[str, None]
     consequence_terms: list[str]
-    domains: Union[List[Domain], None]
+    # domains: Union[List[Domain], None]
+    domains: Union[List[str], None]
     gene_id: Union[str, None]
     gene_symbol: Union[str, None]
     hgvsc: Union[str, None]
@@ -25,13 +25,14 @@ class TranscriptConsequence:
     lof: Union[str, None]
     lof_flags: Union[str, None]
     lof_filter: Union[str, None]
-    lof_info: Union[str, None]
+    # lof_info: Union[str, None]
     major_consequence: str
-    polyphen_prediction: Union[str, None]
-    sift_prediction: Union[str, None]
+    # polyphen_prediction: Union[str, None]
+    # sift_prediction: Union[str, None]
     transcript_id: Union[str, None]
     transcript_version: Union[str, None]
     transcript_id: Union[str, None]
+    gene_version: str
     is_mane_select: bool
     is_mane_select_version: bool
     refseq_id: str
@@ -42,9 +43,8 @@ class TranscriptConsequence:
 class Variant:
     locus: Locus
     alleles: list[str]
-    grpmax: List[Grpmax]
+    # grpmax: List[Grpmax]
     rsids: Union[Set[str], None]
-    rf: Rf
     in_silico_predictors: InSilicoPredictors
     variant_id: str
     colocated_variants: ColocatedVariants
