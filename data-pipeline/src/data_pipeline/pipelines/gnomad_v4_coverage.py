6190fecb5dabf90a7e663c6885a21497790aa4a3
Matthew Solomonson <ggmatt@gmail.com>
2023-10-20 11:02:15 -0400

    Export variants and coverage to elasticsearch

--- data-pipeline/src/data_pipeline/pipelines/gnomad_v4_coverage.py
+++ data-pipeline/src/data_pipeline/pipelines/gnomad_v4_coverage.py
@@ -8,24 +8,23 @@ pipeline = Pipeline()
 pipeline.add_task(
     name="prepare_gnomad_v4_exome_coverage",
     task_function=prepare_coverage,
-    output_path="/gnomad_v4/gnomad_v4_exome_coverage_cnv_test.ht",
-    # Using v3 coverage as mock for now
+    output_path="/gnomad_v4/gnomad_v4_exome_coverage.ht",
     inputs={
         "coverage_path": "gs://gnomad-v4-coverage-testing/release/4.0/ht/exomes/gnomad.exomes.v4.0.coverage.ht",
     },
-    params={"filter_intervals": ["chr1:1007979-119987180"]},
+    params={"filter_intervals": ["chr1:55039447-55064852"]},
 )
 
-# pipeline.add_task(
-#     name="prepare_gnomad_v4_genome_coverage",
-#     task_function=prepare_coverage,
-#     output_path="/gnomad_v4/gnomad_v4_genome_coverage.ht",
-#     # Using v3 coverage as mock for now
-#     inputs={
-#         "coverage_path": "gs://gcp-public-data--gnomad/release/3.0.1/coverage/genomes/gnomad.genomes.r3.0.1.coverage.ht",
-#     },
-#     params={"filter_intervals": ["chr1:55039447-55064852"]},
-# )
+pipeline.add_task(
+    name="prepare_gnomad_v4_genome_coverage",
+    task_function=prepare_coverage,
+    output_path="/gnomad_v4/gnomad_v4_genome_coverage.ht",
+    # Using v3 coverage as mock for now
+    inputs={
+        "coverage_path": "gs://gcp-public-data--gnomad/release/3.0.1/coverage/genomes/gnomad.genomes.r3.0.1.coverage.ht",
+    },
+    params={"filter_intervals": ["chr1:55039447-55064852"]},
+)
 
 ###############################################
 # Outputs
@@ -34,7 +33,7 @@ pipeline.add_task(
 pipeline.set_outputs(
     {
         "exome_coverage": "prepare_gnomad_v4_exome_coverage",
-        # "genome_coverage": "prepare_gnomad_v4_genome_coverage",
+        "genome_coverage": "prepare_gnomad_v4_genome_coverage",
     }
 )
 
