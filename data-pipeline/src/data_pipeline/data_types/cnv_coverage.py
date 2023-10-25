db258fe81dae330001d7ef70d7aee664224d3339
Elissa Alarmani <ealarman@broadinstitute.org>
2023-10-25 10:28:09 -0400

    draft track callable coverage

--- data-pipeline/src/data_pipeline/data_types/cnv_coverage.py
+++ data-pipeline/src/data_pipeline/data_types/cnv_coverage.py
@@ -3,7 +3,7 @@ import hail as hl
 
 
 def prepare_cnv_track_callable_coverage(coverage_path):
-    coverage = hl.import_table(coverage_path, force_bgz=True)
+    coverage = hl.import_table(coverage_path, force_bgz=True, types={'xpos': hl.tfloat64, 'percent_callable': hl.tfloat64})
 
     coverage = coverage.select(
         "xpos",
