004ba1e36110bd30c0ec02b599a13f77c91aca3f
Elissa Alarmani <ealarman@broadinstitute.org>
2023-10-26 18:15:35 -0400

    coverage edits

--- graphql-api/src/queries/cnv-coverage-queries.ts
+++ graphql-api/src/queries/cnv-coverage-queries.ts
@@ -1,4 +1,3 @@
-import { withCache } from '../cache'
 import { UserVisibleError } from '../errors'
 
 import { extendRegions, mergeOverlappingRegions, totalRegionSize } from './helpers/region-helpers'
@@ -15,49 +14,33 @@ const COVERAGE_INDICES = {
 // Base query
 // ================================================================================================
 
-const fetchTrackCallableCoverage = async (esClient: any, { index, regions }: any) => {
-  try {
-    console.log('fetchtrackcallablecoverage')
-    const response = await esClient.search({
-      index,
-      type: '_doc',
-      size: 0,
-      body: {
-        query: {
-          bool: {
-            filter: [
-              {
-                bool: {
-                  should: regions.map(({ start, stop }: any) => ({
-                    range: { xpos: { gte: start, lte: stop } },
-                  })),
-                },
-              },
-            ],
-          },
-        },
-        aggregations: {
-          chart: {
-            terms: {
-              field: 'xpos',
-              interval: 10000,
-            },
-            aggregations: {
-              percent_callable: { avg: { field: 'percent_callable' } },
+const fetchTrackCallableCoverage = async (esClient: any, { regions }: any) => {
+  const requestBody = {
+    query: {
+      bool: {
+        filter: [
+          {
+            bool: {
+              should: regions.map(({ xstart, xstop }: any) => ({
+                range: { xpos: { gte: xstart, lte: xstop } },
+              })),
             },
           },
-        },
+        ],
       },
-    })
-
-    return response.body.aggregations.chart.buckets.map((bucket: any) => ({
-      pos: bucket.key,
-      percent_callable: [Math.ceil((bucket.percent_callable.value || 0) * 100) / 100],
-    }))
-  } catch (error) {
-    console.error('Error fetching coverage:', error)
-    throw error
+    },
   }
+
+  const response = await esClient.search({
+    index: 'gnomad_v4_cnv_track_callable_coverage',
+    type: '_doc',
+    size: 2,
+    body: requestBody,
+  })
+  return response.body.hits.hits.map((hit: any) => ({
+    xpos: parseFloat(hit._source.xpos),
+    percent_callable: Math.ceil((hit._source.percent_callable || 0) * 100) / 100,
+  }))
 }
 
 // ================================================================================================
@@ -79,7 +62,6 @@ export const fetchTrackCallableCoverageForRegion = (esClient: any, datasetId: an
 
   return fetchTrackCallableCoverage(esClient, {
     index: trackCallableCoverageIndex,
-    //contig: `chr${region.chrom}` ,
     regions: [{ start: region.start - 75, stop: region.stop + 75 }],
     bucketSize,
   })
@@ -95,38 +77,31 @@ export const _fetchTrackCallableCoverageForGene = async (
   gene: any
 ) => {
   assertDatasetAndReferenceGenomeMatch(datasetId, gene.reference_genome)
-  console.log('fetch gene')
 
   if (!datasetId.startsWith('gnomad_cnv_')) {
     throw new UserVisibleError('Track callabe coverage is not available for non-CNVs')
   }
 
   const paddedExons = extendRegions(75, gene.exons)
-  console.log(paddedExons, 'paddedexons')
+
   const mergedExons = mergeOverlappingRegions(
     paddedExons.sort((a: any, b: any) => a.start - b.start)
   )
-  console.log(mergedExons, 'mergedExons')
 
   const totalIntervalSize = totalRegionSize(mergedExons)
   const bucketSize = Math.max(Math.floor(totalIntervalSize / 500), 1)
 
   // @ts-expect-error TS(7053) FIXME: Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
   const trackCallableCoverageIndex = COVERAGE_INDICES[datasetId]
-  console.log(trackCallableCoverageIndex, 'trackCallableCoverageIndex')
-
   const trackCallableCoverage = await fetchTrackCallableCoverage(esClient, {
     index: trackCallableCoverageIndex,
-    //  contig: `chr${gene.chrom}`,
     regions: mergedExons,
     bucketSize,
   })
-  console.log(trackCallableCoverage, 'trackCallableCoverage')
 
   return {
-    track_callable: trackCallableCoverage,
+    cnv_track_callable_coverage: trackCallableCoverage || [],
   }
 }
-export const fetchTrackCallableCoverageForGene = 
-  _fetchTrackCallableCoverageForGene
 
+export const fetchTrackCallableCoverageForGene = _fetchTrackCallableCoverageForGene
