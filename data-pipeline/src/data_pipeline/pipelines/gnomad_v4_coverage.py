a9ea9a7c7baeaef0b96bbe0d04b8bba3da42bbfa
Matthew Solomonson <ggmatt@gmail.com>
2023-10-28 10:44:47 -0400

    Update pipeline tables to current state

--- data-pipeline/src/data_pipeline/pipelines/gnomad_v4_coverage.py
+++ data-pipeline/src/data_pipeline/pipelines/gnomad_v4_coverage.py
@@ -2,29 +2,30 @@ from data_pipeline.pipeline import Pipeline, run_pipeline
 
 from data_pipeline.data_types.coverage import prepare_coverage
 
+output_sub_dir = "gnomad_v4_20231027T203139"
 
 pipeline = Pipeline()
 
 pipeline.add_task(
     name="prepare_gnomad_v4_exome_coverage",
     task_function=prepare_coverage,
-    output_path="/gnomad_v4/gnomad_v4_exome_coverage.ht",
+    output_path=f"/{output_sub_dir}/gnomad_v4_exome_coverage.ht",
     inputs={
         "coverage_path": "gs://gnomad-v4-coverage-testing/release/4.0/ht/exomes/gnomad.exomes.v4.0.coverage.ht",
     },
-    params={"filter_intervals": ["chr1:55039447-55064852"]},
+    # params={"filter_intervals": ["chr1:55039447-55064852"]},
 )
 
-pipeline.add_task(
-    name="prepare_gnomad_v4_genome_coverage",
-    task_function=prepare_coverage,
-    output_path="/gnomad_v4/gnomad_v4_genome_coverage.ht",
-    # Using v3 coverage as mock for now
-    inputs={
-        "coverage_path": "gs://gcp-public-data--gnomad/release/3.0.1/coverage/genomes/gnomad.genomes.r3.0.1.coverage.ht",
-    },
-    params={"filter_intervals": ["chr1:55039447-55064852"]},
-)
+# pipeline.add_task(
+#     name="prepare_gnomad_v4_genome_coverage",
+#     task_function=prepare_coverage,
+#     output_path="/gnomad_v4/gnomad_v4_genome_coverage.ht",
+#     # Using v3 coverage as mock for now
+#     inputs={
+#         "coverage_path": "gs://gcp-public-data--gnomad/release/3.0.1/coverage/genomes/gnomad.genomes.r3.0.1.coverage.ht",
+#     },
+#     # params={"filter_intervals": ["chr1:55039447-55064852"]},
+# )
 
 ###############################################
 # Outputs
@@ -33,7 +34,7 @@ pipeline.add_task(
 pipeline.set_outputs(
     {
         "exome_coverage": "prepare_gnomad_v4_exome_coverage",
-        "genome_coverage": "prepare_gnomad_v4_genome_coverage",
+        # "genome_coverage": "prepare_gnomad_v4_genome_coverage",
     }
 )
 
