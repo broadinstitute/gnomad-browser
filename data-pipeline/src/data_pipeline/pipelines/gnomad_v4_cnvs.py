aa51e93b6d2b1a3ba90db3b320697d78ef20b9f3
Elissa Alarmani <ealarman@broadinstitute.org>
2023-10-29 13:32:36 -0400

    add new vcf file path

--- data-pipeline/src/data_pipeline/pipelines/gnomad_v4_cnvs.py
+++ data-pipeline/src/data_pipeline/pipelines/gnomad_v4_cnvs.py
@@ -14,7 +14,7 @@ pipeline.add_task(
     prepare_gnomad_v4_cnvs,
     "/gnomad_v4/gnomad_v4_cnvs/cnvs.ht",
     {
-        "vcf_path": "gs://gnomad-v4-cnvs/2023-10-24-jfu/GNOMAD_V4_release_1.0.vcf.gz",
+        "vcf_path": "gs://gnomad-v4-cnvs/2023-10-28-jfu/GNOMAD_V4_release_1.1.vcf.gz",
     },
 )
 
