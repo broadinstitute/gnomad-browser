b131bd1b342eea2908efa729afbd9b2b9b3729d8
Matthew Solomonson <ggmatt@gmail.com>
2023-10-30 08:34:48 -0400

    Capitalize remaining population

--- dataset-metadata/gnomadPopulations.ts
+++ dataset-metadata/gnomadPopulations.ts
@@ -12,6 +12,7 @@ export const GNOMAD_POPULATION_NAMES = {
   fin: 'European (Finnish)',
   oth: 'Remaining individuals',
   sas: 'South Asian',
+  remaining: 'Remaining',
 
   // EAS subpopulations
   eas_jpn: 'Japanese',
