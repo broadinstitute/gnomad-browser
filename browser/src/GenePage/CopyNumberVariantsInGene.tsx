9956b8a425d578440ea16a8e00d84b780f8772e4
Elissa Alarmani <ealarman@broadinstitute.org>
2023-10-30 13:06:06 -0400

    remove console.log

--- browser/src/GenePage/CopyNumberVariantsInGene.tsx
+++ browser/src/GenePage/CopyNumberVariantsInGene.tsx
@@ -61,7 +61,6 @@ const CopyNumberVariantsInGene = ({ datasetId, gene, zoomRegion, ...rest }: Prop
       success={(data: any) => data.gene && data.gene.copy_number_variants}
     >
       {({ data }: any) => {
-        console.log(data) // Add this line to log the 'data' object
         const copy_number_variants = filterCopyNumberVariantsInZoomRegion(
           data.gene.copy_number_variants,
           zoomRegion
