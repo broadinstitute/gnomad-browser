bdbff7c65cf72f905a04f3d7c1173b6c217a7a5d
Riley Grant <grantrileyh@gmail.com>
2023-10-31 20:53:38 -0500

    Update v4 variant queries to include all variants

--- graphql-api/src/queries/variant-datasets/gnomad-v4-variant-queries.ts
+++ graphql-api/src/queries/variant-datasets/gnomad-v4-variant-queries.ts
@@ -423,7 +423,10 @@ const fetchVariantsByRegion = async (esClient: any, region: any, _subset: any) =
 
   return hits
     .map((hit: any) => hit._source.value)
-    .filter((variant: any) => variant.genome.freq[subset].ac_raw > 0)
+    .filter(
+      (variant: any) =>
+        variant.genome.freq[subset].ac_raw > 0 || variant.exome.freq[subset].ac_raw > 0
+    )
     .map(shapeVariantSummary(subset, { type: 'region' }))
 }
 
@@ -491,7 +494,10 @@ const fetchVariantsByTranscript = async (esClient: any, transcript: any, _subset
 
   return hits
     .map((hit: any) => hit._source.value)
-    .filter((variant: any) => variant.genome.freq[subset].ac_raw > 0)
+    .filter(
+      (variant: any) =>
+        variant.genome.freq[subset].ac_raw > 0 || variant.exome.freq[subset].ac_raw > 0
+    )
     .map(
       shapeVariantSummary(subset, { type: 'transcript', transcriptId: transcript.transcript_id })
     )
@@ -533,13 +539,16 @@ const fetchMatchingVariants = async (
 
   return hits
     .map((hit: any) => hit._source.value)
-    .filter((variant: any) => variant.genome.freq[subset].ac_raw > 0)
+    .filter(
+      (variant: any) =>
+        variant.genome.freq[subset].ac_raw > 0 || variant.exome.freq[subset].ac_raw > 0
+    )
     .map((variant: any) => ({
       variant_id: variant.variant_id,
     }))
 }
 
-const gnomadV3VariantQueries = {
+const gnomadV4VariantQueries = {
   countVariantsInRegion,
   fetchVariantById,
   fetchVariantsByGene,
@@ -548,4 +557,4 @@ const gnomadV3VariantQueries = {
   fetchMatchingVariants,
 }
 
-export default gnomadV3VariantQueries
+export default gnomadV4VariantQueries
