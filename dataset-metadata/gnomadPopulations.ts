029999ee75d97af5914f347fae7eff33fc26a40f
Riley Grant <grantrileyh@gmail.com>
2023-11-21 12:44:28 -0500

    Update Gene page csv export's group columns to be based on dataset

--- dataset-metadata/gnomadPopulations.ts
+++ dataset-metadata/gnomadPopulations.ts
@@ -32,3 +32,52 @@ export type PopulationId = keyof typeof GNOMAD_POPULATION_NAMES
 
 export const populationName = (populationId: string) =>
   textOrMissingTextWarning('genetic ancestry group name', GNOMAD_POPULATION_NAMES, populationId)
+
+const ExACPopulations: PopulationId[] = ['sas', 'afr', 'amr', 'eas', 'fin', 'nfe', 'remaining']
+const v2Populations: PopulationId[] = ['afr', 'nfe', 'afr', 'asj', 'eas', 'fin', 'sas', 'remaining']
+const v3Populations: PopulationId[] = [
+  'nfe',
+  'amr',
+  'eur',
+  'ami',
+  'eas',
+  'mid',
+  'afr',
+  'sas',
+  'asj',
+  'remaining',
+]
+const v4Populations: PopulationId[] = [
+  'afr',
+  'amr',
+  'asj',
+  'eas',
+  'fin',
+  'mid',
+  'nfe',
+  'ami', // v4 does not directly include amish, but v3 does and v4 genomes are from v3
+  'sas',
+  'remaining',
+]
+const allPopulations: PopulationId[] = [
+  'afr',
+  'ami',
+  'amr',
+  'asj',
+  'eas',
+  'mid',
+  'eur',
+  'nfe',
+  'fin',
+  'oth',
+  'sas',
+  'remaining',
+]
+
+export const populationsInDataset = {
+  ExAC: ExACPopulations,
+  v2: v2Populations,
+  v3: v3Populations,
+  v4: v4Populations,
+  default: allPopulations,
+}
