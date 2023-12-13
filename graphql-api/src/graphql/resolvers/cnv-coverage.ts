41de2384c73aa602c3b97a50172e99a1779e30bc
Elissa Alarmani <ealarman@broadinstitute.org>
2023-12-13 10:39:21 -0500

    add v4 CNV tracks to GraphQL API

--- graphql-api/src/graphql/resolvers/cnv-coverage.ts
+++ graphql-api/src/graphql/resolvers/cnv-coverage.ts
@@ -1,12 +1,33 @@
-import { fetchTrackCallableCoverageForGene } from '../../queries/cnv-coverage-queries'
+import {
+  fetchDelBurdenCoverageForGene,
+  fetchDelBurdenCoverageForRegion,
+  fetchDupBurdenCoverageForGene,
+  fetchDupBurdenCoverageForRegion,
+  fetchTrackCallableCoverageForGene,
+  fetchTrackCallableCoverageForRegion,
+} from '../../queries/cnv-coverage-queries'
 
-const resolveTrackCallableCoverageInGene = async (obj: any, args: any, ctx: any) => {
-  return fetchTrackCallableCoverageForGene(ctx.esClient, args.dataset, obj)
+const createResolver = (fetchCoverage: any) => async (obj: any, args: any, ctx: any) => {
+  return fetchCoverage(ctx.esClient, args.dataset, obj)
 }
+
+const resolveTrackCallableCoverageInGene = createResolver(fetchTrackCallableCoverageForGene)
+const resolveDelBurdenCoverageInGene = createResolver(fetchDelBurdenCoverageForGene)
+const resolveDupBurdenCoverageInGene = createResolver(fetchDupBurdenCoverageForGene)
+const resolveTrackCallableCoverageInRegion = createResolver(fetchTrackCallableCoverageForRegion)
+const resolveDelBurdenCoverageInRegion = createResolver(fetchDelBurdenCoverageForRegion)
+const resolveDupBurdenCoverageInRegion = createResolver(fetchDupBurdenCoverageForRegion)
 
 const resolvers = {
   Gene: {
     cnv_track_callable_coverage: resolveTrackCallableCoverageInGene,
+    cnv_del_burden_coverage: resolveDelBurdenCoverageInGene,
+    cnv_dup_burden_coverage: resolveDupBurdenCoverageInGene,
+  },
+  Region: {
+    cnv_track_callable_coverage: resolveTrackCallableCoverageInRegion,
+    cnv_del_burden_coverage: resolveDelBurdenCoverageInRegion,
+    cnv_dup_burden_coverage: resolveDupBurdenCoverageInRegion,
   },
 }
 
--- graphql-api/src/queries/cnv-coverage-queries.ts
+++ graphql-api/src/queries/cnv-coverage-queries.ts
@@ -7,6 +7,8 @@ import { assertDatasetAndReferenceGenomeMatch } from './helpers/validation-helpe
 const COVERAGE_INDICES = {
   gnomad_cnv_r4: {
     track_callable: 'gnomad_v4_cnv_track_callable_coverage',
+    del_burden: 'gnomad_v4_cnv_del_burden',
+    dup_burden: 'gnomad_v4_cnv_dup_burden',
   },
 }
 
@@ -14,44 +16,122 @@ const COVERAGE_INDICES = {
 // Base query
 // ================================================================================================
 
-const fetchTrackCallableCoverage = async (esClient: any, { regions }: any) => {
-  const requestBody = {
-    query: {
-      bool: {
-        filter: [
-          {
-            bool: {
-              should: regions.map(({ xstart, xstop }: any) => ({
-                range: { xpos: { gte: xstart, lte: xstop } },
-              })),
-            },
+const fetchTrackCallableCoverage = async (esClient: any, { index, contig, regions }: any) => {
+  try {
+    const response = await esClient.search({
+      index,
+      type: '_doc',
+      size: 10000,
+      body: {
+        query: {
+          bool: {
+            filter: [
+              { term: { contig } },
+              {
+                bool: {
+                  should: regions.map(({ start, stop }: any) => ({
+                    range: { position: { gte: start, lte: stop } },
+                  })),
+                },
+              },
+            ],
           },
-        ],
+        },
       },
-    },
+    })
+    return response.body.hits.hits.map((hit: any) => ({
+      xpos: hit._source.xpos,
+      percent_callable: Math.ceil(parseFloat(hit._source.percent_callable || 0) * 100) / 100,
+      position: hit._source.position,
+      contig: hit._source.contig
+    }))
+  } catch (error) {
+    throw new Error(`Couldn't fetch coverage, ${error}`)
   }
+}
 
-  const response = await esClient.search({
-    index: 'gnomad_v4_cnv_track_callable_coverage',
-    type: '_doc',
-    size: 2,
-    body: requestBody,
-  })
-  return response.body.hits.hits.map((hit: any) => ({
-    xpos: parseFloat(hit._source.xpos),
-    percent_callable: Math.ceil((hit._source.percent_callable || 0) * 100) / 100,
-  }))
+
+const fetchDelBurdenCoverage = async (esClient: any, { index, contig, regions }: any) => {
+  try {
+    const response = await esClient.search({
+      index,
+      type: '_doc',
+      size: 10000,
+      body: {
+        query: {
+          bool: {
+            filter: [
+              { term: { contig } },
+              {
+                bool: {
+                  should: regions.map(({ start, stop }: any) => ({
+                    range: { position: { gte: start, lte: stop } },
+                  })),
+                },
+              },
+            ],
+          },
+        },
+      },
+    })
+    return response.body.hits.hits.map((hit: any) => ({
+      xpos: hit._source.xpos,
+      burden_del: parseFloat(hit._source.burden_del) ,
+      position: hit._source.position,
+      contig: hit._source.contig
+    }))
+  } catch (error) {
+    throw new Error(`Couldn't fetch coverage, ${error}`)
+  }
+}
+
+const fetchDupBurdenCoverage = async (esClient: any, { index, contig, regions }: any) => {
+  try {
+    const response = await esClient.search({
+      index,
+      type: '_doc',
+      size: 10000,
+      body: {
+        query: {
+          bool: {
+            filter: [
+              { term: { contig } },
+              {
+                bool: {
+                  should: regions.map(({ start, stop }: any) => ({
+                    range: { position: { gte: start, lte: stop } },
+                  })),
+                },
+              },
+            ],
+          },
+        },
+      },
+    })
+    return response.body.hits.hits.map((hit: any) => ({
+      xpos: hit._source.xpos,
+      burden_dup: parseFloat(hit._source.burden_dup) ,
+      position: hit._source.position,
+      contig: hit._source.contig
+    }))
+  } catch (error) {
+    throw new Error(`Couldn't fetch coverage, ${error}`)
+  }
 }
 
 // ================================================================================================
 // Region queries
 // ================================================================================================
 
-export const fetchTrackCallableCoverageForRegion = (esClient: any, datasetId: any, region: any) => {
+export const fetchTrackCallableCoverageForRegion = async (
+  esClient: any,
+  datasetId: any,
+  region: any
+) => {
   assertDatasetAndReferenceGenomeMatch(datasetId, region.reference_genome)
 
-  if (!datasetId.startsWith('gnomad_cnv_')) {
-    throw new UserVisibleError('Track callabe coverage is not available for non-CNVs')
+  if (!datasetId.startsWith('gnomad_cnv')) {
+    throw new UserVisibleError('Track callable coverage is not available for non-CNVs')
   }
 
   // @ts-expect-error TS(7053) FIXME: Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
@@ -60,11 +140,67 @@ export const fetchTrackCallableCoverageForRegion = (esClient: any, datasetId: an
   const regionSize = region.stop - region.start + 150
   const bucketSize = Math.max(Math.floor(regionSize / 500), 1)
 
-  return fetchTrackCallableCoverage(esClient, {
+  const trackCallableCoverage = await fetchTrackCallableCoverage(esClient, {
     index: trackCallableCoverageIndex,
+    contig: region.chrom,
     regions: [{ start: region.start - 75, stop: region.stop + 75 }],
     bucketSize,
   })
+
+  return trackCallableCoverage
+}
+
+export const fetchDelBurdenCoverageForRegion = async (
+  esClient: any,
+  datasetId: any,
+  region: any
+) => {
+  assertDatasetAndReferenceGenomeMatch(datasetId, region.reference_genome)
+
+  if (!datasetId.startsWith('gnomad_cnv')) {
+    throw new UserVisibleError('Deletion burden track coverage is not available for non-CNVs')
+  }
+
+  // @ts-expect-error TS(7053) FIXME: Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
+  const delBurdenCoverageIndex = COVERAGE_INDICES[datasetId]
+
+  const regionSize = region.stop - region.start + 150
+  const bucketSize = Math.max(Math.floor(regionSize / 500), 1)
+
+  const delBurdenCoverage = await fetchDelBurdenCoverage(esClient, {
+    index: delBurdenCoverageIndex,
+    contig: region.chrom,
+    regions: [{ start: region.start - 75, stop: region.stop + 75 }],
+    bucketSize,
+  })
+
+  return delBurdenCoverage
+}
+
+export const fetchDupBurdenCoverageForRegion = async (
+  esClient: any,
+  datasetId: any,
+  region: any
+) => {
+  assertDatasetAndReferenceGenomeMatch(datasetId, region.reference_genome)
+
+  if (!datasetId.startsWith('gnomad_cnv')) {
+    throw new UserVisibleError('Duplication burden track coverage is not available for non-CNVs')
+  }
+
+  // @ts-expect-error TS(7053) FIXME: Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
+  const dupBurdenCoverageIndex = COVERAGE_INDICES[datasetId]
+
+  const regionSize = region.stop - region.start + 150
+  const bucketSize = Math.max(Math.floor(regionSize / 500), 1)
+
+  const dupBurdenCoverage = await fetchDupBurdenCoverage(esClient, {
+    index: dupBurdenCoverageIndex,
+    regions: [{ start: region.start - 75, stop: region.stop + 75 }],
+    bucketSize,
+  })
+
+  return dupBurdenCoverage
 }
 
 // ================================================================================================
@@ -78,8 +214,8 @@ export const _fetchTrackCallableCoverageForGene = async (
 ) => {
   assertDatasetAndReferenceGenomeMatch(datasetId, gene.reference_genome)
 
-  if (!datasetId.startsWith('gnomad_cnv_')) {
-    throw new UserVisibleError('Track callabe coverage is not available for non-CNVs')
+  if (!datasetId.startsWith('gnomad_cnv')) {
+    throw new UserVisibleError('Track callable coverage is not available for non-CNVs')
   }
 
   const paddedExons = extendRegions(75, gene.exons)
@@ -95,13 +231,71 @@ export const _fetchTrackCallableCoverageForGene = async (
   const trackCallableCoverageIndex = COVERAGE_INDICES[datasetId]
   const trackCallableCoverage = await fetchTrackCallableCoverage(esClient, {
     index: trackCallableCoverageIndex,
+    contig: gene.chrom,
     regions: mergedExons,
     bucketSize,
   })
 
-  return {
-    cnv_track_callable_coverage: trackCallableCoverage || [],
-  }
+  return trackCallableCoverage
 }
 
 export const fetchTrackCallableCoverageForGene = _fetchTrackCallableCoverageForGene
+
+export const _fetchDelBurdenCoverageForGene = async (esClient: any, datasetId: any, gene: any) => {
+  assertDatasetAndReferenceGenomeMatch(datasetId, gene.reference_genome)
+
+  if (!datasetId.startsWith('gnomad_cnv')) {
+    throw new UserVisibleError('Deletion burden track coverage is not available for non-CNVs')
+  }
+
+  const paddedExons = extendRegions(75, gene.exons)
+
+  const mergedExons = mergeOverlappingRegions(
+    paddedExons.sort((a: any, b: any) => a.start - b.start)
+  )
+
+  const totalIntervalSize = totalRegionSize(mergedExons)
+  const bucketSize = Math.max(Math.floor(totalIntervalSize / 500), 1)
+
+  // @ts-expect-error TS(7053) FIXME: Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
+  const delBurdenCoverageIndex = COVERAGE_INDICES[datasetId]
+  const delBurdenCoverage = await fetchDelBurdenCoverage(esClient, {
+    index: delBurdenCoverageIndex,
+    contig: gene.chrom,
+    regions: mergedExons,
+    bucketSize,
+  })
+
+  return delBurdenCoverage
+}
+
+export const fetchDelBurdenCoverageForGene = _fetchDelBurdenCoverageForGene
+
+export const _fetchDupBurdenCoverageForGene = async (esClient: any, datasetId: any, gene: any) => {
+  assertDatasetAndReferenceGenomeMatch(datasetId, gene.reference_genome)
+
+  if (!datasetId.startsWith('gnomad_cnv')) {
+    throw new UserVisibleError('Duplication burden track coverage is not available for non-CNVs')
+  }
+
+  const paddedExons = extendRegions(75, gene.exons)
+
+  const mergedExons = mergeOverlappingRegions(
+    paddedExons.sort((a: any, b: any) => a.start - b.start)
+  )
+
+  const totalIntervalSize = totalRegionSize(mergedExons)
+  const bucketSize = Math.max(Math.floor(totalIntervalSize / 500), 1)
+
+  // @ts-expect-error TS(7053) FIXME: Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
+  const dupBurdenCoverageIndex = COVERAGE_INDICES[datasetId]
+  const dupBurdenCoverage = await fetchDupBurdenCoverage(esClient, {
+    index: dupBurdenCoverageIndex,
+    regions: mergedExons,
+    bucketSize,
+  })
+
+  return dupBurdenCoverage
+}
+
+export const fetchDupBurdenCoverageForGene = _fetchDupBurdenCoverageForGene
