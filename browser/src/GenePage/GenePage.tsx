21a92b658c42e70c5edd1f6022e0ed9320b7901f
ignatiusm <11403879+ignatiusm@users.noreply.github.com>
2024-01-19 11:08:24 +1300

    feat(styles): make more dry (#1384)

--- browser/src/GenePage/GenePage.tsx
+++ browser/src/GenePage/GenePage.tsx
@@ -62,6 +62,8 @@ import {
   CopyNumberVariant,
 } from '../VariantPage/VariantPage'
 import CopyNumberVariantsInGene from './CopyNumberVariantsInGene'
+import { ControlPanel, Legend, LegendItemWrapper, Label, CheckboxInput, LegendSwatch } from '../ChartStyles'
+
 
 export type Strand = '+' | '-'
 
@@ -165,61 +167,6 @@ const ConstraintOrCooccurrenceColumn = styled.div`
 
   @media (max-width: 1200px) {
     width: 100%;
-  }
-`
-
-const ControlPanel = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-end;
-  align-items: center;
-  width: ${(props: any) => props.width}px;
-  margin-left: ${(props: any) => props.marginLeft}px;
-
-  @media (max-width: 600px) {
-    flex-direction: column;
-    align-items: flex-start;
-    margin-left: 0;
-  }
-`
-
-const Legend = styled.ul`
-  display: flex;
-  flex-direction: row;
-  padding: 0;
-  margin: 0.5em 0;
-  list-style-type: none;
-`
-
-const LegendItemWrapper = styled.li`
-  display: flex;
-  align-items: stretch;
-  margin-left: 1em;
-`
-
-const Label = styled.label`
-  user-select: none;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-`
-
-const CheckboxInput = styled.input.attrs({ type: 'checkbox' })`
-  margin-right: 0.5em;
-`
-
-const LegendSwatch = styled.span`
-  display: flex;
-  align-items: center;
-  width: 16px;
-  margin-left: 0.5em;
-
-  &::before {
-    content: '';
-    display: inline-block;
-    width: 16px;
-    height: ${(props: any) => props.height}px;
-    background: ${(props: any) => props.color};
   }
 `
 
