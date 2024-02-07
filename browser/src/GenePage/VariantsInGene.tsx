broadinstitute/gnomad-browser

commit 269a79d4f9f23ef5824b512b57e08910acf25e44

Update gene page CSV export columns (#1397)

browser/src/GenePage/VariantsInGene.tsx
@@ -218,10 +218,22 @@ query ${operationName}($geneId: String!, $datasetId: DatasetId!, $referenceGenom
       transcript_id
       transcript_version
       variant_id
+      faf95_joint {
+        popmax
+        popmax_population
+      }
+      faf99_joint {
+        popmax
+        popmax_population
+      }
       exome {
         ac
         ac_hemi
         ac_hom
+        faf95 {
+          popmax
+          popmax_population
+        }
         an
         af
         filters
@@ -237,6 +249,10 @@ query ${operationName}($geneId: String!, $datasetId: DatasetId!, $referenceGenom
         ac
         ac_hemi
         ac_hom
+        faf95 {
+          popmax
+          popmax_population
+        }
         an
         af
         filters
@@ -247,6 +263,11 @@ query ${operationName}($geneId: String!, $datasetId: DatasetId!, $referenceGenom
           ac_hemi
           ac_hom
         }
+      }
+      in_silico_predictors {
+        id
+        value
+        flags
       }
       lof_curation {
         verdict

browser/src/VariantList/ExportVariantsButton.tsx
@@ -70,12 +70,89 @@ export const createPopulationColumns = (datasetId: DatasetId) => {
   return populationColumns
 }
 
+export const createVersionSpecificColumns = (datasetId: DatasetId) => {
+  const inSilicoPredictorIds = [
+    'cadd',
+    'revel_max',
+    'spliceai_ds_max',
+    'pangolin_largest_ds',
+    'phylop',
+    'sift_max',
+    'polyphen_max',
+  ]
+
+  let versionSpecificColumns: Column[] = []
+
+  if (isV4(datasetId)) {
+    versionSpecificColumns = [
+      {
+        label: 'GroupMax FAF group',
+        getValue: (variant: any) =>
+          variant.faf95_joint.popmax_population !== null
+            ? variant.faf95_joint.popmax_population
+            : '',
+      },
+      {
+        label: 'GroupMax FAF frequency',
+        getValue: (variant: any) =>
+          variant.faf95_joint.popmax !== null ? JSON.stringify(variant.faf95_joint.popmax) : '',
+      },
+    ]
+
+    inSilicoPredictorIds.forEach((id) => {
+      const inSilicoColumn = {
+        label: id,
+        getValue: (variant: any) =>
+          variant.in_silico_predictors.filter((predictor: any) => predictor.id === id).length > 0
+            ? variant.in_silico_predictors.filter((predictor: any) => predictor.id === id)[0].value
+            : '',
+      }
+      versionSpecificColumns = versionSpecificColumns.concat(inSilicoColumn)
+    })
+  }
+
+  if (isV2(datasetId)) {
+    versionSpecificColumns = [
+      {
+        label: 'Exome GroupMax FAF group',
+        getValue: (variant: any) =>
+          variant.exome.faf95.popmax_population !== null
+            ? variant.exome.faf95.popmax_population
+            : '',
+      },
+      {
+        label: 'Exome GroupMax FAF frequency',
+        getValue: (variant: any) =>
+          variant.exome.faf95.popmax !== null ? JSON.stringify(variant.exome.faf95.popmax) : '',
+      },
+      {
+        label: 'Genome GroupMax FAF group',
+        getValue: (variant: any) =>
+          variant.genome.faf95.popmax_population !== null
+            ? variant.genome.faf95.popmax_population
+            : '',
+      },
+      {
+        label: 'Genome GroupMax FAF frequency',
+        getValue: (variant: any) =>
+          variant.genome.faf95.popmax !== null ? JSON.stringify(variant.genome.faf95.popmax) : '',
+      },
+    ]
+  }
+
+  return versionSpecificColumns
+}
+
 const exportVariantsToCsv = (
   variants: VariantTableVariant[],
   datasetId: any,
   baseFileName: any
 ) => {
   const DEFAULT_COLUMNS = [
+    {
+      label: 'gnomAD ID',
+      getValue: (variant: any) => variant.variant_id,
+    },
     {
       label: 'Chromosome',
       getValue: (variant: any) => variant.variant_id.split('-')[0],
@@ -185,9 +262,11 @@ const exportVariantsToCsv = (
     },
   ]
 
+  const versionSpecificColumns = createVersionSpecificColumns(datasetId)
+
   const populationColumns = createPopulationColumns(datasetId)
 
-  const columns = DEFAULT_COLUMNS.concat(populationColumns)
+  const columns = DEFAULT_COLUMNS.concat(versionSpecificColumns, populationColumns)
 
   const headerRow = columns.map((c) => c.label)
 

graphql-api/src/queries/variant-datasets/gnomad-v4-variant-queries.ts
@@ -174,9 +174,9 @@ const fetchVariantById = async (esClient: any, variantIdOrRsid: any, subset: any
       populations: variant.exome.freq[subset].ancestry_groups,
       faf95: hasExomeVariant &&
         variant.exome.faf95 && {
-        popmax_population: variant.exome.faf95.grpmax_gen_anc,
-        popmax: variant.exome.faf95.grpmax,
-      },
+          popmax_population: variant.exome.faf95.grpmax_gen_anc,
+          popmax: variant.exome.faf95.grpmax,
+        },
       quality_metrics: {
         // TODO: An older version of the data pipeline stored only adj quality metric histograms.
         // Maintain the same behavior by returning the adj version until the API schema is updated to allow
@@ -205,9 +205,9 @@ const fetchVariantById = async (esClient: any, variantIdOrRsid: any, subset: any
       populations: genome_ancestry_groups,
       faf95: hasGenomeVariant &&
         variant.genome.faf95 && {
-        popmax_population: variant.genome.faf95.grpmax_gen_anc,
-        popmax: variant.genome.faf95.grpmax,
-      },
+          popmax_population: variant.genome.faf95.grpmax_gen_anc,
+          popmax: variant.genome.faf95.grpmax,
+        },
       quality_metrics: {
         // TODO: An older version of the data pipeline stored only adj quality metric histograms.
         // Maintain the same behavior by returning the adj version until the API schema is updated to allow
@@ -266,6 +266,33 @@ const shapeVariantSummary = (subset: any, context: any) => {
 
     const hasJointFafData = variant.faf95_joint && variant.faf99_joint
 
+    const inSilicoPredictorIds = [
+      'cadd',
+      'revel_max',
+      'spliceai_ds_max',
+      'pangolin_largest_ds',
+      'phylop',
+      'sift_max',
+      'polyphen_max',
+    ]
+
+    const inSilicoPredictorsList = inSilicoPredictorIds
+      .map((id) => {
+        if (variant.in_silico_predictors[id] || variant.in_silico_predictors[id] === 0) {
+          const name: string = id
+          if (id === 'cadd') {
+            return {
+              id: name,
+              value: variant.in_silico_predictors.cadd.phred.toPrecision(3),
+              flags: [],
+            }
+          }
+          return { id: name, value: variant.in_silico_predictors[id].toPrecision(3), flags: [] }
+        }
+        return null
+      })
+      .filter((item) => item)
+
     return {
       ...omit(variant, 'transcript_consequences', 'locus', 'alleles'), // Omit full transcript consequences list to avoid caching it
       reference_genome: 'GRCh38',
@@ -275,23 +302,23 @@ const shapeVariantSummary = (subset: any, context: any) => {
       alt: variant.alleles[1],
       exome: hasExomeVariant
         ? {
-          ...omit(variant.exome, 'freq'), // Omit freq field to avoid caching extra copy of frequency information
-          ...variant.exome.freq[subset],
-          populations: variant.exome.freq[subset].ancestry_groups.filter(
-            (pop: any) => !(pop.id.includes('_') || pop.id === 'XX' || pop.id === 'XY')
-          ),
-          filters: exomeFilters,
-        }
+            ...omit(variant.exome, 'freq'), // Omit freq field to avoid caching extra copy of frequency information
+            ...variant.exome.freq[subset],
+            populations: variant.exome.freq[subset].ancestry_groups.filter(
+              (pop: any) => !(pop.id.includes('_') || pop.id === 'XX' || pop.id === 'XY')
+            ),
+            filters: exomeFilters,
+          }
         : null,
       genome: hasGenomeVariant
         ? {
-          ...omit(variant.genome, 'freq'), // Omit freq field to avoid caching extra copy of frequency information
-          ...variant.genome.freq[subset],
-          populations: variant.genome.freq[subset].ancestry_groups.filter(
-            (pop: any) => !(pop.id.includes('_') || pop.id === 'XX' || pop.id === 'XY')
-          ),
-          filters: genomeFilters,
-        }
+            ...omit(variant.genome, 'freq'), // Omit freq field to avoid caching extra copy of frequency information
+            ...variant.genome.freq[subset],
+            populations: variant.genome.freq[subset].ancestry_groups.filter(
+              (pop: any) => !(pop.id.includes('_') || pop.id === 'XX' || pop.id === 'XY')
+            ),
+            filters: genomeFilters,
+          }
         : null,
       flags,
       transcript_consequence: transcriptConsequence,
@@ -303,6 +330,7 @@ const shapeVariantSummary = (subset: any, context: any) => {
         popmax_population: variant.faf99_joint.grpmax_gen_anc,
         popmax: variant.faf99_joint.grpmax,
       },
+      in_silico_predictors: inSilicoPredictorsList,
     }
   }
 }
@@ -353,12 +381,14 @@ const fetchVariantsByGene = async (esClient: any, gene: any, _subset: any) => {
         'value.exome.filters',
         'value.genome.filters',
         'value.alleles',
-        // 'value.caid',
         'value.locus',
         'value.flags',
         'value.rsids',
         'value.transcript_consequences',
         'value.variant_id',
+        'value.faf95_joint',
+        'value.faf99_joint',
+        'value.in_silico_predictors',
       ],
       body: {
         query: {

dataset-metadata/gnomadPopulations.ts
@@ -34,7 +34,7 @@ export const populationName = (populationId: string) =>
   textOrMissingTextWarning('genetic ancestry group name', GNOMAD_POPULATION_NAMES, populationId)
 
 const ExACPopulations: PopulationId[] = ['sas', 'afr', 'amr', 'eas', 'fin', 'nfe', 'remaining']
-const v2Populations: PopulationId[] = ['afr', 'nfe', 'afr', 'asj', 'eas', 'fin', 'sas', 'remaining']
+const v2Populations: PopulationId[] = ['amr', 'nfe', 'afr', 'asj', 'eas', 'fin', 'sas', 'remaining']
 const v3Populations: PopulationId[] = [
   'nfe',
   'amr',

graphql-api/src/graphql/types/variant.graphql
@@ -106,6 +106,7 @@ type VariantSequencingTypeData {
   hemizygote_count: Int
   filters: [String!]
   populations: [VariantPopulation]
+  faf95: VariantFilteringAlleleFrequency
 
   # Deprecated - calculate from AC and AN
   # Preserved for compatibility with existing browser queries
@@ -141,6 +142,9 @@ type Variant {
   flags: [String!]
   lof_curation: LoFCurationInGene
   transcript_consequence: TranscriptConsequence
+  faf95_joint: VariantFilteringAlleleFrequency
+  faf99_joint: VariantFilteringAlleleFrequency
+  in_silico_predictors: [VariantInSilicoPredictor!]
 
   # Deprecated - use rsids
   rsid: String

commit 5fe6dfed3f60f188e28319a2debd023bfc01cda0

Fix CSV export regressions introduced by #1397 (#1411)

browser/src/GenePage/VariantsInGene.tsx
@@ -222,10 +222,6 @@ query ${operationName}($geneId: String!, $datasetId: DatasetId!, $referenceGenom
         popmax
         popmax_population
       }
-      faf99_joint {
-        popmax
-        popmax_population
-      }
       exome {
         ac
         ac_hemi

browser/src/RegionPage/VariantsInRegion.tsx
@@ -127,10 +127,18 @@ query ${operationName}($chrom: String!, $start: Int!, $stop: Int!, $datasetId: D
       transcript_id
       transcript_version
       variant_id
+      faf95_joint {
+        popmax
+        popmax_population
+      }
       exome {
         ac
         ac_hemi
         ac_hom
+        faf95 {
+          popmax
+          popmax_population
+        }
         an
         af
         filters
@@ -146,6 +154,10 @@ query ${operationName}($chrom: String!, $start: Int!, $stop: Int!, $datasetId: D
         ac
         ac_hemi
         ac_hom
+        faf95 {
+          popmax
+          popmax_population
+        }
         an
         af
         filters
@@ -156,6 +168,11 @@ query ${operationName}($chrom: String!, $start: Int!, $stop: Int!, $datasetId: D
           ac_hemi
           ac_hom
         }
+      }
+      in_silico_predictors {
+        id
+        value
+        flags
       }
       lof_curation {
         verdict

browser/src/TranscriptPage/VariantsInTranscript.tsx
@@ -159,10 +159,18 @@ query ${operationName}($transcriptId: String!, $datasetId: DatasetId!, $referenc
       transcript_id
       transcript_version
       variant_id
+      faf95_joint {
+        popmax
+        popmax_population
+      }
       exome {
         ac
         ac_hemi
         ac_hom
+        faf95 {
+          popmax
+          popmax_population
+        }
         an
         af
         filters
@@ -178,6 +186,10 @@ query ${operationName}($transcriptId: String!, $datasetId: DatasetId!, $referenc
         ac
         ac_hemi
         ac_hom
+        faf95 {
+          popmax
+          popmax_population
+        }
         an
         af
         filters
@@ -188,6 +200,15 @@ query ${operationName}($transcriptId: String!, $datasetId: DatasetId!, $referenc
           ac_hemi
           ac_hom
         }
+      }
+      in_silico_predictors {
+        id
+        value
+        flags
+      }
+      lof_curation {
+        verdict
+        flags
       }
     }
   }

browser/src/VariantList/ExportVariantsButton.tsx
@@ -116,26 +116,30 @@ export const createVersionSpecificColumns = (datasetId: DatasetId) => {
       {
         label: 'Exome GroupMax FAF group',
         getValue: (variant: any) =>
-          variant.exome.faf95.popmax_population !== null
+          variant.exome && variant.exome.faf95.popmax_population !== null
             ? variant.exome.faf95.popmax_population
             : '',
       },
       {
         label: 'Exome GroupMax FAF frequency',
         getValue: (variant: any) =>
-          variant.exome.faf95.popmax !== null ? JSON.stringify(variant.exome.faf95.popmax) : '',
+          variant.exome && variant.exome.faf95.popmax !== null
+            ? JSON.stringify(variant.exome.faf95.popmax)
+            : '',
       },
       {
         label: 'Genome GroupMax FAF group',
         getValue: (variant: any) =>
-          variant.genome.faf95.popmax_population !== null
-            ? variant.genome.faf95.popmax_population
+          variant.genome && variant.genome?.faf95.popmax_population !== null
+            ? variant.genome?.faf95.popmax_population
             : '',
       },
       {
         label: 'Genome GroupMax FAF frequency',
         getValue: (variant: any) =>
-          variant.genome.faf95.popmax !== null ? JSON.stringify(variant.genome.faf95.popmax) : '',
+          variant.genome && variant.genome?.faf95.popmax !== null
+            ? JSON.stringify(variant.genome.faf95.popmax)
+            : '',
       },
     ]
   }

graphql-api/src/queries/variant-datasets/gnomad-v4-variant-queries.ts
@@ -381,6 +381,7 @@ const fetchVariantsByGene = async (esClient: any, gene: any, _subset: any) => {
         'value.exome.filters',
         'value.genome.filters',
         'value.alleles',
+        // 'value.caid',
         'value.locus',
         'value.flags',
         'value.rsids',
@@ -439,6 +440,9 @@ const fetchVariantsByRegion = async (esClient: any, region: any, _subset: any) =
       'value.rsids',
       'value.transcript_consequences',
       'value.variant_id',
+      'value.faf95_joint',
+      'value.faf99_joint',
+      'value.in_silico_predictors',
     ],
     body: {
       query: {
@@ -523,6 +527,9 @@ const fetchVariantsByTranscript = async (esClient: any, transcript: any, _subset
       'value.rsids',
       'value.transcript_consequences',
       'value.variant_id',
+      'value.faf95_joint',
+      'value.faf99_joint',
+      'value.in_silico_predictors',
     ],
     body: {
       query: {