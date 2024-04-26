6b68e9f12d607fa4ce2b3e9336d228eb21b4cab0
Riley Grant <grantrileyh@gmail.com>
2024-04-25 21:18:46 -0500

    feat(scripts): update md5 and size script, use for v4 md5s

--- development/scripts/get_object_metadata.py
+++ development/scripts/get_object_metadata.py
@@ -5,7 +5,9 @@ Use this script to get sizes and MD5 hashes for objects listed on the downloads 
 
 Usage:
 
-./get_object_metadata.py gs://bucket/file_1 gs://bucket/file_2 ...
+python ./get_object_metadata.py --urls gs://bucket/file_1 gs://bucket/file_2 ...
+
+python ./get_object_metadata.py --bucket gs://gcp-public-data--gnomad/release/4.1/vcf/exomes
 """
 
 import argparse
@@ -14,8 +16,8 @@ import json
 import subprocess
 
 
-ONE_GIBIBYTE = 2 ** 30
-ONE_MEBIBYTE = 2 ** 20
+ONE_GIBIBYTE = 2**30
+ONE_MEBIBYTE = 2**20
 
 
 def fetch_object_metadata(url):
@@ -43,14 +45,62 @@ def fetch_object_metadata(url):
     return info
 
 
+def fetch_object_metadata_from_bucket(bucket_prefix):
+    output = subprocess.check_output(["gsutil", "ls", "-L", bucket_prefix]).decode("utf8")
+
+    aggregated_info = {}
+
+    lines = output.split("\n")
+    current_filename = None
+    for line in lines:
+        if line.startswith("gs://"):
+            current_filename = line.split("/")[-1]
+            aggregated_info[current_filename] = {}
+        elif line.strip().startswith("Content-Length:"):
+            size = int(line.split(":")[1].strip())
+            if size >= ONE_GIBIBYTE:
+                aggregated_info[current_filename]["size"] = f"{round(size / ONE_GIBIBYTE, 2)} GiB"
+            else:
+                aggregated_info[current_filename]["size"] = f"{round(size / ONE_MEBIBYTE, 2)} MiB"
+        elif line.strip().startswith("Hash (md5):"):
+            md5_hash = line.split(":")[1].strip()
+            aggregated_info[current_filename]["md5"] = base64.b64decode(md5_hash).hex()
+
+    return aggregated_info
+
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("urls", metavar="url", nargs="+")
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--urls", metavar="url", nargs="+")
+    group.add_argument("--bucket")
     args = parser.parse_args()
 
-    for url in args.urls:
-        info = fetch_object_metadata(url)
-        print(json.dumps(info))
+    if args.urls:
+        for url in args.urls:
+            info = fetch_object_metadata(url)
+            print(json.dumps(info))
+    elif args.bucket:
+        aggregated_info = fetch_object_metadata_from_bucket(args.bucket)
+        del aggregated_info[":"]
+        aggregated_info = {k: v for k, v in aggregated_info.items() if not k.endswith("tbi:")}
+
+        sorted_filenames = sorted(
+            aggregated_info.keys(),
+            key=lambda x: (
+                (int(x.split(".")[5][3:]) if x.split(".")[5][3:].isdigit() else float("inf")),
+                x.split(".")[3],
+            ),
+        )
+
+        print(f"Results for {args.bucket}\n")
+        print("[")
+        for i, filename in enumerate(sorted_filenames):
+            if not filename.endswith(".tbi:"):
+                line = f"    {{ chrom: '{filename.split('.')[5][3:]}', size: {json.dumps(aggregated_info[filename]['size'])}, md5: {json.dumps(aggregated_info[filename]['md5'])} }}"
+                print(line + "," if i < len(sorted_filenames) - 1 else line)
+        print("]")
 
 
 if __name__ == "__main__":
