b650a176fa1961a94d00fb55661bfb082579a86c
Phil Darnowsky <pdarnows@broadinstitute.org>
2024-05-23 17:13:10 -0400

    Add VRS data to v4 variant data pipeline

--- data-pipeline/src/data_pipeline/data_types/variant/__init__.py
+++ data-pipeline/src/data_pipeline/data_types/variant/__init__.py
@@ -1,4 +1,4 @@
-from .annotate_variants import annotate_variants, annotate_caids
+from .annotate_variants import annotate_variants, annotate_caids, annotate_vrs_ids
 from .transcript_consequence.annotate_transcript_consequences import annotate_transcript_consequences
 from .variant_id import variant_id, variant_ids, compressed_variant_id
 
@@ -9,4 +9,5 @@ __all__ = [
     "variant_id",
     "variant_ids",
     "compressed_variant_id",
+    "annotate_vrs_ids",
 ]
--- data-pipeline/src/data_pipeline/data_types/variant/annotate_variants.py
+++ data-pipeline/src/data_pipeline/data_types/variant/annotate_variants.py
@@ -23,3 +23,31 @@ def annotate_caids(variants_path, caids_path=None):
         ds = ds.annotate(caid=caids[ds.key].caid)
 
     return ds
+
+
+def annotate_vrs_ids(variants_path, exome_variants_path, genome_variants_path):
+    ds = hl.read_table(variants_path)
+    exomes = hl.read_table(exome_variants_path)
+    genomes = hl.read_table(genome_variants_path)
+    exome_vrs = exomes.select(vrs=exomes.info.vrs)
+    genome_vrs = genomes.select(vrs=genomes.info.vrs)
+    vrs = exome_vrs.union(genome_vrs)
+    vrs = vrs.group_by(vrs.locus, vrs.alleles).aggregate(vrs=hl.agg.collect(vrs.vrs)[0])
+    vrs = vrs.transmute(
+        vrs=hl.struct(
+            ref=hl.struct(
+                allele_id=vrs.vrs.VRS_Allele_IDs[0],
+                start=vrs.vrs.VRS_Starts[0],
+                end=vrs.vrs.VRS_Ends[0],
+                state=vrs.vrs.VRS_States[0],
+            ),
+            alt=hl.struct(
+                allele_id=vrs.vrs.VRS_Allele_IDs[1],
+                start=vrs.vrs.VRS_Starts[1],
+                end=vrs.vrs.VRS_Ends[1],
+                state=vrs.vrs.VRS_States[1],
+            ),
+        )
+    )
+    ds = ds.join(vrs)
+    return ds
