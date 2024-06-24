9dbec2261014f65282f499fe789a46c1a579d233
Riley Grant <grantrileyh@gmail.com>
2024-06-24 13:29:10 -0500

    feat(browser): add snapshot test for GnomadPopulationsTable

---
+++ browser/src/VariantPage/GnomadPopulationsTable.spec.tsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import { describe, expect, test } from '@jest/globals'
+import renderer from 'react-test-renderer'
+
+import { GnomadPopulationsTable } from './GnomadPopulationsTable'
+import { allDatasetIds } from '@gnomad/dataset-metadata/metadata'
+import { createAncestryGroupObjects } from '../__factories__/Variant'
+
+describe('GnomadPopulationsTable', () => {
+  describe.each(allDatasetIds)('with a dataset of %s', (dataset: any) => {
+    test('has no unexpected changes', () => {
+      const exomeGeneticAncestryGroupObjexts = createAncestryGroupObjects(
+        [
+          { id: 'afr', value: 1 },
+          { id: 'remaining', value: 2 },
+          { id: 'eur', value: 4 },
+          { id: 'XX', value: 8 },
+          { id: 'XY', value: 16 },
+        ],
+        false
+      )
+
+      const genomeGeneticAncestryGroupObjexts = createAncestryGroupObjects(
+        [
+          { id: 'afr', value: 32 },
+          { id: 'remaining', value: 64 },
+          { id: 'eur', value: 128 },
+          { id: 'XX', value: 256 },
+          { id: 'XY', value: 512 },
+        ],
+        false
+      )
+
+      const tree = renderer.create(
+        <GnomadPopulationsTable
+          datasetId={dataset}
+          exomePopulations={exomeGeneticAncestryGroupObjexts}
+          genomePopulations={genomeGeneticAncestryGroupObjexts}
+          jointPopulations={null}
+          showHemizygotes={false}
+        />
+      )
+
+      expect(tree).toMatchSnapshot()
+    })
+    test('has no unexpected changes when missing genetic ancestry groups are filled in', () => {
+      const jointGeneticAncestryGroupObjects = createAncestryGroupObjects(
+        [
+          { id: 'afr', value: 1 },
+          { id: 'remaining', value: 2 },
+          { id: 'eur', value: 4 },
+          { id: 'XX', value: 8 },
+          { id: 'XY', value: 16 },
+        ],
+        true
+      )
+
+      const tree = renderer.create(
+        <GnomadPopulationsTable
+          datasetId={dataset}
+          exomePopulations={[]}
+          genomePopulations={[]}
+          jointPopulations={jointGeneticAncestryGroupObjects}
+          showHemizygotes={false}
+        />
+      )
+
+      expect(tree).toMatchSnapshot()
+    })
+  })
+})
