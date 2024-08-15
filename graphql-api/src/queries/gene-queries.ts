598833d2a73772c1ba4b051f2928b54836be119c
Riley Grant <grantrileyh@gmail.com>
2024-08-15 17:10:26 -0500

    feat(graphql-api): adjust api types for new gtex/pext schema

--- graphql-api/src/queries/gene-queries.ts
+++ graphql-api/src/queries/gene-queries.ts
@@ -3,8 +3,10 @@ import { withCache } from '../cache'
 import { fetchAllSearchResults } from './helpers/elasticsearch-helpers'
 
 const GENE_INDICES = {
-  GRCh37: 'genes_grch37',
-  GRCh38: 'genes_grch38',
+  // GRCh37: 'genes_grch37',
+  // GRCh38: 'genes_grch38',
+  GRCh37: 'genes_grch37-2024-09-30--21-33',
+  GRCh38: 'genes_grch38-2024-10-01--19-16',
 }
 
 const _fetchGeneById = async (esClient: any, geneId: any, referenceGenome: any) => {
