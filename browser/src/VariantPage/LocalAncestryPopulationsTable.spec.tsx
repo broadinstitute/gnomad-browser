32b3a685b3d5369e2720c6c8563c1b71b06f5487
Phil Darnowsky <pdarnows@broadinstitute.org>
2024-09-27 15:21:56 -0400

    Characterize existing local ancestry group behavior with types and tests
    We're about to add local ancestry group data for the African/African-American ancestry group. In preparation for this, we add some types and a basic snapshot test to characterize the behavior with existing Admixed American local ancestry data, to help guard against regressions.

---
+++ browser/src/VariantPage/LocalAncestryPopulationsTable.spec.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { expect, test } from '@jest/globals'
+import renderer from 'react-test-renderer'
+import { LocalAncestryPopulation } from './VariantPage'
+
+import LocalAncestryPopulationsTable from './LocalAncestryPopulationsTable'
+
+describe('local ancestry populations table', () => {
+  test(`has no unexpected changes`, () => {
+    const localAncestryData: LocalAncestryPopulation[] = [
+      { id: 'amr_african', ac: 1, an: 2 },
+      { id: 'amr_amerindigenous', ac: 3, an: 4 },
+      { id: 'amr_european', ac: 4, an: 5 },
+    ]
+    const tree = renderer.create(<LocalAncestryPopulationsTable populations={localAncestryData} />)
+    expect(tree).toMatchSnapshot()
+  })
+})
