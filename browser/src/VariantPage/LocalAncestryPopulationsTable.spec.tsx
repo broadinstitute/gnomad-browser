36a86df7c3132dbd5cfffbcf0b373c5a1f052c2f
Phil Darnowsky <pdarnows@broadinstitute.org>
2024-10-03 16:37:18 -0400

    Add tests to verify that local ancestry table works with AFR data

--- browser/src/VariantPage/LocalAncestryPopulationsTable.spec.tsx
+++ browser/src/VariantPage/LocalAncestryPopulationsTable.spec.tsx
@@ -11,6 +11,8 @@ describe('local ancestry populations table', () => {
       { id: 'amr_african', ac: 1, an: 2 },
       { id: 'amr_amerindigenous', ac: 3, an: 4 },
       { id: 'amr_european', ac: 4, an: 5 },
+      { id: 'afr_african', ac: 5, an: 6 },
+      { id: 'afr_european', ac: 7, an: 8 },
     ]
     const tree = renderer.create(<LocalAncestryPopulationsTable populations={localAncestryData} />)
     expect(tree).toMatchSnapshot()
