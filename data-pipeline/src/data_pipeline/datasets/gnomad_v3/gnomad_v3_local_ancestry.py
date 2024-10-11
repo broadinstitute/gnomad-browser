9029cdc23c05fb672ffbbe56a3d0e5e6d3139a61
Phil Darnowsky <pdarnows@broadinstitute.org>
2024-10-11 16:12:11 -0400

    Fix loading of local ancestry with one top-level ancestry group

--- data-pipeline/src/data_pipeline/datasets/gnomad_v3/gnomad_v3_local_ancestry.py
+++ data-pipeline/src/data_pipeline/datasets/gnomad_v3/gnomad_v3_local_ancestry.py
@@ -2,6 +2,9 @@ import hail as hl
 
 from data_pipeline.data_types.variant import variant_id
 
+data_item = hl.tstruct(id=hl.tstr, ac=hl.tint32, an=hl.tint32)
+data_array = hl.tarray(data_item)
+
 
 def prepare_local_ancestry(sources):
     result = None
@@ -15,24 +18,29 @@ def prepare_local_ancestry(sources):
 
         ds = ds.select(
             variant_id=variant_id(ds.locus, ds.alleles),
-            populations=hl.struct(
-                genome=hl.array(
-                    [
-                        hl.struct(
-                            id=f"{ancestry_group_id}_{key}", ac=ds.info[f"AC_{vcf_key}"], an=ds.info[f"AN_{vcf_key}"]
-                        )
-                        for key, vcf_key in local_ancestry_group_keys
-                    ]
-                ),
+            genome=hl.array(
+                [
+                    hl.struct(id=f"{ancestry_group_id}_{key}", ac=ds.info[f"AC_{vcf_key}"], an=ds.info[f"AN_{vcf_key}"])
+                    for key, vcf_key in local_ancestry_group_keys
+                ]
             ),
         )
         ds = ds.key_by(ds.variant_id)
+        ds = ds.select(ds.genome, ds.locus, ds.alleles)
 
         if result is None:
             result = ds
         else:
+            result = result.join(ds, how="outer")
             result = result.transmute(
-                populations=hl.struct(genome=result.populations.genome.extend(ds[result.variant_id].populations.genome))
+                genome=hl.or_else(result.genome, hl.literal([], dtype=data_array)),
+                genome_1=hl.or_else(result.genome_1, hl.literal([], dtype=data_array)),
+                locus=hl.or_else(result.locus, result.locus_1),
+                alleles=hl.or_else(result.alleles, result.alleles_1),
+            )
+            result = result.transmute(
+                genome=result.genome.extend(result.genome_1),
             )
 
-    return result
+    shaped_result = result.transmute(populations=hl.struct(genome=result.genome))
+    return shaped_result
