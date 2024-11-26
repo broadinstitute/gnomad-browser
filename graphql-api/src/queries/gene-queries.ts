bd36b6684f2ad044ab8ee45fb6afa5316a6e68de
Riley Grant <grantrileyh@gmail.com>
2024-11-26 15:30:16 -0500

    DONTMERGE(graphql-api): rig genes indices

--- graphql-api/src/queries/gene-queries.ts
+++ graphql-api/src/queries/gene-queries.ts
@@ -3,8 +3,12 @@ import { withCache } from '../cache'
 import { fetchAllSearchResults } from './helpers/elasticsearch-helpers'
 
 const GENE_INDICES = {
-  GRCh37: 'genes_grch37',
-  GRCh38: 'genes_grch38',
+  // GRCh37: 'genes_grch37',
+  // GRCh38: 'genes_grch38',
+  // GRCh37: 'genes_grch37-2024-11-15--15-23',
+  // GRCh38: 'genes_grch38-2024-11-20--16-28',
+  GRCh37: 'genes_grch37-2024-11-25--15-55',
+  GRCh38: 'genes_grch38-2024-11-25--19-00',
 }
 
 const _fetchGeneById = async (esClient: any, geneId: any, referenceGenome: any) => {
