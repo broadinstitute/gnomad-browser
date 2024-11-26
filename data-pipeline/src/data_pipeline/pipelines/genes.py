12124695d8ff00b609951a8bc1c546a36a999bc3
Riley Grant <grantrileyh@gmail.com>
2024-11-26 09:20:26 -0500

    feat(data-pipeline): add steps for v4 rmc

--- data-pipeline/src/data_pipeline/pipelines/genes.py
+++ data-pipeline/src/data_pipeline/pipelines/genes.py
@@ -24,7 +24,7 @@ from data_pipeline.datasets.exac.exac_constraint import prepare_exac_constraint
 from data_pipeline.datasets.exac.exac_regional_missense_constraint import prepare_exac_regional_missense_constraint
 from data_pipeline.datasets.gnomad_v2.gnomad_v2_constraint import prepare_gnomad_v2_constraint
 from data_pipeline.datasets.gnomad_v2.gnomad_v2_regional_missense_constraint import (
-    prepare_gnomad_v2_regional_missense_constraint,
+    prepare_gnomad_regional_missense_constraint,
 )
 
 from data_pipeline.pipelines.variant_cooccurrence_counts import (
@@ -256,6 +256,9 @@ pipeline.add_task(
     {
         "gtex_struct_path": pipeline.get_task("prepare_gtex_v10_expression_data"),
     },
+    {
+        "exclude_v10_tissues": True,
+    },
 )
 
 pipeline.add_task(
@@ -315,9 +318,20 @@ pipeline.add_task(
 
 pipeline.add_task(
     "prepare_gnomad_v2_regional_missense_constraint",
-    prepare_gnomad_v2_regional_missense_constraint,
+    prepare_gnomad_regional_missense_constraint,
     f"/{constraint_subdir}/gnomad_v2_regional_missense_constraint.ht",
-    {"path": "gs://gcp-public-data--gnomad/release/2.1.1/regional_missense_constraint/gnomad_v2.1.1_rmc.ht"},
+    {
+        "path": "gs://gcp-public-data--gnomad/release/2.1.1/regional_missense_constraint/gnomad_v2.1.1_rmc.ht",
+    },
+)
+
+pipeline.add_task(
+    "prepare_gnomad_v4_regional_missense_constraint",
+    prepare_gnomad_regional_missense_constraint,
+    f"/{constraint_subdir}/gnomad_v4_regional_missense_constraint.ht",
+    {
+        "path": "gs://gnomad-v4-data-pipeline/inputs/regional_missense_constraint/rmc_browser.ht",
+    },
 )
 
 ###############################################
@@ -377,7 +391,7 @@ pipeline.add_task(
         "exac_constraint": pipeline.get_task("prepare_exac_constraint"),
         "exac_regional_missense_constraint": pipeline.get_task("prepare_exac_regional_missense_constraint"),
         "gnomad_constraint": pipeline.get_task("prepare_gnomad_v2_constraint"),
-        "gnomad_v2_regional_missense_constraint": pipeline.get_task("prepare_gnomad_v2_regional_missense_constraint"),
+        "gnomad_regional_missense_constraint": pipeline.get_task("prepare_gnomad_v2_regional_missense_constraint"),
     },
     {"join_on": "preferred_transcript_id"},
 )
@@ -469,19 +483,30 @@ pipeline.add_task(
 
 pipeline.add_task(
     "annotate_grch38_genes_step_6",
-    reject_par_y_genes,
+    annotate_table,
     f"/{genes_subdir}/genes_grch38_annotated_6.ht",
     {
-        "genes_path": pipeline.get_task("annotate_grch38_genes_step_5"),
+        "table_path": pipeline.get_task("annotate_grch38_genes_step_5"),
+        "gnomad_regional_missense_constraint": pipeline.get_task("prepare_gnomad_v4_regional_missense_constraint"),
+    },
+    {"join_on": "preferred_transcript_id"},
+)
+
+pipeline.add_task(
+    "annotate_grch38_genes_step_7",
+    reject_par_y_genes,
+    f"/{genes_subdir}/genes_grch38_annotated_7.ht",
+    {
+        "genes_path": pipeline.get_task("annotate_grch38_genes_step_6"),
     },
 )
 
 pipeline.add_task(
     "remove_grch38_genes_constraint_for_release",
     remove_gnomad_v4_constraint,
-    f"/{genes_subdir}/genes_grch38_annotate_5_removed_constraint",
+    f"/{genes_subdir}/genes_grch38_annotate_7_removed_constraint",
     {
-        "genes_path": pipeline.get_task("annotate_grch38_genes_step_5"),
+        "genes_path": pipeline.get_task("annotate_grch38_genes_step_7"),
     },
 )
 
@@ -548,7 +573,7 @@ pipeline.add_task(
 pipeline.set_outputs(
     {
         "genes_grch37": "annotate_grch37_genes_step_5",
-        "genes_grch38": "annotate_grch38_genes_step_6",
+        "genes_grch38": "annotate_grch38_genes_step_7",
         "base_transcripts_grch37": "extract_grch37_transcripts",
         "base_transcripts_grch38": "extract_grch38_transcripts",
         "transcripts_grch37": "annotate_grch37_transcripts",
