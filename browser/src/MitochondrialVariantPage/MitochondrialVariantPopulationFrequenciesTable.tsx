d5a81bf9ddbd7b4410671ed00ed304eba52d9fbb
Phil Darnowsky <pdarnows@broadinstitute.org>
2025-02-10 10:45:41 -0500

    Move column rendering logic into extracted useSort

--- browser/src/MitochondrialVariantPage/MitochondrialVariantPopulationFrequenciesTable.tsx
+++ browser/src/MitochondrialVariantPage/MitochondrialVariantPopulationFrequenciesTable.tsx
@@ -1,11 +1,11 @@
-import React, { useCallback, useState } from 'react'
+import React, { ReactNode, useCallback, useState } from 'react'
 import styled from 'styled-components'
 
 import { BaseTable, TooltipAnchor, TooltipHint } from '@gnomad/ui'
 
 import { GNOMAD_POPULATION_NAMES } from '@gnomad/dataset-metadata/gnomadPopulations'
 
-import { MitochondrialVariant } from './MitochondrialVariantPage'
+import { MitochondrialVariant, MitochondrialVariantPopulation } from './MitochondrialVariantPage'
 
 const CountCell = styled.span`
   display: inline-block;
@@ -31,37 +31,23 @@ const Table = styled(BaseTable)`
   }
 `
 
-export const sortKeys = ['id', 'an', 'af_het', 'af_hom', 'ac_het', 'ac_hom'] as const
-type SortKey = (typeof sortKeys)[number]
+type RowCompareFunction<RowData> = (a: RowData, b: RowData) => number
 
-const useSort = <K extends string>(
-  defaultSortKey: K
-): [{ key: K; ascending: boolean }, (key: K) => void] => {
-  const [key, setKey] = useState<K>(defaultSortKey)
-  const [ascending, setAscending] = useState<boolean>(false)
-
-  const setSortKey = useCallback(
-    (newKey: K) => {
-      setKey(newKey)
-      setAscending(newKey === key ? !ascending : false)
-    },
-    [key, ascending]
-  )
-
-  return [{ key, ascending }, setSortKey]
+type ColumnSpecifier<RowData> = {
+  key: keyof RowData
+  label: string
+  tooltip: string | null
+  compareValueFunction: RowCompareFunction<RowData>
 }
 
-type MitochondrialVariantPopulationFrequenciesTableProps = {
-  variant: MitochondrialVariant
-}
-
-const renderColumnHeader = <K extends string>(
-  key: K,
-  sortBy: K,
-  setSortBy: (key: K) => void,
+const renderColumnHeader = <RowData,>(
+  key: keyof RowData,
+  sortBy: keyof RowData,
+  setSortBy: (key: keyof RowData) => void,
   sortAscending: boolean,
   label: string,
-  tooltip: string | null
+  tooltip: string | null,
+  compareValueFunction: RowCompareFunction<RowData>
 ) => {
   let ariaSortAttr: React.AriaAttributes['aria-sort'] = 'none'
   if (sortBy === key) {
@@ -72,7 +58,7 @@ const renderColumnHeader = <K extends string>(
     <th aria-sort={ariaSortAttr} scope="col">
       {/* @ts-expect-error TS(2322) FIXME: Type '{ children: Element; tooltip: any; }' is not... Remove this comment to see the full error message */}
       <TooltipAnchor tooltip={tooltip}>
-        <button type="button" onClick={() => setSortBy(key as K)}>
+        <button type="button" onClick={() => setSortBy(key)}>
           {/* @ts-expect-error TS(2745) FIXME: This JSX tag's 'children' prop expects type 'never... Remove this comment to see the full error message */}
           <TooltipHint>{label}</TooltipHint>
         </button>
@@ -80,45 +66,156 @@ const renderColumnHeader = <K extends string>(
     </th>
   ) : (
     <th aria-sort={ariaSortAttr} scope="col">
-      <button type="button" onClick={() => setSortBy(key as K)}>
+      <button type="button" onClick={() => setSortBy(key)}>
         {label}
       </button>
     </th>
   )
 }
 
+//  .sort((a, b) => {
+//   const [population1, population2] = sortAscending ? [a, b] : [b, a]
+//
+//   return sortBy === 'id'
+//     ? GNOMAD_POPULATION_NAMES[population1.id].localeCompare(
+//         GNOMAD_POPULATION_NAMES[population2.id]
+//       )
+//     : population1[sortBy] - population2[sortBy]
+// }) */
+
+const useSort = <RowData,>(
+  columnSpecifiers: ColumnSpecifier<RowData>[],
+  defaultSortKey: keyof RowData,
+  rowData: RowData[]
+): { headers: ReactNode; sortedRowData: RowData[] } => {
+  const [key, setKey] = useState<keyof RowData>(defaultSortKey)
+  const [ascending, setAscending] = useState<boolean>(false)
+
+  const setSortKey = useCallback(
+    (newKey: keyof RowData) => {
+      setKey(newKey)
+      setAscending(newKey === key ? !ascending : false)
+    },
+    [key, ascending]
+  )
+
+  const { compareValueFunction } = columnSpecifiers.find((column) => column.key === key)!
+  const sortedRowData = [...rowData].sort((a, b) => {
+    const ascendingCompare = compareValueFunction(a, b)
+    return ascending ? ascendingCompare : -ascendingCompare
+  })
+
+  const headers = (
+    <>
+      {columnSpecifiers.map((columnSpecifier) =>
+        renderColumnHeader(
+          columnSpecifier.key,
+          key,
+          setSortKey,
+          ascending,
+          columnSpecifier.label,
+          columnSpecifier.tooltip,
+          columnSpecifier.compareValueFunction
+        )
+      )}
+    </>
+  )
+  return { headers, sortedRowData }
+}
+
+type MitochondrialVariantPopulationFrequenciesTableProps = {
+  variant: MitochondrialVariant
+}
+
+type MitochondrialVariantPopulationWithFrequency = MitochondrialVariantPopulation & {
+  af_hom: number
+  af_het: number
+}
+
+type NumberHolder<Key extends string> = {
+  [K in Key]: number
+}
+
+export const numericCompareFunction =
+  <Key extends string>(key: Key) =>
+  <RowData extends NumberHolder<Key>>(a: RowData, b: RowData) =>
+    a[key] - b[key]
+
+const comparePopulationNames = (
+  a: MitochondrialVariantPopulationWithFrequency,
+  b: MitochondrialVariantPopulationWithFrequency
+) => GNOMAD_POPULATION_NAMES[a.id].localeCompare(GNOMAD_POPULATION_NAMES[b.id])
+
+const columnSpecifiers: ColumnSpecifier<MitochondrialVariantPopulationWithFrequency>[] = [
+  {
+    key: 'id',
+    label: 'Genetic Ancestry Group',
+    tooltip: null,
+    compareValueFunction: comparePopulationNames,
+  },
+  {
+    key: 'an',
+    label: 'Allele Number',
+    tooltip:
+      'Total number of individuals in this population with high quality sequence at this position.',
+    compareValueFunction: numericCompareFunction('an'),
+  },
+  {
+    key: 'ac_hom',
+    label: 'Homoplasmic AC',
+    tooltip:
+      'Number of individuals in this population with homoplasmic or near-homoplasmic variant (heteroplasmy level ≥ 0.95).',
+    compareValueFunction: numericCompareFunction('ac_hom'),
+  },
+  {
+    key: 'af_hom',
+    label: 'Homoplasmic AF',
+    tooltip:
+      'Proportion of individuals in this population with homoplasmic or near-homoplasmic variant (heteroplasmy level ≥ 0.95).',
+    compareValueFunction: numericCompareFunction('af_hom'),
+  },
+  {
+    key: 'ac_het',
+    label: 'Heteroplasmic AC',
+    tooltip:
+      'Number of individuals in this population with a variant at heteroplasmy level 0.10 - 0.95.',
+    compareValueFunction: numericCompareFunction('ac_het'),
+  },
+  {
+    key: 'af_het',
+    label: 'Heteroplasmic AF',
+    tooltip:
+      'Proportion of individuals in this population with a variant at heteroplasmy level 0.10 - 0.95.',
+    compareValueFunction: numericCompareFunction('af_het'),
+  },
+]
+
 const MitochondrialVariantPopulationFrequenciesTable = ({
   variant,
 }: MitochondrialVariantPopulationFrequenciesTableProps) => {
-  const [{ key: sortBy, ascending: sortAscending }, setSortBy] = useSort<SortKey>('af_hom')
+  const populationsWithFrequencies = variant.populations.map((population) => ({
+    ...population,
+    af_hom: population.an !== 0 ? population.ac_hom / population.an : 0,
+    af_het: population.an !== 0 ? population.ac_het / population.an : 0,
+  }))
 
-  const renderedPopulations = variant.populations
-    .map((population) => ({
-      ...population,
-      af_hom: population.an !== 0 ? population.ac_hom / population.an : 0,
-      af_het: population.an !== 0 ? population.ac_het / population.an : 0,
-    }))
-    .sort((a, b) => {
-      const [population1, population2] = sortAscending ? [a, b] : [b, a]
+  const { headers, sortedRowData } = useSort<MitochondrialVariantPopulationWithFrequency>(
+    columnSpecifiers,
+    'af_hom',
+    populationsWithFrequencies
+  )
 
-      return sortBy === 'id'
-        ? GNOMAD_POPULATION_NAMES[population1.id].localeCompare(
-            GNOMAD_POPULATION_NAMES[population2.id]
-          )
-        : population1[sortBy] - population2[sortBy]
-    })
-
-  const totalAlleleNumber = renderedPopulations
+  const totalAlleleNumber = sortedRowData
     .map((population) => population.an)
     .reduce((acc, n) => acc + n, 0)
 
-  const totalHomoplasmicAlleleCount = renderedPopulations
+  const totalHomoplasmicAlleleCount = sortedRowData
     .map((population) => population.ac_hom)
     .reduce((acc, n) => acc + n, 0)
   const totalHomoplasmicAlleleFrequency =
     totalAlleleNumber !== 0 ? totalHomoplasmicAlleleCount / totalAlleleNumber : 0
 
-  const totalHeteroplasmicAlleleCount = renderedPopulations
+  const totalHeteroplasmicAlleleCount = sortedRowData
     .map((population) => population.ac_het)
     .reduce((acc, n) => acc + n, 0)
   const totalHeteroplasmicAlleleFrequency =
@@ -127,59 +224,10 @@ const MitochondrialVariantPopulationFrequenciesTable = ({
   return (
     <Table>
       <thead>
-        <tr>
-          {renderColumnHeader(
-            'id',
-            sortBy,
-            setSortBy,
-            sortAscending,
-            'Genetic Ancestry Group',
-            null
-          )}
-          {renderColumnHeader(
-            'an',
-            sortBy,
-            setSortBy,
-            sortAscending,
-            'Allele Number',
-            'Total number of individuals in this population with high quality sequence at this position.'
-          )}
-          {renderColumnHeader(
-            'ac_hom',
-            sortBy,
-            setSortBy,
-            sortAscending,
-            'Homoplasmic AC',
-            'Number of individuals in this population with homoplasmic or near-homoplasmic variant (heteroplasmy level ≥ 0.95).'
-          )}
-          {renderColumnHeader(
-            'af_hom',
-            sortBy,
-            setSortBy,
-            sortAscending,
-            'Homoplasmic AF',
-            'Proportion of individuals in this population with homoplasmic or near-homoplasmic variant (heteroplasmy level ≥ 0.95).'
-          )}
-          {renderColumnHeader(
-            'ac_het',
-            sortBy,
-            setSortBy,
-            sortAscending,
-            'Heteroplasmic AC',
-            'Number of individuals in this population with a variant at heteroplasmy level 0.10 - 0.95.'
-          )}
-          {renderColumnHeader(
-            'af_het',
-            sortBy,
-            setSortBy,
-            sortAscending,
-            'Heteroplasmic AF',
-            'Proportion of individuals in this population with a variant at heteroplasmy level 0.10 - 0.95.'
-          )}
-        </tr>
+        <tr>{headers}</tr>
       </thead>
       <tbody>
-        {renderedPopulations.map((population) => (
+        {sortedRowData.map((population) => (
           <tr key={population.id}>
             <th scope="row">{GNOMAD_POPULATION_NAMES[population.id]}</th>
             <td>
