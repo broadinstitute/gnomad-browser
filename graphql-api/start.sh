f2a5f0cc35d040f156c8b463cf14d2ea4f56a6a1
said-pager <said.smajic@pager.com>
2025-03-07 10:35:42 +0100

    graphql working

--- graphql-api/start.sh
+++ graphql-api/start.sh
@@ -9,10 +9,16 @@ DEFAULT_ELASTICSEARCH_URL="http://localhost:9200"
 export ELASTICSEARCH_URL="${ELASTICSEARCH_URL:-$DEFAULT_ELASTICSEARCH_URL}"
 
 DEFAULT_CACHE_REDIS_URL="redis://localhost:6379/1"
-export CACHE_REDIS_URL="${CACHE_REDIS_URL:-$DEFAULT_CACHE_REDIS_URL}"
+export CACHE_REDIS_URL="redis://localhost:6379/1"
 
 DEFAULT_RATE_LIMITER_REDIS_URL="redis://localhost:6379/2"
 export RATE_LIMITER_REDIS_URL="${RATE_LIMITER_REDIS_URL:-$DEFAULT_RATE_LIMITER_REDIS_URL}"
+
+echo "Before hsot and port."
+export REDIS_HOST=localhost
+export REDIS_PORT=6379
+
+echo "Script finished."
 
 pnpm ts-node ./src/app.ts
 # To use an interactive debugger with the API codebase, comment out the
