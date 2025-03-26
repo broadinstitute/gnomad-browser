114a603e98fad28f6f0ba05a19dc012e04e5e62c
bw2 <ben.weisburd@gmail.com>
2025-03-26 17:42:38 -0400

    Reordered external links list in STR page

--- browser/src/ShortTandemRepeatPage/ShortTandemRepeatPage.tsx
+++ browser/src/ShortTandemRepeatPage/ShortTandemRepeatPage.tsx
@@ -164,19 +164,6 @@ const ExternalResources = ({ shortTandemRepeat }: { shortTandemRepeat: ShortTand
       {/* @ts-expect-error TS(2745) FIXME: This JSX tag's 'children' prop expects type 'never... Remove this comment to see the full error message */}
       <List>
         <>
-          {stripy_id && (
-            <>
-              {/* @ts-expect-error TS(2745) FIXME: This JSX tag's 'children' prop expects type 'never... Remove this comment to see the full error message */}
-              <ListItem>
-                {/* @ts-expect-error TS(2786) FIXME: 'ExternalLink' cannot be used as a JSX component. */}
-                <ExternalLink href={`https://stripy.org/database/${stripy_id}`}>
-                  STRipy
-                </ExternalLink>
-              </ListItem>
-            </>
-          )}
-        </>
-        <>
           {strchive_id && (
             <>
               {/* @ts-expect-error TS(2745) FIXME: This JSX tag's 'children' prop expects type 'never... Remove this comment to see the full error message */}
@@ -184,6 +171,19 @@ const ExternalResources = ({ shortTandemRepeat }: { shortTandemRepeat: ShortTand
                 {/* @ts-expect-error TS(2786) FIXME: 'ExternalLink' cannot be used as a JSX component. */}
                 <ExternalLink href={`https://strchive.org/loci/${strchive_id}`}>
                   STRchive
+                </ExternalLink>
+              </ListItem>
+            </>
+          )}
+        </>
+        <>
+          {stripy_id && (
+            <>
+              {/* @ts-expect-error TS(2745) FIXME: This JSX tag's 'children' prop expects type 'never... Remove this comment to see the full error message */}
+              <ListItem>
+                {/* @ts-expect-error TS(2786) FIXME: 'ExternalLink' cannot be used as a JSX component. */}
+                <ExternalLink href={`https://stripy.org/database/${stripy_id}`}>
+                  STRipy
                 </ExternalLink>
               </ListItem>
             </>
