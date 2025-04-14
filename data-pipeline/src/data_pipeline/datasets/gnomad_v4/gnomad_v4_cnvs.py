4800e562fff3ebdb841b50f2cdbdead1383e3b0e
Phil Darnowsky <pdarnows@broadinstitute.org>
2025-04-14 10:43:38 -0400

    Build version of CNV HT for public release
    This drops two unused fields from the internal version of the HT, and renames "populations" to "gen_anc_grps" in keeping with our new conventions.

--- data-pipeline/src/data_pipeline/datasets/gnomad_v4/gnomad_v4_cnvs.py
+++ data-pipeline/src/data_pipeline/datasets/gnomad_v4/gnomad_v4_cnvs.py
@@ -60,3 +60,11 @@ def prepare_gnomad_v4_cnvs(vcf_path):
 
     ds = ds.drop("locus", "alleles", "info", "rsid")
     return ds
+
+
+def prepare_public_gnomad_v4_cnvs(input_path):
+    ds = hl.read_table(input_path)
+    ds = ds.transmute(freq=ds.freq.annotate(gen_anc_grps=ds.freq.populations).drop("populations"))
+    ds = ds.drop(ds.filters)
+    ds = ds.drop(ds.qual)
+    return ds
