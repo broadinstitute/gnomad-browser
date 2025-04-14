f67ac2c33c004af5acc45853cef6340e7fb76f14
Phil Darnowsky <pdarnows@broadinstitute.org>
2025-04-14 10:43:38 -0400

    Build version of CNV HT for public release
    This drops two unused fields from the internal version of the HT, and renames "populations" to "gen_anc_grps" in keeping with our new conventions.

--- data-pipeline/src/data_pipeline/pipelines/gnomad_v4_cnvs.py
+++ data-pipeline/src/data_pipeline/pipelines/gnomad_v4_cnvs.py
@@ -1,6 +1,6 @@
 from data_pipeline.pipeline import Pipeline, run_pipeline
 
-from data_pipeline.datasets.gnomad_v4.gnomad_v4_cnvs import prepare_gnomad_v4_cnvs
+from data_pipeline.datasets.gnomad_v4.gnomad_v4_cnvs import prepare_gnomad_v4_cnvs, prepare_public_gnomad_v4_cnvs
 
 
 pipeline = Pipeline()
@@ -16,11 +16,18 @@ pipeline.add_task(
     {"vcf_path": "gs://gnomad-v4-cnvs/2024-03-28-v4.1/GNOMAD_V4.4.5_browser_4.1.vcf.gz"},
 )
 
+pipeline.add_task(
+    "prepare_public_gnomad_v4_cnvs",
+    prepare_public_gnomad_v4_cnvs,
+    "/gnomad_v4/gnomad_v4_cnvs/gnomad.v4.1.cnv.all.ht",
+    {"input_path": pipeline.get_task("prepare_gnomad_v4_cnvs")},
+)
+
 ###############################################
 # Outputs
 ###############################################
 
-pipeline.set_outputs({"cnvs": "prepare_gnomad_v4_cnvs"})
+pipeline.set_outputs({"cnvs": "prepare_gnomad_v4_cnvs", "public_cnvs": "prepare_public_gnomad_v4_cnvs"})
 
 ###############################################
 # Run
