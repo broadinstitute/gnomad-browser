6e91d578c6cc7f1f0cc1db04ae70abdfbe57fb9d
Phil Darnowsky <pdarnows@broadinstitute.org>
2025-04-18 11:26:29 -0400

    Fix expansion of gene list in structural variant consequences

--- browser/src/StructuralVariantPage/StructuralVariantConsequenceList.spec.tsx
+++ browser/src/StructuralVariantPage/StructuralVariantConsequenceList.spec.tsx
@@ -23,4 +23,47 @@ describe('StructuralVariantConsequenceList', () => {
     )
     expect(asFragment()).toMatchSnapshot()
   })
+
+  const consequenceTermsByCategory = {
+    lof: ['lof'],
+    dup_lof: ['dup_lof', 'partial_exon_dup'],
+    copy_gain: ['copy_gain'],
+    other: ['inv_span', 'dup_partial'],
+  } as const
+
+  const geneLists = [
+    ['ABC', 'DEF', 'GHI', 'JKL'],
+    ['MNO', 'PQR', 'STU', 'VWX', 'YZ1'],
+  ]
+
+  describe.each(Object.keys(consequenceTermsByCategory))(
+    'expanding genes list for consequence in category %s with >3 genes',
+    (category) => {
+      const consequences = consequenceTermsByCategory[
+        category as keyof typeof consequenceTermsByCategory
+      ].map((consequenceTerm, i) => ({
+        consequence: consequenceTerm,
+        genes: geneLists[i],
+      }))
+
+      const variant = svFactory.build({ consequences })
+
+      test('expands to show all genes', async () => {
+        const { asFragment, getByText, queryAllByText, findAllByText } = render(
+          <BrowserRouter>
+            <StructuralVariantConsequenceList variant={variant} />
+          </BrowserRouter>
+        )
+        expect(asFragment()).toMatchSnapshot()
+
+        expect(queryAllByText('JKL')).toHaveLength(0)
+        expect(queryAllByText('YZ1')).toHaveLength(0)
+        const expandButton = getByText('and 1 more')
+        expandButton.click()
+        expect(await findAllByText('JKL')).toHaveLength(1)
+        // ensure that if there is a second consequence in the category, we didn't expand that one
+        expect(queryAllByText('YZ1')).toHaveLength(0)
+      })
+    }
+  )
 })
