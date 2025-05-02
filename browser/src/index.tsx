1488fa253aad364f2ba8b21783b561c51b3fedf5
Phil Darnowsky <pdarnows@broadinstitute.org>
2025-05-02 18:14:14 -0400

    Use new top-level rendering API

--- browser/src/index.tsx
+++ browser/src/index.tsx
@@ -1,10 +1,10 @@
 import 'core-js/stable'
 import 'whatwg-fetch'
 import React from 'react'
-import { render } from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 import App from './App'
 
-const mount = document.getElementById('root')
-
-render(<App />, mount)
+const container = document.getElementById('root')
+const root = createRoot(container!)
+root.render(<App />)
