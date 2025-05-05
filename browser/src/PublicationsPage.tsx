70d8cfe6f4fe727e4b9da659e2a38125872723b2
Amy Miniter <40307120+amyminiter@users.noreply.github.com>
2025-05-05 14:40:48 +1000

    Apply formatting for gnomAD styled table (#25)
    * Apply formatting for gnomAD styled table
    
    * Add centred container style definition
    
    * Fixing type check issue.
    
    ---------
    
    Co-authored-by: Milo Hyben <milo.hyben@gmail.com>

--- browser/src/PublicationsPage.tsx
+++ browser/src/PublicationsPage.tsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import styled from 'styled-components'
 import { ExternalLink, PageHeading } from '@gnomad/ui'
 
 import DocumentTitle from './DocumentTitle'
 import InfoPage from './InfoPage'
+import {
+  StatsTable,
+  StatsTableHeaderRow,
+  StatsTableBody,
+} from '../src/StatsPage/StatsPageTables/TableStyles'
+
+const CenteredContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+`
+
+type Dataset = {
+  resource: string
+  description: string
+}
+
+const datasets: Dataset[] = [
+  {
+    resource: 'OurDNA flagship paper (forthcoming)',
+    description: 'Methodologies, study design; pipeline parameters, data collection details.'
+  },
+  {
+    resource: 'OurDNA dataset',
+    description: 'Details on individual variants or genes; summary of population distributions; use of dataset for population programmatic annotation of OurDNA variants with global allele frequencies from gnomAD.'
+  },
+  {
+    resource: 'OurDNA browser (software)',
+    description: '        <td>Descriptions of software functionality; data access methods; comparisons of OurDNA browser to gnomAD browser; references to statistics or visualisations listed in the browser.'
+  }
+]
 
 export default () => (
   <InfoPage>
@@ -16,30 +47,33 @@ export default () => (
       </ExternalLink>. 
       There are several distinct outputs from the OurDNA program and we request that you cite the specific resource/s that have been used as input to your work. Please use Digital Object Identifiers (DOIs) in your citations to refer to definitive, persistent records of OurDNA resources. 
     </p>
-
-    <table>
-      <tr>
-        <th>OurDNA Resource</th>
-        <th>Indicative use case requiring citation</th>
-      </tr>
-      <tr>
-        <td>OurDNA flagship paper (forthcoming)</td>
-        <td>Methodologies, study design; pipeline parameters, data collection details.</td>
-      </tr>
-      <tr>
-        <td>OurDNA dataset</td>
-        <td>Details on individual variants or genes; summary of population distributions; use of dataset for population programmatic annotation of OurDNA variants with global allele frequencies from gnomAD.</td>
-      </tr>
-      <tr>
-        <td>OurDNA browser (software)</td>
-        <td>Descriptions of software functionality; data access methods; comparisons of OurDNA browser to gnomAD browser; references to statistics or visualisations listed in the browser.</td>
-      </tr>
-    </table>
+    <br/>
+    <CenteredContainer>
+      <StatsTable>
+        <thead>
+          <StatsTableHeaderRow>
+            <th>OurDNA resource</th>
+            <th>Indicative citation use case</th>
+          </StatsTableHeaderRow>
+        </thead>
+        <StatsTableBody>
+          {datasets.map((dataset) => {
+            return (
+              <tr key={dataset.resource}>
+                <td>{dataset.resource}</td>
+                <td>{dataset.description}</td>
+              </tr>
+            )
+          })}
+        </StatsTableBody>
+      </StatsTable>
+    </CenteredContainer>
 
     <h2>Flagship paper</h2>
 
     <p>
       Publication of the OurDNA flagship paper is forthcoming. 
+      <br/>
       <br/>
       We have a range of additional resources published in the{' '}
        {/* @ts-expect-error TS(2786) FIXME: 'ExternalLink' cannot be used as a JSX component. */}
