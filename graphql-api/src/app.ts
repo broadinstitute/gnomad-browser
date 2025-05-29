d96a3e6fcae17ef7bab9fa58b386d304e718432f
Phil Darnowsky <pdarnows@broadinstitute.org>
2025-05-29 17:05:32 -0400

    Add basic GC stats to API logging

--- graphql-api/src/app.ts
+++ graphql-api/src/app.ts
@@ -7,6 +7,7 @@ import config from './config'
 import { client as esClient } from './elasticsearch'
 import graphQLApi from './graphql/graphql-api'
 import logger from './logger'
+import { PerformanceObserver, PerformanceObserverEntryList } from 'node:perf_hooks'
 
 process.on('uncaughtException', (error) => {
   logger.error(error)
@@ -31,18 +32,40 @@ app.get('/health/ready', (_request: any, response: any) => {
   response.send('ok')
 })
 
+const logGCDurations = (items: PerformanceObserverEntryList, durationLog: number[]) => {
+  items.getEntries().forEach((item) => durationLog.push(item.duration))
+}
+
+const gcDurationStats = (gcDurations: number[]) => {
+  const nDurations = gcDurations.length
+  if (nDurations > 0) {
+    const totalDuration = gcDurations.reduce((acc, duration) => acc + duration, 0)
+    const avgDuration = totalDuration / nDurations
+
+    return { nDurations, totalDuration, avgDuration }
+  }
+  return undefined
+}
+
 // Log requests
 // Add logging here to avoid logging health checks
 app.use(function requestLogMiddleware(request: any, response: any, next: any) {
   let memoryBefore: NodeJS.MemoryUsage | undefined
   request.startAt = process.hrtime()
   response.startAt = undefined
+
+  const gcDurations: number[] = []
+  const gcObserver = new PerformanceObserver((items) => logGCDurations(items, gcDurations))
+  gcObserver.observe({ type: 'gc' })
+
   onHeaders(response, () => {
     memoryBefore = process.memoryUsage()
     response.startAt = process.hrtime()
   })
 
   onFinished(response, () => {
+    gcObserver.disconnect()
+
     const memoryAfter = process.memoryUsage()
     const memoryDelta: NodeJS.MemoryUsage | undefined = memoryBefore && {
       rss: memoryAfter.rss - memoryBefore.rss,
@@ -81,6 +104,7 @@ app.use(function requestLogMiddleware(request: any, response: any, next: any) {
             graphqlQueryCost: request.graphqlQueryCost,
           }
         : undefined,
+      gc: gcDurationStats(gcDurations),
     })
   })
 
