e38ec68dd2bc5ea873a45d6427f626061d9de0b5
Phil Darnowsky <pdarnows@broadinstitute.org>
2025-06-02 10:17:23 -0400

    Revert bespoke logging
    We're about to add OpenTelemetry to the API so we can get rid of this recently added one-off code.

--- graphql-api/src/app.ts
+++ graphql-api/src/app.ts
@@ -7,7 +7,6 @@ import config from './config'
 import { client as esClient } from './elasticsearch'
 import graphQLApi from './graphql/graphql-api'
 import logger from './logger'
-import { PerformanceObserver, PerformanceObserverEntryList } from 'node:perf_hooks'
 
 process.on('uncaughtException', (error) => {
   logger.error(error)
@@ -32,51 +31,17 @@ app.get('/health/ready', (_request: any, response: any) => {
   response.send('ok')
 })
 
-const logGCDurations = (items: PerformanceObserverEntryList, durationLog: number[]) => {
-  items.getEntries().forEach((item) => durationLog.push(item.duration))
-}
-
-const gcDurationStats = (gcDurations: number[]) => {
-  const nDurations = gcDurations.length
-  if (nDurations > 0) {
-    const totalDuration = gcDurations.reduce((acc, duration) => acc + duration, 0)
-    const avgDuration = totalDuration / nDurations
-
-    return { nDurations, totalDuration, avgDuration }
-  }
-  return undefined
-}
-
 // Log requests
 // Add logging here to avoid logging health checks
 app.use(function requestLogMiddleware(request: any, response: any, next: any) {
-  let memoryBefore: NodeJS.MemoryUsage | undefined
   request.startAt = process.hrtime()
   response.startAt = undefined
-
-  const gcDurations: number[] = []
-  const gcObserver = new PerformanceObserver((items) => logGCDurations(items, gcDurations))
-  gcObserver.observe({ type: 'gc' })
-
   onHeaders(response, () => {
-    memoryBefore = process.memoryUsage()
     response.startAt = process.hrtime()
   })
 
   onFinished(response, () => {
-    gcObserver.disconnect()
-
-    const memoryAfter = process.memoryUsage()
-    const memoryDelta: NodeJS.MemoryUsage | undefined = memoryBefore && {
-      rss: memoryAfter.rss - memoryBefore.rss,
-      heapTotal: memoryAfter.heapTotal - memoryBefore.heapTotal,
-      heapUsed: memoryAfter.heapUsed - memoryBefore.heapUsed,
-      external: memoryAfter.external - memoryBefore.external,
-      arrayBuffers: memoryAfter.arrayBuffers - memoryBefore.arrayBuffers,
-    }
-
     logger.info({
-      memory: { before: memoryBefore, after: memoryAfter, delta: memoryDelta },
       httpRequest: {
         requestMethod: request.method,
         requestUrl: `${request.protocol}://${request.hostname}${
@@ -104,7 +69,6 @@ app.use(function requestLogMiddleware(request: any, response: any, next: any) {
             graphqlQueryCost: request.graphqlQueryCost,
           }
         : undefined,
-      gc: gcDurationStats(gcDurations),
     })
   })
 
