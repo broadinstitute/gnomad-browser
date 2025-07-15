48f9ac0c7ff5aa249fb52bf5a2abc1a35ff90c70
Matthew Solomonson <ggmatt@gmail.com>
2025-07-15 11:17:51 -0400

    Add go linter

--- gnomad-go-api/internal/config/config.go
+++ gnomad-go-api/internal/config/config.go
@@ -1,28 +1,28 @@
 package config
 
 import (
-    "github.com/spf13/viper"
+	"github.com/spf13/viper"
 )
 
 type Config struct {
-    Port              string
-    ElasticsearchURL  string
-    RedisURL         string
-    EnablePlayground bool
+	Port             string
+	ElasticsearchURL string
+	RedisURL         string
+	EnablePlayground bool
 }
 
 func Load() (*Config, error) {
-    viper.SetDefault("PORT", "8010")
-    viper.SetDefault("ELASTICSEARCH_URL", "http://localhost:9200")
-    viper.SetDefault("REDIS_URL", "redis://localhost:6379")
-    viper.SetDefault("ENABLE_PLAYGROUND", true)
-    
-    viper.AutomaticEnv()
-    
-    return &Config{
-        Port:             viper.GetString("PORT"),
-        ElasticsearchURL: viper.GetString("ELASTICSEARCH_URL"),
-        RedisURL:        viper.GetString("REDIS_URL"),
-        EnablePlayground: viper.GetBool("ENABLE_PLAYGROUND"),
-    }, nil
+	viper.SetDefault("PORT", "8010")
+	viper.SetDefault("ELASTICSEARCH_URL", "http://localhost:9200")
+	viper.SetDefault("REDIS_URL", "redis://localhost:6379")
+	viper.SetDefault("ENABLE_PLAYGROUND", true)
+
+	viper.AutomaticEnv()
+
+	return &Config{
+		Port:             viper.GetString("PORT"),
+		ElasticsearchURL: viper.GetString("ELASTICSEARCH_URL"),
+		RedisURL:         viper.GetString("REDIS_URL"),
+		EnablePlayground: viper.GetBool("ENABLE_PLAYGROUND"),
+	}, nil
 }
--- gnomad-go-api/internal/graph/model/enums.go
+++ gnomad-go-api/internal/graph/model/enums.go
@@ -3,14 +3,14 @@ package model
 type DatasetId string
 
 const (
-    DatasetIdGnomadR4       DatasetId = "gnomad_r4"
-    DatasetIdGnomadR4NonUkb DatasetId = "gnomad_r4_non_ukb"
-    DatasetIdGnomadR21      DatasetId = "gnomad_r2_1"
+	DatasetIdGnomadR4       DatasetId = "gnomad_r4"
+	DatasetIdGnomadR4NonUkb DatasetId = "gnomad_r4_non_ukb"
+	DatasetIdGnomadR21      DatasetId = "gnomad_r2_1"
 )
 
 type ReferenceGenomeId string
 
 const (
-    ReferenceGenomeIdGRCh37 ReferenceGenomeId = "GRCh37"
-    ReferenceGenomeIdGRCh38 ReferenceGenomeId = "GRCh38"
+	ReferenceGenomeIdGRCh37 ReferenceGenomeId = "GRCh37"
+	ReferenceGenomeIdGRCh38 ReferenceGenomeId = "GRCh38"
 )
