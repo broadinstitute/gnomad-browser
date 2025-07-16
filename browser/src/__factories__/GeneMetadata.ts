3a237135f2f45e070a6e5aa45968c4e326e094aa
Riley Grant <grantrileyh@gmail.com>
2025-07-16 12:32:39 -0400

    feat(frontend): add CMRG warning to specific genes

--- browser/src/__factories__/GeneMetadata.ts
+++ browser/src/__factories__/GeneMetadata.ts
@@ -5,6 +5,7 @@ const geneMetadataFactory = Factory.define<GeneMetadata>(() => ({
   gene_id: 'dummy_gene',
   gene_version: '5.6.7.8',
   symbol: 'FAKEGENE',
+  reference_genome: 'GRCh38',
   canonical_transcript_id: 'some-transcript',
   flags: [],
   mane_select_transcript: null,
