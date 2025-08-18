3557f9952ec0a38b94f0829637447bd2992d88d4
Riley Grant <grantrileyh@gmail.com>
2025-08-18 16:43:04 -0500

    chore(pipeline): update v4 CNVs to use public bucket VCF

--- data-pipeline/src/data_pipeline/pipelines/gnomad_v4_cnvs.py
+++ data-pipeline/src/data_pipeline/pipelines/gnomad_v4_cnvs.py
@@ -12,7 +12,7 @@ pipeline.add_task(
     "prepare_gnomad_v4_cnvs",
     prepare_gnomad_v4_cnvs,
     "/gnomad_v4/gnomad_v4_cnvs/cnvs.ht",
-    {"vcf_path": "gs://gnomad-v4-cnvs/2024-03-28-v4.1/GNOMAD_V4.4.5_browser_4.1.vcf.gz"},
+    {"vcf_path": "gs://gcp-public-data--gnomad/release/4.1/exome_cnv/gnomad.v4.1.cnv.all.vcf.gz"},
 )
 
 pipeline.add_task(
