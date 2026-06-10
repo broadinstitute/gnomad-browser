8e8f3f6b63bcd82f097bcf7cdde035bcc6e316ba
Riley Grant <grantrileyh@gmail.com>
2026-06-10 10:17:52 -0500

    chore(frontend): format tsx files with prettier

--- browser/src/GenePage/GenePage.tsx
+++ browser/src/GenePage/GenePage.tsx
@@ -171,10 +171,10 @@ export type Gene = GeneMetadata & {
   exac_constraint: ExacConstraint | null
   pext: Pext | null
   short_tandem_repeats:
-  | {
-    id: string
-  }[]
-  | null
+    | {
+        id: string
+      }[]
+    | null
   exac_regional_missense_constraint_regions: any | null
   gnomad_regional_missense_constraint: RegionalMissenseConstraint | null
   variants: Variant[]
@@ -339,22 +339,22 @@ const GenePage = ({ datasetId, gene, geneId }: Props) => {
 
   const regionViewerRegions = !hasExons(datasetId)
     ? [
-      {
-        start: Math.max(1, gene.start - 75),
-        stop: gene.stop + 75,
-      },
-    ]
+        {
+          start: Math.max(1, gene.start - 75),
+          stop: gene.stop + 75,
+        },
+      ]
     : gene.exons
-      .filter(
-        (exon) =>
-          exon.feature_type === 'CDS' ||
-          (exon.feature_type === 'UTR' && includeUTRs) ||
-          (exon.feature_type === 'exon' && includeNonCodingTranscripts)
-      )
-      .map((exon) => ({
-        start: Math.max(1, exon.start - 75),
-        stop: exon.stop + 75,
-      }))
+        .filter(
+          (exon) =>
+            exon.feature_type === 'CDS' ||
+            (exon.feature_type === 'UTR' && includeUTRs) ||
+            (exon.feature_type === 'exon' && includeNonCodingTranscripts)
+        )
+        .map((exon) => ({
+          start: Math.max(1, exon.start - 75),
+          stop: exon.stop + 75,
+        }))
 
   const [zoomRegion, setZoomRegion] = useState(null)
 
@@ -480,7 +480,7 @@ const GenePage = ({ datasetId, gene, geneId }: Props) => {
                   checked={hasCodingExons}
                   disabled
                   id="include-cds-regions"
-                  onChange={() => { }}
+                  onChange={() => {}}
                 />
                 Coding regions (CDS)
                 <LegendSwatch
@@ -625,20 +625,20 @@ const GenePage = ({ datasetId, gene, geneId }: Props) => {
         {hasStructuralVariants(datasetId) ? (
           <StructuralVariantsInGene datasetId={datasetId} gene={gene} zoomRegion={zoomRegion} />
         ) : // eslint-disable-next-line no-nested-ternary
-          hasCopyNumberVariants(datasetId) ? (
-            <CopyNumberVariantsInGene datasetId={datasetId} gene={gene} zoomRegion={zoomRegion} />
-          ) : gene.chrom === 'M' ? (
-            <MitochondrialVariantsInGene datasetId={datasetId} gene={gene} zoomRegion={zoomRegion} />
-          ) : (
-            <VariantsInGene
-              datasetId={datasetId}
-              gene={gene}
-              includeNonCodingTranscripts={includeNonCodingTranscripts}
-              includeUTRs={includeUTRs}
-              zoomRegion={zoomRegion}
-              hasOnlyNonCodingTranscripts={!hasCodingExons && hasNonCodingTranscripts}
-            />
-          )}
+        hasCopyNumberVariants(datasetId) ? (
+          <CopyNumberVariantsInGene datasetId={datasetId} gene={gene} zoomRegion={zoomRegion} />
+        ) : gene.chrom === 'M' ? (
+          <MitochondrialVariantsInGene datasetId={datasetId} gene={gene} zoomRegion={zoomRegion} />
+        ) : (
+          <VariantsInGene
+            datasetId={datasetId}
+            gene={gene}
+            includeNonCodingTranscripts={includeNonCodingTranscripts}
+            includeUTRs={includeUTRs}
+            zoomRegion={zoomRegion}
+            hasOnlyNonCodingTranscripts={!hasCodingExons && hasNonCodingTranscripts}
+          />
+        )}
       </RegionViewer>
     </TrackPage>
   )
