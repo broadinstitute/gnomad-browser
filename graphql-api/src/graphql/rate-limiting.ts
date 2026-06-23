fea5d29e0691d80de6a58e493310bceaf485cfc3
Benjamin Blankenmeister <bblanken@broadinstitute.org>
2026-06-22 19:16:54 -0600

    bugfix: clean rate-limiting implementation if redis host not specified

--- graphql-api/src/graphql/rate-limiting.ts
+++ graphql-api/src/graphql/rate-limiting.ts
@@ -6,41 +6,48 @@ import { isWhitelistedIP } from '../whitelist'
 
 let rateLimitDb: Redis
 
-if (config.REDIS_USE_SENTINEL) {
-  rateLimitDb = new Redis({
-    sentinels: [{ host: config.REDIS_HOST, port: config.REDIS_PORT }],
-    name: config.REDIS_GROUP_NAME,
-    db: 2,
-  })
+let increaseRateLimitCounter = async (
+  _key: string,
+  _value: number
+): Promise<number> => 0
+if (config.REDIS_HOST) {
+  if (config.REDIS_USE_SENTINEL) {
+    rateLimitDb = new Redis({
+      sentinels: [{ host: config.REDIS_HOST, port: config.REDIS_PORT }],
+      name: config.REDIS_GROUP_NAME,
+      db: 2,
+    })
+  } else {
+    rateLimitDb = new Redis({
+      host: config.REDIS_HOST,
+      port: config.REDIS_PORT,
+      db: 2,
+    })
+  }
+  increaseRateLimitCounter = (key: any, value: any): Promise<number> => {
+    return Promise.race([
+      new Promise<number>((resolve, reject) => {
+        rateLimitDb
+          .multi()
+          .set(key, 0, 'EX', 59, 'NX')
+          .incrby(key, value)
+          .exec((err: any, replies: any) => {
+            if (err) {
+              reject(err)
+            } else {
+              resolve(replies[1])
+            }
+          })
+      }),
+      new Promise<number>((_resolve, reject) => {
+        setTimeout(() => {
+          reject(new Error('Timed out'))
+        }, 500)
+      }),
+    ])
+  }
 } else {
-  rateLimitDb = new Redis({
-    host: config.REDIS_HOST,
-    port: config.REDIS_PORT,
-    db: 2,
-  })
-}
-
-const increaseRateLimitCounter = (key: any, value: any) => {
-  return Promise.race([
-    new Promise((resolve, reject) => {
-      rateLimitDb
-        .multi()
-        .set(key, 0, 'EX', 59, 'NX')
-        .incrby(key, value)
-        .exec((err: any, replies: any) => {
-          if (err) {
-            reject(err)
-          } else {
-            resolve(replies[1])
-          }
-        })
-    }),
-    new Promise((_resolve, reject) => {
-      setTimeout(() => {
-        reject(new Error('Timed out'))
-      }, 500)
-    }),
-  ])
+  logger.warn('No cache configured')
 }
 
 export const applyRateLimits = async (request: any) => {
@@ -58,7 +65,6 @@ export const applyRateLimits = async (request: any) => {
       1
     )
 
-    // @ts-expect-error TS(2571) FIXME: Object is of type 'unknown'.
     if (totalRequestsInWindow > config.MAX_REQUESTS_PER_MINUTE) {
       throw new UserVisibleError('Query rate limit exceeded. Please try again in a few minutes.')
     }
@@ -68,7 +74,6 @@ export const applyRateLimits = async (request: any) => {
       request.graphqlQueryCost || 0
     )
 
-    // @ts-expect-error TS(2571) FIXME: Object is of type 'unknown'.
     if (totalCostInWindow > config.MAX_QUERY_COST_PER_MINUTE) {
       throw new UserVisibleError('Query rate limit exceeded. Please try again in a few minutes.')
     }
