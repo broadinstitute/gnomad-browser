ebdc03cfad96670c5cf5eb788091f1cd296207bd
bw2 <ben.weisburd@gmail.com>
2026-07-29 13:47:10 -0400

    Updated the browser and data pipeline for the July 2026 tandem repeat release

--- data-pipeline/src/data_pipeline/pipelines/gnomad_v3_short_tandem_repeats.py
+++ data-pipeline/src/data_pipeline/pipelines/gnomad_v3_short_tandem_repeats.py
@@ -1,3 +1,5 @@
+import os
+
 from data_pipeline.pipeline import Pipeline, run_pipeline
 
 from data_pipeline.datasets.gnomad_v3.gnomad_v3_short_tandem_repeats import prepare_gnomad_v3_short_tandem_repeats
@@ -10,7 +12,12 @@ pipeline.add_task(
     prepare_gnomad_v3_short_tandem_repeats,
     "/gnomad_v4/gnomad_v4_short_tandem_repeats.ht",
     {
-        "path": "gs://gnomad-browser-data-pipeline/inputs/secondary-analyses/strs/2025_03_17/gnomAD_STR_distributions__gnomad-v2__2025_03_17.json"
+        # Override with a local path for local dev (avoids GCS auth), e.g.
+        #   STR_DISTRIBUTIONS_JSON=/path/to/gnomAD_STR_distributions__gnomad-v2__2026_07_20.json.gz
+        "path": os.environ.get(
+            "STR_DISTRIBUTIONS_JSON",
+            "gs://gnomad-browser/STRs/gnomAD_STR_distributions__gnomad-v2__2026_07_20.json.gz",
+        )
     },
 )
 
