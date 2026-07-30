6b25352091185c6c09a071e1a79af8d34965a188
bw2 <ben.weisburd@gmail.com>
2026-07-30 09:28:03 -0400

    Restored the full readviz db path prefix

--- reads/src/datasets.js
+++ reads/src/datasets.js
@@ -40,7 +40,8 @@ const variantDatasets = {
 }
 
 // mounted via GCS Fuse
-const SHORT_TANDEM_REPEAT_READS_DB_PATH = 'str_reads_2026_07_20.db'
+const SHORT_TANDEM_REPEAT_READS_DB_PATH =
+  '/readviz/datasets/gnomad_r4_short_tandem_repeats/str_reads_2026_07_20.db'
 
 const shortTandemRepeatDatasets = {
   gnomad_r3: {
