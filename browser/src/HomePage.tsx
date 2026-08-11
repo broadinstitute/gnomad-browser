ee2e51885ababcff4451812c1195bff11013f172
Matthew Solomonson <ggmatt@gmail.com>
2026-08-11 12:13:57 -0400

    fix(lr): keep home links scoped to LR pages

--- browser/src/HomePage.tsx
+++ browser/src/HomePage.tsx
@@ -107,11 +107,6 @@ export default () => (
           Long-read literature workflows
         </Link>
       </ListItem>
-      <ListItem>
-        <Link preserveSelectedDataset={false} to="/reference-sequence-context">
-          Chromosome 22 reference sequence-context regions
-        </Link>
-      </ListItem>
     </List>
 
     <p>
