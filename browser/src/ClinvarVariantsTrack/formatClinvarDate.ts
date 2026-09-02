04cfba3ec2c93a595a5d972da1085ae902cbfac9
Riley Grant <grantrileyh@gmail.com>
2026-09-02 11:08:35 -0500

    fixup code review; add fallback text for malformed clinvar release dates

--- browser/src/ClinvarVariantsTrack/formatClinvarDate.ts
+++ browser/src/ClinvarVariantsTrack/formatClinvarDate.ts
@@ -1,10 +1,14 @@
 const dateFormatter = new Intl.DateTimeFormat([], { dateStyle: 'long' })
 
-// Dates in ClinVar date are formatted YYYY-MM-DD
-const formatClinvarDate = (dateString: any) => {
-  const [year, month, day] = dateString.split('-').map(Number)
-  const date = new Date(year, month - 1, day)
-  return dateFormatter.format(date)
+const formatClinvarDate = (dateString: string): string => {
+  try {
+    // Dates in ClinVar date are formatted YYYY-MM-DD
+    const [year, month, day] = dateString.split('-').map(Number)
+    const date = new Date(year, month - 1, day)
+    return dateFormatter.format(date)
+  } catch (error) {
+    return `Malformed date string: "${dateString}"`
+  }
 }
 
 export default formatClinvarDate
